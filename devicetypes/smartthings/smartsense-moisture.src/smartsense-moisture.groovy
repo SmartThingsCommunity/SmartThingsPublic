b5e3b9133659b0b7a0f8644ff46b49230bd1ca32
Vinay Rao <workingmonk@users.noreply.github.com>
2015-08-25 11:35:00 -0700

    Update smartsense-moisture.groovy

--- devicetypes/smartthings/smartsense-moisture.src/smartsense-moisture.groovy
+++ devicetypes/smartthings/smartsense-moisture.src/smartsense-moisture.groovy
@@ -128,6 +128,15 @@ def zwaveEvent(physicalgraph.zwave.commands.alarmv2.AlarmReport cmd)
 	map
 }
 
+def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicSet cmd)
+{
+	def map = [:]
+	map.name = "water"
+	map.value = cmd.value ? "wet" : "dry"
+	map.descriptionText = "${device.displayName} is ${map.value}"
+	map
+}
+
 def zwaveEvent(physicalgraph.zwave.Command cmd)
 {
 	log.debug "COMMAND CLASS: $cmd"
