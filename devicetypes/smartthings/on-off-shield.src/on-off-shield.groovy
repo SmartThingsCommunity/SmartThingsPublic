f74560f0efff46b47b6a3b4d49fdb19903d5ab20
Kemsukseo <kemsuk.seo@samsung.com>
2015-08-26 06:15:35 -0500

    Modifying 'test'

--- devicetypes/smartthings/on-off-shield.src/on-off-shield.groovy
+++ devicetypes/smartthings/on-off-shield.src/on-off-shield.groovy
@@ -1,59 +1,96 @@
-/**
- *  Copyright 2015 SmartThings
+/*
+ *  On/Off Shield (example)
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
- *
+ *  Author: urman
+ *  Date: 2013-03-14
+ *  Revision: 2014-07-10
+ *  Capabilities:
+ *   Switch
+ *  Custom Attributes:
+ *   greeting
+ *  Custom Commands
+ *   hello
+ *   goodbye
  */
+ 
+ 
 metadata {
-	definition (name: "On/Off Shield", namespace: "smartthings", author: "SmartThings") {
-		capability "Actuator"
-		capability "Switch"
-		capability "Sensor"
+	definition (name: "On/Off Shield (example)", author: "urman") {
+    		capability "Switch"
+        	command "hello"
+        	command "goodbye"
+        	attribute "greeting","string"
 	}
 
-	// Simulator metadata
-	simulator {
-		status "on":  "catchall: 0104 0000 01 01 0040 00 0A21 00 00 0000 0A 00 0A6F6E"
-		status "off": "catchall: 0104 0000 01 01 0040 00 0A21 00 00 0000 0A 00 0A6F6666"
-
-		// reply messages
-		reply "raw 0x0 { 00 00 0a 0a 6f 6e }": "catchall: 0104 0000 01 01 0040 00 0A21 00 00 0000 0A 00 0A6F6E"
-		reply "raw 0x0 { 00 00 0a 0a 6f 66 66 }": "catchall: 0104 0000 01 01 0040 00 0A21 00 00 0000 0A 00 0A6F6666"
-	}
-
-	// UI tile definitions
 	tiles {
 		standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true, canChangeBackground: true) {
 			state "on", label: '${name}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821"
 			state "off", label: '${name}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
 		}
-
+		standardTile("greeting", "device.greeting", width: 1, height: 1, canChangeIcon: true, canChangeBackground: true) {
+			state "default", label: 'hello', action: "hello", icon: "st.switches.switch.off", backgroundColor: "#ccccff"
+		}     
+		valueTile("message", "device.greeting", inactiveLabel: false) {
+			state "greeting", label:'${currentValue}', unit:""
+		}
+        
 		main "switch"
-		details "switch"
+		details(["switch","greeting","message"])
 	}
+    simulator {
+        status "on":  "catchall: 0104 0000 01 01 0040 00 0A21 00 00 0000 0A 00 0A6F6E"
+        status "off": "catchall: 0104 0000 01 01 0040 00 0A21 00 00 0000 0A 00 0A6F6666"
+    
+        // reply messages
+        reply "raw 0x0 { 00 00 0a 0a 6f 6e }": "catchall: 0104 0000 01 01 0040 00 0A21 00 00 0000 0A 00 0A6F6E"
+        reply "raw 0x0 { 00 00 0a 0a 6f 66 66 }": "catchall: 0104 0000 01 01 0040 00 0A21 00 00 0000 0A 00 0A6F6666"
+    }
 }
 
-// Parse incoming device messages to generate events
-def parse(String description) {
+Map parse(String description) {
+
 	def value = zigbee.parse(description)?.text
-	def name = value in ["on","off"] ? "switch" : null
-	def result = createEvent(name: name, value: value)
-	log.debug "Parse returned ${result?.descriptionText}"
-	return result
+	def linkText = getLinkText(device)
+	def descriptionText = getDescriptionText(description, linkText, value)
+	def handlerName = value
+	def isStateChange = value != "ping"
+	def displayed = value && isStateChange
+
+	def result = [
+		value: value,
+		name: value in ["on","off"] ? "switch" : (value && value != "ping" ? "greeting" : null),
+		handlerName: handlerName,
+		linkText: linkText,
+		descriptionText: descriptionText,
+		isStateChange: isStateChange,
+		displayed: displayed
+	]
+
+	log.debug result.descriptionText
+	result
 }
 
-// Commands sent to the device
 def on() {
-	zigbee.smartShield(text: "on").format()
+    zigbee.smartShield(text: "on").format()
 }
 
 def off() {
 	zigbee.smartShield(text: "off").format()
+}
+
+def hello() {
+	log.debug "Hello World!"
+	zigbee.smartShield(text: "hello").format()
+}
+
+def goodbye() {
+	log.debug "Bye Bye!"
+	zigbee.smartShield(text: "goodbye").format()
+}
+
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'switch' attribute
+	// TODO: handle 'greeting' attribute
+
 }
