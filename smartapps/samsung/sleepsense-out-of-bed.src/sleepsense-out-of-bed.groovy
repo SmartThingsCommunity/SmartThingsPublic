30bfe6506f15632fde8fae270c1c1b2fe6356966
Mobileapp <dahell0001@gmail.com>
2015-08-26 22:17:26 -0500

    MSA-53: I could not solve a problem completely. But I solved the several problems.
    I added a comment.
    Would you check it.

---
+++ smartapps/samsung/sleepsense-out-of-bed.src/sleepsense-out-of-bed.groovy
@@ -0,0 +1,139 @@
+/**
+ *  SLEEPsense-in bed
+ *
+ *  Copyright 2015 Samsung Mobileapp
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "SLEEPsense-out of bed",
+    namespace: "samsung",
+    author: "Samsung Mobileapp",
+    description: "Control lights when you’re out of bed.",
+    category: "Health & Wellness",
+    iconUrl: "http://cdn.device-icons.smartthings.com/Health%20&%20Wellness/health9-icn@2x.png",
+    iconX2Url: "http://cdn.device-icons.smartthings.com/Health%20&%20Wellness/health9-icn@2x.png",
+    iconX3Url: "http://cdn.device-icons.smartthings.com/Health%20&%20Wellness/health9-icn@2x.png")
+
+
+preferences {
+	page(name:"Mainpage", install:true, uninstall:true) {
+        section("Control switchs when you're out of bed.") {
+            input "sleepsensor", "capability.sleepSensor", title: "Select sleep Sensor", multiple: false, required: true
+        }
+       section("Choose devices for out of bed") {
+            input "DeviceController", "capability.switch", title: "choose devices for out of bed", multiple: true, required: false
+            input (name: "lightlevel", type: "enum", title: "Set dimmers to this level", options: ["off", "10%","20%","30%","40%","50%","60%","70%","80%","90%","100%"])
+        }
+    }
+    
+
+}
+
+
+def installed()
+{
+	log.trace "installed()"
+
+    subscribe(sleepsensor, "sleeping", sleepsensorHandler)
+    subscribe(sleepsensor, "bedstate", sleepsensorHandler)
+}
+
+def updated()
+{
+	log.trace "updated()"
+	unsubscribe()
+    subscribe(sleepsensor, "sleeping", sleepsensorHandler)
+    subscribe(sleepsensor, "bedstate", sleepsensorHandler)
+}
+
+def initialize() {
+	// TODO: subscribe to attributes, devices, locations, etc.
+}
+
+
+def sleepsensorHandler(evt) {
+    log.debug "sleepsensorHandler: $evt.value"
+    
+    if(evt.value == "out of bed")
+    {
+        LightLevelConv()
+
+        def hasSetLevelCommand = DeviceController.hasCommand("setLevel")
+	
+    	log.debug "Out Bed actions will be excuted"
+        log.debug "${DeviceController.displayName} has command setLevel? $hasSetLevelCommand"   
+        log.debug "dimmer level : $state.lightlevel"
+	
+       def cnt = 0
+
+        DeviceController.each
+        {
+
+            if(hasSetLevelCommand[cnt] == null)
+            {
+                it?.on()
+	            log.debug "off device $it"
+            }
+            else
+            {
+	            log.debug "dimmer device $it"
+                if(state.lightlevel == 0)
+                {
+                    it?.off()
+                }
+                else
+                {
+	                it?.setLevel(state.lightlevel)
+                }
+            }
+            cnt++;
+        }        
+    }
+}
+
+def LightLevelConv() {
+	if (lightlevel == "off") {
+    	state.lightlevel = 0;
+    }
+    else if (lightlevel == "10%") {
+    	state.lightlevel = 10
+    }
+    else if (lightlevel == "20%") {
+    	state.lightlevel = 20
+    }
+    else if (lightlevel == "30%") {
+    	state.lightlevel = 30
+    }
+    else if (lightlevel == "40%") {
+    	state.lightlevel = 40
+    }
+    else if (lightlevel == "50%") {
+    	state.lightlevel = 50
+    }
+    else if (lightlevel == "60%") {
+    	state.lightlevel = 60
+    }
+    else if (lightlevel == "70%") {
+    	state.lightlevel = 70
+    }
+    else if (lightlevel == "80%") {
+    	state.lightlevel = 80
+    }
+    else if (lightlevel == "90%") {
+    	state.lightlevel = 90
+    }
+    else if (lightlevel == "100%") {
+    	state.lightlevel = 100
+    }
+    
+}
+
