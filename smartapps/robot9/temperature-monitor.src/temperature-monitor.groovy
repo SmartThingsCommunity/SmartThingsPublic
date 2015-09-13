c0c1c99a7d396124260adb1d16588923b9e00729
Yanan <flxyn90@gmail.com>
2015-09-12 22:06:53 -0700

    first temperature monitor

---
+++ smartapps/robot9/temperature-monitor.src/temperature-monitor.groovy
@@ -0,0 +1,62 @@
+/**
+ *  Temperature monitor 
+ *
+ *  Copyright 2015 Yanan
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Temperature monitor ",
+    namespace: "robot9",
+    author: "Yanan",
+    description: "send an notification when temperature goes up or down",
+    category: "Green Living",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    oauth: true)
+
+
+preferences {
+	section("Sensors to monitor...") {
+		input "sensor", "capability.temperatureMeasurement", title: "Temp Sensor", required: true, multiple: false
+	}
+    section("Set a temperature threshold..."){
+    	input "max_", "decimal", title: "Set Max Temp"
+        input "min_", "decimal", title: "Set Min Temp"
+  	}
+    section("Send Notifications?") {
+        input("recipients", "contact", title: "Send notifications to")
+    }
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+    subscribe(sensor, "temperature", weatherHandler)
+}
+
+def weatherHandler(evt) {
+  if(evt.doubleValue > max_) {
+    sendNotificationToContacts("Room too hot! Current ${evt.doubleValue}F!", recipients)
+  } else if (evt.doubleValue < min_) {
+  	sendNotificationToContacts("Room too cold! Current ${evt.doubleValue}F!", recipients)
+  }
+}
