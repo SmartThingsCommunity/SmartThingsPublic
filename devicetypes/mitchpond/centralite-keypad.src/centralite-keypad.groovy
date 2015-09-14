72360c8957c332f62c6bb5d634cfa6547cb62f3a
Mitch Pond <mitchp@gmail.com>
2015-09-14 10:10:11 -0500

    Initial commit for Centralite Keypad.

---
+++ devicetypes/mitchpond/centralite-keypad.src/centralite-keypad.groovy
@@ -0,0 +1,148 @@
+/**
+ *  Centralite Keypad
+ *
+ *  Copyright 2015 Mitch Pond
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "Centralite Keypad", namespace: "mitchpond", author: "Mitch Pond") {
+		capability "Battery"
+		capability "Button"
+        capability "Configuration"
+		capability "Sensor"
+        
+        fingerprint endpointId: "01", profileId: "0104", inClusters: "0000,0001,0003,0020,0402,0500,0B05", outClusters: "0019,0501"
+	}
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+        valueTile("battery", "device.battery", decoration: "flat") {
+			state "battery", label:'${currentValue}% battery', unit:""
+		}
+        main ("battery")
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+    
+    def results = [];
+    
+	// TODO: handle 'battery' attribute
+	// TODO: handle 'button' attribute
+	if (description?.startsWith('catchall:')) {
+    	//log.debug zigbee.parse(description)
+		results = zigbee.parse(description)
+        if (results?.command == 0x07) log.debug 'Heartbeat??'
+        else if (results?.command == 0x00) log.debug 'Received arm request with keycode: '+results.data
+	}
+    else if (description?.startsWith('enroll request')) {
+		List cmds = enrollResponse()
+		log.debug "enroll response: ${cmds}"
+		results = cmds?.collect { new physicalgraph.device.HubAction(it) }
+	}
+    else if (description?.startsWith('read attr -')) {
+		results = parseReportAttributeMessage(description)
+	}
+    
+	return results
+}
+
+def configure() {
+	String zigbeeId = swapEndianHex(device.hub.zigbeeId)
+	def cmd = [
+    "zcl global write 0x500 0x10 0xf0 {${zigbeeId}}", "delay 200",
+	"send 0x${device.deviceNetworkId} 1 1", "delay 1500",
+    
+    "zdo bind 0x${device.deviceNetworkId} 1 1 0x500 {${device.zigbeeId}} {}", "delay 500",
+    "zdo bind 0x${device.deviceNetworkId} 1 1 0x501 {${device.zigbeeId}} {}", "delay 500",
+	"zdo bind 0x${device.deviceNetworkId} 1 1 1 {${device.zigbeeId}} {}", "delay 500",
+	"st rattr 0x${device.deviceNetworkId} 1 1 0x20"
+	]
+    cmd
+}
+
+//Sends IAS Zone Enroll response
+def enrollResponse() {
+	log.debug "Sending enroll response"
+	[	
+	"raw 0x500 {01 23 00 00 00}", "delay 200",
+	"send 0x${device.deviceNetworkId} 1 1"
+	]
+}
+
+private parseReportAttributeMessage(String description) {
+	Map descMap = (description - "read attr - ").split(",").inject([:]) { map, param ->
+		def nameAndValue = param.split(":")
+		map += [(nameAndValue[0].trim()):nameAndValue[1].trim()]
+	}
+	//log.debug "Desc Map: $descMap"
+
+	def results = []
+    
+	if (descMap.cluster == "0001" && descMap.attrId == "0020") {
+		log.debug "Received battery level report"
+		results = createEvent(getBatteryResult(Integer.parseInt(descMap.value, 16)))
+	}
+
+	return results
+}
+
+//Converts the battery level response into a percentage to display in ST
+//and creates appropriate message for given level
+
+private getBatteryResult(rawValue) {
+	def linkText = getLinkText(device)
+
+	def result = [name: 'battery']
+
+	def volts = rawValue / 10
+	def descriptionText
+	if (volts > 3.5) {
+		result.descriptionText = "${linkText} battery has too much power (${volts} volts)."
+	}
+	else {
+		def minVolts = 2.4
+		def maxVolts = 3.0
+		def pct = (volts - minVolts) / (maxVolts - minVolts)
+		result.value = Math.min(100, (int) pct * 100)
+		result.descriptionText = "${linkText} battery was ${result.value}%"
+	}
+
+	return result
+}
+private hex(value) {
+	new BigInteger(Math.round(value).toString()).toString(16)
+}
+
+private String swapEndianHex(String hex) {
+	reverseArray(hex.decodeHex()).encodeHex()
+}
+
+private byte[] reverseArray(byte[] array) {
+	int i = 0;
+	int j = array.length - 1;
+	byte tmp;
+	while (j > i) {
+		tmp = array[j];
+		array[j] = array[i];
+		array[i] = tmp;
+		j--;
+		i++;
+	}
+	return array
+}
