48b5d0703ee5d3e036c759d32c7f4e6f5dae4700
kathiresan <kathiresan.c@rediffmail.com>
2015-11-24 03:43:53 -0600

    MSA-700: NetworkScanner Device Discovery

---
+++ smartapps/networkscanner/scanner-smartthings-apps.src/scanner-smartthings-apps.groovy
@@ -0,0 +1,42 @@
+/**
+ *  SCANNER SMARTTHINGS APPS
+ *
+ *  Copyright 2015 kathiresan
+ *
+ */
+definition(
+    name: "SCANNER SMARTTHINGS APPS",
+    namespace: "NetworkScanner",
+    author: "kathiresan",
+    description: "Network Scanner Smartthings Apps",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    oauth: true)
+
+
+preferences {
+	section("Title") {
+		// TODO: put inputs here
+	}
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	// TODO: subscribe to attributes, devices, locations, etc.
+}
+
+// TODO: implement event handlers
