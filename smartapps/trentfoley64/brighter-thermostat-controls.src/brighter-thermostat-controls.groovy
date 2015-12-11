ad981cf7541320402a3849daa93a88c9e54e87c0
A. Trent Foley <a.trentfoley@gmail.com>
2015-12-10 21:57:34 -0600

    pedantic

--- smartapps/trentfoley64/brighter-thermostat-controls.src/brighter-thermostat-controls.groovy
+++ smartapps/trentfoley64/brighter-thermostat-controls.src/brighter-thermostat-controls.groovy
@@ -9,6 +9,9 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
+ TODO:
+ 1) Move the thermostats preference to the main parent app, allowing multiple instances of parent
+ 2) Implement filter change notices
  */
 
 definition(
@@ -16,7 +19,7 @@ definition(
 	singleInstance: true,
 	namespace: "trentfoley64",
 	author: "A. Trent Foley, Sr.",
-	description: "Smart Thermostat Controls - time, day, and presence sensitive.",
+	description: "Brighter Thermostat Controls - time, day, and presence sensitive.",
 	category: "My Apps",
 	iconUrl: "http://www.trentfoley.com/ST/icons/thermostat.png",
 	iconX2Url: "http://www.trentfoley.com/ST/icons/thermostat@2x.png",
@@ -25,13 +28,13 @@ definition(
 
 preferences {
 	page(name: "mainPage", title: "Brighter Thermostat Controls", install: true, uninstall: true, submitOnChange: true) {
-		section("Brighter Thermostat Controls") {
+		section {
 			app(name: "childThermostatControls", appName: "New Brighter Thermostat Control", namespace: "trentfoley64", title: "new thermostat control...", multiple: true)
 		}
-    	section("Filter info") {
-        	input "filterLastChanged", "date", title: "Date last changed?", required: false
-            input "filterMonthsBeforeChange", "number", title: "Lasts how many months?", required: false
-        }
+		section("Filter info", hideable: true) {
+			input "filterLastChanged", "date", title: "Date last changed?", required: false
+			input "filterMonthsBeforeChange", "number", title: "Lasts how many months?", required: false
+		}
 	}
 }
 
--- smartapps/trentfoley64/new-brighter-thermostat-control.src/new-brighter-thermostat-control.groovy
+++ smartapps/trentfoley64/new-brighter-thermostat-control.src/new-brighter-thermostat-control.groovy
@@ -1,5 +1,9 @@
 /********
 
+todo:
+1) move thermostat to parent
+2) figure out how to provide default name of child smartapp
+
 */
 
 definition(
@@ -15,13 +19,13 @@ definition(
 )
 
 preferences {
-    page(name: "setPrefs")
-    page(name: "setName")
+	page(name: "setPrefs")
+	page(name: "setName")
 }
 
 def setPrefs() {
 	dynamicPage(name: "setPrefs", title: "Brighter Thermostat Control", nextPage: "setName", install: false, uninstall: true) {
-    	// Let use pick thermostats
+		// Let user pick thermostats
 		section("Set these thermostats") {
 			input "thermostats", "capability.thermostat", title: "Which?", multiple: true
 		}
@@ -32,19 +36,8 @@ def setPrefs() {
 		}
 		// Let user pick which days of week
 		section("for Days of Week") {
-			input "dayOfWeek", "enum",
-				title: "Which days?",
-				required: true,
-				multiple: true,
-				options: [
-					'Monday',
-					'Tuesday',
-					'Wednesday',
-					'Thursday',
-					'Friday',
-					'Saturday',
-					'Sunday'
-				]
+			input "daysOfWeekList", "enum", title: "Which days?", required: true, multiple: true,
+				options: ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
 		}
 		// Let user specify Time
 		section("Time of day") {
@@ -55,9 +48,9 @@ def setPrefs() {
 			input "anyMustBePresent", "capability.presenceSensor", title: "At least one must be present", multiple: true, required: false
 			input "allMustBePresent", "capability.presenceSensor", title: "All must be present", multiple: true, required: false
 			input "anyMustBeAbsent", "capability.presenceSensor", title: "At least one must be absent", multiple: true, required: false
-            input "allMustBeAbsent", "capability.presenceSensor", title: "All must be absent", multiple: true, required: false
-        }
-
+			input "allMustBeAbsent", "capability.presenceSensor", title: "All must be absent", multiple: true, required: false
+		}
+		// Let user specify notification recipients
 		section( "Notifications" ) {
 			input "sendPushMessage", "enum", title: "Send a push notification?", options:["Yes", "No"], required: false, default: "No"
 			input "phoneNumber", "phone", title: "Send a text message?", required: false
@@ -68,14 +61,10 @@ def setPrefs() {
 def setName() {
 	dynamicPage(name: "setName", title: "Brighter Thermostat Control", install: true, uninstall: true) {
 		section("Control name") {
-            label title: "Assign a name", required: true, description: getDefaultName(), default: "$thermostats"
-            mode title: "Set for specific mode(s)", required: false
-        }
-    }
-}
-
-private getDefaultName() {
-	return "$thermostats $time $dayOfWeek $anyMustBePresent $allMustBepresent $anyMustBeAbsent $allMustBeAbsent"
+			label title: "Assign a name", required: true, description: getDefaultLabel(), default: "$thermostats"
+			//mode title: "Set for specific mode(s)", required: false
+		}
+	}
 }
 
 def installed() {
@@ -89,106 +78,109 @@ def updated() {
 }
 
 def initialize() {
-	unschedule()
-	def scheduleTime = timeToday(time, location.timeZone)
-	def timeNow = now() + 2000
+	// get a date/time object for today at time specified by schedule
+	def scheduleTime=timeToday(time,location.timeZone)
+    // compute next date/time for schedule determined by daysOfWeekList
+	def nextRunTime=nextDayOfWeekDate(scheduleTime,daysOfWeekList)
+    
+    // Schedule thermostat control to run on computed date/time
+    if(nextRunTime) {
+		log.debug "Scheduling next run for " + nextRunTime.format("EEE MMM dd yyyy HH:mm z", location.timeZone)
+		runOnce(nextRunTime,runThermostatControl)
+    }
+    else {
+    	// This should never happen.  Since both daysOfWeekList and time are required there is no
+        // reason why nextRunTime should ever be null
+    	def msg="Aborting Brighter Thermostat Control: ${app.label}." +
+		        " Could not schedule next run for " + scheduleTime.format("EEE MMM dd yyyy HH:mm z", location.timeZone) +
+                " for daysOfWeekList=" + daysOfWeekList
+    	log.debug msg
+		sendPush msg
+    }
+}
 
-	// If it is past current time, schedule for next day
-    // need to make this smarter. Use dow data to compute next date
-    // Also, don't use "schedule" which runs every day.  instead
-    // use runOnce
-	if (scheduleTime.time < timeNow) {
-		scheduleTime = scheduleTime + 1 // Adding one adds a day
-	}
+private getDefaultLabel() {
+	// still trying to figure out a way to provide a default, or to automatically name a child smartapp
+	def msg="$thermostats $time $daysOfWeekList $anyMustBePresent $allMustBepresent $anyMustBeAbsent $allMustBeAbsent"
+	return msg
+}
 
-	log.debug "Scheduling next run for " + scheduleTime // .format("EEE MMM dd yyyy HH:mm z", location.timeZone)
-	schedule(scheduleTime, runThermostatControl)
+private nextDayOfWeekDate(startTime,daysOfWeekList) {
+    // If it is early enough for schedule to run today, start from today; otherwise, start from tomorrow.
+    // Also, there is a need to add a couple of minutes (2000ms) to now() to cover any sort of smartthings lag.
+	def nextScheduleTime=((now()+2000) < startTime.time) ? startTime : startTime + 1
+    // Check for up to 7 days ahead to find a date that matches our daysOfWeekList
+	// use EEEE format to convert to long form day of week (Sunday,Monday,...Saturday)
+    for(def i=0; i<6; i++) {
+        if (daysOfWeekList.contains(nextScheduleTime.format("EEEE",location.timeZone))) {
+        	// found the dayOfWeek so return it
+        	return nextScheduleTime
+        }
+        // nextScheduleTime wasn't in our daysOfWeekList so skip to next day
+        nextScheduleTime=nextScheduleTime + 1
+    }
+    // If we get to this point, we are on the wrong planet and something is very wrong.
+	log.debug "Wrong Planet Error. Unable to compute nextDayOfWeekDate for $startTime and $daysOfWeekList."
+	return null
 }
 
 def runThermostatControl() {
-	def passesChecks = false
-	Calendar localCalendar = Calendar.getInstance()
-	localCalendar.setTimeZone(location.timeZone)
-	int currentDayOfWeek = localCalendar.get(Calendar.DAY_OF_WEEK)
-
-	// Check the current day of week against the list of days of the week for scheduling
-	if (dayOfWeek.contains('Monday') && currentDayOfWeek == Calendar.instance.MONDAY) {
-		passesChecks = true
-	}
-	else if (dayOfWeek.contains('Tuesday') && currentDayOfWeek == Calendar.instance.TUESDAY) {
-		passesChecks = true
-	}
-	else if (dayOfWeek.contains('Wednesday') && currentDayOfWeek == Calendar.instance.WEDNESDAY) {
-		passesChecks = true
-	}
-	else if (dayOfWeek.contains('Thursday') && currentDayOfWeek == Calendar.instance.THURSDAY) {
-		passesChecks = true
-	}
-	else if (dayOfWeek.contains('Friday') && currentDayOfWeek == Calendar.instance.FRIDAY) {
-		passesChecks = true
-	}
-	else if (dayOfWeek.contains('Saturday') && currentDayOfWeek == Calendar.instance.SATURDAY) {
-		passesChecks = true
-	}
-	else if (dayOfWeek.contains('Sunday') && currentDayOfWeek == Calendar.instance.SUNDAY) {
-		passesChecks = true
-	}
-	// If day of week checks out, check presences
+	// before doing anything else, schedule our next run
+	initialize()
+ 	// Check presences
+ 	def passedChecks=checkPresences()
+	// If we have hit the conditions to execute this then lets do it
 	if (passesChecks) {
-		// If defined, check anyMustBePresent
-		if (anyMustBePresent) {
-			// If anyMustBePresent does not contain anyone present, do not change thermostats
-			if (!anyMustBePresent.currentValue('presence').contains('present')) {
-				log.debug "Scheduled thermostat change cancelled due to all of ${anyMustBePresent} being absent."
-				passesChecks = false
-			}
-		}
-		// If defined, check allMustBePresent
-		if (passesChecks && allMustBePresent) {
-			// If allMustBePresent contains anyone not present, do not change thermostats
-			if (allMustBePresent.currentValue('presence').contains('not present')) {
-				log.debug "Scheduled thermostat change cancelled due to one of ${allMustBePresent} being absent."
-				passesChecks = false
-			}
-		}
-		// If defined, check anyMustBeAbsent
-		if (passesChecks && anyMustBeAbsent) {
-			// If anyMustBeAbsent does not contain anyone not present, do not change thermostats
-			if (!anyMustBeAbsent.currentValue('presence').contains('not present')) {
-				log.debug "Scheduled thermostat change cancelled due to all of ${anyMustBeAbsent} being present."
-				passesChecks = false
-			}
-		}
-		// If defined, check allMustBeAbsent
-		if (passesChecks && allMustBeAbsent) {
-			// If allMustBeAbsent contains anyone present, do not change thermostats
-			if (allMustBeAbsent.currentValue('presence').contains('present')) {
-				log.debug "Scheduled thermostat change cancelled due to one of ${allMustBeAbsent} being present."
-				passesChecks = false
-			}
-		}
-	}
-
-	// If we have hit the condition to schedule this then lets do it
-	if (passesChecks) {
-		def msg = "$thermostats heat setpoint to '${heatSetpoint}' and cool setpoint to '${coolSetpoint}'"
-		log.debug msg
+		def msg="$thermostats heat setpoint to '${heatSetpoint}' and cool setpoint to '${coolSetpoint}'"
+		log.debug app.label + ": " + msg
 		thermostats.setHeatingSetpoint(heatSetpoint)
 		thermostats.setCoolingSetpoint(coolSetpoint)
 		send msg
 	}
-
-	log.debug "Scheduling next check"
-
-	// reinitialize to set next time schedule
-	initialize()
 }
+
+private checkPresences() {
+	// If defined, check anyMustBePresent
+	if (anyMustBePresent) {
+		// If anyMustBePresent does not contain anyone present, do not change thermostats
+		if (!anyMustBePresent.currentValue('presence').contains('present')) {
+			log.debug app.label + ": change cancelled due to all of ${anyMustBePresent} being absent."
+			return false
+		}
+	}
+	// If defined, check allMustBePresent
+	if (allMustBePresent) {
+		// If allMustBePresent contains anyone not present, do not change thermostats
+		if (allMustBePresent.currentValue('presence').contains('not present')) {
+			log.debug app.label + ": cancelled due to one of ${allMustBePresent} being absent."
+			return false
+		}
+	}
+	// If defined, check anyMustBeAbsent
+	if (anyMustBeAbsent) {
+		// If anyMustBeAbsent does not contain anyone not present, do not change thermostats
+		if (!anyMustBeAbsent.currentValue('presence').contains('not present')) {
+			log.debug app.label + ": cancelled due to all of ${anyMustBeAbsent} being present."
+			return false
+		}
+	}
+	// If defined, check allMustBeAbsent
+	if (allMustBeAbsent) {
+		// If allMustBeAbsent contains anyone present, do not change thermostats
+		if (allMustBeAbsent.currentValue('presence').contains('present')) {
+			log.debug app.label + ": cancelled due to one of ${allMustBeAbsent} being present."
+			return false
+		}
+	}
+    // If we've gotten to here, all checks have passed
+    return true
+} 
 
 private send(msg) {
 	if (sendPushMessage == "Yes") {
 		sendPush(msg)
 	}
 	if (phoneNumber) {
-		sendSms(phoneNumber, msg)
+		sendSms(phoneNumber,msg)
 	}
 }
