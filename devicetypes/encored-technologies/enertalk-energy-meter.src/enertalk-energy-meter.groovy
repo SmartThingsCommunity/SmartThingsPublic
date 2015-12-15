9cda82651158a925cc2808a852b31582afce3d01
hyeon seok yang <hsyg@encoredtech.com>
2015-12-15 14:01:34 +0900

    deep linking update

--- devicetypes/encored-technologies/enertalk-energy-meter.src/enertalk-energy-meter.groovy
+++ devicetypes/encored-technologies/enertalk-energy-meter.src/enertalk-energy-meter.groovy
@@ -38,7 +38,19 @@ metadata {
        		state "plan", label:' ${currentValue}'
 		}
         
-        htmlTile(name:"deepLink", action:"linkApp", whiltelist:["code.jquery.com", "ajax.googleapis.com", "code.highcharts.com", "enertalk-card.encoredtech.com", "s3-ap-northeast-1.amazonaws.com"], width:2, height:2){}
+        htmlTile(name:"deepLink", action:"linkApp", whiltelist:["code.jquery.com", 
+    	"ajax.googleapis.com", 
+        "fonts.googleapis.com",
+    	"code.highcharts.com", 
+    	"enertalk-card.encoredtech.com", 
+    	"s3-ap-northeast-1.amazonaws.com",
+        "s3.amazonaws.com", 
+    	"ui-hub.encoredtech.com",
+        "enertalk-auth.encoredtech.com",
+        "api.encoredtech.com",
+        "cdnjs.cloudflare.com",
+        "encoredtech.com",
+        "itunes.apple.com"], width:2, height:2){}
         
         main (["view"])
         details (["month", "real", "tier", "plan", "deepLink"])
@@ -61,8 +73,22 @@ def getLinkedApp() {
         body {
         """
          	<div id="container">
-            	<a id=\'st-deep-link\' href=\'market://details?id=com.ionicframework.enertalkhome874425\'><img alt='EnerTalk' src='https://s3-ap-northeast-1.amazonaws.com/smartthings-images/btn_setup_normal.svg' /></a>
+            	<a id=\'st-deep-link\' href=\'#\' onclick=\'openApp()\'><svg version="1.1" id="레이어_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 40 40" style="enable-background:new 0 0 40 40;" xml:space="preserve">
+              <path class="st0" d="M20,0C9,0,0,9,0,20C0,30.5,8,39,18.2,40l3.8-4.8l-3.9-4.8c-4.9-0.9-8.6-5.2-8.6-10.4c0-5.8,4.7-10.5,10.5-10.5 S30.5,14.2,30.5,20c0,5.1-3.7,9.4-8.5,10.3l3.7,4.5L21.8,40C32,39.1,40,30.5,40,20C40,9,31,0,20,0z"/>
+            </svg></a>
           	</div>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+            <script>
+            	function openApp() {
+                  	 var ua = navigator.userAgent.toLowerCase();
+                    var isAndroid = ua.indexOf("android") > -1;
+                    if(!isAndroid) {
+                    	document.getElementById(\'st-deep-link\').href=\'tms-appss://itunes.apple.com/app/id1024660780\'; 
+                    } else {
+                    	document.getElementById(\'st-deep-link\').href=\'market://details?id=com.ionicframework.enertalkhome874425\'; 
+					}
+                }
+            </script>
         """
         }
 	}
