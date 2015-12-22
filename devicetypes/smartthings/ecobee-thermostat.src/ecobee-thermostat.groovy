bc1d312fce5a42bb56eba044efd55682ff3cb4b8
Sean Schneyer <sean@linuxbox.org>
2015-12-22 00:47:28 -0600

    Spaces to tabs
    Cleaned up the inconsistency between tabs and spaces used in the file

--- devicetypes/smartthings/ecobee-thermostat.src/ecobee-thermostat.groovy
+++ devicetypes/smartthings/ecobee-thermostat.src/ecobee-thermostat.groovy
@@ -21,16 +21,16 @@ metadata {
 		capability "Thermostat"
 		capability "Polling"
 		capability "Sensor"
-        	capability "Refresh"
+		capability "Refresh"
 
-        	command "generateEvent"
-        	command "raiseSetpoint"
-        	command "lowerSetpoint"
-        	command "resumeProgram"
-        	command "switchMode"
+		command "generateEvent"
+		command "raiseSetpoint"
+		command "lowerSetpoint"
+		command "resumeProgram"
+		command "switchMode"
 
-        	attribute "thermostatSetpoint","number"
-        	attribute "thermostatStatus","string"
+		attribute "thermostatSetpoint","number"
+		attribute "thermostatStatus","string"
 	}
 
 	simulator { }
@@ -38,15 +38,15 @@ metadata {
     	tiles {
 		valueTile("temperature", "device.temperature", width: 2, height: 2) {
 			state("temperature", label:'${currentValue}°', unit:"F",
-	                backgroundColors:[
-                    		[value: 31, color: "#153591"],
-	                	[value: 44, color: "#1e9cbb"],
-        	        	[value: 59, color: "#90d2a7"],
-                		[value: 74, color: "#44b621"],
-	                	[value: 84, color: "#f1d801"],
-        	        	[value: 95, color: "#d04e00"],
-                	    	[value: 96, color: "#bc2323"]
-	                ]
+				backgroundColors:[
+                	[value: 31, color: "#153591"],
+					[value: 44, color: "#1e9cbb"],
+					[value: 59, color: "#90d2a7"],
+					[value: 74, color: "#44b621"],
+					[value: 84, color: "#f1d801"],
+					[value: 95, color: "#d04e00"],
+					[value: 96, color: "#bc2323"]
+				]
 			)
 		}
 		standardTile("mode", "device.thermostatMode", inactiveLabel: false, decoration: "flat") {
