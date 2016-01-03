076aa8d8d105f411e998a165ead6bd0f692fe7ae
Sean Schneyer <sean@linuxbox.org>
2016-01-03 00:07:00 -0600

    Updated to match changes to SmartApp

--- devicetypes/smartthings/ecobee-sensor.src/ecobee-sensor.groovy
+++ devicetypes/smartthings/ecobee-sensor.src/ecobee-sensor.groovy
@@ -29,15 +29,24 @@ metadata {
 	tiles {
 		valueTile("temperature", "device.temperature", width: 2, height: 2) {
 			state("temperature", label:'${currentValue}°', unit:"F",
-					backgroundColors:[
-							[value: 31, color: "#153591"],
-							[value: 44, color: "#1e9cbb"],
-							[value: 59, color: "#90d2a7"],
-							[value: 74, color: "#44b621"],
-							[value: 84, color: "#f1d801"],
-							[value: 95, color: "#d04e00"],
-							[value: 96, color: "#bc2323"]
-					]
+				backgroundColors:[
+                	// Celsius Color Range
+					[value: 0, color: "#153591"],
+					[value: 7, color: "#1e9cbb"],
+					[value: 15, color: "#90d2a7"],
+					[value: 23, color: "#44b621"],
+					[value: 29, color: "#f1d801"],
+					[value: 33, color: "#d04e00"],
+					[value: 36, color: "#bc2323"],
+					// Fahrenheit Color Range
+                	[value: 40, color: "#153591"],
+					[value: 44, color: "#1e9cbb"],
+					[value: 59, color: "#90d2a7"],
+					[value: 74, color: "#44b621"],
+					[value: 84, color: "#f1d801"],
+					[value: 95, color: "#d04e00"],
+					[value: 96, color: "#bc2323"]
+				]
 			)
 		}
 
@@ -64,6 +73,14 @@ void poll() {
 	log.debug "Executing 'poll' using parent SmartApp"
 	parent.pollChildren(this)
 }
+
+def generateEvent(Map results) {
+	results.each { name, value ->
+		sendEvent(name: name, value: value)
+	}
+	return null
+}
+
 
 //generate custom mobile activity feeds event
 def generateActivityFeedsEvent(notificationMessage) {
