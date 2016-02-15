68808fe2107cb57e9a8a70dc9e614bb45e8713ad
Jared Fisher <kyse@kyse.us>
2016-02-14 23:37:50 -0500

    Updated to include Apache License and give credit to SmartThings as initial template.

--- devicetypes/kyse/kyses-aeon-minimote.src/kyses-aeon-minimote.groovy
+++ devicetypes/kyse/kyses-aeon-minimote.src/kyses-aeon-minimote.groovy
@@ -1,3 +1,21 @@
+/*
+ *
+ *  Modified by: Jared Fisher (Kyse@kyse.us)
+ *  Modified and extended from SmartThings Aeon Minimote Device Handler Template.
+ *
+ *  Copyright 2015 SmartThings
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+
 metadata {
 	definition (name: "Kyse's Aeon Minimote", namespace: "kyse", author: "Jared Fisher") {
 		capability "Actuator"
