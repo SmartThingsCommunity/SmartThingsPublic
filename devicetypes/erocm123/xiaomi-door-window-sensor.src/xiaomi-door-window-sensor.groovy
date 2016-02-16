d323424d4675b0097a3046ffad944f7f40b5a3b3
Eric Maycock <erocmail@gmail.com>
2016-02-16 15:38:39 -0700

    Initial Commit

---
+++ devicetypes/erocm123/xiaomi-door-window-sensor.src/xiaomi-door-window-sensor.groovy
@@ -0,0 +1,155 @@
+/**
+ *  Xiaomi Door/Window Sensor
+ *
+ *  Copyright 2015 Eric Maycock
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+   definition (name: "Xiaomi Door/Window Sensor", namespace: "erocm123", author: "Eric Maycock") {
+   capability "Configuration"
+   capability "Sensor"
+   capability "Contact Sensor"
+   capability "Refresh"
+   
+   command "enrollResponse"
+   
+   //fingerprint endpointId: "01", inClusters: "0000,0001", outClusters: "1234"//, model: "3320-L", manufacturer: "CentraLite"
+   fingerprint endpoint: "01",
+   //profileId: "0104",
+   inClusters: "0000,0001"
+   //outClusters: "1234"
+ 
+   }
+    
+   simulator {
+      status "closed": "on/off: 0"
+      status "open": "on/off: 1"
+   }
+    
+   tiles(scale: 2) {
+      multiAttributeTile(name:"contact", type: "generic", width: 6, height: 4){
+         tileAttribute ("device.contact", key: "PRIMARY_CONTROL") {
+            attributeState "open", label:'${name}', icon:"st.contact.contact.open", backgroundColor:"#ffa81e"
+            attributeState "closed", label:'${name}', icon:"st.contact.contact.closed", backgroundColor:"#79b821"
+         }
+      }
+      standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+            state "default", action:"refresh.refresh", icon:"st.secondary.refresh"
+      }
+      standardTile("configure", "device.configure", inactiveLabel: false, width: 2, height: 2, decoration: "flat") {
+			state "configure", label:'', action:"configuration.configure", icon:"st.secondary.configure"
+	  }
+
+      main (["contact"])
+      details(["contact","refresh","configure"])
+   }
+}
+
+def parse(String description) {
+   log.debug "Parsing '${description}'"
+   Map map = [:]
+   //def descMap = zigbee.parseDescriptionAsMap(description)
+   def resultMap = zigbee.getKnownDescription(description)
+   log.debug "${resultMap}"
+   if (description?.startsWith('on/off: '))
+      map = parseCustomMessage(description) 
+   log.debug "Parse returned $map"
+   def results = map ? createEvent(map) : null
+   return results;
+}
+
+def configure() {
+	String zigbeeEui = swapEndianHex(device.hub.zigbeeEui)
+	log.debug "${device.deviceNetworkId}"
+    def endpointId = 1
+    log.debug "${device.zigbeeId}"
+    log.debug "${zigbeeEui}"
+	def configCmds = [
+			//battery reporting and heartbeat
+			"zdo bind 0x${device.deviceNetworkId} 1 ${endpointId} 1 {${device.zigbeeId}} {}", "delay 200",
+			"zcl global send-me-a-report 1 0x20 0x20 600 3600 {01}", "delay 200",
+			"send 0x${device.deviceNetworkId} 1 ${endpointId}", "delay 1500",
+
+
+			// Writes CIE attribute on end device to direct reports to the hub's EUID
+			"zcl global write 0x500 0x10 0xf0 {${zigbeeEui}}", "delay 200",
+			"send 0x${device.deviceNetworkId} 1 1", "delay 500",
+	]
+
+	log.debug "configure: Write IAS CIE"
+	return configCmds
+}
+
+def enrollResponse() {
+	log.debug "Enrolling device into the IAS Zone"
+	[
+			// Enrolling device into the IAS Zone
+			"raw 0x500 {01 23 00 00 00}", "delay 200",
+			"send 0x${device.deviceNetworkId} 1 1"
+	]
+}
+
+def refresh() {
+	log.debug "Refreshing Battery"
+    def endpointId = 1
+	[
+			"st rattr 0x${device.deviceNetworkId} ${endpointId} 1 0x20", "delay 200"
+	] + enrollResponse()
+}
+
+private Map parseCustomMessage(String description) {
+   def result
+   if (description?.startsWith('on/off: ')) {
+      if (description == 'on/off: 0') 		//contact closed
+         result = getContactResult("closed")
+      else if (description == 'on/off: 1') 	//contact opened
+         result = getContactResult("open")
+      return result
+   }
+}
+
+private Map getContactResult(value) {
+   def linkText = getLinkText(device)
+   def descriptionText = "${linkText} was ${value == 'open' ? 'opened' : 'closed'}"
+   return [
+      name: 'contact',
+      value: value,
+      descriptionText: descriptionText
+	]
+}
+
+private String swapEndianHex(String hex) {
+	reverseArray(hex.decodeHex()).encodeHex()
+}
+private getEndpointId() {
+	new BigInteger(device.endpointId, 16).toString()
+}
+
+Integer convertHexToInt(hex) {
+	Integer.parseInt(hex,16)
+}
+
+private byte[] reverseArray(byte[] array) {
+	int i = 0;
+	int j = array.length - 1;
+	byte tmp;
+
+	while (j > i) {
+		tmp = array[j];
+		array[j] = array[i];
+		array[i] = tmp;
+		j--;
+		i++;
+	}
+
+	return array
+}
---
+++ devicetypes/erocm123/xiaomi-smart-button.src/xiaomi-smart-button.groovy
@@ -0,0 +1,113 @@
+/**
+ *  Xiaomi Smart Button
+ *
+ *  Copyright 2015 Eric Maycock
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "Xiaomi Smart Button", namespace: "erocm123", author: "Eric Maycock") {
+		capability "Button"
+        capability "Configuration"
+		    capability "Sensor"
+        capability "Refresh"
+        
+        attribute "lastPress", "string"
+
+	}
+    
+    simulator {
+   		status "button 1 pressed": "on/off: 0"
+      status "button 1 released": "on/off: 1"
+    }
+    
+    preferences{
+    	input ("holdTime", "number", title: "Minimum time in seconds for a press to count as \"held\"",
+        		defaultValue: 4, displayDuringSetup: false)
+    }
+
+	tiles(scale: 2) {
+    	standardTile("button", "device.button", decoration: "flat", width: 2, height: 2) {
+        	state "default", icon: "st.unknown.zwave.remote-controller", backgroundColor: "#ffffff"
+        }
+        standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+			state "default", action:"refresh.refresh", icon:"st.secondary.refresh"
+	}
+
+		main (["button"])
+		details(["button","refresh"])
+	}
+}
+
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+  def descMap = zigbee.parseDescriptionAsMap(description)
+	def results = []
+  if (description?.startsWith('on/off: '))
+		results = parseCustomMessage(description)
+	return results;
+}
+
+def configure(){
+	refresh()
+}
+
+def refresh(){
+}
+
+private Map parseCustomMessage(String description) {
+	if (description?.startsWith('on/off: ')) {
+    	if (description == 'on/off: 0') 		//button pressed
+    		createPressEvent(1)
+    	else if (description == 'on/off: 1') 	//button released
+    		createButtonEvent(1)
+	}
+}
+
+//this method determines if a press should count as a push or a hold and returns the relevant event type
+private createButtonEvent(button) {
+	def currentTime = now()
+    def startOfPress = device.latestState('lastPress').date.getTime()
+    def timeDif = currentTime - startOfPress
+    def holdTimeMillisec = (settings.holdTime?:3).toInteger() * 1000
+    
+    if (timeDif < 0) 
+    	return []	//likely a message sequence issue. Drop this press and wait for another. Probably won't happen...
+    else if (timeDif < holdTimeMillisec) 
+    	return createButtonPushedEvent(button)
+    else 
+    	return createButtonHeldEvent(button)
+}
+
+private createPressEvent(button) {
+	return createEvent([name: 'lastPress', value: now(), data:[buttonNumber: button], displayed: false])
+}
+
+private createButtonPushedEvent(button) {
+	log.debug "Button ${button} pushed"
+	return createEvent([
+    	name: "button",
+        value: "pushed", 
+        data:[buttonNumber: button], 
+        descriptionText: "${device.displayName} button ${button} was pushed",
+        isStateChange: true, 
+        displayed: true])
+}
+
+private createButtonHeldEvent(button) {
+	log.debug "Button ${button} held"
+	return createEvent([
+    	name: "button",
+        value: "held", 
+        data:[buttonNumber: button], 
+        descriptionText: "${device.displayName} button ${button} was held",
+        isStateChange: true])
+}
