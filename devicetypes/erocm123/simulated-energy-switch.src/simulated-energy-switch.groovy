f7284b270fcf5e16ab55fe91211fccfa4cabe86c
Eric Maycock <erocmail@gmail.com>
2016-02-23 11:40:26 -0700

    Initial Commit

---
+++ devicetypes/erocm123/simulated-energy-switch.src/simulated-energy-switch.groovy
@@ -0,0 +1,85 @@
+/**
+ *  Copyright 2015 Eric Maycock
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	
+    definition (name: "Simulated Energy Switch", namespace: "erocm123", author: "Eric Maycock") {
+		capability "Switch"
+        capability "Relay Switch"
+        capability "Energy Meter"
+		capability "Power Meter"
+
+		command "onPhysical"
+		command "offPhysical"
+        command "energy"
+        command "power"
+	}
+
+	tiles {
+		standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
+			state "off", label: '${currentValue}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
+			state "on", label: '${currentValue}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821"
+		}
+		standardTile("on", "device.switch", decoration: "flat") {
+			state "default", label: 'On', action: "onPhysical", backgroundColor: "#ffffff"
+		}
+		standardTile("off", "device.switch", decoration: "flat") {
+			state "default", label: 'Off', action: "offPhysical", backgroundColor: "#ffffff"
+		}
+        valueTile("power", "power", decoration: "flat") {
+			state "default", label:'${currentValue} W'
+		}
+		valueTile("energy", "energy", decoration: "flat") {
+			state "default", label:'${currentValue} kWh'
+		}
+        main "switch"
+		details(["switch", "power", "energy"])
+	}
+}
+
+def parse(String description) {
+	//def pair = description.split(":")
+	//createEvent(name: pair[0].trim(), value: pair[1].trim())
+}
+
+def on() {
+	log.debug "$version on()"
+	sendEvent(name: "switch", value: "on")
+}
+
+def off() {
+	log.debug "$version off()"
+	sendEvent(name: "switch", value: "off")
+}
+
+def onPhysical() {
+	log.debug "$version onPhysical()"
+	sendEvent(name: "switch", value: "on", type: "physical")
+}
+
+def offPhysical() {
+	log.debug "$version offPhysical()"
+	sendEvent(name: "switch", value: "off", type: "physical")
+}
+
+def energy(value) {
+   sendEvent(name: "energy", value: value)
+}
+
+def power(value) {
+   sendEvent(name: "power", value: value)
+}
+
+private getVersion() {
+	"PUBLISHED"
+}
