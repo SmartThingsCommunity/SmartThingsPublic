67c0d509ca7654d84f4993450b3ed82f9f96ef93
Laurence Kahn <kahn-st@lgk.com>
2016-02-22 20:38:50 -0500

    add power readings and labels for non switcheable outlets. also add temp reading for switch overall.

--- devicetypes/smartthings/aeon-smartstrip-lgk.src/aeon-smartstrip-lgk.groovy
+++ devicetypes/smartthings/aeon-smartstrip-lgk.src/aeon-smartstrip-lgk.groovy
@@ -18,6 +18,9 @@
  * screwed up below the fold.
  * change scale to 2 for smaller tiles, but for some strange reason had to put blank tile
  * on the end of the label otherwise it screws up and puts weird stuff there.
+ * v3 integrate power readings for outlet 5 and 6,
+ * add internal temperature tile.
+ 
  
  */
 metadata {
@@ -44,18 +47,25 @@ metadata {
 			command "on$n"
 			command "off$n"
 			command "reset$n"
-		}
+            }
+            attribute "power5", "number"
+			attribute "energy5", "number"
+            command "reset5"
+            command "reset6"
 
 		fingerprint deviceId: "0x1001", inClusters: "0x25,0x32,0x27,0x70,0x85,0x72,0x86,0x60", outClusters: "0x82"
 	}
 
 
 preferences {
- input("SwitchLabel1", "text", title: "Label for Switch 1?", required: false, defaultValue: "Switch 1", description: "Label for the first switch.")  
- input("SwitchLabel2", "text", title: "Label for Switch 2?", required: false, defaultValue: "Switch 2", description: "Label for the second switch.")  
- input("SwitchLabel3", "text", title: "Label for Switch 3?", required: false, defaultValue: "Switch 3", description: "Label for the third switch.")  
- input("SwitchLabel4", "text", title: "Label for Switch 4?", required: false, defaultValue: "Switch 4", description: "Label for the fourth switch.")  
- }
+ input("SwitchLabel1", "text", title: "Label for Switch 1?", required: false, defaultValue: "Switch 1", description: "Label for the 1st switch.")  
+ input("SwitchLabel2", "text", title: "Label for Switch 2?", required: false, defaultValue: "Switch 2", description: "Label for the 2nd switch.")  
+ input("SwitchLabel3", "text", title: "Label for Switch 3?", required: false, defaultValue: "Switch 3", description: "Label for the 3rd switch.")  
+ input("SwitchLabel4", "text", title: "Label for Switch 4?", required: false, defaultValue: "Switch 4", description: "Label for the 4th switch.")  
+ input("OutletLabel1", "text", title: "Label for non switcheable outlet 1?", required: false, defaultValue: "Outlet 1", description: "Label for the 1st non switcheable outlet.")  
+ input("OutletLabel2", "text", title: "Label for non switcheable outlet 2?", required: false, defaultValue: "Outlet 2", description: "Label for the 2nd non switcheable outlet.")  
+
+}
 
 	// simulator metadata
 	simulator {
@@ -97,8 +107,7 @@ preferences {
 		standardTile("refresh", "device.power", width:2, height: 2, inactiveLabel: false, decoration: "flat") {
 			state "default", label:'', action:"refresh.refresh", icon:"st.secondary.refresh"
 		}
-
-		
+	
 			standardTile("switch1", "switch1", canChangeIcon: true, width: 2, height: 2) {
 				state "on", label: '${name}', action: "off1", icon: "st.switches.switch.on", backgroundColor: "#79b821"
 				state "off", label: '${name}', action: "on1", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
@@ -129,7 +138,8 @@ preferences {
 			valueTile("energy3", "energy3", width: 2, height: 2, decoration: "flat") {
 				state "default", label:'${currentValue} kWh'
 			}
-		standardTile("switch4", "switch4", width: 2, height: 2, canChangeIcon: true) {
+		
+        	standardTile("switch4", "switch4", width: 2, height: 2, canChangeIcon: true) {
 				state "on", label: '${name}', action: "off4", icon: "st.switches.switch.on", backgroundColor: "#79b821"
 				state "off", label: '${name}', action: "on4", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
 			}
@@ -139,6 +149,27 @@ preferences {
 			valueTile("energy4", "energy4", width: 2, height: 2, decoration: "flat") {
 				state "default", label:'${currentValue} kWh', icon: ""
 			}
+            
+            valueTile("power5", "power5", width: 2, height: 2, decoration: "flat") {
+				state "default", label:'${currentValue} W', icon: ""
+			}
+			valueTile("energy5", "energy5", width: 2, height: 2, decoration: "flat") {
+				state "default", label:'${currentValue} kWh', icon: ""
+			}
+            valueTile("power6", "power6", width: 2, height: 2, decoration: "flat") {
+				state "default", label:'${currentValue} W', icon: ""
+			}
+			valueTile("energy6", "energy6", width: 2, height: 2, decoration: "flat") {
+				state "default", label:'${currentValue} kWh', icon: ""
+			}
+            
+            valueTile("outlet1", "outlet1", width: 2, height: 2, decoratetion: "flat") { 
+ 				state "default", label:"outlet1" 
+			} 
+  			valueTile("outlet2", "outlet2", width: 2, height: 2, decoratetion: "flat") { 
+ 				state "default", label:"outlet2" 
+			}
+            
 		
   		 valueTile("label1", "device.Label1", width: 4, height: 1, decoration: "flat") {
 			state "default", label: '${currentValue}'
@@ -150,6 +181,12 @@ preferences {
 			state "default", label: '${currentValue}'
             }
   		valueTile("label4", "device.Label4", width: 4, height: 1, decoration: "flat") {
+			state "default", label: '${currentValue}'
+            }
+        valueTile("label5", "device.Label5", width: 4, height: 1, decoration: "flat") {
+			state "default", label: '${currentValue}'
+            }
+        valueTile("label6", "device.Label6", width: 4, height: 1, decoration: "flat") {
 			state "default", label: '${currentValue}'
             }
 		standardTile("blank1", "blank1", width: 2, height: 1, decoration: "flat") {
@@ -167,6 +204,26 @@ preferences {
         standardTile("blank4", "blank4", width: 2, height: 1, decoration: "flat") {
 			state "default", label: " "
             }
+          standardTile("blank5", "blank5", width: 2, height: 1, decoration: "flat") {
+			state "default", label: " "
+            }
+          standardTile("blank6", "blank6", width: 2, height: 1, decoration: "flat") {
+			state "default", label: " "
+            }
+            
+           valueTile("temperature", "device.temperature", inactiveLabel: false, width: 2, height: 2) { 
+             state "temperature", label:'${currentValue}°',icon:"st.Weather.weather2",
+             backgroundColors: 
+             [ 
+                	[value: 44, color: "#1e9cbb"],
+					[value: 59, color: "#90d2a7"],
+					[value: 74, color: "#44b621"],
+					[value: 84, color: "#f1d801"],
+                    [value: 92, color: "#d04e00"],
+					[value: 98, color: "#bc2323"]
+             ] 
+         } 
+
 		main(["switch", "power", "energy"])
 		details(["switch","power","energy",
 				 "label1","blank1",
@@ -177,7 +234,11 @@ preferences {
                  "switch3","power3","energy3",
 				 "label4","blank4",
                  "switch4","power4","energy4",
-				 "refresh","reset"])
+                 "label5","blank5",
+                 "outlet1","power5","energy5", 
+                 "label6","blank6",
+                 "outlet2","power6","energy6", 
+				 "temperature","refresh","reset"])
 	}
 }
 
@@ -199,7 +260,7 @@ def parse(String description) {
 			result = zwaveEvent(cmd, null)
 		}
 	}
-	log.debug "parsed '${description}' to ${result.inspect()}"
+//	log.debug "parsed '${description}' to ${result.inspect()}"
 	result
 }
 
@@ -216,13 +277,15 @@ def zwaveEvent(physicalgraph.zwave.commands.multichannelv3.MultiChannelCmdEncap 
 		if (encapsulatedCommand.commandClassId == 0x32) {
 			// Metered outlets are numbered differently than switches
 			Integer endpoint = cmd.sourceEndPoint
-			if (endpoint > 2) {
-            log.debug ""
+			if (endpoint > 2) 
+            {
             def epnew  = endpoint - 2
-            log.debug "setting endpoint to $epnew old ep = $ep old endpoint = $endpoint"
+        //    log.debug "setting endpoint to $epnew old ep = $ep old endpoint = $endpoint"
 				zwaveEvent(encapsulatedCommand, endpoint - 2)
 			} else if (endpoint == 0) {
 				zwaveEvent(encapsulatedCommand, 0)
+			} else if (endpoint == 1 || endpoint == 2) { 
+                zwaveEvent(encapsulatedCommand, endpoint + 4) 
 			} else {
 				log.debug("Ignoring metered outlet $endpoint msg: $encapsulatedCommand")
 				[]
@@ -234,7 +297,12 @@ def zwaveEvent(physicalgraph.zwave.commands.multichannelv3.MultiChannelCmdEncap 
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicReport cmd, endpoint) {
-//log.debug "in basic report cmd = $cmd ep = $endpoint"
+log.debug "in basic report cmd = $cmd ep = $endpoint"
+
+if (!ep)
+{
+zwave.configurationV1.configurationGet(parameterNumber: 90).format()
+}	
 
 	def map = [name: "switch", type: "physical", value: (cmd.value ? "on" : "off")]
 	def events = [endpointEvent(endpoint, map)]
@@ -246,7 +314,8 @@ def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicReport cmd, endpoint) {
 		events += (1..4).collect { ep -> endpointEvent(ep, map.clone()) }
 		cmds << "delay 3000"
 		cmds += delayBetween((0..4).collect { ep -> encap(zwave.meterV3.meterGet(scale: 2), ep) }, 800)
-	}
+		
+    }
 	if(cmds) events << response(cmds)
 	events
 }
@@ -278,7 +347,7 @@ def zwaveEvent(physicalgraph.zwave.commands.switchbinaryv1.SwitchBinaryReport cm
 def zwaveEvent(physicalgraph.zwave.commands.meterv3.MeterReport cmd, ep) {
 	def event = [:]
 	def cmds = []
-    log.debug "in meter report cmd = $cmd ep = $ep"
+   // log.debug "in meter report cmd = $cmd ep = $ep"
 		if (cmd.scale == 0) {
         log.debug " got kwh $cmd.scaledMeterValue"
 			event = endpointEvent(ep, [name: "energy", value: cmd.scaledMeterValue, unit: "kWh"])
@@ -295,11 +364,12 @@ def zwaveEvent(physicalgraph.zwave.commands.meterv3.MeterReport cmd, ep) {
 		//event = endpointEvent(ep, [name: "energy", value: cmd.scaledMeterValue, unit: ["kWh", "kVAh"][cmd.scale]])
 	//} 
     else {
+   //  log.debug "in power event for endpoint = $ep"
 		event = endpointEvent(ep, [name: "power", value: Math.round(cmd.scaledMeterValue), unit: "W"])
 	}
 	if (!ep && event.isStateChange && event.name == "energy") {
-		// Total strip energy consumption changed, check individual outlets
-		(1..4).each { endpoint ->
+  	// Total strip energy consumption changed, check individual outlets
+		(1..6).each { endpoint ->
 			cmds << encap(zwave.meterV2.meterGet(scale: 0), endpoint)
 			cmds << "delay 400"
 		}
@@ -349,8 +419,28 @@ log.debug "in refresh"
 		zwave.basicV1.basicGet().format(),
 		zwave.meterV3.meterGet(scale: 0).format(),
 		zwave.meterV3.meterGet(scale: 2).format(),
-		encap(zwave.basicV1.basicGet(), 1000)  // further gets are sent from the basic report handler
-	])
+        zwave.configurationV1.configurationGet(parameterNumber: 90).format(), //temp
+		encap(zwave.basicV1.basicGet(), 1000),
+        encap(zwave.switchBinaryV1.switchBinaryGet(), null),
+        encap(zwave.switchBinaryV1.switchBinaryGet(), 1), 
+        encap(zwave.switchBinaryV1.switchBinaryGet(), 2), 
+        encap(zwave.switchBinaryV1.switchBinaryGet(), 3), 
+        encap(zwave.switchBinaryV1.switchBinaryGet(), 4), 
+        encap(zwave.switchBinaryV1.switchBinaryGet(), 5), 
+        encap(zwave.switchBinaryV1.switchBinaryGet(), 6), 
+    	encap(zwave.meterV2.meterGet(scale: 0), 1),
+        encap(zwave.meterV2.meterGet(scale: 2), 1),
+		encap(zwave.meterV2.meterGet(scale: 0), 2),
+        encap(zwave.meterV2.meterGet(scale: 2), 2),
+        encap(zwave.meterV2.meterGet(scale: 0), 3),
+        encap(zwave.meterV2.meterGet(scale: 2), 3),
+       	encap(zwave.meterV2.meterGet(scale: 0), 4),
+        encap(zwave.meterV2.meterGet(scale: 2), 4),
+       	encap(zwave.meterV2.meterGet(scale: 0), 5),
+        encap(zwave.meterV2.meterGet(scale: 2), 5),
+       	encap(zwave.meterV2.meterGet(scale: 0), 6),
+        encap(zwave.meterV2.meterGet(scale: 2), 6)	
+	],1000)
 }
 
 def resetCmd(endpoint = null) {
@@ -363,13 +453,15 @@ log.debug "in reset cmd"
 
 def reset() {
 log.debug "in reset"
-	delayBetween([resetCmd(null), reset1(), reset2(), reset3(), reset4()])
+	delayBetween([resetCmd(null), reset1(), reset2(), reset3(), reset4(), reset5(), reset6(),1000])
 }
 
 def reset1() { resetCmd(1) }
 def reset2() { resetCmd(2) }
 def reset3() { resetCmd(3) }
 def reset4() { resetCmd(4) }
+def reset5() { resetCmd(5) }
+def reset6() { resetCmd(6) }
 
 def configure() {
 log.debug "in configure"
@@ -382,11 +474,17 @@ if (settings.SwitchLabel3 == null)
    settings.SwitchLabel3 = ""
 if (settings.SwitchLabel4 == null)
    settings.SwitchLabel4 = ""
- 
+if (settings.OutletLabel1 == null)
+   settings.OutletLabel1 = ""
+if (settings.OutletLabel2 == null)
+   settings.OutletLabel2 = ""
+   
    sendEvent(name: "Label1", value: settings.SwitchLabel1, descriptionText: "Set switch 1 label: $settings.SwitchLabel1")
    sendEvent(name: "Label2", value: settings.SwitchLabel2, descriptionText: "Set switch 2 label: $settings.SwitchLabel2")
    sendEvent(name: "Label3", value: settings.SwitchLabel3, descriptionText: "Set switch 3 label: $settings.SwitchLabel3")
    sendEvent(name: "Label4", value: settings.SwitchLabel4, descriptionText: "Set switch 4 label: $settings.SwitchLabel4")
+   sendEvent(name: "Label5", value: settings.OutletLabel1, descriptionText: "Set non switcheable outlet 1 label: $settings.OutletLabel1")
+   sendEvent(name: "Label6", value: settings.OutletLabel2, descriptionText: "Set non switcheable outlet 2 label: $settings.OutletLabel2")
       
 	def cmds = [
 		zwave.configurationV1.configurationSet(parameterNumber: 101, size: 4, configurationValue: [0, 0, 0, 1]).format(),
@@ -402,22 +500,68 @@ if (settings.SwitchLabel4 == null)
 	cmds += [
 		zwave.configurationV1.configurationSet(parameterNumber: 111, size: 4, scaledConfigurationValue: 15*60).format(),
 		zwave.configurationV1.configurationSet(parameterNumber: 4, size: 1, configurationValue: [1]).format(),
+        zwave.configurationV1.configurationSet(parameterNumber: 80, size: 1, scaledConfigurationValue: 2).format(),
 	]
 	delayBetween(cmds) + "delay 5000" + refresh()
 }
 
-private encap(cmd, endpoint) {
-	if (endpoint) {
-		if (cmd.commandClassId == 0x32) {
+ def convertTemp(value) { 
+    def highbit = value[0] 
+    def lowbit = value[1] 
+     
+    if (highbit > 127) 
+    {
+       highbit = highbit - 256
+     }
+   		 lowbit = lowbit * 0.00390625 
+       return highbit+lowbit 
+ } 
+
+ def zwaveEvent(physicalgraph.zwave.commands.configurationv2.ConfigurationReport cmd, ep)
+ { 
+ //log.debug "in configuration report cmd = $cmd"
+ 
+     if (cmd.parameterNumber == 90) 
+     {  
+        def thetemp = convertTemp(cmd.configurationValue) 
+        if(getTemperatureScale() == "C")
+         { 
+ 	      createEvent(name:"temperature", value: Math.round(thetemp * 10) / 10)
+ 	     } 
+       else 
+         { 
+         def convtemp = Math.round(celsiusToFahrenheit(thetemp) * 10) / 10
+         log.debug "got temp = $convtemp"
+       	createEvent(name:"temperature", value: Math.round(celsiusToFahrenheit(thetemp) * 10) / 10)
+ 	   } 
+    } 
+ }
+
+private encap(cmd, endpoint) 
+{
+	if (endpoint) 
+    {
+		if (cmd.commandClassId == 0x32) 
+        {
+        if (endpoint == 5 || endpoint == 6) 
+        	{ 
+                 endpoint -= 4 
+			}
 			// Metered outlets are numbered differently than switches
-			if (endpoint < 0x80) {
+		else if (endpoint < 0x80) 
+        	{
 				endpoint += 2
-			} else {
+			} 
+        else 
+        	{
 				endpoint = ((endpoint & 0x7F) << 2) | 0x80
 			}
-		}
+		} // = 0x32
 		zwave.multiChannelV3.multiChannelCmdEncap(destinationEndPoint:endpoint).encapsulate(cmd).format()
-	} else {
+	} // got an endpoint
+    
+    else 
+    {
 		cmd.format()
 	}
 }
