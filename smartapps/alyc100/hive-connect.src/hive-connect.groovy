02bbca865155994563565eeabbbcbcc65f1e0fc8
Alex Lee Yuk Cheung <alyc100@hotmail.com>
2016-03-08 19:45:56 +0000

    v2.1 - Improved authentication process and overhaul to UI. Added notification capability.

--- smartapps/alyc100/hive-connect.src/hive-connect.groovy
+++ smartapps/alyc100/hive-connect.src/hive-connect.groovy
@@ -16,6 +16,7 @@
  *  24.02.2016
  *  v2.0 BETA - New Hive Connect App
  *  v2.0.1 BETA - Fix bug for accounts that do not have capabilities attribute against thermostat nodes.
+ *	v2.1 - Improved authentication process and overhaul to UI. Added notification capability.
  *
  */
 definition(
@@ -30,6 +31,9 @@ definition(
 
 preferences {
 	page(name:"firstPage", title:"Hive Device Setup", content:"firstPage", install: true)
+    page(name: "loginPAGE")
+    page(name: "selectDevicePAGE")
+	page(name: "preferencesPAGE")
 }
 
 def apiURL(path = '/') 			 { return "https://api.prod.bgchprod.info:443/omnia${path}" }
@@ -39,38 +43,156 @@ def firstPage() {
 	if (username == null || username == '' || password == null || password == '') {
 		return dynamicPage(name: "firstPage", title: "", install: true, uninstall: true) {
 			section {
-    			input("username", "text", title: "Username", description: "Your Hive username (usually an email address)", required: true)
-				input("password", "password", title: "Password", description: "Your Hive password", required: true, submitOnChange: true)
+    			headerSECTION()
+                href("loginPAGE", title: null, description: authenticated() ? "Authenticated as " +username : "Tap to enter Hive crednentials", state: authenticated())
   			}
     	}
     }
     else
     {
     	log.debug "next phase"
-    	getHiveAccessToken()
-        updateDevices()
         return dynamicPage(name: "firstPage", title: "", install: true, uninstall: true) {
 			section {
-    			input("username", "text", title: "Username", description: "Your Hive username (usually an email address)", required: true)
-				input("password", "password", title: "Password", description: "Your Hive password", required: true, submitOnChange: true)
+            	headerSECTION()
+                href("loginPAGE", title: null, description: authenticated() ? "Authenticated as " +username : "Tap to enter Hive credentials", state: authenticated())
             }
-            if (state.hiveAccessToken != null && state.hiveAccessToken != '') {
-            	section {
-                	paragraph "You have successfully connected to Hive. Your devices should be discovered and selectable below."
-  				}
-                section("Select a device...") {
-					input "selectedHeating", "enum", required:false, title:"Select Hive Heating Devices \n(${state.hiveHeatingDevices.size() ?: 0} found)", multiple:true, options:state.hiveHeatingDevices
-					input "selectedHotWater", "enum", required:false, title:"Select Hive Hot Water Devices \n(${state.hiveHotWaterDevices.size() ?: 0} found)", multiple:true, options:state.hiveHotWaterDevices
-					
-				}
+            if (stateTokenPresent()) {           	
+                section ("Choose your devices:") {
+					href("selectDevicePAGE", title: null, description: devicesSelected() ? "Devices: " + getDevicesSelectedString() : "Tap to select devices", state: devicesSelected())
+        		}
+                section ("Notifications:") {
+					href("preferencesPAGE", title: null, description: preferencesSelected() ? getPreferencesString() : "Tap to configure notifications", state: preferencesSelected())
+        		}
             } else {
             	section {
-            		paragraph "There was a problem connecting to Hive. Check your user credential and error logs in SmartThings web console."
+            		paragraph "There was a problem connecting to Hive. Check your user credentials and error logs in SmartThings web console.\n\n${state.loginerrors}"
            		}
            }
     	}
     }
 }
+
+def headerSECTION() {
+	return paragraph (image: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/10457773_334250273417145_3395772416845089626_n.png?oh=fb96583154582526d24409597fbccc18&oe=57248CDA",
+                  "Hive (Connect)\nVersion: 2.1\nBuild: 142008032016")
+}
+
+def stateTokenPresent() {
+	return state.hiveAccessToken != null && state.hiveAccessToken != ''
+}
+
+def authenticated() {
+	return (state.hiveAccessToken != null && state.hiveAccessToken != '') ? "complete" : null
+}
+
+def devicesSelected() {
+	return (getChildDevices().size > 0) ? "complete" : null
+}
+
+def preferencesSelected() {
+	return (sendPush || sendSMS != null) && (maxtemp != null || mintemp != null || sendBoost || sendOff || sendManual || sendSchedule) ? "complete" : null
+}
+
+def getDevicesSelectedString() {
+	def listString = ""
+	getChildDevices().each { childDevice -> 
+    	if (listString == "") {
+        	listString += childDevice.name
+        }
+        else {
+        	listString += "\n" + childDevice.name
+        }
+    }
+    return listString
+}
+
+def getPreferencesString() {
+	def listString = ""
+    if (sendPush) listString += "Send Push, "
+    if (sendSMS != null) listString += "Send SMS, "
+    if (maxtemp != null) listString += "Max Temp: ${maxtemp}, "
+    if (mintemp != null) listString += "Min Temp: ${mintemp}, "
+    if (sendBoost) listString += "Boost, "
+    if (sendOff) listString += "Off, "
+    if (sendManual) listString += "Manual, "
+    if (sendSchedule) listString += "Schedule, "
+    if (listString != "") listString = listString.substring(0, listString.length() - 2)
+    return listString
+   
+    
+}
+
+def loginPAGE() {
+	if (username == null || username == '' || password == null || password == '') {
+		return dynamicPage(name: "loginPAGE", title: "Login", uninstall: false, install: false) {
+    		section { headerSECTION() }
+        	section { paragraph "Enter your Hive credentials below to enable SmartThings and Hive integration." }
+    		section("Hive Credentials:") {
+				input("username", "text", title: "Username", description: "Your Hive username (usually an email address)", required: true)
+				input("password", "password", title: "Password", description: "Your Hive password", required: true, submitOnChange: true)		
+			}    	
+    	}
+    }
+    else {
+    	getHiveAccessToken()
+        dynamicPage(name: "loginPAGE", title: "Login", uninstall: false, install: false) {
+    		section { headerSECTION() }
+        	section { paragraph "Enter your Hive credentials below to enable SmartThings and Hive integration." }
+    		section("Hive Credentials:") {
+				input("username", "text", title: "Username", description: "Your Hive username (usually an email address)", required: true)
+				input("password", "password", title: "Password", description: "Your Hive password", required: true, submitOnChange: true)		
+			}    	
+    	
+    		if (stateTokenPresent()) {
+        		section {
+                	paragraph "You have successfully connected to Hive. Click 'Done' to select your Hive devices."
+  				}
+        	}
+        	else {
+        		section {
+            		paragraph "There was a problem connecting to Hive. Check your user credentials and error logs in SmartThings web console.\n\n${state.loginerrors}"
+           		}
+        	}
+        }
+    }
+}
+
+def selectDevicePAGE() {
+	updateDevices()
+	dynamicPage(name: "selectDevicePAGE", title: "Devices", uninstall: false, install: false) {
+    	section { headerSECTION() }
+    	section("Select your devices:") {
+			input "selectedHeating", "enum", image: "https://www.hivehome.com/assets/thermostat-frame-6c75d5394d102f52cb8cf73704855446.png", required:false, title:"Select Hive Heating Devices \n(${state.hiveHeatingDevices.size() ?: 0} found)", multiple:true, options:state.hiveHeatingDevices
+			input "selectedHotWater", "enum", image: "https://www.hivehome.com/assets/thermostat-frame-6c75d5394d102f52cb8cf73704855446.png", required:false, title:"Select Hive Hot Water Devices \n(${state.hiveHotWaterDevices.size() ?: 0} found)", multiple:true, options:state.hiveHotWaterDevices
+					
+		}
+    }
+}
+
+def preferencesPAGE() {
+	dynamicPage(name: "preferencesPAGE", title: "Preferences", uninstall: false, install: false) {
+    	section {
+    		input "sendPush", "bool", title: "Send as Push?", required: false, defaultValue: false
+			input "sendSMS", "phone", title: "Send as SMS?", required: false, defaultValue: null	
+        }
+    	section("Thermostat Notifications:") {
+			
+			input "sendBoost", "bool", title: "Notify when mode is Boosting?", required: false, defaultValue: false
+			input "sendOff", "bool", title: "Notify when mode is Off?", required: false, defaultValue: false
+			input "sendManual", "bool", title: "Notify when mode is Manual?", required: false, defaultValue: false     
+            input "sendSchedule", "bool", title: "Notify when mode is Schedule?", required: false, defaultValue: false  
+		}
+        section("Thermostat Max Temperature") {
+        	input ("maxtemp", "number", title: "Alert when temperature is above this value", required: false, defaultValue: 25)
+        }
+        section("Thermostat Min Temperature") {
+        	input ("mintemp", "number", title: "Alert when temperature is below this value", required: false, defaultValue: 10)
+        }
+        
+    }
+}
+
+
 
 // App lifecycle hooks
 
@@ -86,6 +208,7 @@ def installed() {
 // called after settings are changed
 def updated() {
 	log.debug "updated"
+    unsubscribe()
 	initialize()
     unschedule('refreshDevices')
     runEvery10Minutes('refreshDevices')
@@ -113,8 +236,85 @@ def initialize() {
 		addHotWater()
 
     runIn(10, 'refreshDevices') // Asynchronously refresh devices so we don't block
+    
+    //subscribe to events for notifications
+    getChildDevices().each { childDevice -> 
+    	if (childDevice.typeName == "Hive Heating V2.0" || childDevice.typeName == "Hive Hot Water V2.0") {
+    		subscribe(childDevice, "thermostatMode", modeHandler)
+        }
+        if (childDevice.typeName == "Hive Heating V2.0") {
+        	subscribe(childDevice, "temperature", tempHandler)
+        }
+    }
+    state.maxNotificationSent = false
+    state.minNotificationSent = false
+    
 }
 
+def tempHandler(evt) {
+	def msg
+    log.trace "temperature: $evt.value, $evt"
+    
+    if (settings.maxtemp != null) {
+    	def maxTemp = settings.maxtemp
+        if (evt.doubleValue >= maxTemp) {
+        	msg = "${evt.displayName} temperature reading is very hot."
+            if (state.maxNotificationSent == null || state.maxNotificationSent == false) {
+            	generateNotification(msg)
+                //Avoid constant messages
+            	state.maxNotificationSent = true
+            }
+        }
+        else {
+        	//Reset if temperature falls back to normal levels
+            state.maxNotificationSent = false
+        }
+    }
+    else if (settings.mintemp != null) {
+    	def minTemp = settings.mintemp
+        if (evt.doubleValue <= minTemp) {
+        	msg = "${evt.displayName} temperature reading is very cold."
+            if (state.minNotificationSent == null || state.minNotificationSent == false) {
+            	generateNotification(msg)
+                //Avoid constant messages
+            	state.minNotificationSent = true
+            }
+        }
+        else {
+        	//Reset if temperature falls back to normal levels
+        	state.minNotificationSent = false
+        }
+    }
+}
+
+def modeHandler(evt) {
+	def msg
+    if (evt.value == "heat") { 
+    	msg = "${evt.displayName} is set to Manual"
+        if (settings.sendSchedule) generateNotification(msg)
+    }
+	else if (evt.value == "off") {
+    	msg = "${evt.displayName} is turned Off"
+        if (settings.sendOff) generateNotification(msg)
+    }
+    else if (evt.value == "auto") {
+    	msg = "${evt.displayName} is set to Schedule"
+        if (settings.sendManual) generateNotification(msg)
+    }
+    else if (evt.value == "emergency heat") {
+    	msg = "${evt.displayName} is in Boost mode" 
+        if (settings.sendBoost) generateNotification(msg)
+    }   
+}
+
+def generateNotification(msg) {
+	if (settings.sendSMS != null) {
+		sendSms(sendSMS, msg) 
+	}	
+	if (settings.sendPush == true) {
+		sendPush(msg)
+	}
+}
 
 def updateDevices() {
 	if (!state.devices) {
@@ -322,7 +522,6 @@ def getHiveAccessToken() {
 		httpPostJson(params) {response ->
 			log.debug "Request was successful, $response.status"
 			log.debug response.headers
-			state.hiveAccessToken = response.data
       
         	state.cookie = response?.headers?.'Set-Cookie'?.split(";")?.getAt(0)
 			log.debug "Adding cookie to collection: $cookie"
@@ -332,9 +531,13 @@ def getHiveAccessToken() {
         
         	state.hiveAccessToken = response.data.sessions[0].id
         	// set the expiration to 5 minutes
-			state.hiveAccessToken_expires_at = new Date().getTime() + 300000;
+			state.hiveAccessToken_expires_at = new Date().getTime() + 300000
+            state.loginerrors = null
 		}
     } catch (groovyx.net.http.HttpResponseException e) {
+    	state.hiveAccessToken = null
+        state.hiveAccessToken_expires_at = null
+   		state.loginerrors = "Error: ${e.response.status}: ${e.response.data}"
     	logResponse(e.response)
 		return e.response
     }
