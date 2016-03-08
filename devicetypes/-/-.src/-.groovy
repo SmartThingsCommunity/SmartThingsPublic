b98d79519327893191fccb21a90d27972a9dac89
임희진 <2000050044@hanmail.net>
2016-03-08 03:18:24 -0600

    MSA-930: test

---
+++ devicetypes/-/-.src/-.groovy
@@ -0,0 +1,37 @@
+/**
+ *  디바이스핸들러
+ *
+ *  Copyright 2016 임희진
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "디바이스핸들러", namespace: "디바이스핸들러", author: "임희진") {
+		capability "Smoke Detector"
+		capability "Sound Sensor"
+	}
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'smoke' attribute
+	// TODO: handle 'sound' attribute
+
+}
