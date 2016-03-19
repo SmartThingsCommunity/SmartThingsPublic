bea3ef8668a8424ec9ce00b6c02adde0956bcf0e
Michael Struck <OregonHA@neo-fx.com>
2016-03-19 08:40:46 -0700

    Updated to 2.1.0

--- smartapps/michaelstruck/smart-bathroom-ventilation.src/smart-bathroom-ventilation.groovy
+++ smartapps/michaelstruck/smart-bathroom-ventilation.src/smart-bathroom-ventilation.groovy
@@ -1,16 +1,17 @@
 /**
  *  Smart Bathroom Ventilation-Parent
  *
- *  Version - 2.0.1 12/29/15
+ *  Version - 2.1.0 3/19/16
  * 
  *  Version 1.0.0 - Initial release
  *  Version 1.1.0 - Added restrictions for time that fan goes on to allow for future features along with logic fixes
  *  Version 1.2.0 - Added the option of starting the fans based on time (to eliminate the time of polling and for those without a humidity sensor)
  *  Version 2.0.0 - Modified to allow more scenarios via parent/child app structure
  *  Version 2.0.1 - Allow ability to see child app version in parent app and moved the remove button
+ *  Version 2.1.0 - Added icon on about page
  *
  * 
- *  Copyright 2015 Michael Struck - Uses code from Lighting Director by Tim Slagle & Michael Struck
+ *  Copyright 2016 Michael Struck - Uses code from Lighting Director by Tim Slagle & Michael Struck
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -53,9 +54,15 @@ def mainPage(){
 }
 
 def pageAbout() {
-	dynamicPage(name: "pageAbout", title: "About ${textAppName()}", uninstall: true) {
+	dynamicPage(name: "pageAbout" , uninstall: true) {
     	section {
-			paragraph "${textVersion()}\n${textCopyright()}\n\n${textLicense()}\n"
+        paragraph "${textAppName()}\n${textCopyright()}", image: "https://raw.githubusercontent.com/MichaelStruck/SmartThings/master/Other-SmartApps/Smart-Bathroom-Ventilation/BathVent@2x.png"
+        }
+        section ("SmartApp Versions") {
+			paragraph textVersion()
+        }    
+        section ("Apache License") { 
+        	paragraph textLicense()
     	}
 		section("Instructions") {
             paragraph textHelp()
@@ -87,14 +94,14 @@ private def textAppName() {
 }	
 
 private def textVersion() {
-    def version = "Parent App Version: 2.0.1 (12/29/2015)"
+    def version = "Parent App Version: 2.1.0 (03/19/2016)"
     def childCount = childApps.size()
     def childVersion = childCount ? childApps[0].textVersion() : "No scenarios installed"  
     return "${version}\n${childVersion}"
 }
 
 private def textCopyright() {
-    def text = "Copyright © 2015 Michael Struck"
+    def text = "Copyright © 2016 Michael Struck"
 }
 
 private def textLicense() {
