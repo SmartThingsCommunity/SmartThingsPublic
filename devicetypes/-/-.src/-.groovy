a3aec8afb2c7f7b858effee1bb41cbcbcd65b3e4
오경환 <kh6417.oh@samsung.com>
2016-03-23 02:38:44 -0500

    MSA-985: hi! good~

---
+++ devicetypes/-/-.src/-.groovy
@@ -0,0 +1,65 @@
+/**
+ *  마이사이렌
+ *
+ *  Copyright 2016 오경환
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+ definition(
+    name: "Button Controller",
+    namespace: "smartthings",
+    author: "SmartThings",
+    description: "Control devices with buttons like the Aeon Labs Minimote",
+    category: "Convenience",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/MyApps/Cat-MyApps.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/MyApps/Cat-MyApps@2x.png"
+)
+metadata {
+	definition (name: "마이사이렌", namespace: "마이사이렌", author: "오경환") {
+		capability "Alarm"
+	}
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'alarm' attribute
+
+}
+
+// handle commands
+def off() {
+	log.debug "Executing 'off'"
+	// TODO: handle 'off' command
+}
+
+def strobe() {
+	log.debug "Executing 'strobe'"
+	// TODO: handle 'strobe' command
+}
+
+def siren() {
+	log.debug "Executing 'siren'"
+	// TODO: handle 'siren' command
+}
+
+def both() {
+	log.debug "Executing 'both'"
+	// TODO: handle 'both' command
+}
