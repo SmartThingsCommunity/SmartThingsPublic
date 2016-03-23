281fc939acc5af4aa778342c34e1d2a0088f504e
Vinay Rao <workingmonk@gmail.com>
2016-03-23 00:27:23 -0700

    refactoring code to get it inline with the new zigbee apis

--- devicetypes/smartthings/zigbee-switch.src/zigbee-switch.groovy
+++ devicetypes/smartthings/zigbee-switch.src/zigbee-switch.groovy
@@ -53,16 +53,9 @@ metadata {
 // Parse incoming device messages to generate events
 def parse(String description) {
     log.debug "description is $description"
-
-    def resultMap = zigbee.getKnownDescription(description)
-    if (resultMap) {
-        log.info resultMap
-        if (resultMap.type == "update") {
-            log.info "$device updates: ${resultMap.value}"
-        }
-        else {
-            sendEvent(name: resultMap.type, value: resultMap.value)
-        }
+    def event = zigbee.getEvent(description)
+    if (event) {
+        sendEvent(event)
     }
     else {
         log.warn "DID NOT PARSE MESSAGE for description : $description"
