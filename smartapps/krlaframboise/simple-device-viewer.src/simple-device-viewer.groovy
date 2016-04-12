7745c22cffc13ab97300ed4ed6b4658f770c4083
Kevin LaFramboise <klaframboise@comcast.net>
2016-04-12 18:45:27 -0400

    - Fixed documentation url

--- smartapps/krlaframboise/simple-device-viewer.src/simple-device-viewer.groovy
+++ smartapps/krlaframboise/simple-device-viewer.src/simple-device-viewer.groovy
@@ -5,7 +5,7 @@
  *    Kevin LaFramboise (krlaframboise)
  *
  *  URL to documentation:
- *    https://community.smartthings.com/t/release-simple-device-viewer/42481/15?u=krlaframboise
+ *    https://community.smartthings.com/t/release-simple-device-viewer/42481?u=krlaframboise
  *
  *  Changelog:
  *
