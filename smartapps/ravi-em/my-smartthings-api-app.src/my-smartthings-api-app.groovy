79684ab43ad8d7d23ee4f9dd4ef36406a4fdcc7f
Ravi Dubey <ravidubey_mca@yahoo.com>
2016-04-21 11:53:16 +0530

    Initial App

---
+++ smartapps/ravi-em/my-smartthings-api-app.src/my-smartthings-api-app.groovy
@@ -0,0 +1,120 @@
+/**
+ *  My SmartThings API App
+ *
+ *  Copyright 2016 Ravi Dubey
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "My SmartThings API App",
+    namespace: "Ravi-em",
+    author: "Ravi Dubey",
+    description: "This will handle list & switches. ",
+    category: "Convenience",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    oauth: true)
+
+
+preferences(oauthPage: "deviceAuthorization") {
+page(name: "deviceAuthorization"){
+	section("Allow Endpoint to Control These Things...") {
+		input "switches", "capability.switch", title: "Which Switches?", multiple: true, required: false
+		//input "locks", "capability.lock", title: "Which Locks?", multiple: true, required: false
+	}
+	}
+}
+
+mappings {
+
+	path("/switches") {
+		action: [
+			GET: "listSwitches"
+		]
+	}
+	path("/switches/:id") {
+		action: [
+			GET: "showSwitch"
+		]
+	}
+	path("/switches/:id/:command") {
+		action: [
+			GET: "updateSwitch"
+		]
+	}
+	
+}
+
+def installed() {}
+
+def updated() {}
+
+
+//switches
+def listSwitches() {
+	switches.collect{device(it,"switch")}
+}
+
+def showSwitch() {
+	show(switches, "switch")
+}
+void updateSwitch() {
+	update(switches)
+}
+
+
+def deviceHandler(evt) {}
+
+private void update(devices) {
+	log.debug "update, request: params: ${params}, devices: $devices.id"
+    
+    
+	//def command = request.JSON?.command
+    def command = params.command
+    //let's create a toggle option here
+	if (command) 
+    {
+		def device = devices.find { it.id == params.id }
+		if (!device) {
+			httpError(404, "Device not found")
+		} else {
+        	if(command == "toggle")
+       		{
+            	if(device.currentValue('switch') == "on")
+                  device.off();
+                else
+                  device.on();
+       		}
+       		else
+       		{
+				device."$command"()
+            }
+		}
+	}
+}
+
+private show(devices, type) {
+	def device = devices.find { it.id == params.id }
+	if (!device) {
+		httpError(404, "Device not found")
+	}
+	else {
+		def attributeName = type == "motionSensor" ? "motion" : type
+		def s = device.currentState(attributeName)
+		[id: device.id, label: device.displayName, value: s?.value, unitTime: s?.date?.time, type: type]
+	}
+}
+
+
+private device(it, type) {
+	it ? [id: it.id, label: it.label, type: type] : null
+}
