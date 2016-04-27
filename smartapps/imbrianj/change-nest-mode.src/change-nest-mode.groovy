89b9120fcc55d271fcaf4d737ac1cb5c217cdec8
Diego <diego_antonino@hotmail.com>
2016-04-26 23:00:37 -0400

    custom apps

---
+++ smartapps/imbrianj/change-nest-mode.src/change-nest-mode.groovy
@@ -0,0 +1,52 @@
+/**
+ *  Change Nest Mode
+ *
+ *  Author: brian@bevey.org
+ *  Date: 5/5/14
+ *
+ *  Simply marks any thermostat "away" if able (primarily focused on the Nest
+ *  thermostat).  This is intended to be used with an "Away" or "Home" mode.
+ */
+
+definition(
+    name:        "Change Nest Mode",
+    namespace:   "imbrianj",
+    author:      "brian@bevey.org",
+    description: "Simply marks any thermostat 'away' if able (primarily focused on the Nest thermostat).  This is intended to be used with an 'Away' or 'Home' mode.",
+    category:    "Green Living",
+    iconUrl:     "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url:   "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience%402x.png"
+)
+
+preferences {
+  section("Change to this mode to...") {
+    input "newMode", "mode", metadata:[values:["Away", "Home"]]
+  }
+
+  section("Change these thermostats modes...") {
+    input "thermostats", "capability.thermostat", multiple: true
+  }
+}
+
+def installed() {
+  subscribe(location, changeMode)
+  subscribe(app, changeMode)
+}
+
+def updated() {
+  unsubscribe()
+  subscribe(location, changeMode)
+  subscribe(app, changeMode)
+}
+
+def changeMode(evt) {
+  if(newMode == "Away") {
+    log.info("Marking Away")
+    thermostats?.away()
+  }
+
+  else {
+    log.info("Marking Present")
+    thermostats?.present()
+  }
+}
