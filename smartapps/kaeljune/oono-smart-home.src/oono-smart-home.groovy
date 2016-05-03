c20dbac29b6230d58bde5e8cd45459152f5aa2ca
Kaeljune <hiepth9x@gmail.com>
2016-05-03 03:39:25 -0500

    MSA-1246: I hope my first app was apply.

---
+++ smartapps/kaeljune/oono-smart-home.src/oono-smart-home.groovy
@@ -0,0 +1,55 @@
+/**
+ *  Oono Smart Home
+ *
+ *  Copyright 2016 Kaeljune
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Oono Smart Home",
+    namespace: "kaeljune",
+    author: "Kaeljune",
+    description: "Smart Home Apps for My Sweet Home",
+    category: "Family",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    oauth: true)
+
+
+preferences {
+	section("When I touch the app, turn on...") {
+		input "switches", "capability.switch", multiple: true
+	}
+}
+
+def installed()
+{
+	subscribe(location, changedLocationMode)
+	subscribe(app, appTouch)
+}
+
+def updated()
+{
+	unsubscribe()
+	subscribe(location, changedLocationMode)
+	subscribe(app, appTouch)
+}
+
+def changedLocationMode(evt) {
+	log.debug "changedLocationMode: $evt"
+	switches?.on()
+}
+
+def appTouch(evt) {
+	log.debug "appTouch: $evt"
+	switches?.on()
+}
