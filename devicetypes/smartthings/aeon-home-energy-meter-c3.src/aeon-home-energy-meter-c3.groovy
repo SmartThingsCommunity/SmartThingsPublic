e1de599668d5e07c41012ec46d537a7397a646e5
Dwight Liu <dwightliu@Dwights-MacBook-Pro.local>
2016-05-10 14:23:46 -0700

    make sure reformat the storeGraphData

--- devicetypes/smartthings/aeon-home-energy-meter-c3.src/aeon-home-energy-meter-c3.groovy
+++ devicetypes/smartthings/aeon-home-energy-meter-c3.src/aeon-home-energy-meter-c3.groovy
@@ -95,7 +95,7 @@ def parse(String description) {
 	if (cmd) {
 		result = createEvent(zwaveEvent(cmd))
 		log.debug "Parse returned ${result?.descriptionText}"
-        storeGraphData(result.name, result.value)
+		storeGraphData(result.name, result.value)
 	} else {
 		log.debug "zwave.parse returned null command. Cannot create event"
 	}
