ef8f7283558e013e3b1db038870a634e95e8bc5f
Philip Rosenberg-Watt <philip+smartthings@rosenberg-watt.com>
2016-05-20 15:49:10 -0600

    Change Thermostat Auto Off to check open sensors
    Removed the expensive unschedule() operations in favor of checking to
    see if any contact sensors are currently open.

--- smartapps/dianoga/thermostat-auto-off.src/thermostat-auto-off.groovy
+++ smartapps/dianoga/thermostat-auto-off.src/thermostat-auto-off.groovy
@@ -1,9 +1,12 @@
 /**
- *  HVAC Auto Off
- *
- *  Author: dianoga7@3dgo.net
- *  Date: 2013-07-21
- */
+*  HVAC Auto Off
+*
+*  Author: dianoga7@3dgo.net
+*  Date: 2013-07-21
+*
+*  Additional code: Philip Rosenberg-Watt
+*  Date: 2016-05-20
+*/
 
 // Automatically generated. Make future change here.
 definition(
@@ -12,72 +15,93 @@ definition(
     author: "dianoga7@3dgo.net",
     description: "Automatically turn off thermostat when windows/doors open. Turn it back on when everything is closed up.",
     category: "Green Living",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience%402x.png",
+    iconUrl: "http://cdn.device-icons.smartthings.com/Home/home1-icn.png",
+    iconX2Url: "http://cdn.device-icons.smartthings.com/Home/home1-icn@2x.png",
     oauth: true
 )
 
+
 preferences {
-	section("Control") {
-		input("thermostat", "capability.thermostat", title: "Thermostat")
-	}
-    
+    section("Control") {
+        input "thermostat", "capability.thermostat", title: "Thermostat" 
+    }
     section("Open/Close") {
-    	input("sensors", "capability.contactSensor", title: "Sensors", multiple: true)
-        input("delay", "number", title: "Delay (seconds)")
+        input "sensors", "capability.contactSensor", title: "Sensors", multiple: true 
+        input "delay", "number", title: "Delay (seconds)"
     }
 }
+
 
 def installed() {
-	log.debug "Installed with settings: ${settings}"
+    log.debug "Installed with settings: ${settings}"
 
-	initialize()
+    initialize()
 }
+
 
 def updated() {
-	log.debug "Updated with settings: ${settings}"
+    log.debug "Updated with settings: ${settings}"
 
-	unsubscribe()
-    unschedule()
-	initialize()
+    unsubscribe()
+    initialize()
 }
+
 
 def initialize() {
-	state.changed = false
-	subscribe(sensors, 'contact', "sensorChange")
+    subscribe(sensors, "contact", contactHandler)
 }
 
-def sensorChange(evt) {
-	log.debug "Desc: $evt.value , $state"
-    if(evt.value == 'open' && !state.changed) {
-    	unschedule()
-        runIn(delay, 'turnOff')
-    } else if(evt.value == 'closed' && state.changed) {
-    	// All closed?
-        def isOpen = false
-        for(sensor in sensors) {
-        	if(sensor.id != evt.deviceId && sensor.currentValue('contact') == 'open') {
-        		isOpen = true
-            }
-        }
-        
-        if(!isOpen) {
-        	unschedule()
-        	runIn(delay, 'restore')
-        }
+
+def contactHandler(evt) {
+    log.trace "contactHandler(${evt})"
+    log.debug "Desc: ${evt.value}, state: ${state}"
+
+    if (state.changed && evt.value == "closed") {
+        log.debug "Closed event received, scheduling restoration..."
+        runIn(delay, restore)
+    } else if (!state.changed && evt.value == "open") {
+        log.debug "Open event received. Scheduling shutdown..."
+        runIn(delay, turnOff)
     }
 }
 
-def turnOff() {
-	log.debug "Turning off thermostat due to contact open"
-	state.thermostatMode = thermostat.currentValue("thermostatMode")
-	thermostat.off()
-    state.changed = true
-    log.debug "State: $state"
+
+def allClosed() {
+    log.trace "allClosed()"
+
+    def openSensors = sensors.findAll { it?.currentValue("contact") == "open" }
+    def allClosed = (openSensors.size() == 0)
+
+    log.debug "Number of open sensors: ${openSensors.size()}, returning ${allClosed}"
+    return allClosed
 }
 
+
+def turnOff() {
+    log.trace "turnOff()"
+
+    if (!allClosed()) {
+        log.debug "Turning off thermostat"
+        state.thermostatMode = thermostat.currentValue("thermostatMode")
+        thermostat.off()
+        state.changed = true
+
+        log.debug "State: $state"
+    } else {
+        log.debug "All sensors are closed. Doing nothing."
+    }
+}
+
+
 def restore() {
-    log.debug "Setting thermostat to $state.thermostatMode"
-    thermostat.setThermostatMode(state.thermostatMode)
-    state.changed = false
+    log.trace "restore()"
+
+
+    if (allClosed()) {
+        log.debug "Restoring thermostat to $state.thermostatMode"
+        thermostat.setThermostatMode(state.thermostatMode)
+        state.changed = false
+    } else {
+        log.debug "Not all sensors are closed. Doing nothing."
+    }
 }
