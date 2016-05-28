debedec4942769824fc988f0866511dd4c7b8e3e
Michael <MichaelStruck@users.noreply.github.com>
2016-05-28 06:50:31 -0700

    Update Node.js

--- smartapps/michaelstruck/ask-alexa.src/Node.js
+++ smartapps/michaelstruck/ask-alexa.src/Node.js
@@ -1,11 +1,12 @@
 /**
  *  Ask Alexa - Lambda Code
  *
- *  Version 1.0.1 - 5/14/16 Copyright © 2016 Michael Struck
+ *  Version 1.1.0 - 5/27/16 Copyright Â© 2016 Michael Struck
  *  Special thanks for Keith DeLong for code and assistance
  *  
  *  Version 1.0.0 - Initial release
  *  Version 1.0.1 - Removed dedicated status operation; added version code
+ *  Version 1.1.0 - Added two addition intent types for groups and macros
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -19,10 +20,12 @@
  */
 'use strict';
 exports.handler = function( event, context ) {
-   var versionTxt = '1.0.1';
-   var versionNum = '101';
+   var versionTxt = '1.1.0';
+   var versionDate= '05/27/2016'
+   var versionNum = '110';
    var https = require( 'https' );
    // Paste app code here between the breaks------------------------------------------------
+    var IName = 'SmartThings';
     var STappID = '';
     var STtoken = '';
     var url='https://graph.api.smartthings.com:443/api/smartapps/installations/' + STappID + '/' ;
@@ -44,31 +47,36 @@ exports.handler = function( event, context ) {
             var Device = event.request.intent.slots.Device.value;
             var Num = event.request.intent.slots.Num.value;
             var Param = event.request.intent.slots.Param.value;
-            url += 'd?Device=' + Device + '&Operator=' + Operator + '&Num=' + Num + '&Param=' + Param + '&lVer=' + versionNum; 
+            url += 'd?Device=' + Device + '&Operator=' + Operator + '&Num=' + Num + '&Param=' + Param; 
             process = true;
             cardName = "SmartThings Devices";
         } 
-        else if (intentName == "ReportOperation") {
-            var Report = event.request.intent.slots.Report.value;
-            url += 'r?Report=' + Report + '&lVer=' + versionNum;
-            process = true; 
-            cardName = "SmartThings Reports";
-        }
         else if (intentName == "ListOperation") {
             var Type = event.request.intent.slots.Type.value;
-            url += 'l?Type=' + Type + '&lVer=' + versionNum;
+            url += 'l?Type=' + Type;
             process = true;
             cardName = "SmartThings Help";
         }
+        else if (intentName == "MacroOperation") {
+            var Macro = event.request.intent.slots.Macro.value;
+            var MNum = event.request.intent.slots.MNum.value;
+            var MCmd = event.request.intent.slots.MCmd.value;
+            var MParam = event.request.intent.slots.MParam.value;
+            var Cancel = event.request.intent.slots.Cancel.value;
+            if (Cancel) {MNum = 9999}
+            url += 'm?Macro=' + Macro + '&Param=' + MParam + '&Cmd=' + MCmd + '&Num=' + MNum;
+            process = true;
+            cardName = "SmartThings Voice Macro";
+        }
         else if (intentName == "VersionOperation") {
-            url += 'v?Ver=' + versionTxt + '&lVer=' + versionNum;
+            url += 'v?Ver=' + versionTxt + '&Date=' + versionDate;
             process = true;
             cardName = "SmartThings Version Help";
         }
         else if (intentName == "SmartHomeOperation") {
             var SHCmd = event.request.intent.slots.SHCmd.value;
             var SHParam = event.request.intent.slots.SHParam.value;
-            url += 'h?SHCmd=' + SHCmd + '&SHParam=' + SHParam + '&lVer=' + versionNum;
+            url += 'h?SHCmd=' + SHCmd + '&SHParam=' + SHParam
             process = true;
             cardName = "SmartThings Home Operation";
         }
@@ -76,10 +84,10 @@ exports.handler = function( event, context ) {
             var help = "With the Ask Alexa SmartApp, you can interface your "+
             "SmartThings household with me. This will allow you to give me commands "+
             "to turn off a light, or unlock a door. As an example you can simply say, "+
-            "'tell SmartThings to turn off the living room', and I'll turn off that device. " +
+            "'tell "+ IName +" to turn off the living room', and I'll turn off that device. " +
             "In addition, you can query your devices to get information such as open or "+
             "close status, or find out the temperature in a room. To use this function, just give "+
-            "me the device name. For example, you could say, 'ask SmartThings about the patio'. and I will "+
+            "me the device name. For example, you could say, 'ask "+ IName +" about the patio'. and I will "+
             "give you all of the common attributes I find with that device, including battery levels. "+
             "For those who are curious, this is version" + versionTxt +" of the Lambda code, written by Michael Struck. ";
             output(help, context, "Ask Alexa Help");
@@ -88,7 +96,7 @@ exports.handler = function( event, context ) {
             output("I am not sure what you are asking. Please try again", context, "Ask Alexa Error");   
         }
         else {
-            url += '&access_token=' + STtoken;
+            url += '&lVer=' + versionNum + '&access_token=' + STtoken;
             https.get( url, function( response ) {
                 response.on( 'data', function( data ) {
                 var resJSON = JSON.parse(data);
@@ -102,7 +110,7 @@ exports.handler = function( event, context ) {
     }
 };
 
-function output( text, context, card ) {
+function output( text, context, card ) { 
    var response = {
       outputSpeech: {
          type: "PlainText",
