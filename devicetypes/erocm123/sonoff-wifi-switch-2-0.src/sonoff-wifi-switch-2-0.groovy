6b69ba963dc5913e7a12412afef0499501203d3d
Eric Maycock <erocmail@gmail.com>
2016-06-02 22:29:16 -0600

    Updates

--- devicetypes/erocm123/sonoff-wifi-switch-2-0.src/sonoff-wifi-switch-2-0.groovy
+++ devicetypes/erocm123/sonoff-wifi-switch-2-0.src/sonoff-wifi-switch-2-0.groovy
@@ -34,8 +34,9 @@ metadata {
 	}
     
     preferences {
-        input("override", "boolean", title:"Override detected IP Address", required: false, displayDuringSetup: false)
-        input("ip", "string", title:"IP Address", description: "192.168.1.150", required: false, displayDuringSetup: false)
+        input("ip", "string", title:"IP Address", description: "192.168.1.150" ,required: true, displayDuringSetup: true)
+        // Port should always be 80
+        //input("port", "string", title:"Port", description: "80" , required: true, displayDuringSetup: true)
 	}
 
 	tiles (scale: 2){      
@@ -82,15 +83,40 @@ def updated() {
 def configure() {
 	log.debug "configure()"
 	log.debug "Configuring Device For SmartThings Use"
-    sendEvent(name:"hubInfo", value:"Sonoff switch still being configured") 
-    if (ip != null && override == true) state.dni = setDeviceNetworkId(ip, "80")
+    state.ruleConfigured = false
+    state.switchConfigured = false
+    state.buttonConfigured = false
+    sendEvent(name:"hubInfo", value:"Sonoff switch still being configured")
+    if (state.MAC != null) state.dni = setDeviceNetworkId(state.MAC)
+    else 
+    if (ip != null) state.dni = setDeviceNetworkId(ip, "80")
     state.hubIP = device.hub.getDataValue("localIP")
-    state.hubPort = device.hub.getDataValue("localSrvPortTCP")
-    response(configureInstant(state.hubIP, state.hubPort))
+    response(setupDevices() + setupRules() + refresh())
 }
 
-def configureInstant(ip, port){
-    return [getAction("/config?haip=${ip}&haport=${port}")]
+def setupDevices() {
+    def cmds = []
+    cmds << postAction("/devices?index=1&page=1", "taskdevicenumber=1&edit=1&page=1")
+    cmds << postAction("/devices?index=1&page=1", "taskdevicenumber=1&taskdevicename=BUTTON&taskdevicetimer=0&taskdeviceid=1&taskdevicepin1=0&taskdevicepin1pullup=on&plugin_001_type=1&plugin_001_button=2&taskdevicevaluename1=Switch&edit=1&page=1")
+    cmds << postAction("/devices?index=2&page=1", "taskdevicenumber=1&edit=1&page=1")
+    cmds << postAction("/devices?index=2&page=1", "taskdevicenumber=1&taskdevicename=SWITCH&taskdevicetimer=0&taskdeviceid=2&taskdevicepin1=12&taskdevicepin1pullup=on&plugin_001_type=1&plugin_001_button=0&taskdevicesenddata=on&taskdevicevaluename1=Switch&edit=1&page=1")
+    return delayBetween(cmds, 1000)
+}
+
+def setupRules() {
+    def cmds = []
+    cmds << postAction("/advanced", "mqttsubscribe=&mqttpublish=&messagedelay=1000&ip=0&ntphost=&timezone=0&syslogip=0.0.0.0&sysloglevel=0&udpport=0&useserial=on&serialloglevel=0&webloglevel=0&baudrate=115200&wdi2caddress=0&wireclockstretchlimit=0&userules=on&edit=1")
+    cmds << postAction("/rules", "rules=On+BUTTON%23Switch+do%0D%0A++if+%5BSWITCH%23Switch%5D%3D0%0D%0A++++gpio%2C12%2C1%0D%0A++else%0D%0A++++gpio%2C12%2C0%0D%0A++endif%0D%0Aendon%0D%0A%0D%0AOn+SWITCH%23Switch+do%0D%0A+if+%5BSWITCH%23Switch%5D%3D1%0D%0A++++gpio%2C13%2C0%0D%0A++else%0D%0A++++gpio%2C13%2C1%0D%0A++endif%0D%0Aendon")
+    return delayBetween(cmds, 1000)
+}
+
+def setupConfig() {
+    // Automatic config page submit not working at this time. Has to be done manually
+    /*def hubIP = device.hub.getDataValue("localIP")
+    log.debug "Hub IP: ${hubIP}"
+    def cmds = []
+    //cmds << postAction("/config", "protocol=1&usedns=0&controllerip=$hubIP&controllerport=39500&controlleruser=&controllerpassword=&delay=60")
+    return delayBetween(cmds, 1000)*/
 }
 
 def parse(description) {
@@ -99,17 +125,19 @@ def parse(description) {
     def cmds
     def descMap = parseDescriptionAsMap(description)
     def body
-    //log.debug "descMap: ${descMap}"
+    log.debug "descMap: ${descMap}"
+    
 
-    if (!state.mac || state.mac != descMap["mac"]) {
+    if (!state.MAC || state.MAC != descMap["mac"]) {
 		log.debug "Mac address of device found ${descMap["mac"]}"
-        updateDataValue("mac", descMap["mac"])
+        updateDataValue("MAC", descMap["mac"])
 	}
     
-    if (state.mac != null && state.dni != state.mac) state.dni = setDeviceNetworkId(state.mac)
+    if (state.MAC != null && state.dni != state.MAC) state.dni = setDeviceNetworkId(state.MAC)
     if (descMap["body"]) body = new String(descMap["body"].decodeBase64())
 
     if (body && body != "") {
+
     
     if(body.startsWith("{") || body.startsWith("[")) {
     def slurper = new JsonSlurper()
@@ -131,22 +159,32 @@ def parse(description) {
     if (result.containsKey("pin")) {
         if (result.pin == 12) events << createEvent(name:"switch", value: (result.state.toInteger() == 0 ? 'off' : 'on'))
     }
-    if (result.containsKey("power")) {
-        events << createEvent(name: "switch", value: result.power)
-    }
+
     if (result.containsKey("System")) {
         if (result.System.containsKey("Uptime")) log.debug "System has been up ${result.System.Uptime.toInteger() / 60} hours"
     }
-    if (result.containsKey("success")) {
-        if (result.success == "true") events << createEvent(name:"hubInfo", value:"Switch has been configured")
-    }
     } else {
         //log.debug "Response is not JSON: $body"
+        def ruleSearch = "OnBUTTONSwitchdoifSWITCHSwitch0gpio121elsegpio120endifendonOnSWITCHSwitchdoifSWITCHSwitch1gpio130elsegpio131endifendon"
+        if (body.replaceAll("\\W", "").indexOf(ruleSearch) > 0) state.ruleConfigured = true
+
     }
     } else {
         cmds = refresh()
     }
     
+    if (settings.ip) {
+        //log.debug "switch: $state.switchConfigured, button: $state.buttonConfigured, rule: $state.ruleConfigured"
+        if (state.switchConfigured == true && state.buttonConfigured == true && state.ruleConfigured == true) {
+            events << createEvent(name:"hubInfo", value:"For instant status updates, configure switch at http://$settings.ip/config | Hub Info - IP: ${device.hub.getDataValue("localIP")}, Port: 39500")
+        } else {
+            events << createEvent(name:"hubInfo", value:"Sonoff switch still being configured")
+        }
+    }
+    else { 
+        events << createEvent(name:"hubInfo", value:"IP address of the switch not entered. Please do so in device preferences.") 
+    }
+
     if (cmds) return cmds else return events
 
 }
@@ -160,25 +198,55 @@ def parseDescriptionAsMap(description) {
 	}
 }
 
+private parseHTML(html) {
+   //log.debug html
+   def myHtml = html.split("<")
+   def result = []
+   switch (myHtml[0]) {
+      case "GPIO 12 Set to 0":
+      result = [name: "switch", value: "off"]
+      break
+      case "GPIO 12 Set to 1":
+      result = [name: "switch", value: "on"]
+      break
+      case "GPIO 13 Set to 0":
+      log.debug "LED is on"
+      break
+      case "GPIO 13 Set to 1":
+      log.debug "LED is off"
+      break
+      default:
+      break
+   }
+   return result
+}
+
+private ledOn() {
+   return getAction("/control?cmd=GPIO,13,0")
+}
+
+private ledOff() {
+   return getAction("/control?cmd=GPIO,13,1")
+}
 
 def on() {
 	log.debug "on()"
     def cmds = []
-    cmds << getAction("/on")
+    cmds << getAction("/control?cmd=GPIO,12,1")
     return cmds
 }
 
 def off() {
     log.debug "off()"
 	def cmds = []
-    cmds << getAction("/off")
+    cmds << getAction("/control?cmd=GPIO,12,0")
     return cmds
 }
 
 def refresh() {
 	log.debug "refresh()"
     def cmds = []
-    cmds << getAction("/status")
+    cmds << getAction("/json")
     return cmds
 }
 
@@ -225,14 +293,7 @@ private updateDNI() {
 }
 
 private getHostAddress() {
-    if (override == true && ip != null && ip != ""){
-        return "${ip}:80"
-    }
-    else if(getDeviceDataByName("ip") && getDeviceDataByName("port")){
-        return "${getDeviceDataByName("ip")}:${getDeviceDataByName("port")}"
-    }else{
-	    return "${ip}:80"
-    }
+	return "${ip}:80"
 }
 
 private String convertIPtoHex(ipAddress) { 
@@ -254,6 +315,6 @@ private getHeader(){
 
 def reboot() {
 	log.debug "reboot()"
-    def uri = "/reboot"
+    def uri = "/?cmd=reboot"
     getAction(uri)
 }
--- devicetypes/erocm123/sonoff-wifi-switch.src/sonoff-wifi-switch.groovy
+++ devicetypes/erocm123/sonoff-wifi-switch.src/sonoff-wifi-switch.groovy
@@ -13,7 +13,7 @@
  *  Sonoff Wifi Switch
  *
  *  Author: Eric Maycock (erocm123)
- *  Date: 2016-01-27
+ *  Date: 2016-06-02
  */
  
 import groovy.json.JsonSlurper
@@ -34,9 +34,8 @@ metadata {
 	}
     
     preferences {
-        input("ip", "string", title:"IP Address", description: "192.168.1.150" ,required: true, displayDuringSetup: true)
-        // Port should always be 80
-        //input("port", "string", title:"Port", description: "80" , required: true, displayDuringSetup: true)
+        input("override", "boolean", title:"Override detected IP Address", required: false, displayDuringSetup: false)
+        input("ip", "string", title:"IP Address", description: "192.168.1.150", required: false, displayDuringSetup: false)
 	}
 
 	tiles (scale: 2){      
@@ -83,40 +82,15 @@ def updated() {
 def configure() {
 	log.debug "configure()"
 	log.debug "Configuring Device For SmartThings Use"
-    state.ruleConfigured = false
-    state.switchConfigured = false
-    state.buttonConfigured = false
-    sendEvent(name:"hubInfo", value:"Sonoff switch still being configured")
-    if (state.MAC != null) state.dni = setDeviceNetworkId(state.MAC)
-    else 
-    if (ip != null) state.dni = setDeviceNetworkId(ip, "80")
+    sendEvent(name:"hubInfo", value:"Sonoff switch still being configured") 
+    if (ip != null && override == true) state.dni = setDeviceNetworkId(ip, "80")
     state.hubIP = device.hub.getDataValue("localIP")
-    response(setupDevices() + setupRules() + refresh())
+    state.hubPort = device.hub.getDataValue("localSrvPortTCP")
+    response(configureInstant(state.hubIP, state.hubPort))
 }
 
-def setupDevices() {
-    def cmds = []
-    cmds << postAction("/devices?index=1&page=1", "taskdevicenumber=1&edit=1&page=1")
-    cmds << postAction("/devices?index=1&page=1", "taskdevicenumber=1&taskdevicename=BUTTON&taskdevicetimer=0&taskdeviceid=1&taskdevicepin1=0&taskdevicepin1pullup=on&plugin_001_type=1&plugin_001_button=2&taskdevicevaluename1=Switch&edit=1&page=1")
-    cmds << postAction("/devices?index=2&page=1", "taskdevicenumber=1&edit=1&page=1")
-    cmds << postAction("/devices?index=2&page=1", "taskdevicenumber=1&taskdevicename=SWITCH&taskdevicetimer=0&taskdeviceid=2&taskdevicepin1=12&taskdevicepin1pullup=on&plugin_001_type=1&plugin_001_button=0&taskdevicesenddata=on&taskdevicevaluename1=Switch&edit=1&page=1")
-    return delayBetween(cmds, 1000)
-}
-
-def setupRules() {
-    def cmds = []
-    cmds << postAction("/advanced", "mqttsubscribe=&mqttpublish=&messagedelay=1000&ip=0&ntphost=&timezone=0&syslogip=0.0.0.0&sysloglevel=0&udpport=0&useserial=on&serialloglevel=0&webloglevel=0&baudrate=115200&wdi2caddress=0&wireclockstretchlimit=0&userules=on&edit=1")
-    cmds << postAction("/rules", "rules=On+BUTTON%23Switch+do%0D%0A++if+%5BSWITCH%23Switch%5D%3D0%0D%0A++++gpio%2C12%2C1%0D%0A++else%0D%0A++++gpio%2C12%2C0%0D%0A++endif%0D%0Aendon%0D%0A%0D%0AOn+SWITCH%23Switch+do%0D%0A+if+%5BSWITCH%23Switch%5D%3D1%0D%0A++++gpio%2C13%2C0%0D%0A++else%0D%0A++++gpio%2C13%2C1%0D%0A++endif%0D%0Aendon")
-    return delayBetween(cmds, 1000)
-}
-
-def setupConfig() {
-    // Automatic config page submit not working at this time. Has to be done manually
-    /*def hubIP = device.hub.getDataValue("localIP")
-    log.debug "Hub IP: ${hubIP}"
-    def cmds = []
-    //cmds << postAction("/config", "protocol=1&usedns=0&controllerip=$hubIP&controllerport=39500&controlleruser=&controllerpassword=&delay=60")
-    return delayBetween(cmds, 1000)*/
+def configureInstant(ip, port){
+    return [getAction("/config?haip=${ip}&haport=${port}")]
 }
 
 def parse(description) {
@@ -125,19 +99,17 @@ def parse(description) {
     def cmds
     def descMap = parseDescriptionAsMap(description)
     def body
-    log.debug "descMap: ${descMap}"
-    
+    //log.debug "descMap: ${descMap}"
 
-    if (!state.MAC || state.MAC != descMap["mac"]) {
+    if (!state.mac || state.mac != descMap["mac"]) {
 		log.debug "Mac address of device found ${descMap["mac"]}"
-        updateDataValue("MAC", descMap["mac"])
+        updateDataValue("mac", descMap["mac"])
 	}
     
-    if (state.MAC != null && state.dni != state.MAC) state.dni = setDeviceNetworkId(state.MAC)
+    if (state.mac != null && state.dni != state.mac) state.dni = setDeviceNetworkId(state.mac)
     if (descMap["body"]) body = new String(descMap["body"].decodeBase64())
 
     if (body && body != "") {
-
     
     if(body.startsWith("{") || body.startsWith("[")) {
     def slurper = new JsonSlurper()
@@ -159,32 +131,40 @@ def parse(description) {
     if (result.containsKey("pin")) {
         if (result.pin == 12) events << createEvent(name:"switch", value: (result.state.toInteger() == 0 ? 'off' : 'on'))
     }
-
-    if (result.containsKey("System")) {
-        if (result.System.containsKey("Uptime")) log.debug "System has been up ${result.System.Uptime.toInteger() / 60} hours"
+    if (result.containsKey("power")) {
+        events << createEvent(name: "switch", value: result.power)
+    }
+    if (result.containsKey("success")) {
+        if (result.success == "true") state.configured = true
+    }
+    if (result.containsKey("uptime")) {
+        state.uptime = result.uptime
     }
     } else {
         //log.debug "Response is not JSON: $body"
-        def ruleSearch = "OnBUTTONSwitchdoifSWITCHSwitch0gpio121elsegpio120endifendonOnSWITCHSwitchdoifSWITCHSwitch1gpio130elsegpio131endifendon"
-        if (body.replaceAll("\\W", "").indexOf(ruleSearch) > 0) state.ruleConfigured = true
-
     }
     } else {
         cmds = refresh()
     }
     
-    if (settings.ip) {
-        //log.debug "switch: $state.switchConfigured, button: $state.buttonConfigured, rule: $state.ruleConfigured"
-        if (state.switchConfigured == true && state.buttonConfigured == true && state.ruleConfigured == true) {
-            events << createEvent(name:"hubInfo", value:"For instant status updates, configure switch at http://$settings.ip/config | Hub Info - IP: ${device.hub.getDataValue("localIP")}, Port: 39500")
-        } else {
-            events << createEvent(name:"hubInfo", value:"Sonoff switch still being configured")
-        }
+    def hubInfoText = ""
+    if (getDeviceDataByName("ip") != null && getDeviceDataByName("ip") != "") {
+        hubInfoText = "IP Address: ${getDeviceDataByName("ip")} - "
     }
-    else { 
-        events << createEvent(name:"hubInfo", value:"IP address of the switch not entered. Please do so in device preferences.") 
+    if (override == true && ip != null && ip != "") {
+        hubInfoText = "IP Address: $ip - "
     }
-
+    if (state.uptime) {
+        hubInfoText = hubInfoText + "Uptime: " + state.uptime
+    }
+    if (state.configured == true) {
+        hubInfoText = hubInfoText + "\r\nSwitch has been configured"
+    } else {
+        hubInfoText = hubInfoText + "\r\nSwitch has not been configured"
+    }
+    
+    events << createEvent(name:"hubInfo", value:hubInfoText)
+    
     if (cmds) return cmds else return events
 
 }
@@ -198,55 +178,25 @@ def parseDescriptionAsMap(description) {
 	}
 }
 
-private parseHTML(html) {
-   //log.debug html
-   def myHtml = html.split("<")
-   def result = []
-   switch (myHtml[0]) {
-      case "GPIO 12 Set to 0":
-      result = [name: "switch", value: "off"]
-      break
-      case "GPIO 12 Set to 1":
-      result = [name: "switch", value: "on"]
-      break
-      case "GPIO 13 Set to 0":
-      log.debug "LED is on"
-      break
-      case "GPIO 13 Set to 1":
-      log.debug "LED is off"
-      break
-      default:
-      break
-   }
-   return result
-}
-
-private ledOn() {
-   return getAction("/control?cmd=GPIO,13,0")
-}
-
-private ledOff() {
-   return getAction("/control?cmd=GPIO,13,1")
-}
 
 def on() {
 	log.debug "on()"
     def cmds = []
-    cmds << getAction("/control?cmd=GPIO,12,1")
+    cmds << getAction("/on")
     return cmds
 }
 
 def off() {
     log.debug "off()"
 	def cmds = []
-    cmds << getAction("/control?cmd=GPIO,12,0")
+    cmds << getAction("/off")
     return cmds
 }
 
 def refresh() {
 	log.debug "refresh()"
     def cmds = []
-    cmds << getAction("/json")
+    cmds << getAction("/status")
     return cmds
 }
 
@@ -293,7 +243,14 @@ private updateDNI() {
 }
 
 private getHostAddress() {
-	return "${ip}:80"
+    if (override == true && ip != null && ip != ""){
+        return "${ip}:80"
+    }
+    else if(getDeviceDataByName("ip") && getDeviceDataByName("port")){
+        return "${getDeviceDataByName("ip")}:${getDeviceDataByName("port")}"
+    }else{
+	    return "${ip}:80"
+    }
 }
 
 private String convertIPtoHex(ipAddress) { 
@@ -315,6 +272,6 @@ private getHeader(){
 
 def reboot() {
 	log.debug "reboot()"
-    def uri = "/?cmd=reboot"
+    def uri = "/reboot"
     getAction(uri)
 }
