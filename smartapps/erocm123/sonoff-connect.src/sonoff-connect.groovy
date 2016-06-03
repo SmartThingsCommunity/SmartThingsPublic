4ca2dfc056e2c83f98475e438d9b6379e87d69e2
Eric Maycock <erocmail@gmail.com>
2016-06-03 11:10:56 -0600

    Better handling of IP address changes

--- smartapps/erocm123/sonoff-connect.src/sonoff-connect.groovy
+++ smartapps/erocm123/sonoff-connect.src/sonoff-connect.groovy
@@ -232,6 +232,8 @@ def ssdpHandler(evt) {
     
     if (devices."${ssdpUSN}") {
         def d = devices."${ssdpUSN}"
+        //log.debug "networkAddress: ${convertHexToIP(d.networkAddress)} - ${convertHexToIP(parsedEvent.networkAddress)}"
+        //log.debug "deviceAddress: ${convertHexToInt(d.deviceAddress)} - ${convertHexToInt(parsedEvent.deviceAddress)}"
         if (d.networkAddress != parsedEvent.networkAddress || d.deviceAddress != parsedEvent.deviceAddress) {
             d.networkAddress = parsedEvent.networkAddress
             d.deviceAddress = parsedEvent.deviceAddress
