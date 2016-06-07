f691cf3612e83ab533457179e5b25f9d5ee625e3
Kevin Vaughan <vaughan.k.m@gmail.com>
2016-06-07 12:01:19 -0500

    fixed a few bugs

--- smartapps/kmvaughan03/my-smartapp-all-contacts-closed-set-honeywell-rth9580wf-thermostat.src/my-smartapp-all-contacts-closed-set-honeywell-rth9580wf-thermostat.groovy
+++ smartapps/kmvaughan03/my-smartapp-all-contacts-closed-set-honeywell-rth9580wf-thermostat.src/my-smartapp-all-contacts-closed-set-honeywell-rth9580wf-thermostat.groovy
@@ -12,160 +12,212 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
+ *  --------------------+---------------------------+-------------------------------+------------------------------------
+ *  Device Type         | Attribute Name			| Commands                  	| Attribute Values
+ *  --------------------+---------------------------+-------------------------------+------------------------------------
+ *  --------------------+---------------------------+-------------------------------+------------------------------------
+ *  contactSensors     	| contact       			|                           	| open, closed
+ *  --------------------+---------------------------+-------------------------------+------------------------------------
+ *  temperatureSensors  | temperature   			|                           	|	 
+ *						| heatingSetpoint			| setHeatingSetpoint(number)	|
+ *						| coolingSetpoint			| setCoolingSetpoint(number)	|
+ *						| thermostatSetpoint		|								|
+ *						| thermostatMode			| setThermostatMode(string)		| auto,emergency heat,heat,off,cool
+ *						| thermostatFanMode			| setThermostatFanMode(string)	| auto,on,circulate
+ *						| thermostatOperatingState	| setThermostatFanMode(string)	| heating,idle,pending cool,
+ *						|							|								| vent economizer,cooling,pending heat
+ *						|							|								| fan only
+ *						|							| off()							|
+ *						|							| heat()						|
+ *						|							| emergencyheat()				|
+ *						|							| cool()						|
+ *						|							| fanOn()						|
+ *						|							| fanAuto()						|
+ *						|							| fanCirculate()				|
+ *						|							| auto()						|
+ *  --------------------+---------------------------+-------------------------------+------------------------------------
+ *	NOTE: FOR "Honeywell RTH9580WF thermostat" the thermostatMode Attribute Values are: 
+ *        "autocool,autoheat,idle,heat,cool,off"
  */
+ 
 definition(
-    name: "My SmartApp: All contacts closed - set Honeywell RTH9580WF thermostat",
-    namespace: "kmvaughan03",
-    author: "Kevin Vaughan",
-    description: "when a sensor is closed - verify all other open/close sensors are closed - if so, set thermostat back on (to revert changes made by thermostat mode director (thermostat and doors).",
-    category: "Convenience",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+    name: 			"My SmartApp: All contacts closed - set Honeywell RTH9580WF thermostat",
+    namespace: 		"kmvaughan03",
+    author: 		"Kevin Vaughan",
+    description:	"when a CONTACT sensor is closed - verify all other CONTACT sensors are closed. " +
+					"If so, set thermostat back on (to revert changes made by thermostat mode director smartapp " +
+					"(thermostat and doors).",
+    category: 		"Convenience",
+    iconUrl: 		"https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: 		"https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: 		"https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
 
 
 preferences {
-	section("Check when any of these contacts close:") {
-		input "contact", "capability.contactSensor", title: "pick contact sensors", required: true, multiple: true
+
+
+	section("When any of these contacts close") {
+		input "contact", "capability.contactSensor", title: "Contact Sensors:", required: true, multiple: true
 	}
-    section("If all of these contacts are closed:") {
-		input "allcontacts", "capability.contactSensor", title: "pick contact sensors", required: true, multiple: true
+    
+    
+    section("Check if all of these contacts are also closed") {
+		input "allcontacts", "capability.contactSensor", title: "Contact Sensors:", required: true, multiple: true
     }
-    section("Set this thermostat:") {
-        input "thermostat", "capability.thermostat", title: "pick a thermostat", required: true
+    
+    
+    section("If all contacts are closed, set this thermostat") {
+        input "thermostat", "capability.thermostat", title: "Thermostat:", required: true
     }
-    section("To the thermostat mode to:") {
-        input "tmode", "enum", required: true, title: "pick a thermostat mode", options: ["autocool","autoheat","idle","heat","cool","off"]
-		// NOTE: using different possible options than specified by 'Smartthings Capabilities Reference'in http://docs.smartthings.com/en/latest/capabilities-reference.html
-        // NOTE: the options used here are specific to work with the Honeywell RTH9580WF thermostat
-}
+    
+    
+    section("To this thermostat mode") {
+        paragraph "NOTE: These are the possible values specfic for the Honeywell RTH9580WF thermostat."
+        input "tmode", "enum", required: true, title: "Thermostat Mode:", options: ["autocool","autoheat","idle","heat","cool","off"]
+
+		/**
+         *	NOTE: using different possible options than specified by 
+         *	'Smartthings Capabilities Reference'in http://docs.smartthings.com/en/latest/capabilities-reference.html
+         *	NOTE: the options used here are specific to the Honeywell RTH9580WF thermostat
+         */
+	}
+    
+    
     section("Send this message (optional, sends standard status message if not specified)"){
-		input "messageText", "text", title: "Message Text", required: false
+		input "messageText", "text", title: "Message Text:", required: false
 	}
-	section("Via a push notification and/or an SMS message"){
+	
+    
+    section("Via a push notification and/or an SMS message"){
 		input("recipients", "contact", title: "Send notifications to") {
-			input "phone", "phone", title: "Phone Number (for SMS, optional)", required: false
+			input "phone", "phone", title: "Phone Number (for SMS, optional):", required: false
 			paragraph "If outside the US please make sure to enter the proper country code"
 			input "pushAndPhone", "enum", title: "Both Push and SMS?", required: false, options: ["Yes", "No"]
 		}
 	}
 }
 
-def installed() {
-	log.debug "Installed with settings: ${settings}"
 
+def installed() {
+
+	log.debug "Installed with settings: ${settings}"
 	initialize()
 }
 
-def updated() {
-	log.debug "Updated with settings: ${settings}"
 
+def updated() {
+
+	log.debug "Updated with settings: ${settings}"
 	unsubscribe()
 	initialize()
 }
+
 
 def initialize() {
 
     subscribe(contact, "contact", contactHandler)
 }
 
-def contactHandler(evt)
-{
+
+def contactHandler(evt) {
+
  	log.debug "contactHandler called: $evt"
 
 
+    if("open" == evt.value){
     
-    if("open" == evt.value)
-    // contact was opened - do nothing
-	log.debug "Contact is in ${evt.value} state - DO NOTHING"
-  
-    if("closed" == evt.value)
-    // contact was closed, turn on the thermostat.
-    {
+    	// contact was opened - do nothing
+		log.debug "Contact is in ${evt.value} state - DO NOTHING"
+	
+    }
     
-    //check all sensors closed
-    def currentcontact = allcontacts.currentValue("contact")
-	log.debug currentcontact
-    def contactValue = allcontacts.find{it.currentcontact == "open"}
-	log.debug contactValue
     
-    if(!contactValue)
-        {
-			
-             log.debug "thermostat.thermostatMode = ${thermostat.currentValue("thermostatMode")}"
-   			 log.debug "Attmpting to set thermostat mode to ${tmode.value}"
-             
-             if(thermostat.currentValue("thermostatMode").equalsIgnoreCase(tmode))
-             {
-             	// do nothing - thermostat already set to desired mode
-             	log.debug "thermostat already set to desired mode - do nothing"
-             }
-             else{
-             
-                thermostat.setThermostatMode(tmode)
-            	log.debug "thermostat.thermostatMode = ${thermostat.currentValue("thermostatMode")}"
-             	sendMessage(evt)
-             }
+    log.debug "thermostat.thermostatMode = ${thermostat.currentValue("thermostatMode")}"  
+    
+    if(("closed" == evt.value) && (thermostat.currentValue("thermostatMode").equalsIgnoreCase("off"))){
+    
+    	// contact was closed and thermostat is off, check if all contacts are closed.
+    	
+        //check if all contacts are closed
+    	def currentcontact = allcontacts.currentValue("contact")
+		log.debug currentcontact
+    	def contactValue = allcontacts.find{it.currentcontact == "open"}
+		log.debug contactValue
+    
+
+    	if(!contactValue){
+    
+    		//no contacts are open, set the thermostat mode			
+	  	
+        	log.debug "thermostat.thermostatMode = ${thermostat.currentValue("thermostatMode")}"
+   	 		log.debug "Attmpting to set thermostat mode to ${tmode.value}"
+        	     
+        	thermostat.setThermostatMode(tmode)
+        
+        	log.debug "thermostat.thermostatMode = ${thermostat.currentValue("thermostatMode")}"
+        
+        	//started thermostat, send notification
+        	sendMessage(evt)
+
     	}
-       else{
+    	else{
+    
+    		//A contact closed, but there are other contacts open, do nothing
 			log.debug "Another contact is open - DO NOTHING"
+		
 		}
 
     }
+   	else{
+    
+    		//Thermostat is not off - nothing to do
+			log.debug "Thermostat is not off - DO NOTHING"
+		
+	}
 }
 
+
 private sendMessage(evt) {
+
 	String msg = messageText
 	Map options = [:]
 
 	if (!messageText) {
-		msg = "My SmartApp:" +
-              "'All contacts closed - set Honeywell RTH9580WF thermostat' executed!" + 
-              "Thermostat set to ${tmode}"
-              
+		
+        msg = "My SmartApp: All contacts closed - Honeywell RTH9580WF Thermostat set to ${tmode}"              
 		options = [translatable: true, triggerEvent: evt]
+        
+        log.info msg
+        
 	}
+    
 	log.debug "$evt.name:$evt.value, pushAndPhone:$pushAndPhone, '$msg'"
 
 	if (location.contactBookEnabled) {
-		sendNotificationToContacts(msg, recipients, options)
-	} else {
-		if (!phone || pushAndPhone != 'No') {
-			log.debug 'sending push'
+		
+        sendNotificationToContacts(msg, recipients, options)
+	
+    } else {
+		
+        if (!phone || pushAndPhone != 'No') {
+			
+            log.debug 'sending push'
 			options.method = 'push'
 			//sendPush(msg)
 		}
+        
 		if (phone) {
-			options.phone = phone
+		
+        	options.phone = phone
 			log.debug 'sending SMS'
 			//sendSms(phone, msg)
 		}
+        
 		sendNotification(msg, options)
 	}
 
 	if (frequency) {
-		state[evt.deviceId] = now()
-	}
-}
-
-private defaultText(evt) {
-	if (evt.name == 'presence') {
-		if (evt.value == 'present') {
-			if (includeArticle) {
-				'{{ triggerEvent.linkText }} has arrived at the {{ location.name }}'
-			}
-			else {
-				'{{ triggerEvent.linkText }} has arrived at {{ location.name }}'
-			}
-		} else {
-			if (includeArticle) {
-				'{{ triggerEvent.linkText }} has left the {{ location.name }}'
-			}
-			else {
-				'{{ triggerEvent.linkText }} has left {{ location.name }}'
-			}
-		}
-	} else {
-		'{{ triggerEvent.descriptionText }}'
+		
+        state[evt.deviceId] = now()
 	}
 }
