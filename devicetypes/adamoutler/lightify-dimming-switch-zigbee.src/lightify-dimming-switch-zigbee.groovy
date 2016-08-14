d90cc7aabeeeb4e87ab6cdec36fd5a4900d425de
Adam Amber House <adamamberhouse@gmail.com>
2016-08-14 15:14:38 -0400

    GPL

--- devicetypes/adamoutler/lightify-dimming-switch-zigbee.src/lightify-dimming-switch-zigbee.groovy
+++ devicetypes/adamoutler/lightify-dimming-switch-zigbee.src/lightify-dimming-switch-zigbee.groovy
@@ -1,30 +1,29 @@
 /**
- *  Lightify Dimming Switch- Zigbee
- *
- *  Copyright 2016 Adam Outler
- *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
- *
- */
+*    SmartThings Lightify Dimmer Switch support
+*    Copyright (C) 2016  Adam Outler
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
 
 /**
+* Command reference 
 * on  'catchall: 0104 0006 01 01 0140 00 3A68 01 00 0000 01 00 '
-* breakdown: 0104 0006 01 01 0140 00 3A68 01 00 0000 01 00
 * off  'catchall: 0104 0006 01 01 0140 00 3A68 01 00 0000 00 00 '
 * held up   'catchall: 0104 0008 01 01 0140 00 3A68 01 00 0000 05 00 0032'
 * held down  'catchall: 0104 0008 01 01 0140 00 3A68 01 00 0000 01 00 0132'
 * released   'catchall: 0104 0008 01 01 0140 00 3A68 01 00 0000 03 00 '
-*
-*
-*
-
 */
 
 
