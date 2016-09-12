35099b23c5feb7d72426596a10472d87926bc9d4
Zack Cornelius <zcorneli.iit@gmail.com>
2016-09-12 00:32:23 -0500

    Incorporate changes and ideas from @RudiP

--- devicetypes/mitchpond/centralite-keypad.src/centralite-keypad.groovy
+++ devicetypes/mitchpond/centralite-keypad.src/centralite-keypad.groovy
@@ -17,22 +17,26 @@ metadata {
 	definition (name: "Centralite Keypad", namespace: "mitchpond", author: "Mitch Pond") {
 		capability "Battery"
 		capability "Configuration"
+        capability "Motion Sensor"
 		capability "Sensor"
 		capability "Temperature Measurement"
 		capability "Refresh"
 		capability "Lock Codes"
 		capability "Tamper Alert"
 		capability "Tone"
-        capability "button"
+		capability "button"
+        capability "polling"
+        capability "Contact Sensor"
 		
 		attribute "armMode", "String"
+        attribute "lastUpdate", "String"
 		
 		command "setDisarmed"
 		command "setArmedAway"
 		command "setArmedStay"
 		command "setArmedNight"
-        command "setExitDelay", ['number']
-        command "setEntryDelay", ['number']
+		command "setExitDelay", ['number']
+		command "setEntryDelay", ['number']
 		command "testCmd"
 		command "sendInvalidKeycodeResponse"
 		command "acknowledgeArmRequest"
@@ -46,43 +50,86 @@ metadata {
 				defaultValue: 0, displayDuringSetup: false)
 		input ("beepLength", "number", title: "Enter length of beep in seconds",
 				defaultValue: 3, displayDuringSetup: false)
+                
+        input ("motionTime", "number", title: "Time in seconds for Motion to become Inactive (Default:10, 0=disabled)",	defaultValue: 10, displayDuringSetup: false)
 	}
 
-	tiles {
-		valueTile("battery", "device.battery", decoration: "flat") {
-			state "battery", label:'${currentValue}% battery', unit:""
-		}
-		valueTile("temperature", "device.temperature") {
-			state "temperature", label: '${currentValue}°',
-				backgroundColors:[
-						[value: 31, color: "#153591"],
-						[value: 44, color: "#1e9cbb"],
-						[value: 59, color: "#90d2a7"],
-						[value: 74, color: "#44b621"],
-						[value: 84, color: "#f1d801"],
-						[value: 95, color: "#d04e00"],
-						[value: 96, color: "#bc2323"]
-					]
-		}
-		valueTile("armMode", "device.armMode", decoration: "flat") {
-			state "armMode", label: '${currentValue}'
-		}
-		standardTile("refresh", "device.refresh", decoration: "flat", width: 1, height: 1) {
-			state "default", action:"refresh.refresh", icon:"st.secondary.refresh"
-		}
-		standardTile("configure", "device.configure", decoration: "flat", width: 1, height: 1) {
-			state "default", action:"configuration.configure", icon:"st.secondary.configure"
-		}
-		standardTile("beep", "device.beep", decoration: "flat", width: 1, height: 1) {
-			state "default", action:"tone.beep", icon:"st.secondary.beep", backgroundColor:"#ffffff"
-		}
-		standardTile("test", "device.testCmd", decoration: "flat", width: 1, height: 1) {
-			state "default", action:"testCmd", icon:"st.secondary.tools", label: "Test", backgroundColor:"#ffffff"
-		}
-		main ("battery")
-		//TODO: armMode is in here for debug purposes. Remove later.
-		details (["temperature","battery","armMode","configure","refresh","beep","test"])
-	}
+    tiles (scale: 2) {
+        multiAttributeTile(name: "keypad", type:"generic", width:6, height:4, canChangeIcon: true) {
+            tileAttribute ("device.armMode", key: "PRIMARY_CONTROL") {            		
+                attributeState("disarmed", label:'${currentValue}', icon:"st.Home.home2", backgroundColor:"#44b621")
+                attributeState("armedStay", label:'ARMED/STAY', icon:"st.Home.home3", backgroundColor:"#ffa81e")
+                attributeState("armedAway", label:'ARMED/AWAY', icon:"st.nest.nest-away", backgroundColor:"#d04e00")
+            }
+            tileAttribute("device.lastUpdate", key: "SECONDARY_CONTROL") {
+                attributeState("default", label:'Updated: ${currentValue}')
+            }
+            /*
+			tileAttribute("device.battery", key: "SECONDARY_CONTROL") {
+				attributeState("default", label:'Battery: ${currentValue}%', unit:"%")
+			}
+			tileAttribute("device.battery", key: "VALUE_CONTROL") {
+				attributeState "VALUE_UP", action: "refresh"
+				attributeState "VALUE_DOWN", action: "refresh"
+			}
+			*/
+            tileAttribute("device.temperature", key: "VALUE_CONTROL") {
+                attributeState "VALUE_UP", action: "refresh"
+                attributeState "VALUE_DOWN", action: "refresh"
+            }
+        }
+
+        valueTile("temperature", "device.temperature", width: 2, height: 2) {
+            state "temperature", label: '${currentValue}°',
+                backgroundColors:[
+                    [value: 31, color: "#153591"],
+                    [value: 44, color: "#1e9cbb"],
+                    [value: 59, color: "#90d2a7"],
+                    [value: 74, color: "#44b621"],
+                    [value: 84, color: "#f1d801"],
+                    [value: 95, color: "#d04e00"],
+                    [value: 96, color: "#bc2323"]
+                ]
+        }
+
+        standardTile("motion", "device.motion", decoration: "flat", canChangeBackground: true, width: 2, height: 2) {
+            state "active", label:'motion',icon:"st.motion.motion.active", backgroundColor:"#53a7c0"
+            state "inactive", label:'no motion',icon:"st.motion.motion.inactive", backgroundColor:"#ffffff"
+        }
+        standardTile("tamper", "device.tamper", decoration: "flat", canChangeBackground: true, width: 2, height: 2) {
+            state "clear", label: 'Tamper', icon:"st.motion.acceleration.inactive", backgroundColor: "#ffffff"
+            state "detected",  label: 'Tamper', icon:"st.motion.acceleration.active", backgroundColor:"#cc5c5c"
+        }
+        standardTile("Panic", "device.contact", decoration: "flat", canChangeBackground: true, width: 2, height: 2) {
+            state "open", label: 'Panic', icon:"st.security.alarm.alarm", backgroundColor: "#ffffff"
+            state "closed",  label: 'Panic', icon:"st.security.alarm.clear", backgroundColor:"#bc2323"
+        }
+        
+		standardTile("Mode", "device.armMode", decoration: "flat", canChangeBackground: true, width: 2, height: 2) {
+            state "disarmed", label:'OFF', icon:"st.Home.home2", backgroundColor:"#44b621"
+            state "armedStay", label:'OFF', icon:"st.Home.home3", backgroundColor:"#ffffff"
+            state "armedAway", label:'OFF', icon:"st.net.nest-away", backgroundColor:"#ffffff"
+        }
+        
+        standardTile("beep", "device.beep", decoration: "flat", width: 2, height: 2) {
+            state "default", action:"tone.beep", icon:"st.secondary.beep", backgroundColor:"#ffffff"
+        }
+        valueTile("battery", "device.battery", decoration: "flat", width: 2, height: 2) {
+            state "battery", label:'${currentValue}% battery', unit:""
+        }
+        standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+            state "default", action:"refresh.refresh", icon:"st.secondary.refresh"
+        }
+        standardTile("configure", "device.configure", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+            state "default", action:"configuration.configure", icon:"st.secondary.configure"
+        }
+        valueTile("armMode", "device.armMode", decoration: "flat", width: 2, height: 2) {
+            state "armMode", label: '${currentValue}'
+        }
+
+        main (["keypad"])
+        details (["keypad","motion","tamper","Panic","Mode","beep","refresh","battery"])
+    }
 }
 
 // parse events into attributes
@@ -118,18 +165,11 @@ def parse(String description) {
 			//------IAS ACE------//
 			if (message?.clusterId == 0x0501) {
 				if (message?.command == 0x07) {
-				//---------------------------------------//
-				//Not sure what the device is doing here. It doesn't look like an ACE client should be sending this.
-				//Plus, the command isn't sent with a payload which doesn't seem to follow the spec.
-				//I'm assuming that they're using it as a sort of heartbeat (??)
-				// *** This does not correlate to motion events ***
-				//---------------------------------------//
-					log.debug "${device.displayName} awake and requesting status"
-					results = sendStatusToDevice();
-					log.trace results
+                	motionON()
 				}
                 else if (message?.command == 0x04) {
                 	results = createEvent(name: "button", value: "pushed", data: [buttonNumber: 1], descriptionText: "$device.displayName panic button was pushed", isStateChange: true)
+                    panicContact()
                 }
 				else if (message?.command == 0x00) {
 					results = handleArmRequest(message)
@@ -178,10 +218,30 @@ def configure() {
 	return cmd + refresh()
 }
 
+def poll() { 
+	refresh()
+}
+
 def refresh() {
 	 return sendStatusToDevice() +
 			zigbee.readAttribute(0x0001,0x20) + 
 			zigbee.readAttribute(0x0402,0x00)
+}
+
+private formatLocalTime(time, format = "EEE, MMM d yyyy @ h:mm a z") {
+	if (time instanceof Long) {
+    	time = new Date(time)
+    }
+	if (time instanceof String) {
+    	//get UTC time
+    	time = timeToday(time, location.timeZone)
+    }   
+    if (!(time instanceof Date)) {
+    	return null
+    }
+	def formatter = new java.text.SimpleDateFormat(format)
+	formatter.setTimeZone(location.timeZone)
+	return formatter.format(time)
 }
 
 private parseReportAttributeMessage(String description) {
@@ -244,7 +304,7 @@ private Map parseIasMessage(String description) {
         case '0x0028': // Test Mode
             break
         case '0x0000':
-			resultMap = createEvent(name: "tamper", value: "cleared", isStateChange: true, displayed: false)
+			resultMap = createEvent(name: "tamper", value: "clear", isStateChange: true, displayed: false)
             break
         case '0x0004':
 			resultMap = createEvent(name: "tamper", value: "detected", isStateChange: true, displayed: false)
@@ -253,6 +313,46 @@ private Map parseIasMessage(String description) {
         	log.debug "Invalid message code in IAS message: ${msgCode}"
     }
     return resultMap
+}
+
+
+private Map getMotionResult(value) {
+	String linkText = getLinkText(device)
+	String descriptionText = value == 'active' ? "${linkText} detected motion" : "${linkText} motion has stopped"
+	return [
+		name: 'motion',
+		value: value,
+		descriptionText: descriptionText
+	]
+}
+def motionON() {
+    log.debug "--- Motion Detected"
+    sendEvent(name: "motion", value: "active", displayed:true, isStateChange: true)
+    
+	//-- Calculate Inactive timeout value
+	def motionTimeRun = (settings.motionTime?:0).toInteger()
+
+	//-- If Inactive timeout was configured
+	if (motionTimeRun > 0) {
+		log.debug "--- Will become inactive in $motionTimeRun seconds"
+		runIn(motionTimeRun, "motionOFF")
+	}
+}
+
+def motionOFF() {
+	log.debug "--- Motion Inactive (OFF)"
+    sendEvent(name: "motion", value: "inactive", displayed:true, isStateChange: true)
+}
+
+def panicContact() {
+	log.debug "--- Panic button hit"
+    sendEvent(name: "contact", value: "open", displayed: true, isStateChange: true)
+    runIn(3, "panicContactClose")
+}
+
+def panicContactClose()
+{
+	sendEvent(name: "contact", value: "closed", displayed: true, isStateChange: true)
 }
 
 //TODO: find actual good battery voltage range and update this method with proper values for min/max
@@ -401,6 +501,8 @@ def setExitDelay(delay=30) {
 
 private setModeHelper(String armMode, delay) {
 	sendEvent([name: "armMode", value: armMode, data: [delay: delay as int], isStateChange: true])
+    def lastUpdate = formatLocalTime(now())
+    sendEvent(name: "lastUpdate", value: lastUpdate, displayed: false)
 	sendStatusToDevice()
 }
 
@@ -434,6 +536,10 @@ def sendInvalidKeycodeResponse(){
 }
 
 def beep(def beepLength = settings.beepLength) {
+    if ( beepLength == null )
+    {
+    	beepLength = 0
+    }
 	def len = zigbee.convertToHexString(beepLength, 2)
 	List cmds = ["raw 0x501 {09 01 04 05${len}}", 'delay 200',
 				 "send 0x${device.deviceNetworkId} 1 1", 'delay 500']
