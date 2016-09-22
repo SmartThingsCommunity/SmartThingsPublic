1d180ac487678d6602bd4a97c1cf79e70feea978
twack <twack@wackware.net>
2016-09-22 10:19:23 -0700

    update generic zigbee lock for Yale Key-Free Deadbolt (PROD-736)

--- devicetypes/smartthings/zigbee-lock.src/zigbee-lock.groovy
+++ devicetypes/smartthings/zigbee-lock.src/zigbee-lock.groovy
@@ -31,7 +31,8 @@
         fingerprint profileId: "0104", inClusters: "0000,0001,0003,0009,000A,0101,0020", outClusters: "000A,0019", manufacturer: "Yale", model: "YRD210 PB DB", deviceJoinName: "Yale Push Button Deadbolt Lock"
         fingerprint profileId: "0104", inClusters: "0000,0001,0003,0009,000A,0101,0020", outClusters: "000A,0019", manufacturer: "Yale", model: "YRD220/240 TSDB", deviceJoinName: "Yale Touch Screen Deadbolt Lock"
         fingerprint profileId: "0104", inClusters: "0000,0001,0003,0009,000A,0101,0020", outClusters: "000A,0019", manufacturer: "Yale", model: "YRL210 PB LL", deviceJoinName: "Yale Push Button Lever Lock"
-    }
+		fingerprint profileId: "0104", inClusters: "0000,0001,0003,0009,000A,0101,0020", outClusters: "000A,0019", manufacturer: "Yale", model: "YRD226/246 TSDB", 	deviceJoinName: "Yale Key-Free Touchscreen Deadbolt Lock"
+	}
 
     tiles(scale: 2) {
 		multiAttributeTile(name:"toggle", type:"generic", width:6, height:4){
