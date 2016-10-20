65d4a811b02b03c30c656677d6f0f655c170e13a
Zach Varberg <Zach.Varberg@SmartThings.com>
2016-10-20 11:03:48 -0500

    Clean up smartsense DTHs
    This continues an effort to clean up the SmartSense DTHs and move as
    much of the logic as possible into the library.  This simplifies the DTH
    and has the advantage of having only a single location where issues need
    to be fixed.

--- devicetypes/smartthings/smartsense-motion-sensor.src/smartsense-motion-sensor.groovy
+++ devicetypes/smartthings/smartsense-motion-sensor.src/smartsense-motion-sensor.groovy
@@ -17,7 +17,7 @@ import physicalgraph.zigbee.clusters.iaszone.ZoneStatus
 
 
 metadata {
-	definition (name: "SmartSense Motion Sensor", namespace: "smartthings", author: "SmartThings") {
+	definition(name: "SmartSense Motion Sensor", namespace: "smartthings", author: "SmartThings") {
 		capability "Motion Sensor"
 		capability "Configuration"
 		capability "Battery"
@@ -45,10 +45,10 @@ metadata {
 	preferences {
 		section {
 			image(name: 'educationalcontent', multiple: true, images: [
-				"http://cdn.device-gse.smartthings.com/Motion/Motion1.jpg",
-				"http://cdn.device-gse.smartthings.com/Motion/Motion2.jpg",
-				"http://cdn.device-gse.smartthings.com/Motion/Motion3.jpg"
-				])
+					"http://cdn.device-gse.smartthings.com/Motion/Motion1.jpg",
+					"http://cdn.device-gse.smartthings.com/Motion/Motion2.jpg",
+					"http://cdn.device-gse.smartthings.com/Motion/Motion3.jpg"
+			])
 		}
 		section {
 			input title: "Temperature Offset", description: "This feature allows you to correct any temperature variations by selecting an offset. Ex: If your sensor consistently reports a temp that's 5 degrees too warm, you'd enter '-5'. If 3 degrees too cold, enter '+3'.", displayDuringSetup: false, type: "paragraph", element: "paragraph"
@@ -57,30 +57,30 @@ metadata {
 	}
 
 	tiles(scale: 2) {
-		multiAttributeTile(name:"motion", type: "generic", width: 6, height: 4){
-			tileAttribute ("device.motion", key: "PRIMARY_CONTROL") {
-				attributeState "active", label:'motion', icon:"st.motion.motion.active", backgroundColor:"#53a7c0"
-				attributeState "inactive", label:'no motion', icon:"st.motion.motion.inactive", backgroundColor:"#ffffff"
+		multiAttributeTile(name: "motion", type: "generic", width: 6, height: 4) {
+			tileAttribute("device.motion", key: "PRIMARY_CONTROL") {
+				attributeState "active", label: 'motion', icon: "st.motion.motion.active", backgroundColor: "#53a7c0"
+				attributeState "inactive", label: 'no motion', icon: "st.motion.motion.inactive", backgroundColor: "#ffffff"
 			}
 		}
 		valueTile("temperature", "device.temperature", width: 2, height: 2) {
-			state("temperature", label:'${currentValue}°', unit:"F",
-				backgroundColors:[
-					[value: 31, color: "#153591"],
-					[value: 44, color: "#1e9cbb"],
-					[value: 59, color: "#90d2a7"],
-					[value: 74, color: "#44b621"],
-					[value: 84, color: "#f1d801"],
-					[value: 95, color: "#d04e00"],
-					[value: 96, color: "#bc2323"]
-				]
+			state("temperature", label: '${currentValue}°', unit: "F",
+					backgroundColors: [
+							[value: 31, color: "#153591"],
+							[value: 44, color: "#1e9cbb"],
+							[value: 59, color: "#90d2a7"],
+							[value: 74, color: "#44b621"],
+							[value: 84, color: "#f1d801"],
+							[value: 95, color: "#d04e00"],
+							[value: 96, color: "#bc2323"]
+					]
 			)
 		}
 		valueTile("battery", "device.battery", decoration: "flat", inactiveLabel: false, width: 2, height: 2) {
-			state "battery", label:'${currentValue}% battery', unit:""
+			state "battery", label: '${currentValue}% battery', unit: ""
 		}
 		standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
-			state "default", action:"refresh.refresh", icon:"st.secondary.refresh"
+			state "default", action: "refresh.refresh", icon: "st.secondary.refresh"
 		}
 
 		main(["motion", "temperature"])
@@ -90,19 +90,27 @@ metadata {
 
 def parse(String description) {
 	log.debug "description: $description"
-
-	Map map = [:]
-	if (description?.startsWith('catchall:')) {
-		map = parseCatchAllMessage(description)
-	}
-	else if (description?.startsWith('read attr -')) {
-		map = parseReportAttributeMessage(description)
-	}
-	else if (description?.startsWith('temperature: ')) {
-		map = parseCustomMessage(description)
-	}
-	else if (description?.startsWith('zone status')) {
-		map = parseIasMessage(description)
+	Map map = zigbee.getEvent(description)
+	if (!map) {
+		if (description?.startsWith('zone status')) {
+			map = parseIasMessage(description)
+		} else {
+			Map descMap = zigbee.parseDescriptionAsMap(description)
+			if (descMap?.clusterInt == 0x0001 && descMap.commandInt != 0x07 && descMap?.value) {
+				map = getBatteryResult(Integer.parseInt(descMap.value, 16))
+			} else if (descMap?.clusterInt == zigbee.TEMPERATURE_MEASUREMENT_CLUSTER && descMap.commandInt == 0x07) {
+				if (descMap.data[0] == "00") {
+					log.debug "TEMP REPORTING CONFIG RESPONSE: $descMap"
+					sendEvent(name: "checkInterval", value: 60 * 12, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
+				} else {
+					log.warn "TEMP REPORTING CONFIG FAILED- error code: ${descMap.data[0]}"
+				}
+			} else if (descMap.clusterInt == 0x0406 && descMap.attrInt == 0x0000) {
+				def value = descMap.value.endsWith("01") ? "active" : "inactive"
+				log.warn "Doing a read attr motion event"
+				resultMap = getMotionResult(value)
+			}
+		}
 	}
 
 	log.debug "Parse returned $map"
@@ -116,104 +124,11 @@ def parse(String description) {
 	return result
 }
 
-private Map parseCatchAllMessage(String description) {
-	Map resultMap = [:]
-	def cluster = zigbee.parse(description)
-	if (shouldProcessMessage(cluster)) {
-		switch(cluster.clusterId) {
-			case 0x0001:
-				// 0x07 - configure reporting
-				if (cluster.command != 0x07) {
-					resultMap = getBatteryResult(cluster.data.last())
-				}
-				break
-
-			case 0x0402:
-				if (cluster.command == 0x07) {
-					if (cluster.data[0] == 0x00) {
-						log.debug "TEMP REPORTING CONFIG RESPONSE" + cluster
-						resultMap = [name: "checkInterval", value: 60 * 12, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID]]
-					}
-					else {
-						log.warn "TEMP REPORTING CONFIG FAILED- error code:${cluster.data[0]}"
-					}
-
-				}
-				else {
-					// temp is last 2 data values. reverse to swap endian
-					String temp = cluster.data[-2..-1].reverse().collect { cluster.hex1(it) }.join()
-					def value = getTemperature(temp)
-					resultMap = getTemperatureResult(value)
-				}
-				break
-
-			case 0x0406:
-				// 0x07 - configure reporting
-				if (cluster.command != 0x07) {
-					log.debug 'motion'
-					resultMap.name = 'motion'
-				}
-				break
-		}
-	}
-
-	return resultMap
-}
-
-private boolean shouldProcessMessage(cluster) {
-	// 0x0B is default response indicating message got through
-	boolean ignoredMessage = cluster.profileId != 0x0104 ||
-		cluster.command == 0x0B ||
-		(cluster.data.size() > 0 && cluster.data.first() == 0x3e)
-	return !ignoredMessage
-}
-
-private Map parseReportAttributeMessage(String description) {
-	Map descMap = (description - "read attr - ").split(",").inject([:]) { map, param ->
-		def nameAndValue = param.split(":")
-		map += [(nameAndValue[0].trim()):nameAndValue[1].trim()]
-	}
-	log.debug "Desc Map: $descMap"
-
-	Map resultMap = [:]
-	if (descMap.cluster == "0402" && descMap.attrId == "0000") {
-		def value = getTemperature(descMap.value)
-		resultMap = getTemperatureResult(value)
-	}
-	else if (descMap.cluster == "0001" && descMap.attrId == "0020") {
-		resultMap = getBatteryResult(Integer.parseInt(descMap.value, 16))
-	}
-	else if (descMap.cluster == "0406" && descMap.attrId == "0000") {
-		def value = descMap.value.endsWith("01") ? "active" : "inactive"
-		resultMap = getMotionResult(value)
-	}
-
-	return resultMap
-}
-
-private Map parseCustomMessage(String description) {
-	Map resultMap = [:]
-	if (description?.startsWith('temperature: ')) {
-		def value = zigbee.parseHATemperatureValue(description, "temperature: ", getTemperatureScale())
-		resultMap = getTemperatureResult(value)
-	}
-	return resultMap
-}
-
 private Map parseIasMessage(String description) {
 	ZoneStatus zs = zigbee.parseZoneStatus(description)
 
 	// Some sensor models that use this DTH use alarm1 and some use alarm2 to signify motion
 	return (zs.isAlarm1Set() || zs.isAlarm2Set()) ? getMotionResult('active') : getMotionResult('inactive')
-}
-
-def getTemperature(value) {
-	def celsius = Integer.parseInt(value, 16).shortValue() / 100
-	if(getTemperatureScale() == "C"){
-		return Math.round(celsius)
-	} else {
-		return Math.round(celsiusToFahrenheit(celsius))
-	}
 }
 
 private Map getBatteryResult(rawValue) {
@@ -255,36 +170,14 @@ private Map getBatteryResult(rawValue) {
 	return result
 }
 
-private Map getTemperatureResult(value) {
-	log.debug 'TEMP'
-	if (tempOffset) {
-		def offset = tempOffset as int
-		def v = value as int
-		value = v + offset
-	}
-    def descriptionText
-    if ( temperatureScale == 'C' )
-    	descriptionText = '{{ device.displayName }} was {{ value }}°C'
-    else
-    	descriptionText = '{{ device.displayName }} was {{ value }}°F'
-
-	return [
-		name: 'temperature',
-		value: value,
-		descriptionText: descriptionText,
-		translatable: true,
-		unit: temperatureScale
-	]
-}
-
 private Map getMotionResult(value) {
 	log.debug 'motion'
 	String descriptionText = value == 'active' ? "{{ device.displayName }} detected motion" : "{{ device.displayName }} motion has stopped"
 	return [
-		name: 'motion',
-		value: value,
-		descriptionText: descriptionText,
-        translatable: true
+			name           : 'motion',
+			value          : value,
+			descriptionText: descriptionText,
+			translatable   : true
 	]
 }
 
@@ -299,7 +192,7 @@ def refresh() {
 	log.debug "refresh called"
 
 	def refreshCmds = zigbee.readAttribute(zigbee.POWER_CONFIGURATION_CLUSTER, 0x0020) +
-					  zigbee.readAttribute(zigbee.TEMPERATURE_MEASUREMENT_CLUSTER, 0x0000)
+			zigbee.readAttribute(zigbee.TEMPERATURE_MEASUREMENT_CLUSTER, 0x0000)
 
 	return refreshCmds + zigbee.enrollResponse()
 }
