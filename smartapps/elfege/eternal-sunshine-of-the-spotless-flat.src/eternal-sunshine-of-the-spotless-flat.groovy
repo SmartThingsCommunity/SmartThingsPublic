61e88cb317d516050aacff9b9eb9a45827207da7
Elfege <elfege@elfege.com>
2016-10-30 03:45:28 -0400

    Modifying 'Eternal Sunshine'

--- smartapps/elfege/eternal-sunshine-of-the-spotless-flat.src/eternal-sunshine-of-the-spotless-flat.groovy
+++ smartapps/elfege/eternal-sunshine-of-the-spotless-flat.src/eternal-sunshine-of-the-spotless-flat.groovy
@@ -25,13 +25,41 @@ definition(
 
 
 preferences { 
-    page(name: "settings", title: "Select your preferences", install: true, uninstall: true) 
+    page name:"pageSetup"
+    page name:"settings"
+    page name:"Options"
 }
 
+def pageSetup() {
+    def pageProperties = [
+        name:       "pageSetup",
+        title:      "Status",
+        nextPage:   null,
+        install:    true,
+        uninstall:  true
+    ]
+
+    return dynamicPage(pageProperties) {
+
+        section("Setup Menu") {
+            href "settings", title: "Main Settings", description: ""
+            href "Options", title: "Other Options", description: ""
+
+        }
+    }
+}
 
 def settings() {
 
-    dynamicPage(name: "settings", title: "Select your preferences", install:true, uninstall: true) {
+    def pageProperties = [
+        name:       "settings",
+        title:      "Settings",
+       
+        install:    true,
+        uninstall:  true
+    ]
+
+    return dynamicPage(pageProperties) {
 
         section("select at least one dimmer") {
             input "dimmer", "capability.switchLevel", title: "pick a dimmer", required:true, multiple: true
@@ -42,23 +70,36 @@ def settings() {
         section("set an increment value ") {
             input "DimIncrVal", "decimal", title: "pick an increment value", range: "5..20", required:true, multiple: false
         }
+    }
+}
+
+def Options() {
+    def pageProperties = [
+        name:       "Options",
+        title:      "Options",
+      
+        install:    true,
+        uninstall:  true
+    ]
+
+    return dynamicPage(pageProperties){
         section("Select Optional Rules") {    
-            input "OnlyIfNotOff", "bool", title: "Run this app only if ${dimmer} isn't turned off", default: false
+            input "OnlyIfNotOff", "bool", title: "Run this app only if ${dimmer} isn't turned off", default: false, uninstall: true, install: true
             paragraph "Below you can set a maximum value above which your lights won't be set. Useful after watching a movie or when you want your night mode to be more intimate..."        
-            input "Partvalue", "bool", title: "optional: set a maximum light level?", required:false, default: false, submitOnChange: true
+            input "Partvalue", "bool", title: "optional: set a maximum light level?", required:false, default: false, submitOnChange: true, uninstall: true, install: true
             if(Partvalue){
-                input "SetPartvalue", "decimal", title: "Set a maximul value", range: "1..100", required: true
-                input "exceptionMode", "bool", title: "Apply this threshold only when on specific Modes?", required: false, default: false, submitOnChange: true     
+                input "SetPartvalue", "decimal", title: "Set a maximul value", range: "1..100", required: true, uninstall: true, install: true
+                input "exceptionMode", "bool", title: "Apply this threshold only when on specific Modes?", required: false, default: false, submitOnChange: true, uninstall: true, install: true     
 
                 if(exceptionMode){
-                    input "ExceptionMode", "mode", title: "select which mode", required: true, multiple: false, submitOnChange: true
+                    input "ExceptionMode", "mode", title: "select which mode", required: true, multiple: false, submitOnChange: true, uninstall: true, install: true
                     if(ExceptionMode){
-                        input "ExceptionMode2", "mode", title: "Optional: select another mode", required: false, multiple: false, submitOnChange: true
+                        input "ExceptionMode2", "mode", title: "Optional: select another mode", required: false, multiple: false, submitOnChange: true, uninstall: true, install: true
                         if(ExceptionMode2){
-                            input "SetPartvalue2", "decimal", title: "What maximum value in this mode?", range: "1..100", required: true
+                            input "SetPartvalue2", "decimal", title: "What maximum value in this mode?", range: "1..100", required: true, uninstall: true, install: true
                         }
                     }
-                    paragraph "MAKE SURE TO SELECT below the modes that you just selected in the options above"
+
                 }
             }
         }
@@ -103,7 +144,6 @@ def switchSetLevelHandler(evt) {
 
 }
 
-
 def SwitchHandler(evt){ 
     log.debug "dimmer.currentSwitch is $dimmer.currentSwitch"
     if(evt.value == "on"){
@@ -131,7 +171,6 @@ def illuminanceHandler(evt){
     }
 }
 
-
 private DIM(){
 
     def maxlux = 1000
@@ -143,25 +182,25 @@ private DIM(){
 
     if(state.luxvalue != 0){
         ProportionLux = (maxlux / state.luxvalue) 
+        log.debug "calculating proportionLux multiplier"
     }
     else{
         ProportionLux = 100
+        log.debug "ProportionLux set to 100 because lux = $state.luxvalue"
     }
 
     log.debug "ProportionLux value returns $ProportionLux"
 
     if ( ProportionLux == 1) {
         // this is the case when lux is max at 1000
-        if(Partvalue){
-            state.dim = 1 // don't set to 0 to avoid creating an event that will turn off the entire app
-        }
-        else {
-            state.dim = 0
-            log.debug "ProportionLux is 1 so dim set to 0"
-        }
+
+        state.dim = 1
+        log.debug "ProportionLux is 1 so dim set to 0"
     }
+
     else {
-        state.dim = (ProportionLux * DimIncrVal) 
+        state.dim = (ProportionLux * DimIncrVal)
+        log.debug "calculating state.dim"
         // example 1000 / 500 = 2 so dim = 2 * 5 light will be dimmed down or up? by 10%
         // example 1000 / 58 = 17.xxx so dim = 17 * 5 so if lux is 58 then light will be set to 85%.
     }
@@ -178,27 +217,34 @@ private DIM(){
                 log.debug "currentMode eval"
                 if(state.dim >= SetPartvalue){
                     state.dim = SetPartvalue
+                    log.debug "state.dim value is: $state.dim (SetPartvalue)"
                 }
             } 
             else if(CurrMode == ExceptionMode2){
                 if(state.dim >= SetPartvalue2){
                     state.dim = SetPartvalue2
+                    log.debug "state.dim value is: $state.dim (SetPartvalue2)"
                 }
             }
         }
         else {
             log.debug "Just PartValue eval"
-            if(state.dim >= SetPartvalue){
-                state.dim = SetPartvalue
+            if(Partvalue){
+                if(state.dim >= SetPartvalue){
+                    state.dim = SetPartvalue
+                    log.debug "state.dim value is: $state.dim (SetPartvalue alone)"
+                }
             }
         }
-        int dim = state.dim
-        dimmer.setLevel(dim) 
-        log.debug "light set to $dim %"
-        state.LevelSetByApp = true
     }
-}
 
+    log.debug "state.dim value is: $state.dim"
+    int dim = state.dim
+    dimmer.setLevel(dim) 
+    log.debug "light set to $dim %"
+    state.LevelSetByApp = true
+
+}
 
 private send(msg) {
     if (location.contactBookEnabled) {
