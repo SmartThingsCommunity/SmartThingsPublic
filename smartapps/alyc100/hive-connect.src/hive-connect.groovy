b8f07c2f5988231c920c2d09a49d01efd2d06bed
Alex Lee Yuk Cheung <alyc100@hotmail.com>
2016-11-23 17:07:01 +0000

    Version number update

--- smartapps/alyc100/hive-connect.src/hive-connect.groovy
+++ smartapps/alyc100/hive-connect.src/hive-connect.groovy
@@ -120,7 +120,7 @@ def mainPage() {
 
 def headerSECTION() {
 	return paragraph (image: "https://raw.githubusercontent.com/alyc100/SmartThingsPublic/master/smartapps/alyc100/10457773_334250273417145_3395772416845089626_n.png",
-                  "Hive (Connect)\nVersion: 2.4\nDate: 23112016(1422)")
+                  "Hive (Connect)\nVersion: 2.4b\nDate: 23112016(1700)")
 }
 
 def stateTokenPresent() {
