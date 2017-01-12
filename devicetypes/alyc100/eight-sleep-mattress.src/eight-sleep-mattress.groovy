64d3a2f9292c7231ced4a6dc1efa492d91cc260f
Alex Lee Yuk Cheung <alyc100@hotmail.com>
2017-01-12 18:06:19 +0000

    Change label in events for bed presence event

--- devicetypes/alyc100/eight-sleep-mattress.src/eight-sleep-mattress.groovy
+++ devicetypes/alyc100/eight-sleep-mattress.src/eight-sleep-mattress.groovy
@@ -254,11 +254,11 @@ def refresh() {
 }
 
 def setInBed() {
-	sendEvent(name: "contact", value: "closed")
+	sendEvent(name: "contact", value: "closed", descriptionText: "Is in bed", displayed: true)
 }
 
 def setOutOfBed() {
-	sendEvent(name: "contact", value: "open")
+	sendEvent(name: "contact", value: "open", descriptionText: "Is out of bed", displayed: true)
     state.sleepPeriod = false
     state.desiredLevelChange = false
 }
