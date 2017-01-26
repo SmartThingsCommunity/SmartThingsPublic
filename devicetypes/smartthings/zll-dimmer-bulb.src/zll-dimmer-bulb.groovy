be2e19e4069cd65bfc5317befe943bb4b96a8ff9
Zach Varberg <Zach.Varberg@SmartThings.com>
2017-01-26 10:24:26 -0600

    Manually refresh on/off status after setLevel
    This is because ZLL bulbs do not report or allow for configuration of
    reporting of attributes.  As a result if we change a value, we have to
    manually call a read of that attribute to update our status.
    
    This is a part of: https://smartthings.atlassian.net/browse/DPROT-227

--- devicetypes/smartthings/zll-dimmer-bulb.src/zll-dimmer-bulb.groovy
+++ devicetypes/smartthings/zll-dimmer-bulb.src/zll-dimmer-bulb.groovy
@@ -89,7 +89,7 @@ def on() {
 }
 
 def setLevel(value) {
-    zigbee.setLevel(value) + ["delay 1500"] + zigbee.levelRefresh()         //adding refresh because of ZLL bulb not conforming to send-me-a-report
+    zigbee.setLevel(value) + zigbee.onOffRefresh() + zigbee.levelRefresh()         //adding refresh because of ZLL bulb not conforming to send-me-a-report
 }
 
 def refresh() {
