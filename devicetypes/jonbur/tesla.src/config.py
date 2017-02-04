cac4bb2436d54bbf00e4893653fcfa40666d651d
jonbur <jb@8ft.co.uk>
2017-02-04 19:48:58 +0000

    Create config.py

---
+++ devicetypes/jonbur/tesla.src/config.py
@@ -0,0 +1,8 @@
+#This file contains the variable definitions for your Tesla
+#Email and pasword are for MyTesla accounts
+#your_lat and your_long are the decimal lattitude and longitude of your SmartThings hub location (used to determine presence)
+
+TESLA_EMAIL = 'youremail@address.com'
+TESLA_PASSWORD = 'your mytesla password'
+VEHICLE_VIN = 'your vehicle VIN'
+HOME_LOCATION = (your_lat, your_long) 
