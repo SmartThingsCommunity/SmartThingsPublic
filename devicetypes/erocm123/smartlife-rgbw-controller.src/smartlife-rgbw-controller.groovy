6096598b8135deee0922aa77030d12344a0d67da
Eric Maycock <erocmail@gmail.com>
2017-03-08 22:36:31 -0700

    Added testing to verify a valid color value has been used with setColor.

--- devicetypes/erocm123/smartlife-rgbw-controller.src/smartlife-rgbw-controller.groovy
+++ devicetypes/erocm123/smartlife-rgbw-controller.src/smartlife-rgbw-controller.groovy
@@ -410,12 +410,16 @@ def getWhite(value) {
 def setColor(value) {
     log.debug "setColor being called with ${value}"
     def uri
+    def validValue = true
 
-    if ( !(value.hex) && ((value.saturation) || (value.hue))) {
+    if ( !(value.hex) && ((value.saturation) && (value.hue))) {
         def hue = (value.hue != null) ? value.hue : 13
 		def saturation = (value.saturation != null) ? value.saturation : 13
 		def rgb = huesatToRGB(hue as Integer, saturation as Integer)
         value.hex = rgbToHex([r:rgb[0], g:rgb[1], b:rgb[2]])
+    } else {
+        log.debug "setColor must be given either a valid hex value or a valid hue & saturation value"
+        validValue = false
     }
     
     if (value.hue == 23 && value.saturation == 56) {
@@ -450,12 +454,17 @@ def setColor(value) {
     }
 	else if (value.hex) {
        log.debug "setting color with hex"
-       def rgb = value.hex.findAll(/[0-9a-fA-F]{2}/).collect { Integer.parseInt(it, 16) }
-       def myred = rgb[0] < 40 ? 0 : rgb[0]
-       def mygreen = rgb[1] < 40 ? 0 : rgb[1]
-       def myblue = rgb[2] < 40 ? 0 : rgb[2]
-       def dimmedColor = getDimmedColor(rgbToHex([r:myred, g:mygreen, b:myblue]))
-       uri = "/rgb?value=${dimmedColor.substring(1)}"
+       if (!value.hex ==~ /^\#([A-Fa-f0-9]){6}$/) {
+           log.debug "$value.hex is not valid"
+           validValue = false
+       } else {
+           def rgb = value.hex.findAll(/[0-9a-fA-F]{2}/).collect { Integer.parseInt(it, 16) }
+           def myred = rgb[0] < 40 ? 0 : rgb[0]
+           def mygreen = rgb[1] < 40 ? 0 : rgb[1]
+           def myblue = rgb[2] < 40 ? 0 : rgb[2]
+           def dimmedColor = getDimmedColor(rgbToHex([r:myred, g:mygreen, b:myblue]))
+           uri = "/rgb?value=${dimmedColor.substring(1)}"
+       }
     }
     else if (value.white) {
        uri = "/w1?value=${value.white}"
@@ -497,7 +506,7 @@ def setColor(value) {
        uri = "/w1?value=ff"
     }
     
-    if (uri != null) postAction("$uri&channels=$channels&transition=$transition")
+    if (uri != null && validValue != false) postAction("$uri&channels=$channels&transition=$transition")
 
 }
 
