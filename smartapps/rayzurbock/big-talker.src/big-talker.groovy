3597821ec4209dbe99c98d16ac74bb9af2c70463
Brian Lowrance <brian@rayzurbock.com>
2017-03-12 03:55:06 -0500

    Update big-talker.groovy

--- smartapps/rayzurbock/big-talker.src/big-talker.groovy
+++ smartapps/rayzurbock/big-talker.src/big-talker.groovy
@@ -1,6 +1,5 @@
 /**  
  *  BIG TALKER -- Version 1.1.10 -- A SmartApp for SmartThings Home Automation System
- *  WARNING!  1.1.9 DEVELOPMENT BRANCH, May have unforseen bugs!
  *  Copyright 2014-2016 - rayzur@rayzurbock.com - Brian S. Lowrance
  *  For the latest version, development and test releases visit http://www.github.com/rayzurbock
  *
