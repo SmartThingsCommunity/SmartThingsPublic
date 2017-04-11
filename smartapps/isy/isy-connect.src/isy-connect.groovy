4466af52091ffa6d79aaa90490fc2f25bad5a0c3
Peter Gerguis <gerguis@gmail.com>
2017-04-11 11:43:23 -0500

    Modifying 'isy'

--- smartapps/isy/isy-connect.src/isy-connect.groovy
+++ smartapps/isy/isy-connect.src/isy-connect.groovy
@@ -20,9 +20,9 @@ definition(
     author: "Richard L. Lynch",
     description: "ISY Insteon Connection",
     category: "SmartThings Labs",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png") {
+    iconUrl: "http://cdn.device-icons.smartthings.com/Lighting/light99-hue-v2-icn@2x.png",
+    iconX2Url: "http://cdn.device-icons.smartthings.com/Lighting/light99-hue-v2-icn@2x.png",
+    iconX3Url: "http://cdn.device-icons.smartthings.com/Lighting/light99-hue-v2-icn@2x.png") {
 }
 
 
