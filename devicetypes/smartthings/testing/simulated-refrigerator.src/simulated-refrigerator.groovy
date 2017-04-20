fe7a67024f80c8b087b9b25226f62502b346b488
Raj Karamchedu <raj.karamchedu@smartthings.com>
2017-04-19 18:33:49 -0700

    Tile layout changes for Simulated Refrigerator parent

--- devicetypes/smartthings/testing/simulated-refrigerator.src/simulated-refrigerator.groovy
+++ devicetypes/smartthings/testing/simulated-refrigerator.src/simulated-refrigerator.groovy
@@ -24,28 +24,53 @@ metadata {
 	}
 
 	tiles(scale: 2) {
-		standardTile("contact", "device.contact", width: 4, height: 4) {
+    	// Row 1: Refrigerator door open/close display
+		standardTile("contact", "device.contact", width: 2, height: 2) {
 			state("closed", label:'${name}', icon:"st.fridge.fridge-closed", backgroundColor:"#79b821")
 			state("open", label:'${name}', icon:"st.fridge.fridge-open", backgroundColor:"#ffa81e")
 		}
-		childDeviceTile("freezerDoor", "freezerDoor", height: 2, width: 2, childTileName: "freezerDoor")
+        // Row 1: Fridge and Freezer Door open/close displays
 		childDeviceTile("mainDoor", "mainDoor", height: 2, width: 2, childTileName: "mainDoor")
-		childDeviceTile("freezer", "freezer", height: 2, width: 2, childTileName: "freezer")
-		childDeviceTile("refrigerator", "refrigerator", height: 2, width: 2, childTileName: "refrigerator")
-		childDeviceTile("freezerSetpoint", "freezer", height: 1, width: 2, childTileName: "freezerSetpoint")
-		childDeviceTile("refrigeratorSetpoint", "refrigerator", height: 1, width: 2, childTileName: "refrigeratorSetpoint")
-
-		// for simulator
+		childDeviceTile("freezerDoor", "freezerDoor", height: 2, width: 2, childTileName: "freezerDoor")
+        
+        // Row 2: Label for Door open/close controls
+        valueTile("doorControlLabel", "device.contact", width: 2, height: 2, decoration: "flat") {
+            state "contact", label:'Door Control'
+    	}
+        // Row 2: Door open/close controls from "Simulated Refrigerator Door" child 
+        childDeviceTile("mainDoorControl", "mainDoor", height: 2, width: 2, childTileName: "control")
+        childDeviceTile("freezerDoorControl", "freezerDoor", height: 2, width: 2, childTileName: "control")
+		
+        // Row 3: Refrigerator and freezer temperature displays from "Simulated Refrigerator Temperature Control" child
+		childDeviceTile("refrigerator", "refrigerator", height: 2, width: 3) //, childTileName: "refrigerator")
+		childDeviceTile("freezer", "freezer", height: 2, width: 3, childTileName: "freezer")
+        
+        // Row 4: Label for up/down temp controls for refrigerator
+        valueTile("fridgUpDownLabel", "device.contact", width: 1, height: 1, decoration: "flat") {
+            state "contact", label:'Fridge'
+    	}
+        // Row 4: Up/down temp controls for the refrigerator
+        childDeviceTile("refrigeratorUp", "refrigerator", height: 1, width: 1, childTileName: "tempUp")
+		childDeviceTile("refrigeratorDown", "refrigerator", height: 1, width: 1, childTileName: "tempDown")
+        // Row 4: Label for up/down temp controls for the freezer
+        valueTile("freezerUpDown", "device.contact", width: 1, height: 1, decoration: "flat") {
+            state "contact", label:'Freezer'
+    	}
+        // Row 4: Up/down temp controls for the freezer
 		childDeviceTile("freezerUp", "freezer", height: 1, width: 1, childTileName: "tempUp")
 		childDeviceTile("freezerDown", "freezer", height: 1, width: 1, childTileName: "tempDown")
-		childDeviceTile("refrigeratorUp", "refrigerator", height: 1, width: 1, childTileName: "tempUp")
-		childDeviceTile("refrigeratorDown", "refrigerator", height: 1, width: 1, childTileName: "tempDown")
-		childDeviceTile("freezerDoorControl", "freezerDoor", height: 1, width: 1, childTileName: "control")
-		childDeviceTile("mainDoorControl", "mainDoor", height: 1, width: 1, childTileName: "control")
-		childDeviceTile("freezerSetpointUp", "freezer", height: 1, width: 1, childTileName: "setpointUp")
-		childDeviceTile("freezerSetpointDown", "freezer", height: 1, width: 1, childTileName: "setpointDown")
-		childDeviceTile("refrigeratorSetpointUp", "refrigerator", height: 1, width: 1, childTileName: "setpointUp")
+       
+       	// Row 5: "Set Fridge:" label and up/down controls display from "Simulated Refrigerator Temperature Control" child
+		childDeviceTile("refrigeratorSetpoint", "refrigerator", height: 1, width: 1, childTileName: "refrigeratorSetpoint")  
+        childDeviceTile("refrigeratorSetpointUp", "refrigerator", height: 1, width: 1, childTileName: "setpointUp")
 		childDeviceTile("refrigeratorSetpointDown", "refrigerator", height: 1, width: 1, childTileName: "setpointDown")
+       
+        // Row 5: "Set Freezer:" label and up/down controls display from "Simulated Refrigerator Temperature Control" child
+		childDeviceTile("freezerSetpoint", "freezer", height: 1, width: 1, childTileName: "freezerSetpoint")
+        childDeviceTile("freezerSetpointUp", "freezer", height: 1, width: 1, childTileName: "setpointUp")
+		childDeviceTile("freezerSetpointDown", "freezer", height: 1, width: 1, childTileName: "setpointDown")
+        
+        main "contact"
 	}
 }
 
@@ -53,6 +78,7 @@ def installed() {
 	state.counter = state.counter ? state.counter + 1 : 1
 	if (state.counter == 1) {
 		addChildDevice(
+        		// Explicitly calling the child Device Handler with the name "Simulated Refrigerator Door" is how it is associated with this parent Device Handler.
 				"Simulated Refrigerator Door",
 				"${device.deviceNetworkId}.1",
 				null,
