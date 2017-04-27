05c3b15ec7474767f123d256f6e0d2e164e697d1
Brett Sheleski <brett@sheleski.com>
2017-04-27 10:46:04 -0500

    Got Sonoff-Tasmota device handler working

--- devicetypes/BrettSheleski/sonoff-tasmota.src/sonoff-tasmota.groovy
+++ devicetypes/BrettSheleski/sonoff-tasmota.src/sonoff-tasmota.groovy
@@ -1,8 +1,8 @@
 metadata {
     definition(name: "Sonoff-Tasmota", namespace: "BrettSheleski", author: "Brett Sheleski") {
 		capability "Switch"
-
-        attribute "ipAddress", "string"
+		capability "Polling"
+		capability "Refresh"
     }
 
 	// UI tile definitions
@@ -14,55 +14,143 @@ metadata {
 			}
 		}
 
-		standardTile("indicator", "device.indicatorStatus", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
-			state "when off", action:"indicator.indicatorWhenOn", icon:"st.indicators.lit-when-off"
-			state "when on", action:"indicator.indicatorNever", icon:"st.indicators.lit-when-on"
-			state "never", action:"indicator.indicatorWhenOff", icon:"st.indicators.never-lit"
-		}
 		standardTile("refresh", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
 			state "default", label:'', action:"refresh.refresh", icon:"st.secondary.refresh"
 		}
 
 		main "switch"
-		details(["switch","refresh","indicator"])
+		details(["switch","refresh"])
 	}
 
-     preferences {
-        input "rokuIpAddress", "string", title: "IP Address", description: "IP Address of Roku", displayDuringSetup: true, required: true
-        input "rokuPort", "string", title: "Port", description: "Port", displayDuringSetup: true, defaultValue: "8060", required: true
+    preferences {
+		input(name: "ipAddress", type: "string", title: "IP Address", description: "IP Address of Sonoff", displayDuringSetup: true, required: true)
+		input(name: "port", type: "number", title: "Port", description: "Port", displayDuringSetup: true, required: true, defaultValue: 80)
+		
+		section("Sonoff Host") {
+			
+		}
+
+		section("Authentication") {
+			input(name: "username", type: "string", title: "Username", description: "Username", displayDuringSetup: false, required: false)
+			input(name: "password", type: "password", title: "Password", description: "Password", displayDuringSetup: false, required: false)
+		}
     }
 }
 
 def parse(String description) {
     log.debug "parse()"
+
+	def STATUS_PREFIX = "STATUS = ";
+	def RESULT_PREFIX = "RESULT = ";
+
+	def message = parseLanMessage(description);
+    
+	if (message?.body?.startsWith(STATUS_PREFIX)) {
+		def statusJson = message.body.substring(STATUS_PREFIX.length())
+
+		parseStatus(statusJson);
+	}
+	else if (message?.body?.startsWith(RESULT_PREFIX)) {
+		def resultJson = message.body.substring(RESULT_PREFIX.length())
+
+		parseResult(resultJson);
+	}
 }
 
+def parseStatus(String json){
+	log.debug "status: $json"
+
+	def status = new groovy.json.JsonSlurper().parseText(json);
+
+	def isOn = status.Status.Power == 1;
+
+	setSwitchState(isOn);
+}
+
+def parseResult(String json){
+	log.debug "result: $json"
+
+	def result = new groovy.json.JsonSlurper().parseText(json);
+
+	def isOn = result.POWER == "ON";
+	
+	setSwitchState(isOn);
+}
+
+def setSwitchState(Boolean on){
+	log.debug "The lights are " + (on ? "ON" : "OFF")
+
+	sendEvent(name: "switch", value: on ? "on" : "off");
+}
+
+
 def toggle(){
+	log.debug "TOGGLE"
     sendCommand("Power", "Toggle");
 }
 
 def on(){
+	log.debug "ON"
     sendCommand("Power", "On");
 }
 
 def off(){
+	log.debug "OFF"
     sendCommand("Power", "Off");
+}
+
+def poll(){
+	log.debug "POLL"
+
+	requestStatus()
+
+}
+
+def refresh(){
+	log.debug "REFRESH"
+
+	requestStatus();
+}
+
+def requestStatus(){
+	log.debug "getStatus()"
+
+	def result = sendCommand("Status", null);
+
+	return result;
 }
 
 private def sendCommand(String command, String payload){
 
-    log.debug "remoteKeyPress(${key})"
+    log.debug "sendCommand(${command}:${payload})"
 
-    def hosthex = convertIPtoHex(rokuIpAddress)
-    def porthex = convertPortToHex(rokuPort)
+    def hosthex = convertIPtoHex(ipAddress);
+    def porthex = convertPortToHex(port);
 
-    device.deviceNetworkId = "$hosthex:$porthex" 
+    device.deviceNetworkId = "$hosthex:$porthex";
+
+	def path = "/cm"
+
+	if (payload){
+		path += "?cmnd=${command}%20${payload}"
+	}
+	else{
+		path += "?cmnd=${command}"
+	}
+
+	if (username){
+		path += "&user=${username}"
+
+		if (password){
+			path += "&password=${password}"
+		}
+	}
 
     def result = new physicalgraph.device.HubAction(
-        method: "POST",
-        path: "/keypress/" + key,
+        method: "GET",
+        path: path,
         headers: [
-            HOST: "${rokuIpAddress}:${rokuPort}"
+            HOST: "${ipAddress}:${port}"
         ]
     )
 
@@ -79,15 +167,7 @@ private String convertIPtoHex(ipAddress) {
 }
 
 private String convertPortToHex(port) {
-	String hexport = port.toString().format( '%04x', port.toInteger() )
+	String hexport = port.toString().format('%04x', port.toInteger())
     log.debug hexport
     return hexport
-}
-
-def on(){
-    keyPress_Play()
-}
-
-def off(){
-    keyPress_Play()
 }
