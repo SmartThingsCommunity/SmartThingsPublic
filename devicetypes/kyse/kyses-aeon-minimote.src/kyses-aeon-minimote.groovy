a24cb3937de8571597e0ecbfc15f1e3d37ec91da
Jared Fisher <kyse@kyse.us>
2017-06-05 16:14:23 -0400

    Update numButtons to numberOfButtons per standard http://docs.smartthings.com/en/latest/capabilities-reference.html#button.

--- devicetypes/kyse/kyses-aeon-minimote.src/kyses-aeon-minimote.groovy
+++ devicetypes/kyse/kyses-aeon-minimote.src/kyses-aeon-minimote.groovy
@@ -23,7 +23,7 @@ metadata {
 		capability "Configuration"
 		capability "Sensor"
         
-		attribute "numButtons", "STRING"
+		attribute "numberOfButtons", "STRING"
         
         // Virtual Button Attributes for defining button labels.
         attribute "lblPush1", "STRING"
@@ -185,7 +185,8 @@ def configurationCmds() {
 
 def configure() {
 	// Set the number of buttons to 4
-	sendEvent(name: "numButtons", value: "4", displayed: false)
+    // Updated value to standard per http://docs.smartthings.com/en/latest/capabilities-reference.html#button
+	sendEvent(name: "numberOfButtons", value: "4", displayed: false)
 
 	def cmds = configurationCmds()
 	//log.debug("Sending configuration: $cmds")
