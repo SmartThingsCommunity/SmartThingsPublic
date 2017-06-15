0dea765804eb9e8dd494a72cc5dcbb502a53e754
Kevin Shuk <surfous@users.noreply.github.com>
2017-06-14 17:25:09 -0700

    Squashed commit of the following:
    New simulated devices in part, to provide for hubless/deviceless VX integration testing
    
    * Dimmer switch
      * just a simulated dimmer
    * Light bulbs
      * Dimmable bulb
        * on/off.
      * RGB bulb
        * on/off, dim, color
      * White Color Temperature Bulb
        * on/off, dim, color temperature
    	* aka Tunable White
      * RGBW Bulb
        * on/off, dim, color temperature
        * mixed mode bulb combining the RGB and White Color Temp bulbs
    * Lock (updated)
      * Multi-attribute tile
      * cause lock to "jam" at any point
      * set lock to "jam" on next operation
    * Thermostat (updated)
      * configurable modeset
      * simulated HVAC
        * rasises or lowers the simulated temperature a degree and then reschedules itself in 15 seconds until the setpoint is reached.
      * improved dual setpoint (auto mode) handling
        * heat or cool modes get full setpoint
    	  * switching to auto mode after this honors the last-set setpoint, and modifies the other setpoint if need be.
    	* Auto mode enforces a (configurable) minimum distance between heat and cool setpoints. For example:
    
    	|--------------------------- Full Setpoint Range -------------------------------|
    	|--------------------------- Heat Setpoint Range --------------------------|
    	     |---------------------- Cool Setpoint Range -------------------------------|
    	|----|
    	  \
    	   (minimum setpoint spread)
    
    	* pushing up the heating setpoint when cool is already the miniumum distance above will also raise the cooling setpoint. Similarly,
    	* lowering the cooling setpoint when heat is the minimum distance below will also
    	push the heating setpoint down

--- devicetypes/smartthings/testing/simulated-lock.src/simulated-lock.groovy
+++ devicetypes/smartthings/testing/simulated-lock.src/simulated-lock.groovy
@@ -1,54 +1,158 @@
 /**
- *  Copyright 2014 SmartThings
+ *  Copyright 2017 SmartThings
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *	  http://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
+ *  An enhanced virtual lock that allows for testing failure modes
  */
 metadata {
 	// Automatically generated. Make future change here.
-	definition (name: "Simulated Lock", namespace: "smartthings/testing", author: "bob") {
+	definition (name: "Simulated Test Lock", namespace: "smartthings/testing", author: "SmartThings") {
+    	capability "Actuator"
+		capability "sensor"
+
 		capability "Lock"
-		capability "Sensor"
-		capability "Actuator"
+		capability "Battery"
+		command "jam"
+		command "setBatteryLevel"
+		command "setJamNextOperation"
+		command "clearJamNextOperation"
+		attribute "doesNextOperationJam", "enum", ["true", "false"]
 	}
 
 	// Simulated lock
 	tiles {
-		standardTile("toggle", "device.lock", width: 2, height: 2) {
-			state "unlocked", label:'unlocked', action:"lock.lock", icon:"st.locks.lock.unlocked", backgroundColor:"#ffffff"
-			state "locked", label:'locked', action:"lock.unlock", icon:"st.locks.lock.locked", backgroundColor:"#00A0DC"
+		multiAttributeTile(name:"toggle", type: "generic", width: 6, height: 4){
+			tileAttribute ("device.lock", key: "PRIMARY_CONTROL") {
+				attributeState "locked", label:'locked', action:"lock.unlock", icon:"st.locks.lock.locked", backgroundColor:"#00A0DC", nextState:"unlocking"
+				attributeState "unlocked", label:'unlocked', action:"lock.lock", icon:"st.locks.lock.unlocked", backgroundColor:"#FFFFFF", nextState:"locking"
+				attributeState "unknown", label:"unknown", action:"lock.lock", icon:"st.samsung_sds.sub_door_status_unknown", backgroundColor:"#E86D13"
+				attributeState "locking", label:'locking', icon:"st.locks.lock.locked", backgroundColor:"#00A0DC"
+				attributeState "unlocking", label:'unlocking', icon:"st.locks.lock.unlocked", backgroundColor:"#FFFFFF"
+			}
+			tileAttribute ("device.battery", key: "SECONDARY_CONTROL") {
+				attributeState "battery", label: 'battery ${currentValue}%', unit: "%"
+			}
 		}
-		standardTile("lock", "device.lock", inactiveLabel: false, decoration: "flat") {
-			state "default", label:'lock', action:"lock.lock", icon:"st.locks.lock.locked"
+
+		standardTile("lock", "device.lock", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+			state "default", label:'lock', action:"lock.lock", icon: "st.locks.lock.locked"
 		}
-		standardTile("unlock", "device.lock", inactiveLabel: false, decoration: "flat") {
-			state "default", label:'unlock', action:"lock.unlock", icon:"st.locks.lock.unlocked"
+		standardTile("unlock", "device.lock", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+			state "default", label:'unlock', action:"lock.unlock", icon: "st.locks.lock.unlocked"
+		}
+		standardTile("jam", "device.lock", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+			state "default", label:'jam', action:"jam", icon: "st.locks.lock.unknown"
+		}
+		standardTile("blank", "device.id", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+			state "default", label:''
+		}
+		standardTile("jamToggle", "device.doesNextOperationJam", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+			state "false", label:'jam next', action:"setJamNextOperation", backgroundColor:"#DDDDDD", defaultState: true
+			state "true", label:'jam next', action:"clearJamNextOperation", backgroundColor:"#FFA81E"
+		}
+		valueTile("battery", "device.battery", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+			state "battery", label:'battery ${currentValue}%', unit:"%"
+		}
+		controlTile("batterySliderControl", "device.battery", "slider",
+					height: 2, width: 4, range:"(1..100)") {
+			state "battery", action:"setBatteryLevel"
 		}
 
 		main "toggle"
-		details(["toggle", "lock", "unlock"])
+		details(["toggle",
+			"lock", "unlock", "blank",
+			"jam", "jamToggle", "blank",
+			"battery", "batterySliderControl" ])
 	}
 }
 
-def parse(String description) {
-	log.trace "parse $description"
-	def pair = description.split(":")
-	createEvent(name: pair[0].trim(), value: pair[1].trim())
+def installed() {
+	log.trace "installed()"
+	setBatteryLevel(94)
+	unlock()
+	initialize()
+
+}
+
+def updated() {
+	log.trace "updated()"
+	processPreferences()
+	initialize()
+}
+
+def initialize() {
+	log.trace "initialize()"
+	clearJamNextOperation()
+}
+
+private processPreferences() {
+	log.debug "prefBatteryLevel: $prefBatteryLevel"
+	log.debug "prefJamNextOperation: $prefJamNextOperation"
+	log.debug "prefJamImmediately: $prefJamImmediately"
+
+	String strBatteryLevel = "$prefBatteryLevel"
+	Integer batteryLevel = strBatteryLevel.isInteger() ? strBatteryLevel.toInteger() : null
+	if (batteryLevel) {
+		setBatteryLevel(batteryLevel)
+	}
+
+	if (prefJamNextOperation) {
+		setJamNextOperation()
+	} else {
+		clearJamNextOperation()
+	}
+
+	if (prefJamImmediately) {
+		jam()
+	}
 }
 
 def lock() {
 	log.trace "lock()"
-	sendEvent(name: "lock", value: "locked")
+	if (device.currentValue("doesNextOperationJam") == "true") {
+		jam()
+	} else {
+		sendEvent(name: "lock", value: "locked")
+	}
 }
 
 def unlock() {
 	log.trace "unlock()"
-	sendEvent(name: "lock", value: "unlocked")
+	if (device.currentValue("doesNextOperationJam") == "true") {
+		jam()
+	} else {
+		sendEvent(name: "lock", value: "unlocked")
+	}
 }
+
+def jam() {
+	log.trace "jam()"
+	sendEvent(name: "lock", value: "unknown")
+	if (device.currentValue("doesNextOperationJam") == "true") {
+		clearJamNextOperation()
+	}
+}
+
+def setJamNextOperation() {
+	log.trace "setJamNextOperation() -  next lock operation will jam"
+	sendEvent(name: "doesNextOperationJam", value: "true")
+}
+
+def clearJamNextOperation() {
+	log.trace "clearJamNextOperation() -  next lock operation will NOT jam"
+	sendEvent(name: "doesNextOperationJam", value: "false")
+}
+
+def setBatteryLevel(Number lvl) {
+	log.trace "setBatteryLevel(level)"
+	sendEvent(name: "battery", value: lvl)
+}
+
