606eb6dffaf46ce5d8ce46d8ec0970bddea3ab85
ericyew <eric.ycs@gmail.com>
2017-06-18 18:20:13 +1000

    Update zen-thermostat.groovy

--- devicetypes/zenwithin/zen-thermostat.src/zen-thermostat.groovy
+++ devicetypes/zenwithin/zen-thermostat.src/zen-thermostat.groovy
@@ -36,7 +36,16 @@ metadata {
 
   tiles {
         valueTile("frontTile", "device.temperature", width: 1, height: 1) {
-            state "temperature", label:'${currentValue}°', backgroundColor:"#e8e3d8", icon: "st.Home.home1"
+            state("temperature", label:'${currentValue}°', unit:'C',
+				backgroundColors:[
+					[value: 31, color: "#153591"],
+					[value: 44, color: "#1e9cbb"],
+					[value: 59, color: "#90d2a7"],
+					[value: 74, color: "#44b621"],
+					[value: 84, color: "#f1d801"],
+					[value: 95, color: "#d04e00"],
+					[value: 96, color: "#bc2323"]
+				],  icon: "st.Home.home1")
         }
     
         valueTile("temperature", "device.temperature", width: 1, height: 1) {
