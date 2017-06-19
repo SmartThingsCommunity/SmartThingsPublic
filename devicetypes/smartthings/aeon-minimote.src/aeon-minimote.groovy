370b435874a1fa3b76541aa080f3fbb69813fefc
piyush.c <p.chomal@samsung.com>
2017-06-19 18:02:29 +0530

    [DHF-15] Health Check Aeon Minimote

--- devicetypes/smartthings/aeon-minimote.src/aeon-minimote.groovy
+++ devicetypes/smartthings/aeon-minimote.src/aeon-minimote.groovy
@@ -1,3 +1,4 @@
+import groovy.json.JsonOutput
 /**
  *  Copyright 2015 SmartThings
  *
@@ -18,6 +19,7 @@ metadata {
 		capability "Holdable Button"
 		capability "Configuration"
 		capability "Sensor"
+		capability "Health Check"
 
 		fingerprint deviceId: "0x0101", inClusters: "0x86,0x72,0x70,0x9B", outClusters: "0x26,0x2B"
 		fingerprint deviceId: "0x0101", inClusters: "0x86,0x72,0x70,0x9B,0x85,0x84", outClusters: "0x26" // old style with numbered buttons
@@ -119,5 +121,7 @@ def updated() {
 }
 
 def initialize() {
+	// Arrival sensors only goes OFFLINE when Hub is off
+	sendEvent(name: "DeviceWatch-Enroll", value: JsonOutput.toJson([protocol: "zigbee", scheme:"untracked"]), displayed: false)
 	sendEvent(name: "numberOfButtons", value: 4)
 }
