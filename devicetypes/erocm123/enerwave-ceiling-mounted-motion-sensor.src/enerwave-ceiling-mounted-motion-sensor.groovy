ecb184d7416f37685eea23f05beddac328dcdba9
Eric Maycock <erocmail@gmail.com>
2017-06-18 23:43:47 -0700

    Initial Commit

--- devicetypes/erocm123/enerwave-ceiling-mounted-motion-sensor.src/enerwave-ceiling-mounted-motion-sensor.groovy
+++ devicetypes/erocm123/enerwave-ceiling-mounted-motion-sensor.src/enerwave-ceiling-mounted-motion-sensor.groovy
@@ -1,5 +1,4 @@
 /**
- *  Copyright 2015 SmartThings
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -10,7 +9,7 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *  Generic Z-Wave Motion Sensor
+ *  Enerwave Ceiling Mounted Motion Sensor
  *
  *  Author: erocm123 (Eric Maycock)
  *  Date: 2017-06-18
