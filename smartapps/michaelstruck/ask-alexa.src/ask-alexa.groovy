771c875dca2772cf22d376fc1b3259bdbcbcf0b8
Michael Struck <oregonha@struck-llc.com>
2017-07-14 10:40:38 -0700

    Update to 2.2.9a

--- smartapps/michaelstruck/ask-alexa.src/ask-alexa.groovy
+++ smartapps/michaelstruck/ask-alexa.src/ask-alexa.groovy
@@ -1,12 +1,12 @@
 /**
  *  Ask Alexa 
  *
- *  Version 2.2.9 - 7/13/17 Copyright © 2017 Michael Struck
+ *  Version 2.2.9a - 7/13/17 Copyright © 2017 Michael Struck
  *  Special thanks for Keith DeLong for overall code and assistance; jhamstead for Ecobee climate modes, Yves Racine for My Ecobee thermostat tips
  * 
  *  Version information prior to 2.2.9 listed here: https://github.com/MichaelStruck/SmartThingsPublic/blob/master/smartapps/michaelstruck/ask-alexa.src/Ask%20Alexa%20Version%20History.md
  *
- *  Version 2.2.9 (7/13/17) Added additional advanced features to the WebCoRE macro, begin adding code to allow external items to send to the message queue, updated the brief reply option.
+ *  Version 2.2.9a (7/13/17) Added additional advanced features to the WebCoRE macro, begin adding code to allow external items to send to the message queue, updated the brief reply option.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -2727,7 +2727,7 @@ private replaceVoiceVar(msg, delay, filter, type, name, age, xParam) {
     	}
     }
     if (getWR().size()){	
-        getWR().each{2
+        getWR().each{
         	def wrName = "%" + it.label.toLowerCase().replaceAll("[^a-zA-Z0-9 ]", "") + "%"
             if (msg.contains(wrName)) msg = msg.replace(wrName,processWeatherReport(it.label.toLowerCase().replaceAll("[^a-zA-Z0-9 ]", ""))[0..-4])	
         }
@@ -3475,7 +3475,7 @@ private mqReq() { return 104 }
 private wrReq()  { return 104 }
 private vrReq()  { return 104 }
 private schReq()  { return 103 }
-private versionLong(){ return "2.2.9" }
+private versionLong(){ return "2.2.9a" }
 private versionDate(){ return "07/13/17" }
 private textCopyright() {return "Copyright © 2017 Michael Struck" }
 private textLicense() {
