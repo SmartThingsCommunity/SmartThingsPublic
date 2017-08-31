47c0b35357df8c9545b5cc594456e145c44b518b
Jamie Furtner <jamie@furtner.ca>
2017-08-30 19:19:20 -0600

    Initial version of KornerSafe Alarm device

--- devicetypes/jfurtner/kornersafe-alarm.src/kornersafe-alarm.groovy
+++ devicetypes/jfurtner/kornersafe-alarm.src/kornersafe-alarm.groovy
@@ -13,63 +13,203 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  */
-metadata {
-	definition (name: "KornerSafe Alarm", namespace: "jfurtner", author: "Jamie Furtner") {
-		capability "Contact Sensor"
-		capability "Polling"
-		capability "Relay Switch"
-	}
+include 'asynchttp_v1'
 
-	attribute 'username', 'string'
-    attribute 'password', 'string'
+metadata {
+definition (name: "KornerSafe Alarm", namespace: "jfurtner", author: "Jamie Furtner") {
+		capability "polling"
+		capability "switch"
+        capability "bridge"
+        capability "sensor"
+        capability "Contact Sensor"
+        capability "refresh"
+        
+        command "setOff"
+        command "setOn"
+        command "setOpen"
+        command "setClosed"
+        attribute "message", "string"
+        command "setAPIEndpoints", ["string", "string"]
+	}
     
+
 	simulator {		
 	}
+    
+    preferences{
+    	input("deviceIP", "string", title:"IP Address", required:true, displayDuringSetup:true)
+        input("devicePort", "number", title:"HTTP Port", required:true, displayDuringSetup:true, default:80)
+        input("devicePage", "string", title:"HTTP endpoint", required:true, displayDuringSetup:true, default:"/kornersafe.php")
+    }
 
 	tiles(scale:2) {
     	standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
-        	state 'off', label: 'Disarmed', action: 'on', icon: 'st.locks.lock.unlocked', backgroundColor: '#44ff44'
-            state 'on', label: 'Armed', action: 'off', icon: 'st.locks.lock.locked', backgroundColor: '#ff4444'
+        	state 'off', label: 'Disarmed', action: 'on', icon: 'st.locks.lock.unlocked', backgroundColor: '#44ff44',nextState:"turningOn"
+            state "turningOn", label:'Arming', icon:'st,locks.lock.locking', backgroundColor: '#D59226', nextState:'turningOff'
+            state 'on', label: 'Armed', action: 'off', icon: 'st.locks.lock.locked', backgroundColor: '#ff4444',nextState:'turningOff'
+            state "turningOff", label:'Disarming', icon:'st.locks.lock.unlocking',backgroundColor: '#D59226',nextState: 'turningOn'
         }
-        
-        valueTile('motion', 'device.motion', width:1, height:1) {
-        	state 'val', label: '${currentValue}'
+        standardTile('refresh', 'device.refresh', width:2, height:2) {
+        	state 'refresh', label: 'Refresh', action: 'refresh', icon: 'st.secondary.refresh'
+        }        
+        valueTile('contactSensor', 'device.contact', width:2, height:2){
+        	state 'open', label: 'Alarm', icon: 'st.alarm.alarm.alarm', backgroundColor: '#ff4444'
+            state 'closed', label: 'Ok', icon: ' st.alarm.alarm.alarm', backgroundColor: '#44ff44'
         }
-        
-        standardTile('refresh', 'device.refresh', width:1, height:1) {
-        	state 'refresh', label: 'Refresh', action: 'pooling.poll', icon: 'st.secondary.refresh'
+        valueTile('message', 'device.message', width:6, height:2) {
+        	state 'message', label:'${currentValue}'
         }
 	}
 }
 
-// parse events into attributes
-def parse(String description) {
-	// nothing to do here
+def updated() {
+	logDebug("Updated with settings: ${settings}")
+	setDeviceNetworkId(deviceIP, devicePort)
+	logDebug("Network ID: ${device.networkId}")
+}
+
+def setAPIEndpoints(String newAppUrl, String newToken)
+{
+	logTrace('INIT setAPIEndpoints')
+	logTrace("URL: ${appUrl}")
+    logTrace("Token: ${token}")
+	state.appUrl = newAppUrl
+    state.token = newToken
+}
+
+def parse(msg) {
+	logTrace 'INIT parse'
+	def lanMsg = parseLanMessage(msg)
+    
+    logTrace(lanMsg.json)
+    
+    return result
 }
 
 // handle commands
 def poll() {
-	log.debug "Executing 'poll'"
-	// TODO: handle 'poll' command
+	logTrace "INIT 'poll'"
+    sendMessage('Polling')
+	return hubAction('status')
 }
 
 def on() {
-	log.debug "Executing 'on'"
-	// TODO: handle 'on' command
+	logTrace "INIT 'on'"
+    sendMessage("Arming")
+	return hubAction('arm')
 }
 
 def off() {
-	log.debug "Executing 'off'"
-	// TODO: handle 'off' command
+	logTrace "INIT 'off'"
+    sendMessage("Disarming")
+	return hubAction('disarm')
 }
 
 def refresh() {
-	log.debug "Executing refresh"
+	logTrace "INIT refresh"
+    
+    return hubAction('status')
 }
 
-def login() {
-	def params = [
-    	//uri: 'https://myhome.kornersafe.com/#/account/login',
-        //body: 
-    ]
+// handle smartapp commands
+def setOff() {
+	logTrace('INIT setOff')
+    sendMessage('Disarmed')
+	setSwitch('off')
+}
+
+def setOn() {
+	logTrace('INIT setOn')
+    sendMessage('Armed')
+	setSwitch('on')
+}
+
+def setOpen()
+{
+	logTrace('INIT setOpen')
+    sendMessage('Alarm!')
+    setContact('open')
+}
+
+def setClosed()
+{
+	logTrace('INIT setClosed')
+    sendMessage('OK')
+    setContact('closed')
+}
+
+private def sendMessage(msg)
+{
+	logTrace('INIT sendMessage')
+	def dt = new Date()
+    def completeMessage = "$dt $msg"
+    logTrace("Message: $completeMessage")
+	sendEvent(createEvent(name:"message", value:completeMessage))
+}
+
+
+private def setContact(String openClosed)
+{
+	logTrace('INIT getContactEvent')
+	sendEvent(createEvent(name: "contact", value: openClosed))
+}
+
+private def setSwitch(String onOff)
+{
+	logTrace('INIT setSwitch')
+	sendEvent(createEvent(name: "switch", value: onOff))
+}
+
+def hubAction(String action) {
+	logTrace('INIT hubAction')
+    setDeviceNetworkId(deviceIP, devicePort)
+    
+    def jsonBody = new groovy.json.JsonBuilder([
+    	"appUrl":"${state.appUrl}",
+        "appToken":"${state.token}"
+        ])
+    
+	def hubAction = new physicalgraph.device.HubAction(
+    	method: "POST",
+        path: devicePage + "?" + action,
+        headers: [
+        	HOST: "$deviceIP:$devicePort",
+            'Content-Type':'application/json'
+        ],
+        body: jsonBody
+    )
+    logTrace jsonBody
+    logTrace hubAction
+    return hubAction
+}
+
+def logDebug(msg)
+{
+	log.debug msg
+}
+
+def logTrace(msg)
+{
+	log.trace msg
+}
+
+private setDeviceNetworkId(ip,port){
+	logTrace('INIT setDeviceNetworkId')
+    logTrace("IP: $ip port:$port")
+  	def iphex = convertIPtoHex(ip)
+  	def porthex = convertPortToHex(port)
+  	device.deviceNetworkId = "$iphex:$porthex"
+  	logDebug "Device Network Id set to ${iphex}:${porthex}"
+}
+
+private String convertIPtoHex(ipAddress) { 
+    String hex = ipAddress.tokenize( '.' ).collect {  String.format( '%02x', it.toInteger() ) }.join()
+    //logTrace "IP address entered is $ipAddress and the converted hex code is $hex"
+    return hex
+}
+
+private String convertPortToHex(port) {
+	String hexport = port.toString().format( '%04x', port.toInteger() )
+    //logTrace "Port entered is $port and the converted hex code is $hexport"
+    return hexport
 }
