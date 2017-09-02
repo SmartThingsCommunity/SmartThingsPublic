dba34a0828e677dab4e4d1ebeb8722039a3e7ba3
Donald Kirker <donald@smartthings.com>
2017-09-02 01:24:51 -0700

    ICP-1413 Get initial state of dimmer switches after pairing

--- devicetypes/smartthings/dimmer-switch.src/dimmer-switch.groovy
+++ devicetypes/smartthings/dimmer-switch.src/dimmer-switch.groovy
@@ -87,6 +87,8 @@ metadata {
 def installed() {
 	// Device-Watch simply pings if no device events received for 32min(checkInterval)
 	sendEvent(name: "checkInterval", value: 2 * 15 * 60 + 2 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID])
+
+	response(refresh())
 }
 
 def updated(){
