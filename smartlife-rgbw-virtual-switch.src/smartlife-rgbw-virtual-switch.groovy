7a0d70c2df07e6ae8f9a5fe9ed5272ad6d3b1d63
morganfreemon <brunejh@mail.uc.edu>
2018-01-05 00:26:11 -0500

    Add files via upload

---
+++ smartlife-rgbw-virtual-switch.src/smartlife-rgbw-virtual-switch.groovy
@@ -0,0 +1,75 @@
+/**
+ *  Copyright 2015 SmartThings
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	
+    definition (name: "SmartLife RGBW Virtual Switch", namespace: "erocm123", author: "Eric Maycock") {
+		capability "Switch"
+        capability "Relay Switch"
+        capability "Actuator"
+
+		command "onPhysical"
+		command "offPhysical"
+	}
+
+	tiles(scale: 2) {
+		multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true){
+			tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
+				attributeState "off", label: '${name}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState:"turningOn"
+                attributeState "on", label: '${name}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#00a0dc", nextState:"turningOff"
+				attributeState "turningOff", label:'${name}', action:"switch.on", icon:"st.switches.switch.off", backgroundColor:"#ffffff", nextState:"turningOn"
+                attributeState "turningOn", label:'${name}', action:"switch.off", icon:"st.switches.switch.on", backgroundColor:"#00a0dc", nextState:"turningOff"
+			}
+		}
+        main "switch"
+		details(["switch"])
+	}
+}
+
+def parse(String description) {
+	//def pair = description.split(":")
+	//createEvent(name: pair[0].trim(), value: pair[1].trim())
+}
+
+def parse(Map description) {
+	//def pair = description.split(":")
+	//createEvent(name: pair[0].trim(), value: pair[1].trim())
+    def eventMap
+    if (description.type == null) eventMap = [name:"$description.name", value:"$description.value"]
+    else eventMap = [name:"$description.name", value:"$description.value", type:"$description.type"]
+    createEvent(eventMap)
+}
+
+def on() {
+	log.debug "$version on()"
+	sendEvent(name: "switch", value: "on")
+}
+
+def off() {
+	log.debug "$version off()"
+	sendEvent(name: "switch", value: "off")
+}
+
+def onPhysical() {
+	log.debug "$version onPhysical()"
+	sendEvent(name: "switch", value: "on", type: "physical")
+}
+
+def offPhysical() {
+	log.debug "$version offPhysical()"
+	sendEvent(name: "switch", value: "off", type: "physical")
+}
+
+private getVersion() {
+	"PUBLISHED"
+}
