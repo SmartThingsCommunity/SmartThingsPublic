41f2551f8137d828ca35e37557d786d03e71b120
Robert Thompson <thompson_consulting@msn.com>
2018-02-01 12:42:29 -0800

    MSA-2598: null

---
+++ devicetypes/living-room-light/living-room-light.src/living-room-light.groovy
@@ -0,0 +1,50 @@
+/**
+ *  Living room light
+ *
+ *  Copyright 2018 Robert Thompson
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "Living room light", namespace: "Living Room light", author: "Robert Thompson") {
+		capability "Light"
+
+		attribute "Turn on living room light", "string"
+	}
+
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'switch' attribute
+	// TODO: handle 'Turn on living room light' attribute
+
+}
+
+// handle commands
+def off() {
+	log.debug "Executing 'off'"
+	// TODO: handle 'off' command
+}
+
+def on() {
+	log.debug "Executing 'on'"
+	// TODO: handle 'on' command
+}
