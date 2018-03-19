9852ac3667e0cb7d4f4fd10ec47b576fdff00fe3
greens <steven.green@smartthings.com>
2018-03-19 15:12:21 -0700

    GoControl Integrations

--- devicetypes/smartthings/zwave-switch-generic.src/zwave-switch-generic.groovy
+++ devicetypes/smartthings/zwave-switch-generic.src/zwave-switch-generic.groovy
@@ -40,7 +40,9 @@ metadata {
 		fingerprint mfr: "001A", prod: "5244", deviceJoinName: "Eaton RF Receptacle"
 		fingerprint mfr: "001A", prod: "534C", model: "0000", deviceJoinName: "Eaton RF Master Switch"
 		fingerprint mfr: "001A", prod: "5354", model: "0003", deviceJoinName: "Eaton RF Appliance Plug-In Module"
-    		fingerprint mfr: "001A", prod: "5352", model: "0000", deviceJoinName: "Eaton RF Accessory Switch"
+		fingerprint mfr: "001A", prod: "5352", model: "0000", deviceJoinName: "Eaton RF Accessory Switch"
+		fingerprint mfr: "014F", prod: "5753", model: "3535", deviceJoinName: "GoControl Smart In-Wall Switch"
+		fingerprint mfr: "014F", prod: "5257", model: "3033", deviceJoinName: "GoControl Wall Relay Switch"
 	}
 
 	// simulator metadata
