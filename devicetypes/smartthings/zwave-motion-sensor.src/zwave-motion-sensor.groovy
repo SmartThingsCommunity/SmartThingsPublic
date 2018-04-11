a08b828791618b283c2c6dbe04e2aeda23190e6a
greens <steven.green@smartthings.com>
2018-04-11 14:43:23 -0700

    ICP-4607 Add initial polling to Z-Wave motion sensor

--- devicetypes/smartthings/zwave-motion-sensor.src/zwave-motion-sensor.groovy
+++ devicetypes/smartthings/zwave-motion-sensor.src/zwave-motion-sensor.groovy
@@ -56,6 +56,7 @@ metadata {
 def installed() {
 // Device wakes up every 4 hours, this interval allows us to miss one wakeup notification before marking offline
 	sendEvent(name: "checkInterval", value: 8 * 60 * 60 + 2 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID])
+	response(initialPoll())
 }
 
 def updated() {
@@ -245,4 +246,32 @@ def zwaveEvent(physicalgraph.zwave.commands.manufacturerspecificv2.ManufacturerS
 
 	result << createEvent(descriptionText: "$device.displayName MSR: $msr", isStateChange: false)
 	result
+}
+
+def initialPoll() {
+	def request = []
+	request << zwave.batteryV1.batteryGet()
+	request << zwave.sensorBinaryV2.sensorBinaryGet(sensorType: 0x0C) //motion
+	request << zwave.notificationV3.notificationGet(notificationType: 0x07) //home security
+	request << zwave.sensorMultilevelV5.sensorMultilevelGet(sensorType: 0x01) //temperature
+	request << zwave.sensorMultilevelV5.sensorMultilevelGet(sensorType: 0x03) //illuminance
+	request << zwave.sensorMultilevelV5.sensorMultilevelGet(sensorType: 0x05) //humidity
+	request << zwave.sensorMultilevelV5.sensorMultilevelGet(sensorType: 0x1E) //loudness
+	commands(request) + ["delay 20000", zwave.wakeUpV1.wakeUpNoMoreInformation().format()]
+}
+
+private commands(commands, delay=200) {
+	log.info "sending commands: ${commands}"
+	delayBetween(commands.collect{ command(it) }, delay)
+}
+
+private command(physicalgraph.zwave.Command cmd) {
+	if (zwaveInfo && zwaveInfo.zw?.contains("s")) {
+		zwave.securityV1.securityMessageEncapsulation().encapsulate(cmd).format()
+	} else if (zwaveInfo && zwaveInfo.cc?.contains("56")){
+		zwave.crc16EncapV1.crc16Encap().encapsulate(cmd).format()
+	} else {
+		logging("${device.displayName} - no encapsulation supported for command: $cmd","debug")
+		cmd.format()
+	}
 }
