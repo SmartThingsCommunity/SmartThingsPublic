95e057960405a4d183a2d9c6540edfec53c147c9
sagar amin <sagar@trility.io>
2018-04-17 07:13:25 -0500

    remove poll and fingerprint.

--- devicetypes/smartthings/zwave-metering-switch.src/zwave-metering-switch.groovy
+++ devicetypes/smartthings/zwave-metering-switch.src/zwave-metering-switch.groovy
@@ -27,15 +27,14 @@ metadata {
         command "reset"
 
         fingerprint inClusters: "0x25,0x32"
-		fingerprint mfr: "0086", prod: "0003", model: "0012", deviceJoinName: "Aeotec Micro Smart Switch"
-		fingerprint mfr: "021F", prod: "0003", model: "0087", deviceJoinName: "Dome On/Off Plug-in Switch"
-		fingerprint mfr: "0086", prod: "0103", model: "0060", deviceJoinName: "Aeotec Smart Switch 6"
-    fingerprint mfr: "0086", prod: "0103", model: "0074", deviceJoinName: "Aeon Labs Nano Switch"
-		fingerprint mfr: "0086", prod: "0003", model: "0074", deviceJoinName: "Aeon Labs Nano Switch"
-		fingerprint mfr: "014F", prod: "574F", model: "3535", deviceJoinName: "GoControl Wall-Mounted Outlet"
-		fingerprint mfr: "014F", prod: "5053", model: "3531", deviceJoinName: "GoControl Plug-in Switch"
-		fingerprint mfr: "0063", prod: "4F44", model: "3031", deviceJoinName: "GE Direct-Wire Outdoor Switch"
-}
+		    fingerprint mfr: "0086", prod: "0003", model: "0012", deviceJoinName: "Aeotec Micro Smart Switch"
+		    fingerprint mfr: "021F", prod: "0003", model: "0087", deviceJoinName: "Dome On/Off Plug-in Switch"
+		    fingerprint mfr: "0086", prod: "0103", model: "0060", deviceJoinName: "Aeotec Smart Switch 6"
+        fingerprint mfr: "0086", prod: "0103", model: "0074", deviceJoinName: "Aeon Labs Nano Switch"
+		    fingerprint mfr: "014F", prod: "574F", model: "3535", deviceJoinName: "GoControl Wall-Mounted Outlet"
+		    fingerprint mfr: "014F", prod: "5053", model: "3531", deviceJoinName: "GoControl Plug-in Switch"
+		    fingerprint mfr: "0063", prod: "4F44", model: "3031", deviceJoinName: "GE Direct-Wire Outdoor Switch"
+    }
 
     // simulator metadata
     simulator {
@@ -194,11 +193,6 @@ def off() {
         zwave.switchBinaryV1.switchBinaryGet(),
         meterGet(scale: 2)
     ], 3000)
-}
-
-def poll() {
-    log.debug "poll()"
-    refresh()
 }
 
 /**
