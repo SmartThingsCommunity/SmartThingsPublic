aa02117f3cc12b66a473eaa0d3b6417c32ef05c5
greens <steven.green@smartthings.com>
2018-04-23 10:45:18 -0700

    ICP-4756 Add initial polling to GE Switches

--- devicetypes/smartthings/zwave-switch.src/zwave-switch.groovy
+++ devicetypes/smartthings/zwave-switch.src/zwave-switch.groovy
@@ -70,9 +70,9 @@ def installed() {
 }
 
 def updated(){
-		// Device-Watch simply pings if no device events received for 32min(checkInterval)
-		sendEvent(name: "checkInterval", value: 2 * 15 * 60 + 2 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
-  switch (ledIndicator) {
+	// Device-Watch simply pings if no device events received for 32min(checkInterval)
+	sendEvent(name: "checkInterval", value: 2 * 15 * 60 + 2 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
+	switch (ledIndicator) {
         case "on":
             indicatorWhenOn()
             break
@@ -86,7 +86,7 @@ def updated(){
             indicatorWhenOn()
             break
     }
-    sendHubCommand(new physicalgraph.device.HubAction(zwave.manufacturerSpecificV1.manufacturerSpecificGet().format()))
+	response(refresh())
 }
 
 def getCommandClassVersions() {
