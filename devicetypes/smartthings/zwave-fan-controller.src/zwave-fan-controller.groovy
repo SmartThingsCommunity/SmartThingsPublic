57d1a5d5c4049a383097c5bacb1681e8a7a73189
greens <steven.green@smartthings.com>
2018-05-25 14:58:36 -0700

    Fan Controller DTH
    * fix indentation

--- devicetypes/smartthings/zwave-fan-controller.src/zwave-fan-controller.groovy
+++ devicetypes/smartthings/zwave-fan-controller.src/zwave-fan-controller.groovy
@@ -98,8 +98,8 @@ def zwaveEvent(physicalgraph.zwave.commands.switchmultilevelv1.SwitchMultilevelS
 def fanEvents(physicalgraph.zwave.Command cmd) {
 	def value = (cmd.value ? "on" : "off")
 	def result = [createEvent(name: "switch", value: value)]
-    result << createEvent(name: "level", value: cmd.value == 99 ? 100 : cmd.value)
-    result << createEvent(name: "fanSpeed", value: cmd.value/33)
+	result << createEvent(name: "level", value: cmd.value == 99 ? 100 : cmd.value)
+	result << createEvent(name: "fanSpeed", value: cmd.value/33)
 	return result
 }
 
