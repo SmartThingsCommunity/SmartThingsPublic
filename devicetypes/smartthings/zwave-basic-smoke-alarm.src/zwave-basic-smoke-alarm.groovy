3469261dd922d995bc3c8b93b87f27399e4ac8f8
Donald Kirker <donald@smartthings.com>
2018-06-04 20:48:45 -0700

    Revert "ONEAPP-3931 Update siren ocf icon (#3114)"
    This reverts commit ce5b075615f09be9494eeea95734572d3b85a24d.

--- devicetypes/smartthings/zwave-basic-smoke-alarm.src/zwave-basic-smoke-alarm.groovy
+++ devicetypes/smartthings/zwave-basic-smoke-alarm.src/zwave-basic-smoke-alarm.groovy
@@ -12,7 +12,7 @@
  *
  */
 metadata {
-	definition (name: "Z-Wave Basic Smoke Alarm", namespace: "smartthings", author: "SmartThings", ocfDeviceType: "x.com.st.d.sensor.smoke") {
+	definition (name: "Z-Wave Basic Smoke Alarm", namespace: "smartthings", author: "SmartThings") {
 		capability "Smoke Detector"
 		capability "Sensor"
 		capability "Battery"
