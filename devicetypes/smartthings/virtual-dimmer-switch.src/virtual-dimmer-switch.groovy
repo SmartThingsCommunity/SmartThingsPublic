f293f5a13baaff11787915e911060863a782a5d8
Vinay Rao <vinay@smartthings.com>
2018-06-29 00:53:44 -0700

    Adding generic ui-metadata to simulated switch and dimmers (#3204)

--- devicetypes/smartthings/virtual-dimmer-switch.src/virtual-dimmer-switch.groovy
+++ devicetypes/smartthings/virtual-dimmer-switch.src/virtual-dimmer-switch.groovy
@@ -14,7 +14,7 @@
  *
  */
 metadata {
-    definition (name: "Virtual Dimmer Switch", namespace: "smartthings", author: "SmartThings", runLocally: true, minHubCoreVersion: '000.021.00001', executeCommandsLocally: true) {
+    definition (name: "Virtual Dimmer Switch", namespace: "smartthings", author: "SmartThings", runLocally: true, minHubCoreVersion: '000.021.00001', executeCommandsLocally: true, mnmn: "SmartThings", vid: "generic-dimmer") {
         capability "Actuator"
         capability "Sensor"
         capability "Switch"
