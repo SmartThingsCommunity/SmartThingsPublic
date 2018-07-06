16f51e51bba3e8f4d6babcd3e644968dd1b26228
surfous <surfous@users.noreply.github.com>
2018-07-06 10:59:44 -0700

    Change indent from tabs to spaces
    Whitespace changes only.

--- devicetypes/smartthings/testing/simulated-temperature-sensor.src/simulated-temperature-sensor.groovy
+++ devicetypes/smartthings/testing/simulated-temperature-sensor.src/simulated-temperature-sensor.groovy
@@ -12,85 +12,85 @@
  *
  */
 metadata {
-	// Automatically generated. Make future change here.
-	definition (name: "Simulated Temperature Sensor", namespace: "smartthings/testing", author: "SmartThings") {
-		capability "Temperature Measurement"
-		capability "Switch Level"
-		capability "Sensor"
-		capability "Health Check"
+    // Automatically generated. Make future change here.
+    definition (name: "Simulated Temperature Sensor", namespace: "smartthings/testing", author: "SmartThings") {
+        capability "Temperature Measurement"
+        capability "Switch Level"
+        capability "Sensor"
+        capability "Health Check"
 
-		command "up"
-		command "down"
-		command "setTemperature", ["number"]
-	}
+        command "up"
+        command "down"
+        command "setTemperature", ["number"]
+    }
 
-	// UI tile definitions
-	tiles {
-		valueTile("temperature", "device.temperature", width: 2, height: 2) {
-			state("temperature", label:'${currentValue}', unit:"F",
-				backgroundColors:[
-					[value: 31, color: "#153591"],
-					[value: 44, color: "#1e9cbb"],
-					[value: 59, color: "#90d2a7"],
-					[value: 74, color: "#44b621"],
-					[value: 84, color: "#f1d801"],
-					[value: 95, color: "#d04e00"],
-					[value: 96, color: "#bc2323"]
-				]
-			)
-		}
-		standardTile("up", "device.temperature", inactiveLabel: false, decoration: "flat") {
-			state "default", label:'up', action:"up"
-		}
-		standardTile("down", "device.temperature", inactiveLabel: false, decoration: "flat") {
-			state "default", label:'down', action:"down"
-		}
-		main "temperature"
-		details("temperature","up","down")
-	}
+    // UI tile definitions
+    tiles {
+        valueTile("temperature", "device.temperature", width: 2, height: 2) {
+            state("temperature", label:'${currentValue}', unit:"F",
+                backgroundColors:[
+                    [value: 31, color: "#153591"],
+                    [value: 44, color: "#1e9cbb"],
+                    [value: 59, color: "#90d2a7"],
+                    [value: 74, color: "#44b621"],
+                    [value: 84, color: "#f1d801"],
+                    [value: 95, color: "#d04e00"],
+                    [value: 96, color: "#bc2323"]
+                ]
+            )
+        }
+        standardTile("up", "device.temperature", inactiveLabel: false, decoration: "flat") {
+            state "default", label:'up', action:"up"
+        }
+        standardTile("down", "device.temperature", inactiveLabel: false, decoration: "flat") {
+            state "default", label:'down', action:"down"
+        }
+        main "temperature"
+        details("temperature","up","down")
+    }
 }
 
 // Parse incoming device messages to generate events
 def parse(String description) {
-	def pair = description.split(":")
-	createEvent(name: pair[0].trim(), value: pair[1].trim(), unit:"F")
+    def pair = description.split(":")
+    createEvent(name: pair[0].trim(), value: pair[1].trim(), unit:"F")
 }
 
 def installed() {
-	initialize()
+    initialize()
 }
 
 def updated() {
-	initialize()
+    initialize()
 }
 
 def initialize() {
-	sendEvent(name: "DeviceWatch-DeviceStatus", value: "online")
-	sendEvent(name: "healthStatus", value: "online")
-	sendEvent(name: "DeviceWatch-Enroll", value: [protocol: "cloud", scheme:"untracked"].encodeAsJson(), displayed: false)
-	if (!device.currentState("temperature")) {
-		setTemperature(getTemperature())
-	}
+    sendEvent(name: "DeviceWatch-DeviceStatus", value: "online")
+    sendEvent(name: "healthStatus", value: "online")
+    sendEvent(name: "DeviceWatch-Enroll", value: [protocol: "cloud", scheme:"untracked"].encodeAsJson(), displayed: false)
+    if (!device.currentState("temperature")) {
+        setTemperature(getTemperature())
+    }
 }
 
 def setLevel(value) {
-	setTemperature(value)
+    setTemperature(value)
 }
 
 def up() {
-	setTemperature(getTemperature() + 1)
+    setTemperature(getTemperature() + 1)
 }
 
 def down() {
-	setTemperature(getTemperature() - 1)
+    setTemperature(getTemperature() - 1)
 }
 
 def setTemperature(value) {
-	sendEvent(name:"temperature", value: value)
+    sendEvent(name:"temperature", value: value)
 }
 
 private getTemperature() {
-	def ts = device.currentState("temperature")
-	Integer value = ts ? ts.integerValue : 72
-	return value
+    def ts = device.currentState("temperature")
+    Integer value = ts ? ts.integerValue : 72
+    return value
 }
