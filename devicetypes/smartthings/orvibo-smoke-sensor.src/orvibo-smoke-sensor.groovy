14bc904fc86ae0d063166b8f0e01114f3c5b3222
Fen Mei <f.mei@samsung.com>
2018-07-13 14:08:05 +0800

    change config report of battery

--- devicetypes/smartthings/orvibo-smoke-sensor.src/orvibo-smoke-sensor.groovy
+++ devicetypes/smartthings/orvibo-smoke-sensor.src/orvibo-smoke-sensor.groovy
@@ -157,5 +157,5 @@ def configure() {
 	log.debug "configure"
 	sendEvent(name: "checkInterval", value:6 * 60 * 60 + 2 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
 
-	return zigbee.batteryConfig()+ refresh()
+	return zigbee.configureReporting(zigbee.POWER_CONFIGURATION_CLUSTER, 0x0021, DataType.UINT8, 30, 21600, 0x10) + refresh()
 }
