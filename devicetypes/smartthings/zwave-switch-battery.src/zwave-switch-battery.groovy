93f98c26ce529b30af77b28ce8a3e865fe97121e
Diana Czarnecka <d.czarnecka@samsung.com>
2018-07-26 14:10:52 +0200

    Updates DTH for ECOLINK switches

--- devicetypes/smartthings/zwave-switch-battery.src/zwave-switch-battery.groovy
+++ devicetypes/smartthings/zwave-switch-battery.src/zwave-switch-battery.groovy
@@ -56,7 +56,7 @@ def installed() {
 }
 
 def initialize() {
-	sendEvent(name: "checkInterval", value: 2 * 15 * 60 + 2 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
+	sendEvent(name: "checkInterval", value: 2 * 60 * 60 + 2 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
 	response(refresh())
 }
 
