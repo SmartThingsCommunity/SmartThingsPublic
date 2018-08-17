f315db1e674351738d7bce5a3b74d1fd713d832b
Donald Kirker <donald@smartthings.com>
2018-08-17 04:12:11 -0700

    WWST-1019 Add IKEA TRADFRI RGB bulbs

--- devicetypes/smartthings/zll-rgb-bulb.src/zll-rgb-bulb.groovy
+++ devicetypes/smartthings/zll-rgb-bulb.src/zll-rgb-bulb.groovy
@@ -14,7 +14,7 @@
 import physicalgraph.zigbee.zcl.DataType
 
 metadata {
-	definition (name: "ZLL RGB Bulb", namespace: "smartthings", author: "SmartThings", ocfDeviceType: "oic.d.light", runLocally: true, minHubCoreVersion: '000.021.00001', executeCommandsLocally: true) {
+	definition (name: "ZLL RGB Bulb", namespace: "smartthings", author: "SmartThings", ocfDeviceType: "oic.d.light", runLocally: false, minHubCoreVersion: '000.021.00001', executeCommandsLocally: true) {
 
 		capability "Actuator"
 		capability "Color Control"
@@ -24,6 +24,9 @@ metadata {
 		capability "Switch"
 		capability "Switch Level"
 		capability "Health Check"
+
+		fingerprint profileId: "C05E", inClusters: "0000, 0003, 0004, 0005, 0006, 0008, 0300, 0B05, 1000", outClusters: "0005, 0019, 0020, 1000", manufacturer: "IKEA of Sweden", model: "TRADFRI bulb E27 CWS opal 600lm", deviceJoinName: "TRADFRI bulb E27 CWS opal 600lm"
+		fingerprint profileId: "C05E", inClusters: "0000, 0003, 0004, 0005, 0006, 0008, 0300, 0B05, 1000", outClusters: "0005, 0019, 0020, 1000", manufacturer: "IKEA of Sweden", model: "TRADFRI bulb E26 CWS opal 600lm", deviceJoinName: "TRADFRI bulb E26 CWS opal 600lm"
 	}
 
 	// UI tile definitions
@@ -54,10 +57,32 @@ metadata {
 //Globals
 private getATTRIBUTE_HUE() { 0x0000 }
 private getATTRIBUTE_SATURATION() { 0x0001 }
+private getATTRIBUTE_X() { 0x0003 }
+private getATTRIBUTE_Y() { 0x0004 }
+private getATTRIBUTE_COLOR_CAPABILITIES() { 0x400A }
 private getHUE_COMMAND() { 0x00 }
 private getSATURATION_COMMAND() { 0x03 }
 private getMOVE_TO_HUE_AND_SATURATION_COMMAND() { 0x06 }
+private getMOVE_TO_COLOR_COMMAND() { 0x07 }
 private getCOLOR_CONTROL_CLUSTER() { 0x0300 }
+
+/**
+ * Check if this device can support Hue and Saturation
+ *
+ * Right now this is a manufacturer based check. IKEA only supports CIE xyY
+ */
+private shouldUseHueSaturation() {
+	return device.getDataValue("manufacturer") != "IKEA of Sweden"
+}
+
+/**
+ * `state` object for devices using xyY color:
+ *
+ * currentX - current attribute x
+ * currentY - current attribute y
+ * 
+ *  
+ */
 
 // Parse incoming device messages to generate events
 def parse(String description) {
@@ -73,13 +98,32 @@ def parse(String description) {
 		log.trace "zigbeeMap : $zigbeeMap"
 
 		if (zigbeeMap?.clusterInt == COLOR_CONTROL_CLUSTER) {
-			if(zigbeeMap.attrInt == ATTRIBUTE_HUE){  //Hue Attribute
+			if(zigbeeMap.attrInt == ATTRIBUTE_HUE && shouldUseHueSaturation()){  //Hue Attribute
 				def hueValue = Math.round(zigbee.convertHexToInt(zigbeeMap.value) / 0xfe * 100)
 				sendEvent(name: "hue", value: hueValue, displayed:false)
 			}
-			else if(zigbeeMap.attrInt == ATTRIBUTE_SATURATION){ //Saturation Attribute
+			else if(zigbeeMap.attrInt == ATTRIBUTE_SATURATION && shouldUseHueSaturation()) { //Saturation Attribute
 				def saturationValue = Math.round(zigbee.convertHexToInt(zigbeeMap.value) / 0xfe * 100)
 				sendEvent(name: "saturation", value: saturationValue, displayed:false)
+			}
+			else if(zigbeeMap.attrInt == ATTRIBUTE_X) { //X Attribute
+				def xValue = zigbee.convertHexToInt(zigbeeMap.value)
+				log.debug "xValue = $xValue"
+
+				state.currentX = xValue
+			}
+			else if(zigbeeMap.attrInt == ATTRIBUTE_Y) { //Y Attribute
+				def yValue = zigbee.convertHexToInt(zigbeeMap.value)
+				log.debug "yValue = $yValue"
+
+				state.currentY = yValue
+			}
+
+			if (!shouldUseHueSaturation() && state.currentX && state.currentY) {
+				def hsv = safeColorXy2Hsv(state.currentX, state.currentY)
+                log.debug "x: ${state.currentX} y: ${state.currentY} hue: ${hsv.hue} saturation: ${hsv.saturation}"
+				sendEvent(name: "hue", value: hsv.hue, displayed:false)
+				sendEvent(name: "saturation", value: hsv.saturation, displayed:false)
 			}
 		}
 		else {
@@ -131,15 +175,33 @@ def configureHealthCheck() {
 }
 
 def configureAttributes() {
-	zigbee.onOffConfig() +
-	zigbee.levelConfig()
+	def commands = zigbee.onOffConfig() +
+		zigbee.levelConfig()
+
+	if (shouldUseHueSaturation()) {
+		commands += zigbee.configureReporting(COLOR_CONTROL_CLUSTER, ATTRIBUTE_HUE, DataType.UINT16, 1, 3600, 0x10)
+		commands += zigbee.configureReporting(COLOR_CONTROL_CLUSTER, ATTRIBUTE_SATURATION, DataType.UINT16, 1, 3600, 0x10)
+	} else {
+		commands += zigbee.configureReporting(COLOR_CONTROL_CLUSTER, ATTRIBUTE_X, DataType.UINT16, 1, 3600, 0x10)
+		commands += zigbee.configureReporting(COLOR_CONTROL_CLUSTER, ATTRIBUTE_Y, DataType.UINT16, 1, 3600, 0x10)
+	}
+
+	commands
 }
 
 def refreshAttributes() {
-	zigbee.onOffRefresh() +
-	zigbee.levelRefresh() +
-	zigbee.readAttribute(COLOR_CONTROL_CLUSTER, ATTRIBUTE_HUE) +
-	zigbee.readAttribute(COLOR_CONTROL_CLUSTER, ATTRIBUTE_SATURATION)
+	def commands = zigbee.onOffRefresh() + zigbee.levelRefresh()
+
+	if (shouldUseHueSaturation()) {
+		commands += zigbee.readAttribute(COLOR_CONTROL_CLUSTER, ATTRIBUTE_HUE)
+		commands += zigbee.readAttribute(COLOR_CONTROL_CLUSTER, ATTRIBUTE_SATURATION)
+	} else {
+		commands += zigbee.readAttribute(COLOR_CONTROL_CLUSTER, ATTRIBUTE_X)
+		commands += zigbee.readAttribute(COLOR_CONTROL_CLUSTER, ATTRIBUTE_Y)
+	}
+
+	log.debug "Refreshing $commands"
+	commands
 }
 
 def updated() {
@@ -164,23 +226,259 @@ private getScaledSaturation(value) {
 	zigbee.convertToHexString(Math.round(value * 0xfe / 100.0), 2)
 }
 
-def setColor(value){
+def setColor(value) {
 	log.trace "setColor($value)"
-	zigbee.on() +
-	zigbee.command(COLOR_CONTROL_CLUSTER, MOVE_TO_HUE_AND_SATURATION_COMMAND,
-		getScaledHue(value.hue), getScaledSaturation(value.saturation), "0000") +
-	zigbee.readAttribute(COLOR_CONTROL_CLUSTER, ATTRIBUTE_HUE) +
-	zigbee.readAttribute(COLOR_CONTROL_CLUSTER, ATTRIBUTE_SATURATION)
+	def commands = zigbee.on()
+
+	if (shouldUseHueSaturation()) {
+		commands += zigbee.command(COLOR_CONTROL_CLUSTER, MOVE_TO_HUE_AND_SATURATION_COMMAND,
+			getScaledHue(value.hue), getScaledSaturation(value.saturation), "0000")
+		commands += zigbee.readAttribute(COLOR_CONTROL_CLUSTER, ATTRIBUTE_HUE)
+		commands += zigbee.readAttribute(COLOR_CONTROL_CLUSTER, ATTRIBUTE_SATURATION)
+	} else {
+		def xy = safeColorHsv2Xy(value.hue, value.saturation)
+
+		log.debug "setColor: xy ($xy.x, $xy.y)"
+
+		commands += zigbee.command(COLOR_CONTROL_CLUSTER, MOVE_TO_COLOR_COMMAND,
+			DataType.pack(xy.x, DataType.UINT16, 1), DataType.pack(xy.y, DataType.UINT16, 1), "0000")
+		commands += zigbee.readAttribute(COLOR_CONTROL_CLUSTER, ATTRIBUTE_X)
+		commands += zigbee.readAttribute(COLOR_CONTROL_CLUSTER, ATTRIBUTE_Y)
+	}
+
+	commands
 }
 
 def setHue(value) {
-	//payload-> hue value, direction (00-> shortest distance), transition time (1/10th second)
-	zigbee.command(COLOR_CONTROL_CLUSTER, HUE_COMMAND, getScaledHue(value), "00", "0000") +
-	zigbee.readAttribute(COLOR_CONTROL_CLUSTER, ATTRIBUTE_HUE)
+	if (shouldUseHueSaturation()) {
+		//payload-> hue value, direction (00-> shortest distance), transition time (1/10th second)
+		zigbee.command(COLOR_CONTROL_CLUSTER, HUE_COMMAND, getScaledHue(value), "00", "0000") +
+		zigbee.readAttribute(COLOR_CONTROL_CLUSTER, ATTRIBUTE_HUE)
+	} else {
+		setColor([hue: value, saturation: device.currentValue("saturation")])
+	}
 }
 
 def setSaturation(value) {
-	//payload-> sat value, transition time
-	zigbee.command(COLOR_CONTROL_CLUSTER, SATURATION_COMMAND, getScaledSaturation(value), "0000") +
-	zigbee.readAttribute(COLOR_CONTROL_CLUSTER, ATTRIBUTE_SATURATION)
+	if (shouldUseHueSaturation()) {
+		//payload-> sat value, transition time
+		zigbee.command(COLOR_CONTROL_CLUSTER, SATURATION_COMMAND, getScaledSaturation(value), "0000") +
+		zigbee.readAttribute(COLOR_CONTROL_CLUSTER, ATTRIBUTE_SATURATION)
+	} else {
+		setColor([hue: device.currentValue("hue"), saturation: value])
+	}
 }
+
+/**
+ * Below code from https://github.com/puzzle-star/SmartThings-IKEA-Tradfri-RGB/blob/master/ikea-tradfri-rgb.groovy
+ *  Copyright 2017 Pedro Garcia
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ */
+
+private min(first, ... rest) {
+	def min = first;
+	for (next in rest) {
+		if (next < min) {
+			min = next
+		}
+	}
+
+	min
+}
+
+private max(first, ... rest) {
+	def max = first;
+	for (next in rest) {
+		if (next > max) {
+			max = next
+		}
+	}
+
+	max
+}
+
+private colorGammaAdjust(component) {
+  return (component > 0.04045) ? Math.pow((component + 0.055) / (1.0 + 0.055), 2.4) : (component / 12.92)
+}
+
+private colorGammaRevert(component) {
+  return (component <= 0.0031308) ? 12.92 * component : (1.0 + 0.055) * Math.pow(component, (1.0 / 2.4)) - 0.055
+}
+
+private colorXy2Rgb(x, y) {
+	log.debug "< Color xy: ($x, $y)"
+
+	def Y = 1
+	def X = (Y / y) * x
+	def Z = (Y / y) * (1.0 - x - y)
+
+	log.debug "< Color XYZ: ($X, $Y, $Z)"
+
+	// sRGB, Reference White D65
+	def M = [
+		[  3.2410032, -1.5373990, -0.4986159 ],
+		[ -0.9692243,  1.8759300,  0.0415542 ],
+		[  0.0556394, -0.2040112,  1.0571490 ]
+	]
+
+	def r = X * M[0][0] + Y * M[0][1] + Z * M[0][2]
+	def g = X * M[1][0] + Y * M[1][1] + Z * M[1][2]
+	def b = X * M[2][0] + Y * M[2][1] + Z * M[2][2]
+
+	def max = max(r, g, b)
+	r = colorGammaRevert(r / max)
+	g = colorGammaRevert(g / max)
+	b = colorGammaRevert(b / max)
+
+	log.debug "< Color RGB: ($r, $g, $b)"
+
+	[red: r, green: g, blue: b]
+}
+
+private colorRgb2Xy(r, g, b) {
+	log.debug "> Color RGB: ($r, $g, $b)"
+
+	r = colorGammaAdjust(r)
+	g = colorGammaAdjust(g)
+	b = colorGammaAdjust(b)
+
+	// sRGB, Reference White D65
+	// D65	0.31271	0.32902
+	//  R	0.64000 0.33000
+	//  G	0.30000 0.60000
+	//  B	0.15000 0.06000
+	def M = [
+		[  0.4123866,  0.3575915,  0.1804505 ],
+		[  0.2126368,  0.7151830,  0.0721802 ],
+		[  0.0193306,  0.1191972,  0.9503726 ]
+	]
+
+	def X = r * M[0][0] + g * M[0][1] + b * M[0][2]
+	def Y = r * M[1][0] + g * M[1][1] + b * M[1][2]
+	def Z = r * M[2][0] + g * M[2][1] + b * M[2][2]
+
+	log.debug "> Color XYZ: ($X, $Y, $Z)"
+
+	def x = X / (X + Y + Z)
+	def y = Y / (X + Y + Z)
+
+	log.debug "> Color xy: ($x, $y)"
+
+	[x: x, y: y]
+}
+
+private colorHsv2Rgb(h, s) {
+	log.debug "< Color HSV: ($h, $s, 1)"
+
+	def r
+	def g
+	def b
+
+	if (s == 0) {
+		r = 1
+		g = 1
+		b = 1
+	} else {
+		def region = (6 * h).intValue()
+		def remainder = 6 * h - region
+
+		def p = 1 - s
+		def q = 1 - s * remainder
+		def t = 1 - s * (1 - remainder)
+
+		if (region == 0) {
+			r = 1
+			g = t
+			b = p
+		} else if (region == 1) {
+			r = q
+			g = 1
+			b = p
+		} else if (region == 2) {
+			r = p
+			g = 1
+			b = t
+		} else if (region == 3) {
+			r = p
+			g = q
+			b = 1
+		} else if (region == 4) {
+			r = t
+			g = p
+			b = 1
+		} else {
+			r = 1
+			g = p
+			b = q
+		}
+	}
+
+	log.debug "< Color RGB: ($r, $g, $b)"
+
+	[red: r, green: g, blue: b]
+}
+
+private colorRgb2Hsv(r, g, b)
+{
+	log.debug "> Color RGB: ($r, $g, $b)"
+
+	def min = min(r, g, b)
+	def max = max(r, g, b)
+	def delta = max - min
+
+	def h
+	def s
+	def v = max
+
+	if (delta == 0) {
+		h = 0
+		s = 0
+	} else {
+		s = delta / max
+		if (r == max) { // between yellow & magenta
+			h = ( g - b ) / delta
+		} else if (g == max) { // between cyan & yellow
+			h = 2 + ( b - r ) / delta
+		} else { // between magenta & cyan
+			h = 4 + ( r - g ) / delta
+		}
+		h /= 6
+
+		if (h < 0) {
+			h += 1
+		}
+    }
+
+	log.debug "> Color HSV: ($h, $s, $v)"
+
+	return [hue: h, saturation: s, level: v]
+}
+
+def safeColorHsv2Xy(h, s) {
+	def safeH = h != null ? h / 100 : 0
+	def safeS = s != null ? s / 100 : 0
+	def rgb = colorHsv2Rgb(safeH, safeS)
+
+	def xy = colorRgb2Xy(rgb.red, rgb.green, rgb.blue)
+
+	return [x: Math.round(xy.x * 65536).intValue(), y: Math.round(xy.y * 65536).intValue()]
+}
+
+def safeColorXy2Hsv(x, y) {
+	def safeX = x != null ? x / 65536 : 0
+	def safeY = y != null ? y / 65536 : 0
+	def rgb = colorXy2Rgb(safeX, safeY)
+
+	def hsv = colorRgb2Hsv(rgb.red, rgb.green, rgb.blue)
+
+	return [hue: Math.round(hsv.hue * 100).intValue(), saturation: Math.round(hsv.saturation * 100).intValue()]
+}
+
