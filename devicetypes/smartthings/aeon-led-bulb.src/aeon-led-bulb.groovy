7f778254f8b4049e10c5c8a678e1f27edfae52d8
Joshua Keplinger <joshk@trility.io>
2018-08-17 16:46:07 -0500

    WWST-1263: ZW121-A Aeotec LED Strip - US
    Updated the DTH for the Aeon LED Bulb to support the LED Strip.
    Added multi-attribute tiles on details screen. Fixed some logging,
    cleaned up unused tiles.
    Also fixed WWST-1418.

--- devicetypes/smartthings/aeon-led-bulb.src/aeon-led-bulb.groovy
+++ devicetypes/smartthings/aeon-led-bulb.src/aeon-led-bulb.groovy
@@ -17,7 +17,7 @@
  */
 
 metadata {
-	definition (name: "Aeon LED Bulb", namespace: "smartthings", author: "SmartThings") {
+	definition (name: "Aeon LED Bulb", namespace: "smartthings", author: "SmartThings", ocfDeviceType: "oic.d.light", mnmn: "SmartThings", vid: "generic-rgbw-color-bulb") {
 		capability "Switch Level"
 		capability "Color Control"
 		capability "Color Temperature"
@@ -25,51 +25,64 @@ metadata {
 		capability "Refresh"
 		capability "Actuator"
 		capability "Sensor"
+		capability "Health Check"
 
 		command "reset"
 
 		fingerprint inClusters: "0x26,0x33,0x98"
 		fingerprint deviceId: "0x11", inClusters: "0x98,0x33"
 		fingerprint deviceId: "0x1102", inClusters: "0x98"
+		fingerprint mfr: "0371", prod: "0103", model: "0002", deviceJoinName: "Aeon LED Bulb" //US
+		fingerprint mfr: "0371", prod: "0003", model: "0002", deviceJoinName: "Aeon LED Bulb" //EU
+        fingerprint mfr: "0086", prod: "0103", model: "0079", deviceJoinName: "Aeotec LED Strip" //US
+		fingerprint mfr: "0086", prod: "0003", model: "0079", deviceJoinName: "Aeotec LED Strip" //EU
 	}
 
 	simulator {
 	}
 
-	standardTile("switch", "device.switch", width: 1, height: 1, canChangeIcon: true) {
-		state "on", label:'${name}', action:"switch.off", icon:"st.lights.philips.hue-single", backgroundColor:"#00a0dc", nextState:"turningOff"
-		state "off", label:'${name}', action:"switch.on", icon:"st.lights.philips.hue-single", backgroundColor:"#ffffff", nextState:"turningOn"
-		state "turningOn", label:'${name}', action:"switch.off", icon:"st.lights.philips.hue-single", backgroundColor:"#00a0dc", nextState:"turningOff"
-		state "turningOff", label:'${name}', action:"switch.on", icon:"st.lights.philips.hue-single", backgroundColor:"#ffffff", nextState:"turningOn"
+    tiles(scale: 2) {
+		multiAttributeTile(name:"switch", type: "lighting", width: 1, height: 1, canChangeIcon: true) {
+			tileAttribute("device.switch", key: "PRIMARY_CONTROL") {
+				attributeState("on", label:'${name}', action:"switch.off", icon:"st.lights.philips.hue-single", backgroundColor:"#00a0dc", nextState:"turningOff")
+				attributeState("off", label:'${name}', action:"switch.on", icon:"st.lights.philips.hue-single", backgroundColor:"#ffffff", nextState:"turningOn")
+				attributeState("turningOn", label:'${name}', action:"switch.off", icon:"st.lights.philips.hue-single", backgroundColor:"#00a0dc", nextState:"turningOff")
+				attributeState("turningOff", label:'${name}', action:"switch.on", icon:"st.lights.philips.hue-single", backgroundColor:"#ffffff", nextState:"turningOn")
+			}
+
+			tileAttribute ("device.level", key: "SLIDER_CONTROL") {
+				attributeState "level", action:"switch level.setLevel"
+			}
+
+			tileAttribute ("device.color", key: "COLOR_CONTROL") {
+				attributeState "color", action:"setColor"
+			}
+		}
 	}
-	standardTile("reset", "device.reset", inactiveLabel: false, decoration: "flat") {
+
+	standardTile("reset", "device.reset", inactiveLabel: false, decoration: "flat", height: 2, width: 2,) {
 		state "default", label:"Reset Color", action:"reset", icon:"st.lights.philips.hue-single"
 	}
-	standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat") {
+	standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat", height: 2, width: 2,) {
 		state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
 	}
-	controlTile("levelSliderControl", "device.level", "slider", height: 1, width: 2, inactiveLabel: false, range:"(0..100)") {
-		state "level", action:"switch level.setLevel"
-	}
-	controlTile("rgbSelector", "device.color", "color", height: 3, width: 3, inactiveLabel: false) {
-		state "color", action:"setColor"
-	}
-	valueTile("level", "device.level", inactiveLabel: false, decoration: "flat") {
-		state "level", label: 'Level ${currentValue}%'
-	}
-	controlTile("colorTempControl", "device.colorTemperature", "slider", height: 1, width: 2, inactiveLabel: false) {
+
+	controlTile("colorTempControl", "device.colorTemperature", "slider", height: 2, width: 2, inactiveLabel: false) {
 		state "colorTemperature", action:"setColorTemperature"
-	}
-	valueTile("hue", "device.hue", inactiveLabel: false, decoration: "flat") {
-		state "hue", label: 'Hue ${currentValue}   '
 	}
 
 	main(["switch"])
-	details(["switch", "levelSliderControl", "rgbSelector", "reset", "colorTempControl", "refresh"])
+	details(["switch", "levelSliderControl", "rgbSelector", "colorTempControl", "refresh", "reset"])
 }
 
 def updated() {
+	log.debug "updated().."
 	response(refresh())
+}
+
+def installed() {
+	log.debug "installed()..."
+	sendEvent(name: "checkInterval", value: 500, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
 }
 
 def parse(description) {
@@ -87,6 +100,7 @@ def parse(description) {
 	}
 	result
 }
+
 
 def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicReport cmd) {
 	dimmerEvents(cmd)
@@ -129,18 +143,34 @@ def zwaveEvent(physicalgraph.zwave.commands.securityv1.SecurityMessageEncapsulat
 	}
 }
 
-
 def zwaveEvent(physicalgraph.zwave.Command cmd) {
 	def linkText = device.label ?: device.name
 	[linkText: linkText, descriptionText: "$linkText: $cmd", displayed: false]
 }
 
 def on() {
-	command(zwave.basicV1.basicSet(value: 0xFF))
+	commands([
+		zwave.basicV1.basicSet(value: 0xFF),
+		zwave.switchMultilevelV3.switchMultilevelGet(),
+	], 1000)
 }
 
 def off() {
-	command(zwave.basicV1.basicSet(value: 0x00))
+	commands([
+		zwave.basicV1.basicSet(value: 0x00),
+		zwave.switchMultilevelV3.switchMultilevelGet(),
+	], 1000)
+}
+
+def refresh() {
+	commands([
+		zwave.switchMultilevelV3.switchMultilevelGet(),
+	], 1000)
+}
+
+def ping() {
+	log.debug "ping().."
+	refresh()
 }
 
 def setLevel(level) {
@@ -148,14 +178,12 @@ def setLevel(level) {
 }
 
 def setLevel(level, duration) {
+    log.debug "setLevel($level, $duration)"
 	if(level > 99) level = 99
-	command(zwave.switchMultilevelV3.switchMultilevelSet(value: level, dimmingDuration: duration))
-}
-
-def refresh() {
 	commands([
-		zwave.switchMultilevelV1.switchMultilevelGet(),
-	], 1000)
+		zwave.switchMultilevelV3.switchMultilevelSet(value: level, dimmingDuration: duration),
+		zwave.switchMultilevelV3.switchMultilevelGet(),
+	], (duration && duration < 12) ? (duration * 1000) : 3500)
 }
 
 def setSaturation(percent) {
@@ -169,8 +197,8 @@ def setHue(value) {
 }
 
 def setColor(value) {
+	log.debug "setColor($value)"
 	def result = []
-	log.debug "setColor: ${value}"
 	if (value.hex) {
 		def c = value.hex.findAll(/[0-9a-fA-F]{2}/).collect { Integer.parseInt(it, 16) }
 		result << zwave.switchColorV3.switchColorSet(red:c[0], green:c[1], blue:c[2], warmWhite:0, coldWhite:0)
@@ -192,15 +220,20 @@ def setColor(value) {
 }
 
 def setColorTemperature(percent) {
+	log.debug "setColorTemperature($percent)"
 	if(percent > 99) percent = 99
 	int warmValue = percent * 255 / 99
-	command(zwave.switchColorV3.switchColorSet(red:0, green:0, blue:0, warmWhite:warmValue, coldWhite:(255 - warmValue)))
+    sendEvent(name: "colorTemperature", value: percent)
+    commands([
+        zwave.switchColorV3.switchColorSet(red:0, green:0, blue:0, warmWhite:warmValue, coldWhite:(255 - warmValue)),
+        zwave.switchMultilevelV3.switchMultilevelGet()
+    ], 2000)
 }
 
 def reset() {
 	log.debug "reset()"
 	sendEvent(name: "color", value: "#ffffff")
-	setColorTemperature(99)
+	setColorTemperature(0)
 }
 
 private command(physicalgraph.zwave.Command cmd) {
@@ -236,7 +269,7 @@ def rgbToHSV(red, green, blue) {
 	[hue: hue, saturation: saturation, value: max * 100]
 }
 
-def huesatToRGB(float hue, float sat) {
+def huesatToRGB(hue, sat) {
 	while(hue >= 100) hue -= 100
 	int h = (int)(hue / 100 * 6)
 	float f = hue / 100 * 6 - h
