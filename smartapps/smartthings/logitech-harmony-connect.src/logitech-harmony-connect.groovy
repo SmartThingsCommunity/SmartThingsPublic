a079e388d5c9111fd7cb09917e00cf9d5af3c0c4
Juan Pablo Risso <juano23@gmail.com>
2018-08-20 17:37:52 -0700

    C2C-4 - Harmony Remove Comment (#3382)

--- smartapps/smartthings/logitech-harmony-connect.src/logitech-harmony-connect.groovy
+++ smartapps/smartthings/logitech-harmony-connect.src/logitech-harmony-connect.groovy
@@ -525,7 +525,6 @@ def pollResponse(response, data) {
 		// Device-Watch relies on the Logitech Harmony Cloud to get the Device state.
 		activities.each { activity ->
 			if ("${activity.deviceNetworkId}".contains("harmony-${it.key}")) {
-				//C2C-4 Temporarily disabiling device health for Logitech.
 				activity.sendEvent(name: "DeviceWatch-DeviceStatus", value: "offline", displayed: false, isStateChange: true)
 			}
 		}
