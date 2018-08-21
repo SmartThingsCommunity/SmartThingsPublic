edbcc6d5619cab2565dd9daa290af1f1e6b91282
Jason Rall <jason@trility.io>
2018-08-21 13:36:14 -0600

    Removed secstate hack

--- devicetypes/smartthings/aeon-led-bulb.src/aeon-led-bulb.groovy
+++ devicetypes/smartthings/aeon-led-bulb.src/aeon-led-bulb.groovy
@@ -243,7 +243,7 @@ private crcEncap(physicalgraph.zwave.Command cmd) {
 }
 
 private command(physicalgraph.zwave.Command cmd) {
-	if (zwaveInfo.zw.contains("s") || state.sec != 0) {
+	if (zwaveInfo.zw.contains("s")) {
 		secEncap(cmd)
 	} else if (zwaveInfo.cc.contains("56")){
 		crcEncap(cmd)
