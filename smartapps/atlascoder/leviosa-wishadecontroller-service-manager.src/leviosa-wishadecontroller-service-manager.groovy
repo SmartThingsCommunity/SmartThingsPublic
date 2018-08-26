967d77129e3f5cfe03cf31ae6a198ea8faff292d
Anton Titkov <atlascoder@gmail.com>
2018-08-26 15:49:52 +0300

    All found shade groups are added by default - no need to select every group.

--- smartapps/atlascoder/leviosa-wishadecontroller-service-manager.src/leviosa-wishadecontroller-service-manager.groovy
+++ smartapps/atlascoder/leviosa-wishadecontroller-service-manager.src/leviosa-wishadecontroller-service-manager.groovy
@@ -16,26 +16,37 @@ definition(
 
 preferences {
 	page(name: "searchTargetSelection", title: "Shade Groups Setup", nextPage: "deviceDiscovery") {
-		section("You are starting to connect your Leviosa Motor Shades managed through wiSHadeControllers. Your wiShadesController(s) must be setup correctly before to start to discover and connect them.") {	
-		}
+		section("You are starting to connect your Leviosa Motor Shades managed through wiSHadeControllers. Your wiShadesController(s) must be setup correctly before to start to discover and connect them.") {}
 		section("Your wiShadesController(s) must be setup correctly before to start to discover and connect them.")
 	}
 	page(name: "deviceDiscovery", title: "Shade Groups Discovery", content: "deviceDiscovery")
 }
 
+def getSelectedDevices() {
+	def list = []
+	def devices = getVerifiedDevices()
+	devices.each {
+//    	def key = it.value.ssdpUSN.split(':')[1]
+//		def value = it.value.name ?: "Group ${key}"
+//		list["${key}"] = value
+		list << it.value.ssdpUSN.split(':')[1]
+	}
+    list
+}
+
 def deviceDiscovery() {
 
 	state.searchTarget = "urn:schemas-upnp-org:service:TwoWayMotor:1"
-
+    
 	log.debug "Discovery start for ${state.searchTarget}"
 
-	def options = [:]
-	def devices = getVerifiedDevices()
-	devices.each {
-    	def key = it.value.ssdpUSN.split(':')[1]
-		def value = it.value.name ?: "Group ${key}"
-		options["${key}"] = value
-	}
+//	def options = [:]
+//	def devices = getVerifiedDevices()
+//	devices.each {
+//    	def key = it.value.ssdpUSN.split(':')[1]
+//		def value = it.value.name ?: "Group ${key}"
+//		options["${key}"] = value
+//	}
 
 	ssdpSubscribe()
 
@@ -43,8 +54,28 @@ def deviceDiscovery() {
 	verifyDevices()
 
 	return dynamicPage(name: "deviceDiscovery", title: "Discovery Started!", nextPage: "", refreshInterval: 5, install: true, uninstall: true) {
-		section("Please wait while we discover your shade groups. Discovery can take five minutes or more, so sit back and relax! Select your really connected group from list below once discovered.") {
-			input "selectedDevices", "enum", required: false, title: "Select Groups (${options.size() ?: 0} found)", multiple: true, options: options
+		section("Please wait while we discover your shade groups. Discovery can take five minutes or more, so sit back and relax!") {
+        	def l = getSelectedDevices().size()
+            if (l == 0) {
+        		paragraph "No controllers discovered yet.."
+            }
+        	else if (l == 1) {
+        		paragraph "1 shade group discovered."
+            paragraph "Every controller has 6 groups, please wait.."
+            }
+            else if (l < 6) {
+        		paragraph "${l} shade groups discovered."
+	            paragraph "Every controller has 6 groups, please wait.."
+            }
+            else if (l == 6) {
+        		paragraph "6 shade groups discovered."
+	            paragraph "If you have only 1 controller - you have to finish setup pressing Save. Otherwise - please wait.."
+            }
+            else {
+        		paragraph "${l} shade groups discovered."
+	            paragraph "Every controller has 6 groups, please, decide yourself when stop to discover."
+            }
+//			input "selectedDevices", "enum", required: false, title: "Select Groups (${options.size() ?: 0} found)", multiple: true, options: options
 		}
 	}
 }
@@ -68,10 +99,12 @@ def initialize() {
 	unschedule()
 
 	ssdpSubscribe()
+    
+    def selectedDevices = getSelectedDevices()
 
 	log.debug "Selected devices: ${selectedDevices}"
 
-	if (selectedDevices) {
+	if (selectedDevices.size() > 0) {
 		addDevices()
 	}
 
@@ -79,10 +112,12 @@ def initialize() {
 }
 
 void ssdpDiscover() {
+	log.debug "lan discovery ${state.searchTarget}"
 	sendHubCommand(new physicalgraph.device.HubAction("lan discovery ${state.searchTarget}", physicalgraph.device.Protocol.LAN))
 }
 
 void ssdpSubscribe() {
+	log.debug "subscribing to ${state.searchTarget}"
 	subscribe(location, "ssdpTerm.${state.searchTarget}", ssdpHandler)
 }
 
@@ -126,6 +161,8 @@ def addDevices() {
     log.debug "Selected devices: ${selectedDevices}"
     
     def selDevs = []
+    
+    def selectedDevices = getSelectedDevices()
     
     if (selectedDevices instanceof String) {
         selDevs << selectedDevices
