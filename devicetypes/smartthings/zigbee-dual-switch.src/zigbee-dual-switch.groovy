066fec8b98eec96671cafef74e4abf0f56947c12
Fen Mei <f.mei@samsung.com>
2018-08-29 10:44:23 +0800

    modify child network id

--- devicetypes/smartthings/zigbee-dual-switch.src/zigbee-dual-switch.groovy
+++ devicetypes/smartthings/zigbee-dual-switch.src/zigbee-dual-switch.groovy
@@ -1,30 +1,34 @@
-/**
-*  Copyright 2015 SmartThings
-*
-*  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License. You may obtain a copy of the License at:
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
-*  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
-*  for the specific language governing permissions and limitations under the License.
-*
-*/
+/*
+ *  Copyright 2018 SmartThings
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy
+ *  of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ *  Author : Fen Mei / f.mei@samsung.com
+ *  Date : 2018-08-29
+ */
 
 metadata {
-    definition (name: "ZigBee Multi Switch", namespace: "smartthings", author: "SmartThings", ocfDeviceType: "oic.d.switch", runLocally: false, minHubCoreVersion: '000.019.00012', executeCommandsLocally: true, vid: "generic-switch" ) {
+    definition (name: "ZigBee Multi Switch", namespace: "smartthings", author: "SmartThings", ocfDeviceType: "oic.d.switch", vid: "generic-switch" ) {
         capability "Actuator"
         capability "Configuration"
         capability "Refresh"
         capability "Health Check"
         capability "Switch"
-        
+
         command "childOn", ["string"]
         command "childOff", ["string"]
-        
+
         fingerprint  profileId: "0104", inClusters: "0000, 0005, 0004, 0006", manufacturer: "0000000", model: "cc891c853291418daa4e26cb315a3a0c", deviceJoinName: "Orvibo Smart Switch 1"
-       }
+    }
     // simulator metadata
     simulator {
         // status messages
@@ -54,8 +58,7 @@ metadata {
 }
 
 def installed() {
-         createChildDevices()
-         configure()
+    createChildDevices()
 }
 
 // Parse incoming device messages to generate events
@@ -75,44 +78,44 @@ def parse(String description) {
              } else if (descMap?.clusterId == "0006" && descMap.sourceEndpoint == "02") {
                 def result = map ? createEvent(map) : [:]
                 def childDevice = childDevices.find {
-			it.deviceNetworkId == "$device.deviceNetworkId-ep2"
+                        it.deviceNetworkId == "$device.deviceNetworkId:02"
                 }
                 childDevice.sendEvent(result)
-	    }
+            }
         }
     }
 }
 
 private void createChildDevices() {
-       def i = 2
-       addChildDevice("Child Switch", "${device.deviceNetworkId}-ep${i}", null,
-                         [completedSetup: true, label: "${device.displayName.split("1")[-1]} ${i}",
-                         isComponent: false, componentName: "ch$i", componentLabel: "Channel $i"])
-        
+    def i = 2
+    addChildDevice("Child Switch", "${device.deviceNetworkId}:0${i}", null,
+                     [completedSetup: true, label: "${device.displayName.split("1")[-1]} ${i}",
+                     isComponent: false, componentName: "ch$i", componentLabel: "Channel $i"])
+
 }
 
 private channelNumber(String dni) {
-        dni.split("-ep")[-1] as Integer
+    dni.split(":")[-1] as Integer
 }
 
 def on() {
-     log.debug("on")
-     zigbee.command(0x0006, 0x01, "", [destEndpoint: 0x01])
+    log.debug("on")
+    zigbee.command(0x0006, 0x01, "", [destEndpoint: 0x01])
 }
 
 def off() {
-     log.debug("off")
-     zigbee.command(0x0006, 0x00, "", [destEndpoint: 0x01])
+    log.debug("off")
+    zigbee.command(0x0006, 0x00, "", [destEndpoint: 0x01])
 }
 
 def childOn(String dni) {
-        log.debug("    child on ${dni}")
-        zigbee.command(0x0006, 0x01, "", [destEndpoint: channelNumber(dni)])
+    log.debug("    child on ${dni}")
+    zigbee.command(0x0006, 0x01, "", [destEndpoint: channelNumber(dni)])
 }
 
 def childOff(String dni) {
-        log.debug(" child off ${dni}")
-        zigbee.command(0x0006, 0x00, "", [destEndpoint: channelNumber(dni)])
+    log.debug(" child off ${dni}")
+    zigbee.command(0x0006, 0x00, "", [destEndpoint: channelNumber(dni)])
 }
 
 /**
@@ -122,8 +125,8 @@ def ping() {
     return refresh()
 }
 
-def refresh() 
-{    
+def refresh()
+{
     log.debug "refresh broatcast"
     return zigbee.readAttribute(0x0006, 0x0000, [destEndpoint: 0xFF])
 }
@@ -132,9 +135,9 @@ def configure() {
     log.debug "config"
     // Device-Watch allows 2 check-in misses from device + ping (plus 2 min lag time)
     sendEvent(name: "checkInterval", value: 2 * 10 * 60 + 2 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
-    
+
     return zigbee.configureReporting(0x0006, 0x0000, 0x10, 0, 600, null, [destEndpoint: 0x01]) +
-    		zigbee.configureReporting(0x0006, 0x0000, 0x10, 0, 600, null, [destEndpoint: 0x02]) +
-    		zigbee.readAttribute(0x0006, 0x0000,[destEndpoint: 0x01]) +
-            	zigbee.readAttribute(0x0006, 0x0000,[destEndpoint: 0x02])
+                zigbee.configureReporting(0x0006, 0x0000, 0x10, 0, 600, null, [destEndpoint: 0x02]) +
+                zigbee.readAttribute(0x0006, 0x0000,[destEndpoint: 0xFF])
 }
+
