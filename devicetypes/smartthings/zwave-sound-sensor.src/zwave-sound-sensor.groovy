5595f042b66dee110b940cb8c05a2c38a86712fc
Przemyslaw Kacprowicz <p.kacprowicz@samsung.com>
2018-08-30 14:10:16 +0200

    Small fix in parse() method.

--- devicetypes/smartthings/zwave-sound-sensor.src/zwave-sound-sensor.groovy
+++ devicetypes/smartthings/zwave-sound-sensor.src/zwave-sound-sensor.groovy
@@ -54,11 +54,11 @@ def updated() {
 def parse(String description) {
 	def results = []
 	if (description.startsWith("Err")) {
-		results << createEvent(descriptionText:description, displayed:true)
+		results = createEvent(descriptionText:description, displayed:true)
 	} else {
 		def cmd = zwave.parse(description, [ 0x80: 1, 0x84: 1, 0x71: 2, 0x72: 1 ])
 		if (cmd) {
-			results << zwaveEvent(cmd)
+			results = zwaveEvent(cmd)
 		}
 	}
 	log.debug "'$description' parsed to ${results.inspect()}"
