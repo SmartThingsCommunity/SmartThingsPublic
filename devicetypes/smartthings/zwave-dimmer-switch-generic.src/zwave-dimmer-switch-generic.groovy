ed5e2ac371c652caa1c160f72fc6d3775297ac12
Przemyslaw Kacprowicz <p.kacprowicz@samsung.com>
2018-09-14 10:44:02 +0200

    Removed Light capability in Zwave Dimmer Switch

--- devicetypes/smartthings/zwave-dimmer-switch-generic.src/zwave-dimmer-switch-generic.groovy
+++ devicetypes/smartthings/zwave-dimmer-switch-generic.src/zwave-dimmer-switch-generic.groovy
@@ -20,7 +20,6 @@ metadata {
 		capability "Polling"
 		capability "Refresh"
 		capability "Sensor"
-		capability "Light"
 
 		fingerprint inClusters: "0x26", deviceJoinName: "Z-Wave Dimmer"
 		fingerprint mfr: "001D", prod: "1902", deviceJoinName: "Z-Wave Dimmer"
