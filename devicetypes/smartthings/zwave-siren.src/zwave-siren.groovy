35e4dd3f444c773ad21dd10592cf73d6543a295d
greens <steven.green@smartthings.com>
2018-09-25 10:10:41 -0700

    WWST-1736 Change default siren sound for Zipato siren

--- devicetypes/smartthings/zwave-siren.src/zwave-siren.groovy
+++ devicetypes/smartthings/zwave-siren.src/zwave-siren.groovy
@@ -125,7 +125,7 @@ def configure() {
 		cmds << zwave.configurationV1.configurationSet(parameterNumber: 2, size: 1, configurationValue: [2]).format()
 		cmds << "delay 500"
 		// Set alarm sound to no.1
-		cmds << zwave.configurationV1.configurationSet(parameterNumber: 5, size: 1, configurationValue: [9]).format()
+		cmds << zwave.configurationV1.configurationSet(parameterNumber: 5, size: 1, configurationValue: [10]).format()
 	}
 	response(cmds)
 }
