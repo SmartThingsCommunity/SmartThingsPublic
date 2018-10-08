fba0c67747f0101c597f535816ee2c6b1297be2d
Daniel <d2dindustries@gmail.com>
2018-10-08 14:57:53 -0400

    Update keep-me-cozy-ii.groovy
    Added Minimum and Maximum required Thermostat Setpoint Range input fields to prevent Air Condition / Heater Setpoint from being automatically set dangerously low/high. Refactored "evaluate" function code by simplifying variable names, extracting temperature change constants and conditions. For Example: This will stop the thermostat sensor space from being freezing cold with a setpoint of 62 degrees, if the other sensor is reading 80 degrees. Changed Description to better reflect the functionality of the app.

--- smartapps/smartthings/keep-me-cozy-ii.src/keep-me-cozy-ii.groovy
+++ smartapps/smartthings/keep-me-cozy-ii.src/keep-me-cozy-ii.groovy
@@ -19,15 +19,14 @@ definition(
     name: "Keep Me Cozy II",
     namespace: "smartthings",
     author: "SmartThings",
-    description: "Works the same as Keep Me Cozy, but enables you to pick an alternative temperature sensor in a separate space from the thermostat. Focuses on making you comfortable where you are spending your time rather than where the thermostat is located.",
+    description: "Control the temperature in a separate space from the thermostat using an alternative temperature sensor. Focuses on making you comfortable where you are spending your time rather than where the thermostat is located.",
     category: "Green Living",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Meta/temp_thermo.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Meta/temp_thermo@2x.png",
-    pausable: true
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Meta/temp_thermo@2x.png"
 )
 
 preferences() {
-	section("Choose thermostat...") {
+	section("Choose thermostat... ") {
 		input "thermostat", "capability.thermostat"
 	}
 	section("Heat setting..." ) {
@@ -36,8 +35,12 @@ preferences() {
 	section("Air conditioning setting...") {
 		input "coolingSetpoint", "decimal", title: "Degrees"
 	}
-	section("Optionally choose temperature sensor to use instead of the thermostat's...") {
-		input "sensor", "capability.temperatureMeasurement", title: "Temp Sensors", required: false
+	section("Thermostat setpoint range setting...") {
+		input "temperatureMin", "decimal", title: "Min Degrees"		
+		input "temperatureMax", "decimal", title: "Max Degrees"		
+	}	
+	section("Optionally choose a temperature sensor to use instead of the thermostat's... ") {
+		input "sensor", "capability.temperatureMeasurement", title: "Temp Sensor", required: false
 	}
 }
 
@@ -79,32 +82,46 @@ def temperatureHandler(evt)
 private evaluate()
 {
 	if (sensor) {
-		def threshold = 1.0
-		def tm = thermostat.currentThermostatMode
-		def ct = thermostat.currentTemperature
-		def currentTemp = sensor.currentTemperature
-		log.trace("evaluate:, mode: $tm -- temp: $ct, heat: $thermostat.currentHeatingSetpoint, cool: $thermostat.currentCoolingSetpoint -- "  +
-			"sensor: $currentTemp, heat: $heatingSetpoint, cool: $coolingSetpoint")
-		if (tm in ["cool","auto"]) {
-			// air conditioner
-			if (currentTemp - coolingSetpoint >= threshold) {
-				thermostat.setCoolingSetpoint(ct - 2)
-				log.debug "thermostat.setCoolingSetpoint(${ct - 2}), ON"
+		def THRESHOLD = 1.0
+		def TEMP_CHANGE = 2;
+
+		def sensorTemp = sensor.currentTemperature
+		def thermoTemp = thermostat.currentTemperature
+		def thermoMode = thermostat.currentThermostatMode
+		def thermoCoolSetpoint = thermostat.currentCoolingSetpoint
+		def thermoHeatSetpoint = thermostat.currentHeatingSetpoint
+		def temperatureRaise = thermoTemp + TEMP_CHANGE
+		def temperatureLower = thermoTemp - TEMP_CHANGE		
+		log.trace("evaluate:, mode: $tm -- temp: $thermoTemp, heat: $thermoHeatSetpoint, cool: $thermoCoolSetpoint -- "  +
+			"sensor: $sensorTemp, heat: $heatingSetpoint, cool: $coolingSetpoint, min: $temperatureMin, max: $temperatureMax")
+		if (thermoMode in ["cool","auto"]) {
+			// Air Conditioner
+			if (sensorTemp - coolingSetpoint >= THRESHOLD) { 
+				if(temperatureLower >= temperatureMin){
+					thermostat.setCoolingSetpoint(temperatureLower)
+					log.debug "thermostat.setCoolingSetpoint(${temperatureLower}), ON"
+				}
 			}
-			else if (coolingSetpoint - currentTemp >= threshold && ct - thermostat.currentCoolingSetpoint >= threshold) {
-				thermostat.setCoolingSetpoint(ct + 2)
-				log.debug "thermostat.setCoolingSetpoint(${ct + 2}), OFF"
+			else if (coolingSetpoint - sensorTemp >= THRESHOLD && thermoTemp - thermoCoolSetpoint >= THRESHOLD) {
+				if(temperatureRaise <= temperatureMax){
+					thermostat.setCoolingSetpoint(temperatureRaise)
+					log.debug "thermostat.setCoolingSetpoint(${temperatureRaise}), OFF"
+				}
 			}
 		}
-		if (tm in ["heat","emergency heat","auto"]) {
-			// heater
-			if (heatingSetpoint - currentTemp >= threshold) {
-				thermostat.setHeatingSetpoint(ct + 2)
-				log.debug "thermostat.setHeatingSetpoint(${ct + 2}), ON"
+		if (thermoMode in ["heat","emergency heat","auto"]) {
+			// Heater
+			if (heatingSetpoint - sensorTemp >= THRESHOLD) {
+				if(temperatureRaise <= temperatureMax){
+					thermostat.setHeatingSetpoint(temperatureRaise)
+					log.debug "thermostat.setHeatingSetpoint(${temperatureRaise}), ON"
+				}
 			}
-			else if (currentTemp - heatingSetpoint >= threshold && thermostat.currentHeatingSetpoint - ct >= threshold) {
-				thermostat.setHeatingSetpoint(ct - 2)
-				log.debug "thermostat.setHeatingSetpoint(${ct - 2}), OFF"
+			else if (sensorTemp - heatingSetpoint >= THRESHOLD && thermoHeatSetpoint - thermoTemp >= THRESHOLD) {
+				if(temperatureLower >= temperatureMin){
+					thermostat.setHeatingSetpoint(temperatureLower)
+					log.debug "thermostat.setHeatingSetpoint(${temperatureLower}), OFF"
+				}
 			}
 		}
 	}
