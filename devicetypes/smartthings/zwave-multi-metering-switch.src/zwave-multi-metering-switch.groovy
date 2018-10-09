5bf510c39d4057bedaca4ca3f240319c7603b36a
Przemyslaw Kacprowicz <p.kacprowicz@samsung.com>
2018-10-09 10:48:48 +0200

    Fixing bug with no added child devices.

--- devicetypes/smartthings/zwave-multi-metering-switch.src/zwave-multi-metering-switch.groovy
+++ devicetypes/smartthings/zwave-multi-metering-switch.src/zwave-multi-metering-switch.groovy
@@ -232,6 +232,10 @@ def installed() {
 	sendEvent(name: "checkInterval", value: 2 * 15 * 60 + 2 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
 }
 
+def updated() {
+	sendHubCommand secure(zwave.manufacturerSpecificV2.manufacturerSpecificGet())
+}
+
 def configure() {
 	log.debug "Configure..."
 	response([
