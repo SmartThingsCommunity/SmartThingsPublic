c0c85fd407debeb1b6dabb251f5da4634337cd6d
Diana Czarnecka <d.czarnecka@samsung.com>
2018-10-30 11:11:19 +0100

    WWST-1828 added fingerprint for Aurora RGBW GLS Lamp

--- devicetypes/smartthings/zll-rgbw-bulb.src/zll-rgbw-bulb.groovy
+++ devicetypes/smartthings/zll-rgbw-bulb.src/zll-rgbw-bulb.groovy
@@ -52,6 +52,7 @@ metadata {
 		fingerprint profileId: "C05E", inClusters: "0000,0003,0004,0005,0006,0008,0300,1000", outClusters: "0019", manufacturer: "Philips", model: "LST002", deviceJoinName: "Philips Hue Lightstrip"
 		fingerprint profileId: "C05E", inClusters: "0000, 0003, 0004, 0005, 0006, 0008, 0300", outClusters: "0019", manufacturer: "innr", model: "RB 185 C", deviceJoinName: "innr Smart Bulb RGBW"
 		fingerprint profileId: "C05E", inClusters: "0000, 0003, 0004, 0005, 0006, 0008, 0300", outClusters: "0019", manufacturer: "innr", model: "FL 130 C", deviceJoinName: "innr Flex Light RGBW"
+		fingerprint profileId: "0104", inClusters: "0000, 0004, 0003, 0006, 0008, 0005, 0300, FFFF, 1000", outClusters: "0019", manufacturer: " Aurora", model: "RGBBulb51AU", deviceJoinName: "Aurora RGBW GLS Lamp"
 	}
 
 	// UI tile definitions
