82155ff9888e87500c9923307a7fa545f97903f0
l.ferreira <l.ferreira@samsung.com>
2018-11-15 14:05:04 -0200

    [WWST-1613] Including fingerprint for Ozom Smart Plug
    Change-Id: I54d616cc245e1f6a5df69cd8c70f93fa4b0e170c

--- devicetypes/smartthings/zigbee-switch-power.src/zigbee-switch-power.groovy
+++ devicetypes/smartthings/zigbee-switch-power.src/zigbee-switch-power.groovy
@@ -28,7 +28,7 @@ metadata {
         fingerprint profileId: "0104", inClusters: "0000, 0003, 0004, 0005, 0006, 0702, 0B05", outClusters: "0003, 000A, 0019", manufacturer: "Jasco Products", model: "45853", deviceJoinName: "GE ZigBee Plug-In Switch"
         fingerprint profileId: "0104", inClusters: "0000, 0003, 0004, 0005, 0006, 0702, 0B05", outClusters: "000A, 0019", manufacturer: "Jasco Products", model: "45856", deviceJoinName: "GE ZigBee In-Wall Switch"
         fingerprint profileId: "0104", inClusters: "0000, 0003, 0004, 0005, 0006, 0702", manufacturer: "ClimaxTechnology", model: "PSM_00.00.00.35TC", deviceJoinName: "Ozom Smart Plug"
-        fingerprint profileId: "0104", inClusters: "0000, 0003, 0004, 0005, 0006, 0702", outClusters: "0000", manufacturer: "ClimaxTechnology", model: "PSM_00.00.00.35TC", deviceJoinName: "Ozom Smart Plug" // Fingerprint :"01 0104 0009 00 06 0000 0003 0004 0005 0006 0702 01 0000","application":"10","manufacturer":"ClimaxTechnology","model":"PSM_00.00.00.35TC" 
+        fingerprint profileId: "0104", inClusters: "0000, 0003, 0004, 0005, 0006, 0702", outClusters: "0000", manufacturer: "ClimaxTechnology", model: "PSM_00.00.00.35TC", deviceJoinName: "Ozom Smart Plug"
     }
 
     tiles(scale: 2) {
