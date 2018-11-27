394cff1ab127296cc3ff7ef615480041a3a18106
MGoralczykS <m.goralczyk@samsung.com>
2018-11-27 14:28:25 +0100

    Fix for battery report on YRD 446.

--- devicetypes/smartthings/zwave-lock.src/zwave-lock.groovy
+++ devicetypes/smartthings/zwave-lock.src/zwave-lock.groovy
@@ -1146,7 +1146,7 @@ def unlockWithTimeout() {
  */
 def ping() {
 	log.trace "[DTH] Executing ping() for device ${device.displayName}"
-	runIn(30, followupStateCheck)
+	runIn(30, "followupStateCheck", [forceForLocallyExecuting: true])
 	secure(zwave.doorLockV1.doorLockOperationGet())
 }
 
@@ -1154,7 +1154,7 @@ def ping() {
  * Checks the door lock state. Also, schedules checking of door lock state every one hour.
  */
 def followupStateCheck() {
-	runEvery1Hour(stateCheck)
+	runEvery1Hour(stateCheck, [forceForLocallyExecuting: true])
 	stateCheck()
 }
 
@@ -1162,6 +1162,7 @@ def followupStateCheck() {
  * Checks the door lock state
  */
 def stateCheck() {
+	sendHubCommand(new physicalgraph.device.HubAction(secure(zwave.batteryV1.batteryGet()))) // fix for YRD 446
 	sendHubCommand(new physicalgraph.device.HubAction(secure(zwave.doorLockV1.doorLockOperationGet())))
 }
 
