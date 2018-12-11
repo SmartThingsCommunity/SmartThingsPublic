d4afb025aea6ed094e9a823670e7bfa44cc408c8
Donald Kirker <donald@smartthings.com>
2018-12-10 23:59:31 -0800

    Formatting standardization

--- devicetypes/drzwave/ezmultipli.src/ezmultipli.groovy
+++ devicetypes/drzwave/ezmultipli.src/ezmultipli.groovy
@@ -3,26 +3,28 @@
 // driver for SmartThings
 // The EZMultiPli is also known as the HSM200 from HomeSeer.com
 
+import physicalgraph.zwave.commands.*
+
 metadata {
 	definition (name: "EZmultiPli", namespace: "DrZWave", author: "Eric Ryherd", ocfDeviceType: "x.com.st.d.sensor.motion") {
-	capability "Actuator"
-	capability "Sensor"
-	capability "Motion Sensor"
-	capability "Temperature Measurement"
-	capability "Illuminance Measurement"
-	capability "Switch"
-	capability "Color Control"
-	capability "Configuration"
-    capability "Refresh"
-    capability "Health Check"
+		capability "Actuator"
+		capability "Sensor"
+		capability "Motion Sensor"
+		capability "Temperature Measurement"
+		capability "Illuminance Measurement"
+		capability "Switch"
+		capability "Color Control"
+		capability "Configuration"
+		capability "Refresh"
+		capability "Health Check"
 
-	fingerprint mfr: "001E", prod: "0004", model: "0001" // new format for Fingerprint which is unique for every certified Z-Wave product
-	} // end definition
+		fingerprint mfr: "001E", prod: "0004", model: "0001"
+	}
 
 	simulator {
 		// messages the device returns in response to commands it receives
-		status "motion"     : "command: 7105000000FF07, payload: 07"
-		status "no motion"  : "command: 7105000000FF07, payload: 00"
+		status "motion":	"command: 7105000000FF07, payload: 07"
+		status "no motion":	"command: 7105000000FF07, payload: 00"
 
 		for (int i = 0; i <= 100; i += 20) {
 			status "temperature ${i}F": new physicalgraph.zwave.Zwave().sensorMultilevelV5.sensorMultilevelReport(
@@ -33,11 +35,10 @@ metadata {
 				scaledSensorValue: i, precision: 0, sensorType: 3).incomingMessage()
 		}
 
-	} //end simulator
+	}
 
-    
-    tiles (scale: 2){      
-		multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true){
+	tiles (scale: 2) {
+		multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true) {
 			tileAttribute ("device.switch", key: "PRIMARY_CONTROL", icon: "st.Lighting.light18") {
 				attributeState "on", label:'${name}', action:"switch.off", icon:"st.switches.light.on", backgroundColor:"#00A0DC", nextState:"turningOff"
 				attributeState "off", label:'${name}', action:"switch.on", icon:"st.switches.light.off", backgroundColor:"#ffffff", nextState:"turningOn"
@@ -50,14 +51,14 @@ metadata {
 			tileAttribute ("statusText", key: "SECONDARY_CONTROL") {
 				attributeState "statusText", label:'${currentValue}'
 			}
-        }
+		}
 
 		standardTile("motion", "device.motion", width: 2, height: 2, canChangeIcon: true, canChangeBackground: true) {
 			state "active", label:'motion', icon:"st.motion.motion.active", backgroundColor:"#00A0DC"
 			state "inactive", label:'no motion', icon:"st.motion.motion.inactive", backgroundColor:"#cccccc"
 		}
 		valueTile("temperature", "device.temperature", width: 2, height: 2) {
-			state "temperature", label:'${currentValue}°', unit:"F", icon:"", // would be better if the units would switch to the desired units of the system (imperial or metric)
+			state "temperature", label:'${currentValue}°', unit:"F", icon:"",
 			backgroundColors:[
 				[value: 0,  color: "#153591"], // blue=cold
 				[value: 65, color: "#44b621"], // green
@@ -70,18 +71,15 @@ metadata {
 			]
 		}
 
-        // icons to use would be st.Weather.weather2 or st.alarm.temperature.normal - see http://scripts.3dgo.net/smartthings/icons/ for a list of icons
 		valueTile("illuminance", "device.illuminance", width: 2, height: 2, inactiveLabel: false) {
-// jrs 4/7/2015 - Null on display
-			//state "luminosity", label:'${currentValue} ${unit}'
 			state "luminosity", label:'${currentValue}', unit:'${currentValue}', icon:"",
 			backgroundColors:[
 				[value: 25, color: "#404040"],
 				[value: 50, color: "#808080"],
 				[value: 75, color: "#a0a0a0"],
 				[value: 90, color: "#e0e0e0"],
-                //lux measurement values
-                [value: 150, color: "#404040"],
+				//lux measurement values
+				[value: 150, color: "#404040"],
 				[value: 300, color: "#808080"],
 				[value: 600, color: "#a0a0a0"],
 				[value: 900, color: "#e0e0e0"]
@@ -89,27 +87,24 @@ metadata {
 		}
 		standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
 			state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
-		}    
+		}
 		standardTile("configure", "device.configure", inactiveLabel: false, width: 2, height: 2, decoration: "flat") {
 			state "configure", label:'', action:"configuration.configure", icon:"st.secondary.configure"
 		}
 
-		main (["temperature","motion", "switch"])
+		main (["temperature", "motion", "switch"])
 		details(["switch", "motion", "temperature", "illuminance", "refresh", "configure"])
-	}  // end tiles
-    
-	preferences {
-       input "OnTime",  "number", title: "No Motion Interval", description: "N minutes lights stay on after no motion detected [0, 1-127]", range: "0..127", defaultValue: 2, displayDuringSetup: true, required: false
-       input "OnLevel", "number", title: "Dimmer Onlevel", description: "Dimmer OnLevel for associated node 2 lights [-1, 0, 1-99]", range: "-1..99", defaultValue: -1, displayDuringSetup: true, required: false
-       input "LiteMin", "number", title: "Luminance Report Frequency", description: "Luminance report sent every N minutes [0-127]", range: "0..127", defaultValue: 6, displayDuringSetup: true, required: false
-       input "TempMin", "number", title: "Temperature Report Frequency", description: "Temperature report sent every N minutes [0-127]", range: "0..127", defaultValue: 6, displayDuringSetup: true, required: false
-       input "TempAdj", "number", title: "Temperature Calibration", description: "Adjust temperature up/down N tenths of a degree F [(-127)-(+128)]", range: "-127..128", defaultValue: 0, displayDuringSetup: true, required: false
-       input("lum", "enum", title:"Illuminance Measurement", description: "Percent or Lux", defaultValue: 2 ,required: false, displayDuringSetup: true, options:
-          [1:"Percent",
-           2:"Lux"])
-  }
+	}
 
-} // end metadata
+	preferences {
+		input "OnTime",  "number", title: "No Motion Interval", description: "N minutes lights stay on after no motion detected [0, 1-127]", range: "0..127", defaultValue: 2, displayDuringSetup: true, required: false
+		input "OnLevel", "number", title: "Dimmer Onlevel", description: "Dimmer OnLevel for associated node 2 lights [-1, 0, 1-99]", range: "-1..99", defaultValue: -1, displayDuringSetup: true, required: false
+		input "LiteMin", "number", title: "Luminance Report Frequency", description: "Luminance report sent every N minutes [0-127]", range: "0..127", defaultValue: 6, displayDuringSetup: true, required: false
+		input "TempMin", "number", title: "Temperature Report Frequency", description: "Temperature report sent every N minutes [0-127]", range: "0..127", defaultValue: 6, displayDuringSetup: true, required: false
+		input "TempAdj", "number", title: "Temperature Calibration", description: "Adjust temperature up/down N tenths of a degree F [(-127)-(+128)]", range: "-127..128", defaultValue: 0, displayDuringSetup: true, required: false
+		input("lum", "enum", title:"Illuminance Measurement", description: "Percent or Lux", defaultValue: 2 ,required: false, displayDuringSetup: true, options: [1:"Percent", 2:"Lux"])
+	}
+}
 
 private getRED() { "red" }
 private getGREEN() { "green" }
@@ -127,12 +122,12 @@ def installed() {
 }
 
 def updated() {
-    log.debug "updated() is being called"
-    def cmds = configure()
+	log.debug "updated() is being called"
+	def cmds = configure()
 
 	setupHealthCheck()
 
-    if (cmds != []) response(cmds)
+	if (cmds != []) response(cmds)
 }
 
 // Parse incoming device messages from device to generate events
@@ -143,85 +138,82 @@ def parse(String description) {
 	if (!state.colorReceived) {
 		state.colorReceived = [red: null, green: null, blue: null]
 	}
-	//log.debug "==> New Zwave Event: ${description}"
+
 	def result = []
 	def cmd = zwave.parse(description, [0x31: 5]) // 0x31=SensorMultilevel which we force to be version 5
 	if (cmd) {
 		result = zwaveEvent(cmd)
 	}
-    
-    def statusTextmsg = ""
-    if (device.currentState('temperature') != null && device.currentState('illuminance') != null) {
-		statusTextmsg = "${device.currentState('temperature').value}° - ${device.currentState('illuminance').value}${(lum == 1) ? "%" : "LUX"}"
-        result << createEvent("name":"statusText", "value":statusTextmsg, displayed:false)
+
+	def statusTextmsg = ""
+	if (device.currentState("temperature") != null && device.currentState("illuminance") != null) {
+		statusTextmsg = "${device.currentState("temperature").value}° - ${device.currentState("illuminance").value}${(lum == 1) ? "%" : "LUX"}"
+		result << createEvent("name":"statusText", "value":statusTextmsg, displayed:false)
 	}
-    log.debug "Parse returned ${result}"
-    
-    
+	log.debug "Parse returned ${result}"
+
 	return result
 }
 
 
 // Event Generation
-def zwaveEvent(physicalgraph.zwave.commands.sensormultilevelv5.SensorMultilevelReport cmd){
+def zwaveEvent(sensormultilevelv5.SensorMultilevelReport cmd) {
 	def map = [:]
-	switch (cmd.sensorType) {
-		case 0x01:				// SENSOR_TYPE_TEMPERATURE_VERSION_1
-			def cmdScale = cmd.scale == 1 ? "F" : "C"
-			map.value = convertTemperatureIfNeeded(cmd.scaledSensorValue, cmdScale, cmd.precision)
-			map.unit = getTemperatureScale()
-			map.name = "temperature"
-			log.debug "Temperature report"
-			break;
-		case 0x03 :				// SENSOR_TYPE_LUMINANCE_VERSION_1
-			map.value = cmd.scaledSensorValue.toInteger().toString()
-            if(lum == 1) map.unit = "%"
-            else map.unit = "lux"
-			map.name = "illuminance"
-			log.debug "Luminance report"
-			break;
+	if (cmd.sensorType == sensormultilevelv5.SensorMultilevelReport.SENSOR_TYPE_TEMPERATURE_VERSION_1) {
+		def cmdScale = cmd.scale == 1 ? "F" : "C"
+		map.value = convertTemperatureIfNeeded(cmd.scaledSensorValue, cmdScale, cmd.precision)
+		map.unit = getTemperatureScale()
+		map.name = "temperature"
+		log.debug "Temperature report"
+	} else if (cmd.sensorType == sensormultilevelv5.SensorMultilevelReport.SENSOR_TYPE_LUMINANCE_VERSION_1) {
+		map.value = cmd.scaledSensorValue.toInteger().toString()
+		map.unit = (lum == 1) ? "%" : "lux"
+		map.name = "illuminance"
+		log.debug "Luminance report"
 	}
 	return [createEvent(map)]
 }
 
-def zwaveEvent(physicalgraph.zwave.commands.configurationv2.ConfigurationReport cmd) {
-    log.debug "${device.displayName} parameter '${cmd.parameterNumber}' with a byte size of '${cmd.size}' is set to '${cmd.configurationValue}'"
-    []
+def zwaveEvent(configurationv2.ConfigurationReport cmd) {
+	log.debug "${device.displayName} parameter '${cmd.parameterNumber}' with a byte size of '${cmd.size}' is set to '${cmd.configurationValue}'"
+	[]
 }
 
-def zwaveEvent(physicalgraph.zwave.commands.notificationv3.NotificationReport cmd) {
+def zwaveEvent(notificationv3.NotificationReport cmd) {
 	def map = [:]
-	if (cmd.notificationType==0x07) {	// NOTIFICATION_TYPE_BURGLAR
-    		if (cmd.event==0x07 || cmd.event==0x08) {
-				map.name = "motion"
-            	map.value = "active"
-				map.descriptionText = "$device.displayName motion detected"
-            	log.debug "motion recognized"
-		} else if (cmd.event==0) {
-				map.name = "motion"
-            	map.value = "inactive"
-				map.descriptionText = "$device.displayName no motion detected"
-            	log.debug "No motion recognized"
-    		}
+	if (cmd.notificationType == notificationv3.NotificationReport.NOTIFICATION_TYPE_BURGLAR) {
+		if (cmd.event == 0x07 || cmd.event == 0x08) {
+			map.name = "motion"
+			map.value = "active"
+			map.descriptionText = "$device.displayName motion detected"
+			log.debug "motion recognized"
+		} else if (cmd.event == 0) {
+			map.name = "motion"
+			map.value = "inactive"
+			map.descriptionText = "$device.displayName no motion detected"
+			log.debug "No motion recognized"
+		}
 	} 
 	if (map.name != "motion") {
-    		log.debug "unmatched parameters for cmd: ${cmd.toString()}}"
+			log.debug "unmatched parameters for cmd: ${cmd.toString()}}"
 	}
 	return [createEvent(map)]
 }
 
-def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicReport cmd) {
-    def result = []
-    // The EZMultiPli sets the color back to #ffffff on "off" or at init, so update the ST device to reflect this.
-    if (device.latestState("color") == null || (cmd.value == 0 && device.latestState("color").value != "#ffffff")) {
-        result << createEvent(name: "color", value: "#ffffff", displayed: true)
-    }
-    result << createEvent(name: "switch", value: cmd.value ? "on" : "off", type: "digital")
+def zwaveEvent(basicv1.BasicReport cmd) {
+	def result = []
+	// The EZMultiPli sets the color back to #ffffff on "off" or at init, so update the ST device to reflect this.
+	if (device.latestState("color") == null || (cmd.value == 0 && device.latestState("color").value != "#ffffff")) {
+		result << createEvent(name: "color", value: "#ffffff")
+		result << createEvent(name: "hue", value: 0)
+		result << createEvent(name: "saturation", value: 0)
+	}
+	result << createEvent(name: "switch", value: cmd.value ? "on" : "off")
 
-    result
+	result
 }
 
-def zwaveEvent(physicalgraph.zwave.commands.switchcolorv3.SwitchColorReport cmd) {
+def zwaveEvent(switchcolorv3.SwitchColorReport cmd) {
 	log.debug "got SwitchColorReport: $cmd"
 	state.colorReceived[cmd.colorComponent] = cmd.value
 
@@ -271,58 +263,51 @@ def channelValue(channel) {
 }
 
 def setColor(value) {
-    log.debug "setColor() : ${value}"
+	log.debug "setColor() : ${value}"
 	def hue
 	def saturation
-    def myred
-    def mygreen
-    def myblue
-    def hexValue
-    def cmds = []
+	def myred
+	def mygreen
+	def myblue
+	def hexValue
+	def cmds = []
 
-    // The EZMultiPli has just on/off for each of the 3 channels RGB so convert the 0-255 value into 0 or 255.
-    if (value.containsKey("hue") && value.containsKey("saturation")) {
-        def level = (value.containsKey("level")) ? value.level : 100
+	// The EZMultiPli has just on/off for each of the 3 channels RGB so convert the 0-255 value into 0 or 255.
+	if (value.containsKey("hue") && value.containsKey("saturation")) {
+		def level = (value.containsKey("level")) ? value.level : 100
 		hue = value.hue as Integer
 		saturation = value.saturation as Integer
 
-        if (level == 1 && saturation > 20) {
+		if (level == 1 && saturation > 20) {
 			saturation = 100
-        }
+		}
 
-        if (level >= 1) {
-            def rgb = huesatToRGB(hue, saturation)
-            myred = channelValue(rgb[0])
-            mygreen = channelValue(rgb[1])
-            myblue = channelValue(rgb[2])
-        }
-    } else if (value.hex) {
+		if (level >= 1) {
+			def rgb = huesatToRGB(hue, saturation)
+			myred = channelValue(rgb[0])
+			mygreen = channelValue(rgb[1])
+			myblue = channelValue(rgb[2])
+		}
+	} else if (value.hex) {
 		def rgb = value.hex.findAll(/[0-9a-fA-F]{2}/).collect { Integer.parseInt(it, 16) }
-        myred = channelValue(rgb[0])
-        mygreen = channelValue(rgb[1])
-        myblue = channelValue(rgb[2])
-    }
-    // red, green, blue is not part of the capability definition, but it was possibly used by old SmartApps.
-    // It should be safe to leave this in here for now.
-    else if (value.containsKey("red") && value.containsKey("green") && value.containsKey("blue")) {
-        myred = channelValue(value.red)
-        mygreen = channelValue(value.green)
-        myblue = channelValue(value.blue)
-    } else {
-        return
-    }
-    log.debug "Red: ${myred} Green: ${mygreen} Blue: ${myblue}"
+		myred = channelValue(rgb[0])
+		mygreen = channelValue(rgb[1])
+		myblue = channelValue(rgb[2])
+	}
+	// red, green, blue is not part of the capability definition, but it was possibly used by old SmartApps.
+	// It should be safe to leave this in here for now.
+	else if (value.containsKey("red") && value.containsKey("green") && value.containsKey("blue")) {
+		myred = channelValue(value.red)
+		mygreen = channelValue(value.green)
+		myblue = channelValue(value.blue)
+	} else {
+		return
+	}
 
 	cmds << zwave.switchColorV3.switchColorSet(red: myred, green: mygreen, blue: myblue)
-    cmds << zwave.basicV1.basicGet()
+	cmds << zwave.basicV1.basicGet()
 
-    // TODO: Right now we will generate the events, in the future we should get them
-    /*hexValue = colorUtil.rgbToHex(myred, mygreen, myblue)
-	if (hexValue) sendEvent(name: "color", value: hexValue)
-	sendEvent(name: "hue", value: hue)
-	sendEvent(name: "saturation", value: saturation)*/
-
-    commands(cmds + queryAllColors(), 100)
+	commands(cmds + queryAllColors(), 100)
 }
 
 private queryAllColors() {
@@ -332,24 +317,24 @@ private queryAllColors() {
 
 def validateSetting(value, minVal, maxVal, defaultVal) {
 	if (value == null) {
-        value = defaultVal
-    }
+		value = defaultVal
+	}
 	def adjVal = value.toInteger()
 	if (adjVal < minVal) { // bad value, set to default
-        adjVal = defaultVal
+		adjVal = defaultVal
 	} else if (adjVal > maxVal) { // bad value, greater then MAX, set to MAX
-        adjVal = maxVal
-    }
+		adjVal = maxVal
+	}
 
 	adjVal
 }
 
 def refresh() {
 	def cmd = queryAllColors()
-    cmd << zwave.sensorMultilevelV5.sensorMultilevelGet(sensorType:1, scale:1)
-    cmd << zwave.sensorMultilevelV5.sensorMultilevelGet(sensorType:3, scale:1)
-    cmd << zwave.basicV1.basicGet()
-    commands(cmd, 1000)
+	cmd << zwave.sensorMultilevelV5.sensorMultilevelGet(sensorType:1, scale:1)
+	cmd << zwave.sensorMultilevelV5.sensorMultilevelGet(sensorType:3, scale:1)
+	cmd << zwave.basicV1.basicGet()
+	commands(cmd, 1000)
 }
 
 def ping() {
@@ -359,7 +344,7 @@ def ping() {
 
 def configure() {
 	log.debug "OnTime=${settings.OnTime} OnLevel=${settings.OnLevel} TempAdj=${settings.TempAdj}"
-	def cmd = commands([
+	def cmds = commands([
 		zwave.configurationV1.configurationSet(parameterNumber: 1, size: 1, scaledConfigurationValue: validateSetting(settings.OnTime, 0, 127, 2)),
 		zwave.configurationV1.configurationSet(parameterNumber: 2, size: 1, scaledConfigurationValue: validateSetting(settings.OnLevel, -1, 99, -1)),
 		zwave.configurationV1.configurationSet(parameterNumber: 3, size: 1, scaledConfigurationValue: validateSetting(settings.LiteMin, 0, 127, 6)),
@@ -371,8 +356,8 @@ def configure() {
 		zwave.configurationV1.configurationGet(parameterNumber: 4),
 		zwave.configurationV1.configurationGet(parameterNumber: 5)
 	], 100)
-	//log.debug cmd
-	cmd + refresh()
+
+	cmds + refresh()
 }
 
 private secEncap(physicalgraph.zwave.Command cmd) {
@@ -386,7 +371,7 @@ private crcEncap(physicalgraph.zwave.Command cmd) {
 private command(physicalgraph.zwave.Command cmd) {
 	if (zwaveInfo.zw.contains("s")) {
 		secEncap(cmd)
-	} else if (zwaveInfo.cc.contains("56")){
+	} else if (zwaveInfo.cc.contains("56")) {
 		crcEncap(cmd)
 	} else {
 		cmd.format()
