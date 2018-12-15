3a21b669f7d7734a3d4417944adba1bae79c8601
Fen Mei <f.mei@samsung.com>
2018-12-15 17:00:25 +0800

    Add battery report  and fix some format issue

--- devicetypes/smartthings/zigbee-water-leak.src/zigbee-water-leak.groovy
+++ devicetypes/smartthings/zigbee-water-leak.src/zigbee-water-leak.groovy
@@ -23,127 +23,135 @@ import physicalgraph.zigbee.clusters.iaszone.ZoneStatus
 import physicalgraph.zigbee.zcl.DataType
 
 metadata {
-	definition(name: "Zigbee Water Leak Sensor", namespace: "smartthings", author: "SmartThings", vid: "generic-leak", mnmn:"SmartThings", ocfDeviceType: "x.com.st.d.sensor.moisture") {
-		capability "Configuration"
-		capability "Refresh"
-		capability "Water Sensor"
-		capability "Sensor"
-		capability "Health Check"
-		capability "Battery"
+    definition(name: "Zigbee Water Leak Sensor", namespace: "smartthings", author: "SmartThings", vid: "SmartThings-smartthings-Xiaomi_Aqara_Leak_Sensor-2", mnmn:"SmartThings", ocfDeviceType: "x.com.st.d.sensor.moisture") {
+        capability "Configuration"
+        capability "Refresh"
+        capability "Water Sensor"
+        capability "Sensor"
+        capability "Health Check"
+        capability "Battery"
 
-		fingerprint profileId: "0104", deviceId: "0402", inClusters: "0000,0003,0500", manufacturer: "LUMI", model: "lumi.flood.agl02"
-	}
+        fingerprint profileId: "0104", deviceId: "0402", inClusters: "0000, 0003, 0500", manufacturer: "LUMI", model: "lumi.flood.agl02", deviceJoinName: "LUMI Water Leak Sensor"
+    }
 
-	simulator {
+    simulator {
 
-		status "dry": "zone status 0x0020 -- extended status 0x00"
-		status "wet": "zone status 0x0021 -- extended status 0x00"
+        status "dry": "zone status 0x0020 -- extended status 0x00"
+        status "wet": "zone status 0x0021 -- extended status 0x00"
 
-		for (int i = 0; i <= 90; i += 10) {
-			status "battery 0021 0x${i}": "read attr - raw: 8C900100010A21000020C8, dni: 8C90, endpoint: 01, cluster: 0001, size: 0A, attrId: 0021, result: success, encoding: 20, value: ${i}"
-		}
-	}
+        for (int i = 0; i <= 90; i += 10) {
+            status "battery 0021 0x${i}": "read attr - raw: 8C900100010A21000020C8, dni: 8C90, endpoint: 01, cluster: 0001, size: 0A, attrId: 0021, result: success, encoding: 20, value: ${i}"
+        }
+    }
 
-	tiles(scale: 2) {
-		multiAttributeTile(name:"water", type: "generic", width: 6, height: 4){
-			tileAttribute ("device.water", key: "PRIMARY_CONTROL") {
-				attributeState "dry", icon:"st.alarm.water.dry", backgroundColor:"#ffffff"
-				attributeState "wet", icon:"st.alarm.water.wet", backgroundColor:"#00a0dc"
-			}
-		}
+    tiles(scale: 2) {
+        multiAttributeTile(name:"water", type: "generic", width: 6, height: 4){
+            tileAttribute ("device.water", key: "PRIMARY_CONTROL") {
+                attributeState "dry", icon:"st.alarm.water.dry", backgroundColor:"#ffffff"
+                attributeState "wet", icon:"st.alarm.water.wet", backgroundColor:"#00a0dc"
+            }
+        }
 
-		valueTile("battery", "device.battery", decoration: "flat", inactiveLabel: false, width: 2, height: 2) {
-			state "battery", label:'${currentValue}% battery', unit:""
-		}
+        valueTile("battery", "device.battery", decoration: "flat", inactiveLabel: false, width: 2, height: 2) {
+            state "battery", label:'${currentValue}% battery', unit:""
+        }
 
-		standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
-			state "default", action: "refresh.refresh", icon: "st.secondary.refresh"
-		}
+        standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+            state "default", action: "refresh.refresh", icon: "st.secondary.refresh"
+        }
 
-		main "water"
-		details(["water", "battery", "refresh"])
-	}
+        main "water"
+        details(["water", "battery", "refresh"])
+    }
 }
 
 def parse(String description) {
-	log.debug "description: $description"
+    log.debug "description: $description"
 
-	def result
-	Map map = zigbee.getEvent(description)
+    def result
+    Map map = zigbee.getEvent(description)
 
-	if (!map) {
-		if (description?.startsWith('zone status')) {
-			map = getMoistureResult(description)
-		} else if(description?.startsWith('enroll request')){
-			List cmds = zigbee.enrollResponse()
-			log.debug "enroll response: ${cmds}"
-			result = cmds?.collect { new physicalgraph.device.HubAction(it) }
-		}else {
-			Map descMap = zigbee.parseDescriptionAsMap(description)
+    if (!map) {
+        if (description?.startsWith('zone status')) {
+            map = getMoistureResult(description)
+        } else if (description?.startsWith('enroll request')) {
+            List cmds = zigbee.enrollResponse()
+            log.debug "enroll response: ${cmds}"
+            result = cmds?.collect { new physicalgraph.device.HubAction(it) }
+        } else {
+            Map descMap = zigbee.parseDescriptionAsMap(description)
             log.debug "${descMap}"
-			if (descMap?.clusterInt == 0x0500 && descMap.attrInt == 0x0002) {
-				map = getMoistureResult(description)
-			} else if (descMap?.clusterInt == 0x0001 && descMap?.attrInt == 0x0021 && descMap?.commandInt != 0x07 && descMap?.value) {
-				map = getBatteryPercentageResult(Integer.parseInt(descMap.value, 16))
-			}
+            if (descMap?.clusterInt == 0x0500 && descMap.attrInt == 0x0002) {
+            	if (device.getDataValue("manufacturer") == "LUMI") {
+                	sendBatteryResult(description)
+                }
+                map = getMoistureResult(description)
+            } 
+        }
+    }
+    
+    if (map&&!result) {
+        if (description?.startsWith('zone status')) {
+            if (device.getDataValue("manufacturer") == "LUMI") {
+                sendBatteryResult(description)
+            }
 		}
-	}
-	if(map&&!result){
-		result = createEvent(map)
-	}
-	log.debug "Parse returned $result"
+        result = createEvent(map)
+    }
+    
+    log.debug "Parse returned $result"
 
-	result
+    result
 }
 
 def ping() {
-	refresh()
+    refresh()
 }
 
 def refresh() {
-	log.debug "Refreshing Values"
-	def refreshCmds = []
-	refreshCmds += zigbee.enrollResponse()
+    log.debug "Refreshing Values"
+    def refreshCmds = []
+    refreshCmds += readAttribute(zigbee.IAS_ZONE_CLUSTER, zigbee.ATTRIBUTE_IAS_ZONE_STATUS)
 
-	refreshCmds
+    return refreshCmds
 }
 
 def installed(){
-	log.debug "call installed()"
-	sendEvent(name: "checkInterval", value: 20 * 60 + 1 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
+    log.debug "call installed()"
+    sendEvent(name: "checkInterval", value: 30 * 60 + 1 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
 }
 
 def configure() {
-	sendEvent(name: "checkInterval", value: 20 * 60 + 1 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
-
-	log.debug "Configuring Reporting"
-	                
-	return refresh() 
+    sendEvent(name: "checkInterval", value: 30 * 60 + 1 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
+       
+    return zigbee.enrollResponse() + zigbee.iasZoneConfig(30, 60 * 30)
 }
 
 def getMoistureResult(description) {
-	ZoneStatus zs = zigbee.parseZoneStatus(description)
-	def value = zs?.isAlarm1Set()?"wet":"dry"
-	[
-		name           : 'water',
-		value          : value,
-		descriptionText: "${device.displayName} is $value",
-		translatable   : true
-	]
+    ZoneStatus zs = zigbee.parseZoneStatus(description)
+    def value = zs.isAlarm1Set()?"wet":"dry"
+    [
+        name           : 'water',
+        value          : value,
+        descriptionText: "${device.displayName} is $value",
+        translatable   : true
+    ]
 }
 
-def getBatteryPercentageResult(rawValue) {
-	log.debug "Battery Percentage"
-	def result = [:]
+/*
+ * LUMI IAS sensors use cluster 0x0500, attr 0x0002, the third bit to report battery normal or low.
+ * To adjust it,dicuessed with UI metadata developer, send battery 50% to indicate battery normal, 5% to indicate battery low.
+ */
 
-	if (0 <= rawValue && rawValue <= 200) {
-		result.name = 'battery'
-		result.translatable = true
-		result.value = Math.round(rawValue / 2)
-		result.descriptionText = "${device.displayName} battery was ${result.value}%"
-	}
-
-	log.debug "${device.displayName} battery was ${result.value}%"
-	result
+def sendBatteryResult(description) {
+    def result = [:]
+    ZoneStatus zs = zigbee.parseZoneStatus(description)
+    def value = zs.isBatterySet()?5:50
+    
+    result.name = 'battery'
+    result.value = value
+    result.descriptionText = "${device.displayName} battery value is ${value}"
+    result.translatable = true
+    
+    sendEvent(result)
 }
-
