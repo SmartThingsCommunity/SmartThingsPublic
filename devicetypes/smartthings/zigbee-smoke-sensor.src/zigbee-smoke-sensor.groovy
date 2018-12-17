974259494ddeaf9a17165988c40cfdbef8061edb
BYDengS <40649638+BYDengS@users.noreply.github.com>
2018-12-18 06:52:03 +0800

    ICP-7455 change the name of orvibo gas sensor and orvibo smoke sensor to chinese. (#3798)

--- devicetypes/smartthings/zigbee-smoke-sensor.src/zigbee-smoke-sensor.groovy
+++ devicetypes/smartthings/zigbee-smoke-sensor.src/zigbee-smoke-sensor.groovy
@@ -28,8 +28,8 @@ metadata {
 		capability "Refresh"
 		capability "Health Check"
 
-		fingerprint profileId: "0104", deviceId: "0402", inClusters: "0000,0001,0003,0500,0502,0009", outClusters: "0019", manufacturer: "Heiman", model: "b5db59bfd81e4f1f95dc57fdbba17931", deviceJoinName: "Orvibo Smoke Sensor"
-		fingerprint profileId: "0104", deviceId: "0402", inClusters: "0000,0001,0003,0500,0502,0009", outClusters: "0019", manufacturer: "HEIMAN", model: "98293058552c49f38ad0748541ee96ba", deviceJoinName: "Orvibo Smoke Sensor"
+		fingerprint profileId: "0104", deviceId: "0402", inClusters: "0000,0001,0003,0500,0502,0009", outClusters: "0019", manufacturer: "Heiman", model: "b5db59bfd81e4f1f95dc57fdbba17931", deviceJoinName: "欧瑞博 烟雾报警器(SF21)"
+		fingerprint profileId: "0104", deviceId: "0402", inClusters: "0000,0001,0003,0500,0502,0009", outClusters: "0019", manufacturer: "HEIMAN", model: "98293058552c49f38ad0748541ee96ba", deviceJoinName: "欧瑞博 烟雾报警器(SF21)"
 	}
 
 	tiles {
