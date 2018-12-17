5131d5682dfd5f4b552f086f17f755a9d1b1ed08
Fen Mei <f.mei@samsung.com>
2018-12-17 13:23:08 +0800

    parse sensor status and battery at the same time by using list result map

--- devicetypes/smartthings/zigbee-water-leak.src/zigbee-water-leak.groovy
+++ devicetypes/smartthings/zigbee-water-leak.src/zigbee-water-leak.groovy
@@ -66,42 +66,122 @@ metadata {
 }
 
 def parse(String description) {
-    log.debug "description: $description"
+	Map map = [:]
 
-    def result
-    Map map = zigbee.getEvent(description)
+	List listMap = []
+	List listResult = []
 
-    if (!map) {
-        if (description?.startsWith('zone status')) {
-            map = getMoistureResult(description)
-        } else if (description?.startsWith('enroll request')) {
-            List cmds = zigbee.enrollResponse()
-            log.debug "enroll response: ${cmds}"
-            result = cmds?.collect { new physicalgraph.device.HubAction(it) }
-        } else {
-            Map descMap = zigbee.parseDescriptionAsMap(description)
-            log.debug "${descMap}"
-            if (descMap?.clusterInt == 0x0500 && descMap.attrInt == 0x0002) {
-            	if (device.getDataValue("manufacturer") == "LUMI") {
-                	sendBatteryResult(description)
-                }
-                map = getMoistureResult(description)
-            } 
-        }
-    }
-    
-    if (map&&!result) {
-        if (description?.startsWith('zone status')) {
-            if (device.getDataValue("manufacturer") == "LUMI") {
-                sendBatteryResult(description)
-            }
+	log.debug "parse: Parse message: ${description}"
+
+	if (description?.startsWith("enroll request")) {
+		List cmds = enrollResponse()
+
+		log.debug "parse: enrollResponse() ${cmds}"
+		listResult = cmds?.collect { new physicalgraph.device.HubAction(it) }
+	}
+	else {
+		if (description?.startsWith("zone status")) {
+			listMap = parseIasMessage(description)
 		}
-        result = createEvent(map)
-    }
-    
-    log.debug "Parse returned $result"
+		else if (description?.startsWith("catchall:")) {
+			map = parseCatchAllMessage(description)
+		}
 
-    result
+		// Create events from map or list of maps, whichever was returned
+		if (listMap) {
+			for (msg in listMap) {
+				listResult << createEvent(msg)
+			}
+		}
+		else if (map) {
+			listResult << createEvent(map)
+		}
+	}
+
+	log.debug "parse: listResult ${listResult}"
+	return listResult
+}
+
+private Map parseCatchAllMessage(String description) {
+	Map resultMap = [:]
+	def cluster = zigbee.parse(description)
+
+	if (shouldProcessMessage(cluster)) {
+		def msgStatus = cluster.data[2]
+
+		log.debug "parseCatchAllMessage: msgStatus: ${msgStatus}"
+		if (msgStatus == 0) {
+			switch(cluster.clusterId) {
+				case 0x0500:
+                                        Map descMap = zigbee.parseDescriptionAsMap(description)
+					if (descMap?.attrInt == 0x0002) {
+						resultMap.name = "water"
+						def zs = new ZoneStatus(zigbee.convertToInt(descMap.value, 16))
+						resultMap.value = zs.isAlarm1Set() ? "wet":"dry"
+					}
+					break
+				default:
+					break
+			}
+		}
+		else {
+			log.debug "parseCatchAllMessage: Message error code: Error code: ${msgStatus}    ClusterID: ${cluster.clusterId}    Command: ${cluster.command}"
+		}
+	}
+
+	return resultMap
+}
+
+private boolean shouldProcessMessage(cluster) {
+	// 0x0B is default response indicating message got through
+	// 0x07 is bind message
+	boolean ignoredMessage = cluster.profileId != 0x0104 ||
+			cluster.command == 0x0B ||
+			cluster.command == 0x07 ||
+			(cluster.data.size() > 0 && cluster.data.first() == 0x3e)
+
+	return !ignoredMessage
+}
+
+private List parseIasMessage(String description) {
+	ZoneStatus zs = zigbee.parseZoneStatus(description)
+	log.debug "parseIasMessage: $description"
+
+	List resultListMap = []
+	Map resultMap_battery = [:]
+	Map resultMap_sensor = [:]
+
+	resultMap_sensor.name = "water"
+	resultMap_sensor.value = zs.isAlarm1Set() ? "wet":"dry"
+
+	// Check each relevant bit, create map for it, and add to list
+	log.debug "parseIasMessage: Battery Status ${zs.battery}"
+	log.debug "parseIasMessage: Trouble Status ${zs.trouble}"
+	log.debug "parseIasMessage: Sensor Status ${zs.alarm1}"
+
+        //LUMI IAS sensor report battery ok or low instead of battery percentage. 
+        if(device.getDataValue("manufacturer") == "LUMI") {
+	    	 if (zs.isTroubleSet()) {
+	    		 resultMap_battery.name = "battery"
+	    		 resultMap_battery.value = 50
+	    	 }
+	    	 else {
+	    		 if (zs.isBatterySet()) {
+	    			 // to generate low battery notification by the platform
+	    			 resultMap_battery.name = "battery"
+	    			 resultMap_battery.value = 5
+	    		 }
+	    		 else {
+	    			 resultMap_battery.name = "battery"
+	    			 resultMap_battery.value = 50
+	    		 }
+	    	 }
+
+	    	 resultListMap << resultMap_battery
+    	}
+	resultListMap << resultMap_sensor
+
+	return resultListMap
 }
 
 def ping() {
@@ -118,40 +198,12 @@ def refresh() {
 
 def installed(){
     log.debug "call installed()"
-    sendEvent(name: "checkInterval", value: 30 * 60 + 1 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
+    sendEvent(name: "checkInterval", value: 20 * 60 + 1 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
 }
 
 def configure() {
-    sendEvent(name: "checkInterval", value: 30 * 60 + 1 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
-       
+    sendEvent(name: "checkInterval", value: 20 * 60 + 1 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
+                    
     return zigbee.enrollResponse() + zigbee.iasZoneConfig(30, 60 * 30)
 }
 
-def getMoistureResult(description) {
-    ZoneStatus zs = zigbee.parseZoneStatus(description)
-    def value = zs.isAlarm1Set()?"wet":"dry"
-    [
-        name           : 'water',
-        value          : value,
-        descriptionText: "${device.displayName} is $value",
-        translatable   : true
-    ]
-}
-
-/*
- * LUMI IAS sensors use cluster 0x0500, attr 0x0002, the third bit to report battery normal or low.
- * To adjust it,dicuessed with UI metadata developer, send battery 50% to indicate battery normal, 5% to indicate battery low.
- */
-
-def sendBatteryResult(description) {
-    def result = [:]
-    ZoneStatus zs = zigbee.parseZoneStatus(description)
-    def value = zs.isBatterySet()?5:50
-    
-    result.name = 'battery'
-    result.value = value
-    result.descriptionText = "${device.displayName} battery value is ${value}"
-    result.translatable = true
-    
-    sendEvent(result)
-}
