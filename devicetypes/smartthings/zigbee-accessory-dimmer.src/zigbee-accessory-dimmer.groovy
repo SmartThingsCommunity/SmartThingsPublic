117be0a505d8801f646e80312a9df9a862157f68
MMateusiakS <m.mateusiak@samsung.com>
2019-01-08 16:51:05 +0100

    [WWST-2200] Modify fingerprint for Aurora Wireless Wall Remote (#3861)
    New fingerprint based on email from Thomas Lane (Aurora)

--- devicetypes/smartthings/zigbee-accessory-dimmer.src/zigbee-accessory-dimmer.groovy
+++ devicetypes/smartthings/zigbee-accessory-dimmer.src/zigbee-accessory-dimmer.groovy
@@ -22,7 +22,7 @@ metadata {
 		capability "Switch Level"
 		capability "Configuration"
 
-		fingerprint profileId: "0104", inClusters: "0000,0003", outClusters: "0000,0004,0003,0006,0008,0005", manufacturer: "Aurora", model: "Remote50AU", deviceJoinName: "Aurora Wireless Wall Remote"
+		fingerprint profileId: "0104", inClusters: "0000,1000,0003", outClusters: "0003,0004,0005,0006,0008,1000,0019", manufacturer: "Aurora", model: "Remote50AU", deviceJoinName: "Aurora Wireless Wall Remote"
 	}
 
 	tiles(scale: 2) {
