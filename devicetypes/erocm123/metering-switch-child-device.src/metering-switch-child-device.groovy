6557030c6d5175081a317acbd85870bb8b6f1a4c
ERIC MAYCOCK <ericmaycock@gmail.com>
2019-02-21 11:33:49 -0700

    Adding vid for new Smartthings app

--- devicetypes/erocm123/metering-switch-child-device.src/metering-switch-child-device.groovy
+++ devicetypes/erocm123/metering-switch-child-device.src/metering-switch-child-device.groovy
@@ -14,7 +14,7 @@
  *
  */
 metadata {
-	definition (name: "Metering Switch Child Device", namespace: "erocm123", author: "Eric Maycock") {
+	definition (name: "Metering Switch Child Device", namespace: "erocm123", author: "Eric Maycock", vid:"generic-switch-power-energy") {
 		capability "Switch"
 		capability "Actuator"
 		capability "Sensor"
