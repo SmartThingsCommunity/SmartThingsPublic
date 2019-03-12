fca01670ae7eb43064c97765de66b321713184bd
lukhong <kiwook.hong@samsung.com>
2019-03-12 20:56:51 +0900

    Apply patch after the first review

--- devicetypes/ezex/somfy-blind-controller-ezex.src/somfy-blind-controller-ezex.groovy
+++ devicetypes/ezex/somfy-blind-controller-ezex.src/somfy-blind-controller-ezex.groovy
@@ -1,6 +1,6 @@
 /**
  *
- *	Copyright 2018 eZEX
+ *	Copyright 2019 eZEX
  *
  *	Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *	in compliance with the License. You may obtain a copy of the License at:
@@ -11,7 +11,7 @@
  *	on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *	for the specific language governing permissions and limitations under the License.
  */
- 
+
 metadata {
     definition(name: "SOMFY Blind Controller/eZEX", namespace: "eZEX", author: "eZEX Corp", vid:"SmartThings-smartthings-Somfy_WindowShade", ocfDeviceType: "oic.d.blind") {
         capability "Actuator"
@@ -20,86 +20,83 @@ metadata {
         capability "Window Shade"
         capability "Health Check"
         capability "Switch Level"
-        
+
         command "pause"
 
-        fingerprint profileId: "0104", inClusters: "0000, 0003, 0004, 0102", outClusters: "0019", deviceJoinName: "SOMFY Blind Controller/eZEX", model: "E2B0-KR000Z0-HA"// SY-IoT201-BD
+        fingerprint profileId: "0104", inClusters: "0000, 0003, 0004, 0102", outClusters: "0019", manufacturer: "eZEX", model: "E2B0-KR000Z0-HA" // SY-IoT201-BD
     }
 
 
     tiles(scale: 2) {
-         multiAttributeTile(name:"windowShade", type: "generic", width: 6, height: 4){
-            tileAttribute ("device.windowShade", key: "PRIMARY_CONTROL") {
-              attributeState "open", label:'Open', action:"close", icon:"http://www.ezex.co.kr/img/st/window_open.png", backgroundColor:"#00A0DC", nextState:"turningOff"
-              attributeState "close", label:'Closed', action:"open", icon:"http://www.ezex.co.kr/img/st/window_close.png", backgroundColor:"#ffffff", nextState:"turningOn"
-              attributeState "turningOn", label:'Opening',  action:"close", icon:"http://www.ezex.co.kr/img/st/window_open.png", backgroundColor:"#00A0DC", nextState:"turningOff"
-              attributeState "turningOff", label:'Closing', icon:"http://www.ezex.co.kr/img/st/window_close.png", backgroundColor:"#ffffff", nextState:"turningOn"  
-              //attributeState "partially open", label:'Partially open', action: "switch.off", icon:"http://www.ezex.co.kr/img/st/window_open.png", backgroundColor:"#d45614", nextState: "turningOff"
-            }         
-        }
-        standardTile("home", "device.level", width: 2, height: 2, decoration: "flat") {
-          state "default", label: "home", action:"presetPosition", icon:"st.Home.home2"
+        multiAttributeTile(name:"windowShade", type: "generic", width: 6, height: 4) {
+            tileAttribute("device.windowShade", key: "PRIMARY_CONTROL") {
+                attributeState "open", label: 'Open', action: "close", icon: "http://www.ezex.co.kr/img/st/window_open.png", backgroundColor: "#00A0DC", nextState: "closing"
+                attributeState "closed", label: 'Closed', action: "open", icon: "http://www.ezex.co.kr/img/st/window_close.png", backgroundColor: "#ffffff", nextState: "opening"
+                attributeState "partially open", label: 'Partially open', action: "close", icon: "http://www.ezex.co.kr/img/st/window_open.png", backgroundColor: "#d45614", nextState: "closing"
+                attributeState "opening", label: 'Opening', action: "pause", icon: "http://www.ezex.co.kr/img/st/window_open.png", backgroundColor: "#00A0DC", nextState: "partially open"
+                attributeState "closing", label: 'Closing', action: "pause", icon: "http://www.ezex.co.kr/img/st/window_close.png", backgroundColor: "#ffffff", nextState: "partially open"
+            }
         }
         standardTile("contPause", "device.switch", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
-        	state "pause", label:"", icon:'st.sonos.pause-btn', action:'pause', backgroundColor:"#cccccc"         
+            state "pause", label:"", icon:'st.sonos.pause-btn', action:'pause', backgroundColor:"#cccccc"
         }
         standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 1) {
-          state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
-        }      
+            state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
+        }
         valueTile("ShadeLevel", "device.level", width: 4, height: 1) {
-        	state "level", label: 'Shade is ${currentValue}% up', defaultState: true
+            state "level", label: 'Shade is ${currentValue}% up', defaultState: true
         }
-        
         controlTile("levelSliderControl", "device.level", "slider", width:2, height: 1, inactiveLabel: false) {
-        	state "level", action:"switch level.setLevel"
+            state "level", action:"switch level.setLevel"
         }
-        
-        preferences {
-          input "preset", "number", title: "Default half-open position (1-100). Springs Window Fashions users should consult their manuals.", defaultValue: 50, required: false, displayDuringSetup: false
-        }
-      
+
         main "windowShade"
         details(["windowShade", "contPause", "ShadeLevel", "levelSliderControl","refresh"])
     }
 }
 
+private getCLUSTER_WINDOW_COVERING() { 0x0102 }
+private getATTRIBUTE_POSITION_LIFT() { 0x0008 }
+
 // Parse incoming device messages to generate events
 def parse(String description) {
-
-    
     def parseMap = zigbee.parseDescriptionAsMap(description)
-    def event = zigbee.getEvent(description)
-    
     def attrs = parseMap.additionalAttrs
-    if( attrs != null ) { 
-				attrs.each { attr ->
-            if(attr.value == "64") { //open
-                sendEvent(name: "windowShade", value: "open")
-                sendEvent(name: "level", value: "100")
-            }
-            else if(attr.value == "00"){ //closed
-                sendEvent(name: "windowShade", value: "closed")
-                sendEvent(name: "level", value: "0")
-            } else {           	
-                sendEvent(name: "windowShade", value: "partially open")
-                sendEvent(name: "level", value: zigbee.convertHexToInt(attr.value))
+    log.info "attrs - ${attrs}"
+    if ( attrs != null ) {
+        attrs.each { attr ->
+            if(parseMap.clusterInt == CLUSTER_WINDOW_COVERING && attr.attrInt == ATTRIBUTE_POSITION_LIFT) {
+                if (attr.value == "64") { //open
+                    sendEvent(name: "windowShade", value: "open")
+                    sendEvent(name: "level", value: "100")
+                } else if (attr.value == "00") { //closed
+                    sendEvent(name: "windowShade", value: "closed")
+                    sendEvent(name: "level", value: "0")
+                } else {
+                    sendEvent(name: "windowShade", value: "partially open")
+                    sendEvent(name: "level", value: zigbee.convertHexToInt(attr.value))
+                }
             }
         }
-     }
+    }
 }
 
 def close() {
-	  log.info "LOG-MH-Shade: off()"
-      zigbee.command(0x0102, 0x01)
+    log.info "LOG-INFO-Shade: off()"
+    zigbee.command(CLUSTER_WINDOW_COVERING, 0x01)
 }
 
 def open() {
-	  log.info "LOG-MH-Shade: on()"
-      zigbee.command(0x0102, 0x00)
+    log.info "LOG-INFO-Shade: on()"
+    zigbee.command(CLUSTER_WINDOW_COVERING, 0x00)
 }
 
 def setLevel(data) {
-    zigbee.command(0x0102, 0x05, zigbee.convertToHexString(data, 2))
+    zigbee.command(CLUSTER_WINDOW_COVERING, 0x05, zigbee.convertToHexString(data, 2))
+}
+
+def pause() {
+    zigbee.command(CLUSTER_WINDOW_COVERING, 0x02)
 }
 
 /**
@@ -110,11 +107,12 @@ def ping() {
 }
 
 def refresh() {
-    zigbee.onOffRefresh() + zigbee.onOffConfig()   
+    def cmds =  zigbee.readAttribute(CLUSTER_WINDOW_COVERING, 0x00)
+    log.info "LOG-INFO-Shade: refresh() read covering information : ${cmds}"
 }
 
 def configure() {
     // Device-Watch allows 2 check-in misses from device + ping (plus 2 min lag time)
-    sendEvent(name: "checkInterval", value: 2 * 10 * 60 + 2 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
+    sendEvent(name: "checkInterval", value: 2 * 60 * 60 + 2 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
     log.debug "Configuring Reporting and Bindings."
 }
