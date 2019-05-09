5b3d5d3c51028eccef02f137c9eed74c83a828b1
greens <steven.green@smartthings.com>
2019-05-09 10:05:07 -0700

    revert copyright change

--- devicetypes/smartthings/fibaro-rgbw-controller.src/fibaro-rgbw-controller.groovy
+++ devicetypes/smartthings/fibaro-rgbw-controller.src/fibaro-rgbw-controller.groovy
@@ -7,7 +7,7 @@
  *	Author:				Todd Wackford
  *  Email:				todd@wackford.net
  *
- *  Copyright 2019 SmartThings
+ *  Copyright 2015 SmartThings
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
