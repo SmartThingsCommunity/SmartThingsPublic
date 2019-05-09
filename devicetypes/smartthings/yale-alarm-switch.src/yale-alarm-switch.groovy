180ccbf8702c181ed26aed5c92f2ab1eff7a41a0
Mark Cockcroft <mark.cockcroft@gmail.com>
2019-05-09 15:22:46 +0100

    updated to fix issue, refresh uses token and has error handler when token expired (run login), disarmed done as well

--- devicetypes/smartthings/yale-alarm-switch.src/yale-alarm-switch.groovy
+++ devicetypes/smartthings/yale-alarm-switch.src/yale-alarm-switch.groovy
@@ -1,35 +1,32 @@
 /**
-*Version 0.2 - Added guide to use on settings page  
-*Version 0.1 - First version arms/home/disarm the alarm
-*/
+ *Version 0.2 - Added guide to use on settings page
+ *Version 0.1 - First version arms/home/disarm the alarm
+ */
 preferences {
 	input("userName", "text", title: "Username", description: "Your username for Yale Home System")
 	input("password", "password", title: "Password", description: "Your Password for Yale Home System")
-    input description: "Once you have filled in your details \nUse “Switch off” to Disarm in any mode \nUse “Lock” to Home Arm (Arm Stay) \nUse “Switch on” to Fully Arm (Arm away).", title: "Guide", displayDuringSetup: false, type: "paragraph", element: "paragraph"
+	input description: "Once you have filled in your details \nUse “Switch off” to Disarm in any mode \nUse “Lock” to Home Arm (Arm Stay) \nUse “Switch on” to Fully Arm (Arm away).", title: "Guide", displayDuringSetup: false, type: "paragraph", element: "paragraph"
 
 }
 
 metadata {
-	definition (name: "YALE ALARM SWITCH", namespace: "smartthings", author: "Tapion1ives",ocfDeviceType: "oic.d.switch", mnmn: "SmartThings", vid: "generic-switch") {
+	definition (name: "Yale Alarm Switch", namespace: "smartthings", author: "Tapion1ives/Mark-C-uk/foyst") {
 
 		capability "Refresh"
 		capability "Switch"
 		capability "lock"
 		capability "Polling"
-    	capability "Health Check"
-	
-    	attribute "status", "string"
 
-}
+	}
 	tiles {
 		standardTile("mode", "device.mode", inactiveLabel: false, width: 2, height: 2) {
 			state ("default", label:'${currentValue}', defaultState: true, action: "device.refresh", icon:"st.security.alarm.alarm", backgroundColor:"#e86d13")
 			state ("Armed-Stay", label:'${name}', action: "switch.off", icon:"st.Home.home4", backgroundColor:"#00a0dc", nextState:"Disarming")
 			state ("Disarmed", label:'${name}', action: "lock", icon:"st.Home.home2", backgroundColor:"#ffffff", nextState:"Arming")
 			state ("Armed-Away", label:'${name}', action: "switch.off", icon:"st.Home.home3", backgroundColor:"#00a0dc", nextState:"Disarming")
-        	state ("Arming", label:'${name}', icon:"st.Home.home4", backgroundColor:"#cccccc")
+			state ("Arming", label:'${name}', icon:"st.Home.home4", backgroundColor:"#cccccc")
 			state ("Disarming", label:'${name}', icon:"st.Home.home2", backgroundColor:"#cccccc")
-     	}
+		}
 		standardTile("statusstay", "device.lock", inactiveLabel: false, decoration: "flat") {
 			state "default", label:'Arm Stay', action:"lock.lock", icon:"st.Home.home4"
 		}
@@ -43,34 +40,62 @@ metadata {
 			state "default", label:'', action:"refresh.refresh", icon:"st.secondary.refresh"
 		}
 
-	main (["mode"])
-	details(["mode", "statusaway", "statusstay", "statusdisarm", "refresh"])
+		main (["mode"])
+		details(["mode", "statusaway", "statusstay", "statusdisarm", "refresh"])
 	}
+}
+
+def baseUrl() {
+	return "https://mob.yalehomesystem.co.uk/yapi/"
+}
+def endpointToken() {
+	return "o/token/"
+}
+def endpointMode() {
+	return "api/panel/mode/"
+}
+def yaleAuthToken () {
+	return "VnVWWDZYVjlXSUNzVHJhcUVpdVNCUHBwZ3ZPakxUeXNsRU1LUHBjdTpkd3RPbE15WEtENUJ5ZW1GWHV0am55eGhrc0U3V0ZFY2p0dFcyOXRaSWNuWHlSWHFsWVBEZ1BSZE1xczF4R3VwVTlxa1o4UE5ubGlQanY5Z2hBZFFtMHpsM0h4V3dlS0ZBcGZzakpMcW1GMm1HR1lXRlpad01MRkw3MGR0bmNndQ=="
 }
 
 // ================================== Login/out Function. Returns cookie for rest of the functions =========
-def login(token) {
+def login() {
+	log.debug "Attempting to login"
 	def paramsLogin = [
-		uri: "https://www.yalehomesystem.co.uk/homeportal/api/login/check_login/",
-		body: [id:settings.userName , password: settings.password]
+			uri: baseUrl() + endpointToken(),
+			body: [grant_type: "password", username:settings.userName , password: settings.password],
+			headers: ['Authorization' : "Basic ${yaleAuthToken()}"],
+			requestContentType: "application/x-www-form-urlencoded",
+			contentType: "application/json"
 	]
 	httpPost(paramsLogin) { responseLogin ->
-			token = responseLogin.headers?.'Set-Cookie'?.split(";")?.getAt(0)
+		log.debug "Login response is $responseLogin.data"
+		state.accessToken = responseLogin.data?.access_token
+		state.refreshToken = responseLogin.data?.refresh_token
 	}
-    log.info "'$device' Logged in"
-	return token
-    
+	log.info "'$device' Logged in"
 }
-
-def logout(token) {
-	def paramsLogout = [
-		uri: "https://www.yalehomesystem.co.uk/homeportal/api/logout/",
-		headers: ['Cookie' : "${token}"]
-	]
-	httpPost(paramsLogout) { responseLogout ->
+//
+def error() {
+	state.errorcount = state.errorcount + 1
+    log.warn "$responsecode' - try getting new token, error count is ${state.errorcount}"
+    if (state.errorcount > 2){
+    	log.error "too many errors"
+        state.errorcount = 0
 	}
-    log.info "'$device' Logged out"
+    else {
+		login()
+	}
 }
+//def logout(token) {
+//    def paramsLogout = [
+//            uri: "https://www.yalehomesystem.co.uk/homeportal/api/logout/",
+//            headers: ['Cookie' : "${token}"]
+//    ]
+//    httpPost(paramsLogout) { responseLogout ->
+//    }
+//    log.info "'$device' Logged out"
+//}
 // ================================================ Login /out end ========================
 
 def poll() {
@@ -78,128 +103,153 @@ def poll() {
 	refresh()
 }
 
-def ping() {
-	log.debug "ping"
-	refresh()
-}
-
-def refresh(YaleAlarmState) {
-	def token = login(token)
+def refresh() {
+	//login()
+	def YaleAlarmState
+    def responsecode
 	def getPanelMetaDataAndFullStatus = [
-		uri: "https://www.yalehomesystem.co.uk/homeportal/api/panel/get_panel_mode",
-		body: [id:settings.userName , password: settings.password],
-		headers: ['Cookie' : "${token}"]
-		]
-	try {
-    	httpPost(getPanelMetaDataAndFullStatus) {	response -> 
-    		YaleAlarmState = response.data.message
-		log.debug "'$device' REFRESH - response = '$response.data.message' & $response"
+			uri: baseUrl() + endpointMode(),
+			headers: ['Authorization' : "Bearer ${state.accessToken}"]
+	]
+	httpGet(getPanelMetaDataAndFullStatus) {	response ->
+		log.debug "'$device' REFRESH - response = '${response.data.data[0].mode}, ${response.status}"
+        YaleAlarmState = response.data.data.getAt(0)
+        responsecode = response.status
         }
-    }
-    catch (e) {
-    	log.error " refersh response $e or $response"
-    }
-	if (YaleAlarmState.mode.contains("arm")) {
-        state.mode = 'Armed-Away'
-	} 
-	else if (YaleAlarmState.mode.contains("home")) {
-        state.mode = 'Armed-Stay'
-	}
-	else if (YaleAlarmState.mode.contains("disarm")) {
-    	state.mode = 'Disarmed'
-  	}
-  	else { //if (YaleAlarmState.mode.contains("system.permission_denied")) {
-  		log.warn "system off line / Error, response= '$YaleAlarmState'"
-  		sendEvent(name: "status", value: "offline", displayed: false)
-        state.mode = YaleAlarmState
-        runIn(30,refresh)
+        if (responsecode != 200) {
+        	state.errorcount = state.errorcount + 1
+    		log.warn "${responsecode}' - try getting new token, error count is ${state.errorcount}"
+    		if (state.errorcount > 2){
+    			log.error "too many errors"
+        		state.errorcount = 0
+			}
+    		else {
+				login()
+            	runIn(05, refresh)
+            }
+        }
+        else {
+        	state.errorcount = 0
+			//YaleAlarmState = response.data.data.getAt(0)
+		
+        if (YaleAlarmState.mode.equals("arm")) {
+            state.mode = 'Armed-Away'
+        }
+        else if (YaleAlarmState.mode.equals("home")) {
+            state.mode = 'Armed-Stay'
+        }
+        else if (YaleAlarmState.mode.equals("disarm")) {
+            state.mode = 'Disarmed'
+        }
+        else { //if (YaleAlarmState.mode.contains("system.permission_denied")) {
+            log.warn "system off line / Error, response= '$YaleAlarmState'"
+            state.mode = YaleAlarmState
+            runIn(30,refresh)
+		}
  	}
-  	logout(token)
-    log.info "'$device' REFRESH - Mode is '$state.mode', Response- '$YaleAlarmState' complete"
+//    logout(token)
+	log.info "'$device' REFRESH - Mode is '$state.mode', Response- '$YaleAlarmState' complete"
 	sendEvent(name: "mode", value: state.mode, displayed: true, descriptionText: "Refresh - mode is '$state.mode', response '$YaleAlarmState'")
-  	sendEvent(name: "refresh", value: YaleAlarmState, displayed: true, isStateChange:true)
-    sendEvent(name: "status", value: "online", displayed: false)
-    runEvery3Hours(refresh)
-    return YaleAlarmState
+	runEvery3Hours(refresh)
+	return YaleAlarmState
 }
 
 // ===================  Arm Function. Performs arming function ====================
 def armAway() {
 	def reply = ''
-	def token = login(token)
 	def paramsArm = [
-		uri: "https://www.yalehomesystem.co.uk/homeportal/api/panel/set_panel_mode?area=1&mode=arm",
-		body: [id:settings.userName , password: settings.password],
-		headers: ['Cookie' : "${token}"]
+			uri: baseUrl() + endpointMode(),
+			body: [area: 1, mode: "arm"],
+			headers: ['Authorization' : "Bearer ${state.accessToken}"],
+			requestContentType: "application/x-www-form-urlencoded",
+			contentType: "application/json"
 	]
-        httpPost(paramsArm) {	response -> // Arming Function in away mode
-        	reply = response.data.message
-            log.debug "AA - response = '$reply'"
-		}
-        if (reply != 'OK'){
-        	log.warn "$device - AA - Status '$reply'"
-            state.mode = reply
-            runIn(60,refresh)
-        }
-        else {
-        	state.mode = 'Armed-Away'
-        }
-    logout(token)
-    log.info "AA- Status is: '$reply' - mode '$state.mode', "
-    sendEvent(name: "mode", value: state.mode, displayed: true, descriptionText: "Arm(Away) System - '$reply', mode - '$state.mode'")
+	httpPost(paramsArm) {	response -> // Arming Function in away mode
+		reply = response.data.message
+		log.debug "AA - response = '$reply'"
+	}
+	if (reply != 'OK!'){
+		log.warn "$device - AA - Status '$reply'"
+		state.mode = reply
+		runIn(60,refresh)
+	}
+	else {
+		state.mode = 'Armed-Away'
+	}
+//    logout(token)
+	log.info "AA- Status is: '$reply' - mode '$state.mode', "
+	sendEvent(name: "mode", value: state.mode, displayed: true, descriptionText: "Arm(Away) System - '$reply', mode - '$state.mode'")
 }
 
 def armStay() {
 	def reply = ''
-	def token = login(token)
 	def paramsArm = [
-		uri: "https://www.yalehomesystem.co.uk/homeportal/api/panel/set_panel_mode?area=1&mode=home",
-		body: [id:settings.userName , password: settings.password],
-		headers: ['Cookie' : "${token}"]
+			uri: baseUrl() + endpointMode(),
+			body: [area: 1, mode: "home"],
+			headers: ['Authorization' : "Bearer ${state.accessToken}"],
+			requestContentType: "application/x-www-form-urlencoded",
+			contentType: "application/json"
 	]
-    httpPost(paramsArm) {	response -> // Arming Function in away mode
+	httpPost(paramsArm) {	response -> // Arming Function in away mode
 		reply = response.data.message
-        log.debug "AS - response '$response.data.message'"
+		log.debug "AS - response '$response.data.message'"
 	}
-        if (reply != 'OK'){
-        	log.warn "AS - response '$reply'"
-            state.mode = reply
-            
-            runIn(60,refresh)
-        }
-        else {
-        	state.mode = 'Armed-Stay'
-        }
-    logout(token)
-    log.info "'$device' AS - Status is: '$reply' - mode '$state.mode'"
-	sendEvent(name: "mode", value: state.mode, displayed: true, descriptionText: "Arm(Home) System - '$reply', mode - '$state.mode'") //state: state.mode, 
+	if (reply != 'OK!'){
+		log.warn "AS - response '$reply'"
+		state.mode = reply
+
+		runIn(60,refresh)
+	}
+	else {
+		state.mode = 'Armed-Stay'
+	}
+//    logout(token)
+	log.info "'$device' AS - Status is: '$reply' - mode '$state.mode'"
+	sendEvent(name: "mode", value: state.mode, displayed: true, descriptionText: "Arm(Home) System - '$reply', mode - '$state.mode'") //state: state.mode,
 }
 
 def disarm() {
 	def reply = ''
-	def token = login(token)
+    def responsecode
 	def paramsDisarm = [
-		uri: "https://www.yalehomesystem.co.uk/homeportal/api/panel/set_panel_mode?area=1&mode=disarm",
-		body: [id:settings.userName , password: settings.password],
-		headers: ['Cookie' : "${token}"]
+			uri: baseUrl() + endpointMode(),
+			body: [area: 1, mode: "disarm"],
+			headers: ['Authorization' : "Bearer ${state.accessToken}"],
+			requestContentType: "application/x-www-form-urlencoded",
+			contentType: "application/json"
 	]
-	httpPost(paramsDisarm) {	response -> 
-        reply = response.data.message
-// log.debug "$device - DA - response '$response.data.message'"
+	httpPost(paramsDisarm) {	response ->
+		reply = response.data.message
+        responsecode = response.status
+		log.debug "$device - DA - response '$response.data.message' - $responsecode"
 	}
-	if (reply != 'OK'){
-        	log.warn "$device - DA - response '$reply'"
-            state.mode = reply
-            runIn(10,refresh)
-        }
+    if (responsecode != 200) {
+        state.errorcount = state.errorcount + 1
+    	log.warn "${responsecode}' - try getting new token, error count is ${state.errorcount}"
+   		if (state.errorcount > 2){
+   			log.error "too many errors"
+       		state.errorcount = 0
+		}
+   		else {
+			login()
+         //	runIn(05, disarm)
+		}
+	}
     else {
-    	state.mode = 'Disarmed'
-    }
-    logout(token)
-    log.info "$device DA - Status is: '$reply' -'$state.mode'"
-	sendEvent(name: "mode", value: state.mode, displayed: true, descriptionText: "Disarm - '$reply', mode - '$state.mode'")
+       	state.errorcount = 0
+		if (reply != 'OK!'){
+			log.warn "$device - DA - response '$reply'"
+			state.mode = reply
+			runIn(10,refresh)
+		}
+		else {
+			state.mode = 'Disarmed'
+		}
+//    logout(token)
+		log.info "$device DA - Status is: '$reply' -'$state.mode'"
+		sendEvent(name: "mode", value: state.mode, displayed: true, descriptionText: "Disarm - '$reply', mode - '$state.mode'")
+	}
 }
-
 // handle commands
 def lock() {
 	log.debug "Executing 'Arm Stay'"
@@ -217,7 +267,7 @@ def off() {
 	log.debug "Executing 'Disarm'"
 	disarm()
 }
- // parse events into attributes
+// parse events into attributes
 def parse(String description) {
 	log.debug "Parsing '${description}'"
 }
