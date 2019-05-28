e61608c3516562da5ad086c94aa65c5aa2d68a3f
Carrie Chatterton <time2bbq@gmail.com>
2019-05-28 08:25:19 -0700

    null: My ST hub paired with the water valve, but I am unable to use it from the app. I receive the following error: "Cannot connect to device. Check device and try again"

---
+++ devicetypes/crawlspace/water-shut-off-handler.src/water-shut-off-handler.groovy
@@ -0,0 +1,48 @@
+/**
+ *  Water shut-off handler
+ *
+ *  Copyright 2019 Jeremy Chatterton
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ */
+metadata {
+	definition (name: "Water shut-off handler", namespace: "Crawlspace", author: "Jeremy Chatterton", cstHandler: true) {
+		capability "Valve"
+
+		fingerprint profileId: "C05E", deviceId: "0022A3000004243F", manufacturer: "Waxman", model: "House Water Valve-MDL-8810300L", deviceJoinName: "Water Shut-off"
+	}
+
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'valve' attribute
+
+}
+
+// handle commands
+def open() {
+	log.debug "Executing 'open'"
+	// TODO: handle 'open' command
+}
+
+def close() {
+	log.debug "Executing 'close'"
+	// TODO: handle 'close' command
+}
