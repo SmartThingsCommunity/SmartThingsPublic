a2da7b1021663eab2b04fe7fa78ad535a49232fe
Donald Kirker <donald@smartthings.com>
2019-06-06 17:17:57 -0700

    WWST-2306 IKEA TRADFRI wireless dimmer

--- devicetypes/smartthings/zigbee-battery-accessory-dimmer.src/zigbee-battery-accessory-dimmer.groovy
+++ devicetypes/smartthings/zigbee-battery-accessory-dimmer.src/zigbee-battery-accessory-dimmer.groovy
@@ -15,7 +15,7 @@
 import physicalgraph.zigbee.zcl.DataType
 
 metadata {
-	definition (name: "ZigBee Battery Accessory Dimmer", namespace: "smartthings", author: "SmartThings", ocfDeviceType: "x.com.st.d.remotecontroller") {
+	definition (name: "ZigBee Battery Accessory Dimmer", namespace: "smartthings", author: "SmartThings", ocfDeviceType: "oic.d.light") {
 		capability "Actuator"
 		capability "Battery"
 		capability "Configuration"
@@ -24,6 +24,7 @@ metadata {
 		capability "Switch Level"
 		
 		fingerprint profileId: "0104", inClusters: "0000,0001,0003,0020,FC11", outClusters: "0003,0004,0006,0008,FC10", manufacturer: "sengled", model: "E1E-G7F", deviceJoinName: "Sengled Smart Switch"
+		fingerprint manufacturer: "IKEA of Sweden", model: "TRADFRI wireless dimmer", deviceJoinName: "IKEA TRÅDFRI Wireless dimmer" // 01 [0104 or C05E] 0810 02 06 0000 0001 0003 0009 0B05 1000 06 0003 0004 0006 0008 0019 1000
 	}
 
 	tiles(scale: 2) {
@@ -49,28 +50,74 @@ metadata {
 def getDOUBLE_STEP() {10}
 def getSTEP() {5}
 
-def parse(String description) {
+def getONOFF_ON_COMMAND() { 0x01 }
+def getONOFF_OFF_COMMAND() { 0x00 }
+def getLEVEL_MOVE_LEVEL_COMMAND() { 0x00 }
+def getLEVEL_MOVE_COMMAND() { 0x01 }
+def getLEVEL_STEP_COMMAND() { 0x02 }
+def getLEVEL_STOP_COMMAND() { 0x03 }
+def getLEVEL_MOVE_LEVEL_ONOFF_COMMAND() { 0x04 }
+def getLEVEL_MOVE_ONOFF_COMMAND() { 0x05 }
+def getLEVEL_STEP_ONOFF_COMMAND() { 0x06 }
+def getLEVEL_STOP_ONOFF_COMMAND() { 0x07 }
+def getLEVEL_DIRECTION_UP() { "00" }
+def getLEVEL_DIRECTION_DOWN() { "01" }
 
-	def descMap = zigbee.parseDescriptionAsMap(description)
-	if (isSengledSwitch()) {
-		handleSengledSwitchEvents(descMap)
-	}
-	handleBatteryEvents(descMap)
+def getBATTERY_VOLTAGE_COMMAND() { 0x20 }
+def getBATTERY_PERCENT_COMMAND() { 0x21 }
+
+def getMFR_SPECIFIC_CLUSTER() { 0xFC10 }
+
+def getUINT8_STR() { "20" }
+
+
+private boolean isIkeaDimmer() {
+	device.getDataValue("model") == "TRADFRI wireless dimmer"
 }
-
 private boolean isSengledSwitch() {
 	device.getDataValue("model") == "E1E-G7F"
 }
 
+def parse(String description) {
+	log.debug "description is $description"
+	def results = []
+
+	def event = zigbee.getEvent(description)
+	if (event) {
+		results << createEvent(event)
+	} else {
+		def descMap = zigbee.parseDescriptionAsMap(description)
+
+		if (isSengledSwitch()) {
+			results += handleSengledSwitchEvents(descMap)
+		} else { //if (isIkeaDimmer()) {
+			if (descMap.clusterInt == zigbee.ONOFF_CLUSTER) {
+				results += handleSwitchEvent(descMap)
+			} else if (descMap.clusterInt == zigbee.LEVEL_CONTROL_CLUSTER) {
+				results += handleLevelEvent(descMap)
+			} else {
+				log.warn "DID NOT PARSE MESSAGE for description : $description"
+				log.debug "${descMap}"
+			}
+		}
+		results += handleBatteryEvents(descMap)
+	}
+
+	log.debug "parse returned $results"
+	return results
+}
+
 def handleSengledSwitchEvents(descMap) {
-	if (descMap?.clusterInt == 0xFC10) {
+	def results = []
+
+	if (descMap?.clusterInt == MFR_SPECIFIC_CLUSTER) {
 		def currentLevel = device.currentValue("level") as Integer ?: 0
 		def value = 0
 
 		switch(descMap.data[0]) {
 			case '01':
 				//short press of 'ON' button
-				sendEvent(name: "switch", value: "on")
+				results << createEvent(name: "switch", value: "on")
 				break
 			case '02':
 				// move up
@@ -81,8 +128,8 @@ def handleSengledSwitchEvents(descMap) {
 					//short press of 'BRIGHTEN' button
 					value = Math.min(currentLevel + STEP, 100)
 				}
-				sendEvent(name: "switch", value: "on")
-				sendEvent(name: "level", value: value)
+				results << createEvent(name: "switch", value: "on")
+				results << createEvent(name: "level", value: value)
 				break
 			case '03':
 				//move down
@@ -95,34 +142,141 @@ def handleSengledSwitchEvents(descMap) {
 				}
 
 				if (value == 0) {
-					sendEvent(name: "switch", value: "off")
+					results << createEvent(name: "switch", value: "off")
 				} else {
-					sendEvent(name: "level", value: value)
+					results << createEvent(name: "level", value: value)
 				}
 				break
 			case '04':
 				//short press of 'OFF' button
-				sendEvent(name: "switch", value: "off")
+				results << createEvent(name: "switch", value: "off")
 				break
 			case '06':
 				//long press of 'ON' button
-				sendEvent(name: "switch", value: "on")
+				results << createEvent(name: "switch", value: "on")
 				break
 			case '08':
 				//long press of 'OFF' button
-				sendEvent(name: "switch", value: "off")
+				results << createEvent(name: "switch", value: "off")
 				break
 			default:
 				break
 		}
 	}
+
+	return results
 }
 
-def handleBatteryEvents(descMap) {
-	if (descMap?.clusterInt == zigbee.POWER_CONFIGURATION_CLUSTER && descMap?.value) {
-		def batteryValue = zigbee.convertHexToInt(descMap.value) / 2
-		sendEvent(name: "battery", value: batteryValue)
+def handleSwitchEvent(descMap) {
+	def results = []
+
+	if (descMap.commandInt == ONOFF_ON_COMMAND) {
+		results << createEvent(name: "switch", value: "on")
+	} else if (descMap.commandInt == ONOFF_OFF_COMMAND) {
+		/*if (device.currentValue("level") == 0) {
+			results << createEvent(name: "level", value: STEP)
+		}*/
+		results << createEvent(name: "switch", value: "off")
 	}
+
+	return results
+}
+
+def handleLevelEvent(descMap) {
+	def results = []
+
+	if (descMap.commandInt == LEVEL_STEP_COMMAND) {
+		results += handleStepEvent(descMap)
+	} else if (descMap.commandInt == LEVEL_MOVE_COMMAND || descMap.commandInt == LEVEL_MOVE_ONOFF_COMMAND) {
+		// Treat Level Move and Level Move with On/Off as Level Step
+		//if (isIkeaDimmer()) {
+			results += handleStepEvent(descMap)
+		/*} else {
+			def newLevel = descMap.data[0] == LEVEL_DIRECTION_UP ? 100 : DOUBLE_STEP
+			results << createEvent(name: "level", value: newLevel)
+			results << createEvent(name: "switch", value: "on")
+
+			log.debug "step $DOUBLE_STEP to ${newLevel}"
+		}*/
+	} else if (descMap.commandInt == LEVEL_STOP_COMMAND || descMap.commandInt == LEVEL_STOP_ONOFF_COMMAND) {
+		// We are not going to handle this event because we are not implementing this the way that the Zigbee spec indicates
+		log.debug "Received stop move - not handling"
+	} else if (descMap.commandInt == LEVEL_MOVE_LEVEL_ONOFF_COMMAND) {
+		// The spec defines this as "Move to level with on/off". The IKEA Dimmer sends us 0x00 or 0xFF only, so we will treat this more as a
+		// on/off command for the dimmer. Otherwise, we will treat this as off or on and setLevel.
+		if (descMap.data[0] == "00") {
+			results << createEvent(name: "switch", value: "off", isStateChange: true)
+		} else if (descMap.data[0] == "FF" && isIkeaDimmer()) {
+			// The IKEA Dimmer sends 0xFF -- this is technically not to spec, but we will treat this as an "on"
+			if (device.currentValue("level") == 0) {
+				results << createEvent(name: "level", value: DOUBLE_STEP)
+			}
+
+			results << createEvent(name: "switch", value: "on", isStateChange: true)
+		} else {
+			results << createEvent(name: "switch", value: "on", isStateChange: true)
+			// Handle the Zigbee level the same way as we would normally with the same code path -- commandInt doesn't matter right now
+			// The first byte is the level, the second two bytes are the rate -- we only care about the level right now.
+			results << createEvent(zigbee.getEventFromAttrData(descMap.clusterInt, descMap.commandInt, UINT8_STR, descMap.data[0]))
+		}
+	}
+
+	return results
+}
+
+def handleStepEvent(descMap) {
+	def results = []
+	def currentLevel = device.currentValue("level") as Integer ?: 0
+	def value = null
+
+	log.debug "handleStepEvent - ${descMap.data}"
+
+	if (descMap.data[0] == LEVEL_DIRECTION_UP) {
+		value = Math.min(currentLevel + DOUBLE_STEP, 100)
+	} else if (descMap.data[0] == LEVEL_DIRECTION_DOWN) {
+		value = Math.max(currentLevel - DOUBLE_STEP, 0)
+	}
+
+	if (value != null) {
+		log.debug "Step ${descMap.data[0] == LEVEL_DIRECTION_UP ? "up" : "down"} by $DOUBLE_STEP to $value"
+
+		// don't change level if switch will be turning off
+		if (value == 0) {
+			results << createEvent(name: "switch", value: "off")
+		} else {
+			results << createEvent(name: "switch", value: "on")
+			results << createEvent(name: "level", value: value)
+		}
+	} else {
+		log.debug "Received invalid direction ${descMap.data[0]} - descMap.data = ${descMap.data}"
+	}
+
+	return results
+}
+
+
+def handleBatteryEvents(descMap) {
+	def results = []
+
+	if (descMap?.clusterInt == zigbee.POWER_CONFIGURATION_CLUSTER && descMap.value) {
+		def batteryValue = null
+
+		if (descMap.commandInt == BATTERY_VOLTAGE_COMMAND) {
+			def minVolts = 2.3
+			def maxVolts = 3.0
+			def batteryValueVoltage = zigbee.convertHexToInt(descMap.value) / 10
+
+			batteryValue = Math.round(((batteryValueVoltage- minVolts) / (maxVolts - minVolts)) * 100)
+		} else if (descMap.commandInt == BATTERY_PERCENT_COMMAND) {
+			batteryValue = zigbee.convertHexToInt(descMap.value) / 2
+		}
+
+		if (batteryValue != null) {
+			results << createEvent(name: "battery", value: batteryValue, unit: "%")
+		}
+	}
+
+	return results
 }
 
 def off() {
@@ -143,7 +297,7 @@ def setLevel(value, rate = null) {
 }
 
 def ping() {
-	zigbee.readAttribute(zigbee.POWER_CONFIGURATION_CLUSTER, 0x0021)
+	zigbee.readAttribute(zigbee.POWER_CONFIGURATION_CLUSTER, BATTERY_PERCENT_COMMAND)
 }
 
 def installed() {
@@ -153,6 +307,8 @@ def installed() {
 
 def configure() {
 	sendEvent(name: "checkInterval", value: 2 * 60 * 60 + 10 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
-	zigbee.readAttribute(zigbee.POWER_CONFIGURATION_CLUSTER, 0x0021) + zigbee.configureReporting(zigbee.POWER_CONFIGURATION_CLUSTER, 0x0021, DataType.UINT8, 30, 10 * 60, null)
+
+	zigbee.readAttribute(zigbee.POWER_CONFIGURATION_CLUSTER, BATTERY_PERCENT_COMMAND) +
+		zigbee.configureReporting(zigbee.POWER_CONFIGURATION_CLUSTER, BATTERY_PERCENT_COMMAND, DataType.UINT8, 30, 10 * 60, null)
 }
 
