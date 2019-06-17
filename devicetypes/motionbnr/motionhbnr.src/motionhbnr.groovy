6859f29430a8e095a07ab13d7caf83687ccdd6bf
RogerUI10 Corp qaST <roger_ui10corp_qast@samihub.com>
2019-06-17 14:16:36 -0700

    null: null

---
+++ devicetypes/motionbnr/motionhbnr.src/motionhbnr.groovy
@@ -0,0 +1,60 @@
+/**
+ *  MotionHBNR
+ *
+ *  Copyright 2019 RogerUI10 Corp qaST
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ */
+metadata {
+	definition (name: "MotionHBNR", namespace: "MotionBNR", author: "RogerUI10 Corp qaST", cstHandler: true) {
+		capability "Motion Sensor"
+		capability "Activity Sensor"
+		capability "Alarm"
+	}
+
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'motion' attribute
+	// TODO: handle 'activity' attribute
+	// TODO: handle 'alarm' attribute
+
+}
+
+// handle commands
+def off() {
+	log.debug "Executing 'off'"
+	// TODO: handle 'off' command
+}
+
+def strobe() {
+	log.debug "Executing 'strobe'"
+	// TODO: handle 'strobe' command
+}
+
+def siren() {
+	log.debug "Executing 'siren'"
+	// TODO: handle 'siren' command
+}
+
+def both() {
+	log.debug "Executing 'both'"
+	// TODO: handle 'both' command
+}
