364ef047de027303a82396d1b097658070fbbae5
Eric Gosselin <EricG66@users.noreply.github.com>
2019-07-18 08:07:22 -0400

    Test

--- devicetypes/ericg66/sensibopod.src/sensibopod.groovy
+++ devicetypes/ericg66/sensibopod.src/sensibopod.groovy
@@ -12,6 +12,9 @@
 *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
 *  for the specific language governing permissions and limitations under the License.
 *
+*
+*
+*
 */
 
 import groovyx.net.http.HTTPBuilder
