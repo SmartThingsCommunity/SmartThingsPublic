2b2ca67b40bd545e8de97e59dac87d4cc4361337
Przemyslaw Kacprowicz <p.kacprowicz@samsung.com>
2019-07-22 10:48:00 +0200

    [WWST-4705] Added Smartenit Range Extender to Zigbee Range Extender DTH.

--- devicetypes/smartthings/zigbee-range-extender.src/zigbee-range-extender.groovy
+++ devicetypes/smartthings/zigbee-range-extender.src/zigbee-range-extender.groovy
@@ -17,6 +17,7 @@ metadata {
 		capability "Health Check"
 
 		fingerprint profileId: "0104", inClusters: "0000, 0003, 0009, 0B05, 1000, FC7C", outClusters: "0019, 0020, 1000",  manufacturer: "IKEA of Sweden",  model: "TRADFRI signal repeater", deviceJoinName: "TRÅDFRI Signal Repeater"
+		fingerprint profileId: "0104", inClusters: "0000, 0003", outClusters: "0019",  manufacturer: "Smartenit, Inc",  model: "ZB3RE", deviceJoinName: "Smartenit Range Extender"
 	}
 
 	tiles(scale: 2) {
