e87fea244dd52cafc26760b082c9a2f20f8a682d
greens <steven.green@smartthings.com>
2019-07-30 15:39:53 -0700

    CHAD-3499 Generecize add name for color bulb

--- devicetypes/smartthings/aeon-led-bulb.src/aeon-led-bulb.groovy
+++ devicetypes/smartthings/aeon-led-bulb.src/aeon-led-bulb.groovy
@@ -28,9 +28,12 @@ metadata {
 
 		command "reset"
 
-		fingerprint inClusters: "0x26,0x33,0x98"
-		fingerprint deviceId: "0x11", inClusters: "0x98,0x33"
-		fingerprint deviceId: "0x1102", inClusters: "0x98"
+		fingerprint mfr: "0371", inClusters: "0x26,0x33,0x98"
+		fingerprint mfr: "0371", deviceId: "0x11", inClusters: "0x98,0x33"
+		fingerprint mfr: "0371", deviceId: "0x1102", inClusters: "0x98"
+		fingerprint inClusters: "0x26,0x33,0x98", deviceJoinName: "Z-Wave Color Bulb"
+		fingerprint deviceId: "0x11", inClusters: "0x98,0x33", deviceJoinName: "Z-Wave Color Bulb"
+		fingerprint deviceId: "0x1102", inClusters: "0x98", deviceJoinName: "Z-Wave Color Bulb"
 	}
 
 	simulator {
