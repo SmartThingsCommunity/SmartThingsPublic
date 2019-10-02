a8dafdb1bf477b2f0738f758303a3f3dbfd733be
greens <steven.green@smartthings.com>
2019-10-02 16:04:28 -0700

    DVCSMP-4708 Wallmote buttons not setting supported button values

--- devicetypes/smartthings/aeotec-wallmote.src/aeotec-wallmote.groovy
+++ devicetypes/smartthings/aeotec-wallmote.src/aeotec-wallmote.groovy
@@ -51,8 +51,8 @@ def getNumberOfButtons() {
 def installed() {
     createChildDevices()
     sendEvent(name: "numberOfButtons", value: numberOfButtons)
-    sendEvent(name: "supportedButtonValues", value: ["pushed", "held"])
-    sendEvent(name: "button", value: "pushed", data: [buttonNumber: 1])
+    sendEvent(name: "supportedButtonValues", value: ["pushed", "held"].encodeAsJson(), displayed: false)
+    sendEvent(name: "button", value: "pushed", data: [buttonNumber: 1], displayed: false)
 }
 
 def updated() {
@@ -149,8 +149,8 @@ def createChildDevices() {
             child = addChildDevice("Child Button", "${device.deviceNetworkId}:${i}", device.hubId,
                     [completedSetup: true, label: "${device.displayName} button ${i}",
                      isComponent: true, componentName: "button$i", componentLabel: "Button $i"])
-            child.sendEvent(name: "supportedButtonValues", value: ["pushed", "held"])
-            child.sendEvent(name: "button", value: "pushed", data: [buttonNumber: 1], descriptionText: "$child.displayName was pushed", isStateChange: true)
+            child.sendEvent(name: "supportedButtonValues", value: ["pushed", "held"].encodeAsJson(), displayed: false)
+            child.sendEvent(name: "button", value: "pushed", data: [buttonNumber: 1], descriptionText: "$child.displayName was pushed", isStateChange: true, displayed: false)
         }
     }
 }
