8c335876415ebd696c9950f3ef5cfcc3790a0eb4
k.klimczuk2 <k.klimczuk2@partner.samsung.com>
2019-10-04 15:32:24 +0200

    ICP-10136: Fix/improvement of power meter updates. Improvement of on/off status updates. Code refactoring - for better readability

--- devicetypes/smartthings/zwave-multi-metering-switch.src/zwave-multi-metering-switch.groovy
+++ devicetypes/smartthings/zwave-multi-metering-switch.src/zwave-multi-metering-switch.groovy
@@ -56,205 +56,6 @@ metadata {
 	}
 }
 
-def parse(String description) {
-	def result = null
-	if (description.startsWith("Err")) {
-		result = createEvent(descriptionText:description, isStateChange:true)
-	} else if (description != "updated") {
-		def cmd = zwave.parse(description)
-		if (cmd) {
-			result = zwaveEvent(cmd, null)
-		}
-	}
-	log.debug "parsed '${description}' to ${result.inspect()}"
-	result
-}
-
-def zwaveEvent(physicalgraph.zwave.commands.securityv1.SecurityMessageEncapsulation cmd, ep = null) {
-	log.debug "Security Message Encap ${cmd}"
-	def encapsulatedCommand = cmd.encapsulatedCommand()
-	if (encapsulatedCommand) {
-		zwaveEvent(encapsulatedCommand, null)
-	} else {
-		log.warn "Unable to extract encapsulated cmd from $cmd"
-		createEvent(descriptionText: cmd.toString())
-	}
-}
-
-def zwaveEvent(physicalgraph.zwave.commands.multichannelv3.MultiChannelCmdEncap cmd, ep = null) {
-	log.debug "Multichannel command ${cmd}" + (ep ? " from endpoint $ep" : "")
-	def encapsulatedCommand = cmd.encapsulatedCommand()
-	zwaveEvent(encapsulatedCommand, cmd.sourceEndPoint as Integer)
-}
-
-def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicReport cmd, ep = null) {
-	log.debug "Basic ${cmd}" + (ep ? " from endpoint $ep" : "")
-	def value = cmd.value ? "on" : "off"
-	ep ? changeSwitch(ep, value) : []
-}
-
-def zwaveEvent(physicalgraph.zwave.commands.switchbinaryv1.SwitchBinaryReport cmd, ep = null) {
-	log.debug "Binary ${cmd}" + (ep ? " from endpoint $ep" : "")
-	def value = cmd.value ? "on" : "off"
-	ep ? changeSwitch(ep, value) : []
-}
-
-private changeSwitch(endpoint, value) {
-	def result = []
-	if (endpoint == 1) {
-		result += createEvent(name: "switch", value: value, isStateChange: true, descriptionText: "Switch ${endpoint} is ${value}")
-	} else {
-		String childDni = "${device.deviceNetworkId}:$endpoint"
-		def child = childDevices.find { it.deviceNetworkId == childDni }
-		child?.sendEvent(name: "switch", value: value, isStateChange: true, descriptionText: "Switch ${endpoint} is ${value}")
-	}
-	result
-}
-
-def zwaveEvent(physicalgraph.zwave.commands.meterv3.MeterReport cmd, ep) {
-	log.debug "Meter ${cmd}" + (ep ? " from endpoint $ep" : "")
-	def result = []
-	if (ep == 1) {
-		result += createEvent(createMeterEventMap(cmd))
-	} else if(ep) {
-		String childDni = "${device.deviceNetworkId}:$ep"
-		def child = childDevices.find { it.deviceNetworkId == childDni }
-		child?.sendEvent(createMeterEventMap(cmd))
-	} else {
-		result += zwaveEvent(cmd)
-	}
-	result += response(secureEncap(zwave.meterV3.meterGet(scale: 2), ep))
-	result
-}
-
-def zwaveEvent(physicalgraph.zwave.commands.meterv3.MeterReport cmd) {
-	def event = createEvent([isStateChange:  false, descriptionText: "Wattage change has been detected. Refreshing each endpoint"])
-	[event, response(refreshAll())]
-}
-
-private createMeterEventMap(cmd) {
-	def eventMap = [:]
-	if (cmd.meterType == 1) {
-		if (cmd.scale == 0) {
-			eventMap.name = "energy"
-			eventMap.value = cmd.scaledMeterValue
-			eventMap.unit = "kWh"
-		} else if (cmd.scale == 2) {
-			eventMap.name = "power"
-			eventMap.value = Math.round(cmd.scaledMeterValue)
-			eventMap.unit = "W"
-		}
-	}
-	eventMap
-}
-
-// cmd.endPoints includes the USB ports but we don't want to expose them as child devices since they cannot be controlled so hardcode to just include the outlets
-def zwaveEvent(physicalgraph.zwave.commands.multichannelv3.MultiChannelEndPointReport cmd, ep = null) {
-	if(!childDevices) {
-		if (isZoozZenStripV2()) {
-			addChildSwitches(5)
-		} else if (isZoozDoublePlug()) {
-			addChildSwitches(2)
-		} else {
-			addChildSwitches(cmd.endPoints)
-		}
-	}
-	response([
-		resetAll(),
-		refreshAll()
-	])
-}
-def isZoozZenStripV2() {
-	zwaveInfo.mfr.equals("027A") && zwaveInfo.model.equals("A004")
-}
-def isZoozDoublePlug() {
-	zwaveInfo.mfr.equals("027A") && zwaveInfo.model.equals("A003")
-}
-
-def zwaveEvent(physicalgraph.zwave.commands.manufacturerspecificv2.ManufacturerSpecificReport cmd, ep = null) {
-	def mfr = Integer.toHexString(cmd.manufacturerId)
-	def model = Integer.toHexString(cmd.productId)
-	updateDataValue("mfr", mfr)
-	updateDataValue("model", model)
-	lateConfigure()
-}
-
-def zwaveEvent(physicalgraph.zwave.Command cmd, ep) {
-	log.warn "Unhandled ${cmd}" + (ep ? " from endpoint $ep" : "")
-}
-
-private onOffCmd(value, endpoint = 1) {
-	delayBetween([
-			secureEncap(zwave.basicV1.basicSet(value: value), endpoint),
-			secureEncap(zwave.basicV1.basicGet(), endpoint),
-			"delay 3000",
-			secureEncap(zwave.meterV3.meterGet(scale: 2), endpoint)
-	])
-}
-
-def refresh(endpoint = 1) {
-	delayBetween([
-			secureEncap(zwave.basicV1.basicGet(), endpoint),
-			secureEncap(zwave.meterV3.meterGet(scale: 0), endpoint),
-			secureEncap(zwave.meterV3.meterGet(scale: 2), endpoint),
-			"delay 500"
-	], 500)
-}
-
-def on() {
-	onOffCmd(0xFF)
-}
-
-def off() {
-	onOffCmd(0x00)
-}
-
-def ping() {
-	refresh()
-}
-
-def reset(endpoint = 1) {
-	log.debug "Resetting endpoint: ${endpoint}"
-	delayBetween([
-			secureEncap(zwave.meterV3.meterReset(), endpoint),
-			secureEncap(zwave.meterV3.meterGet(scale: 0), endpoint),
-			"delay 500"
-	], 500)
-}
-
-def childOnOff(deviceNetworkId, value) {
-	def switchId = getSwitchId(deviceNetworkId)
-	if (switchId != null) sendHubCommand onOffCmd(value, switchId)
-}
-
-def childRefresh(deviceNetworkId) {
-	def switchId = getSwitchId(deviceNetworkId)
-	if (switchId != null) sendHubCommand refresh(switchId)
-}
-
-def childReset(deviceNetworkId) {
-	def switchId = getSwitchId(deviceNetworkId)
-	if (switchId != null) {
-		log.debug "Child reset switchId: ${switchId}"
-		sendHubCommand reset(switchId)
-	}
-}
-
-def getSwitchId(deviceNetworkId) {
-	def split = deviceNetworkId?.split(":")
-	return (split.length > 1) ? split[1] as Integer : null
-}
-
-private refreshAll() {
-	childDevices.each { childRefresh(it.deviceNetworkId) }
-	sendHubCommand refresh()
-}
-
-private resetAll() {
-	childDevices.each { childReset(it.deviceNetworkId) }
-	sendHubCommand reset()
-}
-
 def installed() {
 	log.debug "Installed ${device.displayName}"
 	sendEvent(name: "checkInterval", value: 2 * 15 * 60 + 2 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
@@ -272,40 +73,43 @@ def configure() {
 	])
 }
 
-private secure(cmd) {
-	if(zwaveInfo.zw.endsWith("s")) {
-		zwave.securityV1.securityMessageEncapsulation().encapsulate(cmd).format()
-	} else {
-		cmd.format()
-	}
-}
-
-private encap(cmd, endpoint = null) {
-	if (endpoint) {
-		zwave.multiChannelV3.multiChannelCmdEncap(destinationEndPoint:endpoint).encapsulate(cmd)
-	} else {
-		cmd
-	}
-}
-
-private secureEncap(cmd, endpoint = null) {
-	secure(encap(cmd, endpoint))
-}
-
-private addChildSwitches(numberOfSwitches) {
-	for (def endpoint : 2..numberOfSwitches) {
-		try {
-			String childDni = "${device.deviceNetworkId}:$endpoint"
-			def componentLabel = device.displayName[0..-2] + "${endpoint}"
-			addChildDevice("Child Metering Switch", childDni, device.getHub().getId(), [
-					completedSetup: true,
-					label         : componentLabel,
-					isComponent   : false
-			])
-		} catch(Exception e) {
-			log.debug "Exception: ${e}"
+def parse(String description) {
+	def result = null
+	if (description.startsWith("Err")) {
+		result = createEvent(descriptionText:description, isStateChange:true)
+	} else if (description != "updated") {
+		def cmd = zwave.parse(description)
+		if (cmd) {
+			result = zwaveEvent(cmd, null)
 		}
 	}
+	log.debug "parsed '${description}' to ${result.inspect()}"
+	result
+}
+
+// cmd.endPoints includes the USB ports but we don't want to expose them as child devices since they cannot be controlled so hardcode to just include the outlets
+def zwaveEvent(physicalgraph.zwave.commands.multichannelv3.MultiChannelEndPointReport cmd, ep = null) {
+	if(!childDevices) {
+		if (isZoozZenStripV2()) {
+			addChildSwitches(5)
+		} else if (isZoozDoublePlug()) {
+			addChildSwitches(2)
+		} else {
+			addChildSwitches(cmd.endPoints)
+		}
+	}
+	response([
+			resetAll(),
+			refreshAll()
+	])
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.manufacturerspecificv2.ManufacturerSpecificReport cmd, ep = null) {
+	def mfr = Integer.toHexString(cmd.manufacturerId)
+	def model = Integer.toHexString(cmd.productId)
+	updateDataValue("mfr", mfr)
+	updateDataValue("model", model)
+	lateConfigure()
 }
 
 private lateConfigure() {
@@ -328,6 +132,7 @@ private lateConfigure() {
 		case "Zooz Switch":
 			cmds = [
 					secure(zwave.configurationV1.configurationSet(parameterNumber: 2, size: 4, scaledConfigurationValue: 10)),	// makes device report every 5W change
+					secure(zwave.configurationV1.configurationSet(parameterNumber: 3, size: 4, scaledConfigurationValue: 600)), // enabling power Wattage reports every 10 minutes
 					secure(zwave.configurationV1.configurationSet(parameterNumber: 4, size: 4, scaledConfigurationValue: 600))	// enabling kWh energy reports every 10 minutes
 			]
 			break
@@ -336,6 +141,232 @@ private lateConfigure() {
 			break
 	}
 	sendHubCommand cmds
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.securityv1.SecurityMessageEncapsulation cmd, ep = null) {
+	log.debug "Security Message Encap ${cmd}"
+	def encapsulatedCommand = cmd.encapsulatedCommand()
+	if (encapsulatedCommand) {
+		zwaveEvent(encapsulatedCommand, null)
+	} else {
+		log.warn "Unable to extract encapsulated cmd from $cmd"
+		createEvent(descriptionText: cmd.toString())
+	}
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.multichannelv3.MultiChannelCmdEncap cmd, ep = null) {
+	log.debug "Multichannel command ${cmd}" + (ep ? " from endpoint $ep" : "")
+	def encapsulatedCommand = cmd.encapsulatedCommand()
+	zwaveEvent(encapsulatedCommand, cmd.sourceEndPoint as Integer)
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicReport cmd, ep = null) {
+	log.debug "Basic ${cmd}" + (ep ? " from endpoint $ep" : "")
+	def value = cmd.value ? "on" : "off"
+
+	if (isZoozZenStripV2()) {
+		// device sends also reports without any endpoint specified, therefore all endpoins must be querried
+		// sometimes it also reports 0.0 Wattage only until it's queried for it, then it starts reporting real values
+		ep ? [changeSwitch(ep, value), response(secureEncap(zwave.meterV3.meterGet(scale: 0), ep))] : [response(refreshBasicGetAll())]
+	} else {
+		ep ? changeSwitch(ep, value) : []
+	}
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.switchbinaryv1.SwitchBinaryReport cmd, ep = null) {
+	log.debug "Binary ${cmd}" + (ep ? " from endpoint $ep" : "")
+	def value = cmd.value ? "on" : "off"
+
+	if (isZoozZenStripV2()) {
+		// device sends also reports without any endpoint specified, therefore all endpoins must be querried
+		// sometimes it also reports 0.0 Wattage only until it's queried for it, then it starts reporting real values
+		ep ? [changeSwitch(ep, value), response(secureEncap(zwave.meterV3.meterGet(scale: 0), ep))] : [response(refreshBasicGetAll())]
+	} else {
+		ep ? changeSwitch(ep, value) : []
+	}
+}
+
+private changeSwitch(endpoint, value) {
+	if (endpoint == 1) {
+		createEvent(name: "switch", value: value, isStateChange: true, descriptionText: "Switch ${endpoint} is ${value}")
+	} else {
+		String childDni = "${device.deviceNetworkId}:$endpoint"
+		def child = childDevices.find { it.deviceNetworkId == childDni }
+		child?.sendEvent(name: "switch", value: value, isStateChange: true, descriptionText: "Switch ${endpoint} is ${value}")
+	}
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.meterv3.MeterReport cmd) {
+	def event = createEvent([isStateChange:  false, descriptionText: "Wattage change has been detected. Refreshing each endpoint"])
+	isAeotec() ? [event, response(refreshAll())] : event
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.meterv3.MeterReport cmd, ep) {
+	log.debug "Meter ${cmd}" + (ep ? " from endpoint $ep" : "")
+	if (ep == 1) {
+		createEvent(createMeterEventMap(cmd))
+	} else if(ep) {
+		String childDni = "${device.deviceNetworkId}:$ep"
+		def child = childDevices.find { it.deviceNetworkId == childDni }
+		child?.sendEvent(createMeterEventMap(cmd))
+	} else {
+		zwaveEvent(cmd)
+	}
+}
+
+private createMeterEventMap(cmd) {
+	def eventMap = [:]
+	if (cmd.meterType == 1) {
+		if (cmd.scale == 0) {
+			eventMap.name = "energy"
+			eventMap.value = cmd.scaledMeterValue
+			eventMap.unit = "kWh"
+		} else if (cmd.scale == 2) {
+			eventMap.name = "power"
+			eventMap.value = Math.round(cmd.scaledMeterValue)
+			eventMap.unit = "W"
+		}
+	}
+	eventMap
+}
+
+def zwaveEvent(physicalgraph.zwave.Command cmd, ep) {
+	log.warn "Unhandled ${cmd}" + (ep ? " from endpoint $ep" : "")
+}
+
+def on() {
+	onOffCmd(0xFF)
+}
+
+def off() {
+	onOffCmd(0x00)
+}
+
+def ping() {
+	refresh()
+}
+
+def childOnOff(deviceNetworkId, value) {
+	def switchId = getSwitchId(deviceNetworkId)
+	if (switchId != null) sendHubCommand onOffCmd(value, switchId)
+}
+
+private onOffCmd(value, endpoint = 1) {
+	delayBetween([
+			secureEncap(zwave.basicV1.basicSet(value: value), endpoint),
+			secureEncap(zwave.basicV1.basicGet(), endpoint),
+			"delay 3000",
+			secureEncap(zwave.meterV3.meterGet(scale: 0), endpoint),
+			secureEncap(zwave.meterV3.meterGet(scale: 2), endpoint)
+	])
+}
+
+private refreshBasicGetAll() {
+	childDevices.each { childRefreshBasicGet(it.deviceNetworkId) }
+	sendHubCommand refreshBasicGet()
+}
+
+def childRefreshBasicGet(deviceNetworkId) {
+	def switchId = getSwitchId(deviceNetworkId)
+	if (switchId != null) {
+		sendHubCommand refreshBasicGet(switchId)
+	}
+}
+
+def refreshBasicGet(endpoint = 1) {
+	secureEncap(zwave.basicV1.basicGet(), endpoint)
+}
+
+private refreshAll() {
+	childDevices.each { childRefresh(it.deviceNetworkId) }
+	sendHubCommand refresh()
+}
+
+def childRefresh(deviceNetworkId) {
+	def switchId = getSwitchId(deviceNetworkId)
+	if (switchId != null) sendHubCommand refresh(switchId)
+}
+
+def refresh(endpoint = 1) {
+	delayBetween([
+			secureEncap(zwave.basicV1.basicGet(), endpoint),
+			secureEncap(zwave.meterV3.meterGet(scale: 0), endpoint),
+			secureEncap(zwave.meterV3.meterGet(scale: 2), endpoint),
+			"delay 500"
+	], 500)
+}
+
+private resetAll() {
+	childDevices.each { childReset(it.deviceNetworkId) }
+	sendHubCommand reset()
+}
+
+def childReset(deviceNetworkId) {
+	def switchId = getSwitchId(deviceNetworkId)
+	if (switchId != null) {
+		log.debug "Child reset switchId: ${switchId}"
+		sendHubCommand reset(switchId)
+	}
+}
+
+def reset(endpoint = 1) {
+	log.debug "Resetting endpoint: ${endpoint}"
+	delayBetween([
+			secureEncap(zwave.meterV3.meterReset(), endpoint),
+			secureEncap(zwave.meterV3.meterGet(scale: 0), endpoint),
+			"delay 500"
+	], 500)
+}
+
+def getSwitchId(deviceNetworkId) {
+	def split = deviceNetworkId?.split(":")
+	return (split.length > 1) ? split[1] as Integer : null
+}
+
+private secureEncap(cmd, endpoint = null) {
+	secure(encap(cmd, endpoint))
+}
+
+private encap(cmd, endpoint = null) {
+	if (endpoint) {
+		zwave.multiChannelV3.multiChannelCmdEncap(destinationEndPoint:endpoint).encapsulate(cmd)
+	} else {
+		cmd
+	}
+}
+
+private secure(cmd) {
+	if(zwaveInfo.zw.endsWith("s")) {
+		zwave.securityV1.securityMessageEncapsulation().encapsulate(cmd).format()
+	} else {
+		cmd.format()
+	}
+}
+
+private addChildSwitches(numberOfSwitches) {
+	for (def endpoint : 2..numberOfSwitches) {
+		try {
+			String childDni = "${device.deviceNetworkId}:$endpoint"
+			def componentLabel = device.displayName[0..-2] + "${endpoint}"
+			addChildDevice("Child Metering Switch", childDni, device.getHub().getId(), [
+					completedSetup: true,
+					label         : componentLabel,
+					isComponent   : false
+			])
+		} catch(Exception e) {
+			log.debug "Exception: ${e}"
+		}
+	}
+}
+
+def isAeotec() {
+	getDeviceModel() == "Aeotec Nano Switch"
+}
+def isZoozZenStripV2() {
+	zwaveInfo.mfr.equals("027A") && zwaveInfo.model.equals("A004")
+}
+def isZoozDoublePlug() {
+	zwaveInfo.mfr.equals("027A") && zwaveInfo.model.equals("A003")
 }
 
 private getDeviceModel() {
