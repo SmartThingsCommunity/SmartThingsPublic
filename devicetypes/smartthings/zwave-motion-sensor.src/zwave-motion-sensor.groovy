dc60dd4fc533c5cb8664ec8196a37820597dbf85
m.goralczyk <m.goralczyk@samsung.com>
2019-10-11 13:20:06 +0200

    Send motion event in installed() to fix pairing without motion - Everspring Indoor PIR

--- devicetypes/smartthings/zwave-motion-sensor.src/zwave-motion-sensor.groovy
+++ devicetypes/smartthings/zwave-motion-sensor.src/zwave-motion-sensor.groovy
@@ -67,6 +67,7 @@ def installed() {
 // Device wakes up every 4 hours, this interval allows us to miss one wakeup notification before marking offline
 	sendEvent(name: "checkInterval", value: 8 * 60 * 60 + 2 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID])
 	sendEvent(name: "tamper", value: "clear", displayed: false)
+	sendEvent(name: "motion", value: "inactive", displayed: false)
 	response(initialPoll())
 }
 
