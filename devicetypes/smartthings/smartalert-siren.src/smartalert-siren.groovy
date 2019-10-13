22351d0282a8a7d98dbd306fa272c3b5f3686083
Antonio San Roman <asanro46@gmail.com>
2019-10-13 18:20:13 +0900

    The siren is activated with contact and motion sensors
    It is possible to select the mode of siren for simulator

--- devicetypes/smartthings/smartalert-siren.src/smartalert-siren.groovy
+++ devicetypes/smartthings/smartalert-siren.src/smartalert-siren.groovy
@@ -12,25 +12,44 @@
  *
  *  SmartAlert Siren
  *
- *  Author: SmartThings
- *  Date: 2013-03-05
+ *  Author: Antonio San Roman
+ *  Date: 2019-10-13
  */
 metadata {
 	definition (name: "SmartAlert Siren", namespace: "smartthings", author: "SmartThings", ocfDeviceType: "x.com.st.d.siren", runLocally: true, minHubCoreVersion: '000.017.0012', executeCommandsLocally: false) {
 		capability "Actuator"
 		capability "Switch"
-		capability "Sensor"
 		capability "Alarm"
-		capability "Health Check"
 
-		command "test"
+		command "EstableceModoSirena"
+        command "EstableceModoStrobe"
+        command "EstableceModoBoth"
 
-		fingerprint deviceId: "0x1100", inClusters: "0x26,0x71"
-		fingerprint mfr:"0084", prod:"0313", model:"010B", deviceJoinName: "FortrezZ Siren Strobe Alarm"
+		attribute "modosSirena", "string"	
+
+		//fingerprint deviceId: "0x1100", inClusters: "0x26,0x71"
+		//fingerprint mfr:"0084", prod:"0313", model:"010B", deviceJoinName: "FortrezZ Siren Strobe Alarm"
+        fingerprint inClusters: "0x26,0x71", manufacturer: "Sercomm Corp", model:"SZ-SRN12N", deviceJoinName: "SmartThings Siren"
 	}
 
-	simulator {
-		// reply messages
+preferences {
+    section ("Watch the contact sensor...") {
+        input "theContact", "capability.contactSensor",  title: "pick a contact sensor", required: true, multiple: false
+    }
+     section ("Turn on/off a siren...") {
+        input "theSwitch", "capability.switch"
+    }
+    section ("Establish modes of  siren") {
+        input "theSiren", "capability.alarm"
+    } 
+    section ("Establish modes of  siren") {
+        input "motion", "capability.motion", title: "pick a motion sensor", required: true, multiple: true
+     }   
+}
+
+
+simulator {
+        // reply messages
 		reply "2001FF,2002": "command: 2003, payload: FF"
 		reply "200100,2002": "command: 2003, payload: 00"
 		reply "200121,2002": "command: 2003, payload: 21"
@@ -39,26 +58,22 @@ metadata {
 	}
 
 	tiles(scale: 2) {
-		multiAttributeTile(name:"alarm", type: "generic", width: 6, height: 4){
-			tileAttribute ("device.alarm", key: "PRIMARY_CONTROL") {
-				attributeState "off", label:'off', action:'alarm.strobe', icon:"st.alarm.alarm.alarm", backgroundColor:"#ffffff"
-				attributeState "strobe", label:'strobe!', action:'alarm.off', icon:"st.alarm.alarm.alarm", backgroundColor:"#e86d13"
-				attributeState "siren", label:'siren!', action:'alarm.off', icon:"st.alarm.alarm.alarm", backgroundColor:"#e86d13"
-				attributeState "both", label:'alarm!', action:'alarm.off', icon:"st.alarm.alarm.alarm", backgroundColor:"#e86d13"
+			standardTile("contact", "device.contact", width: 2, height: 2) {
+			state "open", label: '${name}', icon: "st.contact.contact.open", backgroundColor: "#e86d13"
+			state "closed", label: '${name}', icon: "st.contact.contact.closed", backgroundColor: "#00A0DC"
+		}
+         multiAttributeTile(name:"alarm", type: "generic", width: 6, height: 4){
+			    tileAttribute("device.alarm", key: "VALUE_CONTROL") {
+                attributeState("strobe", label:'strobe!', action: "EstableceModoStrobe", icon:"st.alarm.alarm.alarm", backgroundColor:"#e86d13")
+                attributeState("siren", label:'siren!', action: "EstableceModoSirena", icon:"st.alarm.alarm.alarm", backgroundColor:"#e86d13")
+                attributeState("both", label:'alarm!', action: "EstableceModoBoth", icon:"st.alarm.alarm.alarm", backgroundColor:"#e86d13")			
+			}
+            
+            tileAttribute ("device.alarm", key: "PRIMARY_CONTROL") {
+                attributeState "off", label:'off', action:'alarm.off', icon:"st.alarm.alarm.alarm", backgroundColor:"#e86d13"
 			}
 		}
-		standardTile("strobe", "device.alarm", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
-			state "off", label:'', action:"alarm.strobe", icon:"st.secondary.strobe", backgroundColor:"#cccccc"
-			state "siren", label:'', action:"alarm.strobe", icon:"st.secondary.strobe", backgroundColor:"#cccccc"
-			state "strobe", label:'', action:'alarm.strobe', icon:"st.secondary.strobe", backgroundColor:"#e86d13"
-			state "both", label:'', action:'alarm.strobe', icon:"st.secondary.strobe", backgroundColor:"#e86d13"
-		}
-		standardTile("siren", "device.alarm", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
-			state "off", label:'', action:"alarm.siren", icon:"st.secondary.siren", backgroundColor:"#cccccc"
-			state "strobe", label:'', action:"alarm.siren", icon:"st.secondary.siren", backgroundColor:"#cccccc"
-			state "siren", label:'', action:'alarm.siren', icon:"st.secondary.siren", backgroundColor:"#e86d13"
-			state "both", label:'', action:'alarm.siren', icon:"st.secondary.siren", backgroundColor:"#e86d13"
-		}
+
 		standardTile("test", "device.alarm", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
 			state "default", label:'', action:"test", icon:"st.secondary.test"
 		}
@@ -71,99 +86,61 @@ metadata {
 }
 
 def installed() {
+    subscribe(theContact, "contact", contactHandler) 
+    subscribe(motion, "motion", motionHandler)
 // Device-Watch simply pings if no device events received for 32min(checkInterval)
-	sendEvent(name: "checkInterval", value: 2 * 15 * 60 + 2 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
+	sendEvent(name: "checkInterval", value: 2 * 15 * 60 + 2 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
 }
 
 def updated() {
+   subscribe(theContact, "contact", contactHandler)
+   subscribe(motion,  "motion", motionHandler)
 // Device-Watch simply pings if no device events received for 32min(checkInterval)
-	sendEvent(name: "checkInterval", value: 2 * 15 * 60 + 2 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
+	sendEvent(name: "checkInterval", value: 2 * 15 * 60 + 2 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
 }
 
-def on() {
-	[
-		zwave.basicV1.basicSet(value: 0xFF).format(),
-		zwave.basicV1.basicGet().format()
-	]
+def EstableceModoSirena() {
+     sendEvent(name: "modosSirena", value: "siren")
 }
 
-def off() {
-	[
-		zwave.basicV1.basicSet(value: 0x00).format(),
-		zwave.basicV1.basicGet().format()
-	]
+def EstableceModoStrobe() {
+     sendEvent(name: "modosSirena", value: "strobe")
 }
 
-def test() {
-	[
-		zwave.basicV1.basicSet(value: 0xFF).format(),
-		"delay 3000",
-		zwave.basicV1.basicSet(value: 0x00).format(),
-		zwave.basicV1.basicGet().format()
-	]
+def EstableceModoBoth() {
+     sendEvent(name: "modosSirena", value: "both")
 }
 
-def strobe() {
-	[
-		zwave.basicV1.basicSet(value: 0x21).format(),
-		zwave.basicV1.basicGet().format()
-	]
+def contactHandler(evt) {
+  def modo = device.currentValue("modosSirena")
+  if("open" == evt.value) {
+    // contact was opened, turn on a sirenn
+    switch(modo) {
+        case "siren":
+              theSiren.siren()
+        case "strobe":
+              theSiren.strobe()
+        case "both":
+              theSiren.both()              
+    }        
+    zigbee.on()       
+    log.debug "Contact is in ${evt.value} state"
+  }
+  if("closed" == evt.value) {
+     // contact was closed, turn off the light?
+     theSiren.off()
+   // log.debug "Contact is in ${evt.value} state"
+   }
 }
 
-def siren() {
-	[
-		zwave.basicV1.basicSet(value: 0x42).format(),
-		zwave.basicV1.basicGet().format()
-	]
-}
-
-def both() {
-	[
-		zwave.basicV1.basicSet(value: 0xFF).format(),
-		zwave.basicV1.basicGet().format()
-	]
+def motionHandler(evt) {
+   if(evt.value == "active") {
+       theSiren.strobe()
+       zigbee.on()
+   } else if(evt.value == "inactive") {
+       theSiren.off()
+       zigbee.off()
+   }
 }
 
 
-def parse(String description) {
-	log.debug "parse($description)"
-	def result = null
-	def cmd = zwave.parse(description, [0x20: 1])
-	if (cmd) {
-		result = createEvents(cmd)
-	}
-	log.debug "Parse returned ${result?.descriptionText}"
-	return result
-}
-
-def createEvents(physicalgraph.zwave.commands.basicv1.BasicReport cmd) {
-	def switchValue = cmd.value ? "on" : "off"
-	def alarmValue
-	if (cmd.value == 0) {
-		alarmValue = "off"
-	}
-	else if (cmd.value <= 33) {
-		alarmValue = "strobe"
-	}
-	else if (cmd.value <= 66) {
-		alarmValue = "siren"
-	}
-	else {
-		alarmValue = "both"
-	}
-	[
-		createEvent([name: "switch", value: switchValue, type: "digital", displayed: false]),
-		createEvent([name: "alarm", value: alarmValue, type: "digital"])
-	]
-}
-
-def createEvents(physicalgraph.zwave.Command cmd) {
-	log.warn "UNEXPECTED COMMAND: $cmd"
-}
-
-/**
- * PING is used by Device-Watch in attempt to reach the Device
- * */
-def ping() {
-	zwave.basicV1.basicGet().format()
-}
