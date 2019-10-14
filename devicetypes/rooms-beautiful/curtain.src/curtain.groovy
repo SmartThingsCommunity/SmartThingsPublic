6a4704b914ffb555b5dcd2c8452cfa5e2831c8e8
Alex Feng <devropes@gmail.com>
2019-10-13 17:09:35 -0700

    switched to use ZigBee Window Shade Capability

--- devicetypes/rooms-beautiful/curtain.src/curtain.groovy
+++ devicetypes/rooms-beautiful/curtain.src/curtain.groovy
@@ -1,36 +1,50 @@
+/**
+ *
+ *	Copyright 2019 SmartThings
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *	in compliance with the License. You may obtain a copy of the License at:
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *	on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *	for the specific language governing permissions and limitations under the License.
+ */
+
+import groovy.json.JsonOutput
 import physicalgraph.zigbee.zcl.DataType
 
 metadata {
-    definition (name: "Curtain", namespace: "Rooms Beautiful", author: "Alex Feng") {
-        capability "Actuator"
-        capability "Configuration"
-        capability "Health Check"
-        capability "Refresh"
-        capability "Switch"        
-        capability "Battery"
-        //capability "Switch Level"
+	definition(name: "Curtain", namespace: "Rooms Beautiful", author: "Alex Feng", ocfDeviceType: "oic.d.blind", mnmn: "SmartThings", vid: "generic-shade") {
+		capability "Actuator"
+		capability "Battery"
+		capability "Configuration"
+		capability "Refresh"
+		capability "Window Shade"
+		capability "Health Check"
         
         attribute("replay", "enum")
         attribute("battLife", "enum")
-        
-        command "pause"
+
+		command "pause"
         command "cont"
-        
-        fingerprint profileId: "0104", inClusters: "0000, 0001, 0003, 0006, FC00, DC00, 0102,", deviceJoinName: "Curtain", manufacturer: "Rooms Beautiful",  model: "C001"
-    }
+
+		fingerprint profileId: "0104", inClusters: "0000, 0001, 0003, 0006, FC00, DC00, 0102,", deviceJoinName: "Curtain", manufacturer: "Rooms Beautiful",  model: "C001"
+	}
     
     preferences {
         input name: "invert", type: "bool", title: "Invert Direction", description: "Invert Curtain Direction", defaultValue: false, displayDuringSetup: false, required: true
     }
 
-    tiles(scale: 2) {
-        multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true){
-            tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
-            	attributeState "on", label:'Open', action:"switch.off", icon:"st.shades.shade-open", backgroundColor:"#00A0DC", nextState:"turningOff"
-                attributeState "off", label:'Closed', action:"switch.on", icon:"st.shades.shade-closed", backgroundColor:"#bc2323", nextState:"turningOn"
-                attributeState "turningOn", label:'Opening', action:"switch.off", icon:"st.shades.shade-opening", backgroundColor:"#00A0DC", nextState:"turningOff"
-                attributeState "turningOff", label:'Closing', action:"switch.on", icon:"st.shades.shade-closing", backgroundColor:"#bc2323", nextState:"turningOn"
-            }
+	tiles(scale: 2) {
+		multiAttributeTile(name:"windowShade", type: "generic", width: 6, height: 4) {
+			tileAttribute("device.windowShade", key: "PRIMARY_CONTROL") {
+				attributeState "open", label: 'Open', action: "close", icon: "http://www.ezex.co.kr/img/st/window_open.png", backgroundColor: "#00A0DC", nextState: "closing"
+				attributeState "closed", label: 'Closed', action: "open", icon: "http://www.ezex.co.kr/img/st/window_close.png", backgroundColor: "#ffffff", nextState: "opening"
+				attributeState "opening", label: 'Opening', action: "close", icon: "http://www.ezex.co.kr/img/st/window_open.png", backgroundColor: "#00A0DC", nextState: "opening"
+				attributeState "closing", label: 'Closing', action: "open", icon: "http://www.ezex.co.kr/img/st/window_close.png", backgroundColor: "#ffffff", nextState: "closing"
+			}
             
             tileAttribute ("device.battLife", key: "SECONDARY_CONTROL") {
                 attributeState "full", icon: "https://raw.githubusercontent.com/gearsmotion789/ST-Images/master/full.png"
@@ -38,51 +52,19 @@ metadata {
                 attributeState "low", icon: "https://raw.githubusercontent.com/gearsmotion789/ST-Images/master/low.png"
                 attributeState "dead", icon: "https://raw.githubusercontent.com/gearsmotion789/ST-Images/master/dead.png"
             }
-            
-            /*tileAttribute ("device.level", key: "SLIDER_CONTROL") {
-                attributeState "level", action:"switch level.setLevel"
-            }*/
-        }        
-        standardTile("contPause", "device.replay", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+		}
+		standardTile("contPause", "device.replay", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
         	state "pause", label: "Pause", icon:'https://raw.githubusercontent.com/gearsmotion789/ST-Images/master/pause.png', action:'pause', backgroundColor:"#e86d13", nextState: "cont"
             state "cont", label: "Cont.", icon:'https://raw.githubusercontent.com/gearsmotion789/ST-Images/master/play.png', action:'cont', backgroundColor:"#90d2a7", nextState: "pause"
         }
-        valueTile("battery", "device.battery", inactiveLabel: false, decoration:"flat", width:1, height:1) {		            
-        	state "battery", label: 'Batt: ${currentValue}%', unit:""/*
-				/*backgroundColors: [
-                    [value: "50", color: "#e86d13"],
-                    [value: "100", color: "#90d2a7"]
-            	]*/
-        }
-        standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 1, height: 1) {
-            state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
-        }        
-        
-        main "switch"
-        details(["switch", "contPause", /*"battery",*/ "refresh"])
-    }
-}
+		standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 1) {
+			state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
+		}
 
-/*def setLevel(value) {
-	if (value >= 70){
-      	log.debug "Battery Full: " + value
-        sendEvent(name: "battLife", value: "full")
-    }
-	else if (value >= 60 && value < 75){
-       	log.debug "Battery Medium: " + value
-      	sendEvent(name: "battLife", value: "medium")
-    }
-    else if (value >= 50 && value < 60){
-      	log.debug "Battery Low: " + value
-       	sendEvent(name: "battLife", value: "low")
-    }
-    else if (value < 50){
-      	log.debug "Battery Dead: " + value
-      	sendEvent(name: "battLife", value: "dead")
-    }
-                
-	sendEvent(name: "level", value: value)
-}*/
+		main "windowShade"
+		details(["windowShade", "contPause", "refresh"])
+	}
+}
 
 // Parse incoming device messages to generate events
 def parse(String description) {
@@ -102,6 +84,13 @@ def parse(String description) {
         
         sendEvent(event)
         log.debug "${linkText} - On/Off: ${event.value}"
+        if(event.name == "switch" || event.name == "windowShade"){
+        	if(event.value == "on"){
+            	sendEvent(name: "windowShade", value: "open")
+            } else {
+            	sendEvent(name: "windowShade", value: "closed")
+            }
+        }
     }
     else {
        	if(descMap.attrId) {
@@ -140,11 +129,11 @@ def parse(String description) {
     }
 }
 
-def off() {
+def close() {
     zigbee.off()
 }
 
-def on() {
+def open() {
     zigbee.on()
 }
 
@@ -203,6 +192,7 @@ def configure() {
 }
 
 def installed() {
+	sendEvent(name: "supportedWindowShadeCommands", value: JsonOutput.toJson(["open", "close", "pause"]), displayed: true)
 	sendEvent(name: "battery", value: 100)
 	sendEvent(name: "battLife", value: "full")
 	response(refresh())
