d4ebf016390fb65e8acf1bba8406239877093180
user <user@linux-2030019672.sa.corp.samsungelectronics.net>
2019-10-15 15:05:22 +0530

    DTH for Aeotec TriSensor AU region

--- devicetypes/smartthings/zwave-motion-temp-light-sensor.src/zwave-motion-temp-light-sensor.groovy
+++ devicetypes/smartthings/zwave-motion-temp-light-sensor.src/zwave-motion-temp-light-sensor.groovy
@@ -28,6 +28,7 @@ metadata {
 
 		fingerprint mfr:"0371", prod:"0002", model:"0005", deviceJoinName: "Aeotec TriSensor"	//ZW005-C EU
 		fingerprint mfr:"0371", prod:"0102", model:"0005", deviceJoinName: "Aeotec TriSensor" 	//ZW005-A US
+	        fingerprint mfr:"0371", prod:"0202", model:"0005", deviceJoinName: "Aeotec TriSensor" 	//ZW005-B AU
 	}
 
 	tiles(scale: 2) {
