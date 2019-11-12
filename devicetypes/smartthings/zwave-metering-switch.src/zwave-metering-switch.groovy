99ae94981cd123c8345fc12d71ee2a02d7211a1b
ZWozniakS <zuz.anna@outlook.com>
2019-11-12 10:02:28 +0100

    Added plug icon for POPP Smart Outdoor Plug

--- devicetypes/smartthings/zwave-metering-switch.src/zwave-metering-switch.groovy
+++ devicetypes/smartthings/zwave-metering-switch.src/zwave-metering-switch.groovy
@@ -50,7 +50,7 @@ metadata {
 		fingerprint mfr: "0371", prod: "0103", model: "00AF", deviceJoinName: "Aeotec Smart Switch 7", ocfDeviceType: "oic.d.smartplug"  //US
 		fingerprint mfr: "0060", prod: "0004", model: "000B", deviceJoinName: "Everspring Smart Plug", ocfDeviceType: "oic.d.smartplug"  //US
 		fingerprint mfr: "031E", prod: "0002", model: "0001", deviceJoinName: "Inovelli Switch Red Series" //US
-		fingerprint mfr: "0154", prod: "0003", model: "000A", deviceJoinName: "POPP Smart Outdoor Plug" //EU
+		fingerprint mfr: "0154", prod: "0003", model: "000A", deviceJoinName: "POPP Smart Outdoor Plug", ocfDeviceType: "oic.d.smartplug" //EU
 	}
 
 	// simulator metadata
