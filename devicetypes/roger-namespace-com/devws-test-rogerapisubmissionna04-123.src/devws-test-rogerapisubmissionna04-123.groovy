5a197ee59c8bde40fcb2b11c8d86db17e0a961ae
Roger Alpha Test <rogeralphatestuser@samihub.com>
2019-12-20 14:03:46 +0900

    DevWs-acceptance for Roger Role Test User containing containing DevWS Test: RogerAPISubmissionNA04_123

---
+++ devicetypes/roger-namespace-com/devws-test-rogerapisubmissionna04-123.src/devws-test-rogerapisubmissionna04-123.groovy
@@ -0,0 +1,72 @@
+/**
+ *  DevWS Test: RogerAPISubmissionNA04_123
+ *
+ *  Copyright 2019 Icl Roger
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ */
+metadata {
+	definition (name: "DevWS Test: RogerAPISubmissionNA04_123", namespace: "roger.namespace.com", author: "Icl Roger", cstHandler: true) {
+		capability "Alarm"
+		capability "Battery"
+		capability "Lock"
+
+		fingerprint mfr: "1234", prod: "2345", model: "3456", deviceJoinName: "RogerDeviceJoinName123"
+	}
+
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'alarm' attribute
+	// TODO: handle 'battery' attribute
+	// TODO: handle 'lock' attribute
+
+}
+
+// handle commands
+def off() {
+	log.debug "Executing 'off'"
+	// TODO: handle 'off' command
+}
+
+def strobe() {
+	log.debug "Executing 'strobe'"
+	// TODO: handle 'strobe' command
+}
+
+def siren() {
+	log.debug "Executing 'siren'"
+	// TODO: handle 'siren' command
+}
+
+def both() {
+	log.debug "Executing 'both'"
+	// TODO: handle 'both' command
+}
+
+def lock() {
+	log.debug "Executing 'lock'"
+	// TODO: handle 'lock' command
+}
+
+def unlock() {
+	log.debug "Executing 'unlock'"
+	// TODO: handle 'unlock' command
+}
