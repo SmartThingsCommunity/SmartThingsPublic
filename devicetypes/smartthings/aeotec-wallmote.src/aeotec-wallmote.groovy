37337c62161b02fecffd77f1f56c9963f625afa4
ZWozniakS <zuz.anna@outlook.com>
2020-01-13 12:08:09 +0100

    Added missing supproted button values for the child button

--- devicetypes/smartthings/aeotec-wallmote.src/aeotec-wallmote.groovy
+++ devicetypes/smartthings/aeotec-wallmote.src/aeotec-wallmote.groovy
@@ -145,7 +145,7 @@ def createChildDevices() {
             child = addChildDevice("Child Button", "${device.deviceNetworkId}:${i}", device.hubId,
                     [completedSetup: true, label: "${device.displayName} button ${i}",
                      isComponent: true, componentName: "button$i", componentLabel: "Button $i"])
-            child.sendEvent(name: "supportedButtonValues", value: ["pushed", "held"].encodeAsJson(), displayed: false)
+            child.sendEvent(name: "supportedButtonValues", value: supportedButtonValues.encodeAsJson(), displayed: false)
             child.sendEvent(name: "button", value: "pushed", data: [buttonNumber: 1], descriptionText: "$child.displayName was pushed", isStateChange: true, displayed: false)
         }
     }
