09b29308864e8144bfef1b3b8e55e1ba7957001b
ZouLianghuiS <lianghui.zou@samsung.com>
2020-02-13 14:31:42 +0800

    Update zigbee-multi-button.groovy

--- devicetypes/smartthings/zigbee-multi-button.src/zigbee-multi-button.groovy
+++ devicetypes/smartthings/zigbee-multi-button.src/zigbee-multi-button.groovy
@@ -30,7 +30,7 @@ metadata {
 
 		fingerprint inClusters: "0000, 0001, 0003, 0007, 0020, 0B05", outClusters: "0003, 0006, 0019", manufacturer: "CentraLite", model:"3450-L", deviceJoinName: "Iris KeyFob", mnmn: "SmartThings", vid: "generic-4-button"
 		fingerprint inClusters: "0000, 0001, 0003, 0007, 0020, 0B05", outClusters: "0003, 0006, 0019", manufacturer: "CentraLite", model:"3450-L2", deviceJoinName: "Iris KeyFob", mnmn: "SmartThings", vid: "generic-4-button"
-		fingerprint profileId: "0104", inClusters: "0004", outClusters: "0000, 0001, 0003, 0004, 0005, 0B05", manufacturer: "HEIMAN", model: "SceneSwitch-EM-3.0", deviceJoinName: "HEIMAN Scene Keypad", vid: "generic-4-button-alt"
+		fingerprint profileId: "0104", inClusters: "0004", outClusters: "0000, 0001, 0003, 0004, 0005, 0B05", manufacturer: "HEIMAN", model: "SceneSwitch-EM-3.0", deviceJoinName: "HEIMAN Scene Keypad", vid: "generic-4-button"
 	}
 
 	tiles {
@@ -70,15 +70,15 @@ def parseAttrMessage(description) {
 		map = getButtonEvent(descMap)
 	} else if(descMap?.clusterInt == 0xFC80) {
 		def buttonNumber
-		buttonNumber = Integer.valueOf(descMap?.command[1].toInteger())
+		buttonNumber = Integer.valueOf(descMap?.command[1].toInteger()) + 1
        
 		log.debug "Number is ${buttonNumber}"
 		def event = createEvent(name: "button", value: "pushed", data: [buttonNumber: buttonNumber], descriptionText: "pushed", isStateChange: true)
-		if (buttonNumber != 0) {
-			sendEventToChild(buttonNumber, event)
-		} else {
+		if (buttonNumber == 1) {
 			sendEvent(event)
+			sendEventToChild(buttonNumber, event)
 		}
+		sendEventToChild(buttonNumber, event)
    	}
 	map
 }
@@ -233,7 +233,7 @@ private getSupportedButtonValues() {
 private getModelNumberOfButtons() {[
 		"3450-L" : 4,
 		"3450-L2" : 4,
-		"SceneSwitch-EM-3.0" : 3
+		"SceneSwitch-EM-3.0" : 4
 ]}
 
 private getModelBindings(model) {
