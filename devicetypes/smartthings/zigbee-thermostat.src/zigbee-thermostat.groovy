3114bf0c619608ab88bcd882a5e0893f88649349
philip-p <philipp@umbrela.co>
2020-02-27 14:44:29 -0600

    DevWs for Umbrela Smart Inc containing containing Zigbee Thermostat (#20655)

--- devicetypes/smartthings/zigbee-thermostat.src/zigbee-thermostat.groovy
+++ devicetypes/smartthings/zigbee-thermostat.src/zigbee-thermostat.groovy
@@ -37,6 +37,7 @@ metadata {
 		capability "Sensor"
 
 		fingerprint profileId: "0104", inClusters: "0000,0001,0003,0004,0005,0020,0201,0202,0204,0B05", outClusters: "000A, 0019",  manufacturer: "LUX", model: "KONOZ", deviceJoinName: "LUX KONOz Thermostat"
+        fingerprint profileId: "0104", inClusters: "0000,0003,0020,0201,0202,0405", outClusters: "0019, 0402", manufacturer: "Umbrela", model: "Thermostat", deviceJoinName: "Umbrela UTee"
 	}
 
 	tiles {
