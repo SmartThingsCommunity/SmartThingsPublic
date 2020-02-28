648f04d0a974f4df8fa61399920c17b474fc77b3
Juhaki Park <37175353+juhaki@users.noreply.github.com>
2020-02-28 10:15:05 +0900

    Update devicetypes/smartthings/dawon-zwave-smart-switch.src/dawon-zwave-wall-smart-switch.groovy
    Co-Authored-By: Steven Green <steven.green@smartthings.com>

--- devicetypes/smartthings/dawon-zwave-smart-switch.src/dawon-zwave-wall-smart-switch.groovy
+++ devicetypes/smartthings/dawon-zwave-smart-switch.src/dawon-zwave-wall-smart-switch.groovy
@@ -26,7 +26,7 @@ metadata {
 	}
 
 	preferences {
-		input "periodOffset", "number", title: "Reporting periodOffset", description: "Adjust reporting periodOffset by this many degrees", range: "*..*", displayDuringSetup: false
+		input "reportingInterval", "number", title: "Reporting interval", description: "How often the device should report in minuted", range: "*..*", displayDuringSetup: false
 		input "tempOffset", "number", title: "Temperature Offset", description: "Adjust temperature by this many degrees", range: "*..*", displayDuringSetup: false
 		input "humidityOffset", "number", title: "Humidity Offset", description: "Adjust humidity by this percentage", range: "*..*", displayDuringSetup: false
 	}
