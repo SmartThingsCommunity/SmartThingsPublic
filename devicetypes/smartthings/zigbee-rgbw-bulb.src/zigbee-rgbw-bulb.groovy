795d077f7aa705790be431d9993801a0d1a08cb2
Konrad Klimczuk <k.klimczuk2@partner.samsung.com>
2020-05-08 09:43:01 +0200

    ICP-10928 - Initially setting a color temperature to 6500K while configuring the device.

--- devicetypes/smartthings/zigbee-rgbw-bulb.src/zigbee-rgbw-bulb.groovy
+++ devicetypes/smartthings/zigbee-rgbw-bulb.src/zigbee-rgbw-bulb.groovy
@@ -201,10 +201,10 @@ def configure() {
 	// enrolls with default periodic reporting until newer 5 min interval is confirmed
 	sendEvent(name: "checkInterval", value: 2 * 10 * 60 + 1 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
 
-	// OnOff, level, colorTemperature minReportTime 0 seconds, maxReportTime 5 min. Reporting interval if no activity
+	// OnOff, level minReportTime 0 seconds, maxReportTime 5 min. Reporting interval if no activity
 	zigbee.onOffConfig(0, 300) +
 	zigbee.levelConfig(0, 300) +
-	zigbee.colorTemperatureConfig(0, 300) +
+	zigbee.setColorTemperature(6500) +
 	refresh()
 }
 
