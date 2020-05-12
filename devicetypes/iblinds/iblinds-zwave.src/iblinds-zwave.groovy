484bfb87f8a4c16428785ccf5a6bfcfed465fddc
Donald Kirker <donald@smartthings.com>
2020-05-12 12:33:49 -0700

    Add beginning of logic organization for handling Z-Wave functional device use cases

--- devicetypes/iblinds/iblinds-zwave.src/iblinds-zwave.groovy
+++ devicetypes/iblinds/iblinds-zwave.src/iblinds-zwave.groovy
@@ -75,7 +75,11 @@ metadata {
 
 		preferences {
 			input "preset", "number", title: "Preset position", description: "Set the window shade preset position", defaultValue: 50, range: "1..100", required: false, displayDuringSetup: false
-			input "reverse", "bool", title: "Reverse", description: "Reverse Blind Direction", defaultValue: false, required: false, displayDuringSetup: false
+
+			// User has the option of opening/closing with slats down or up -- this controls the behavior when using Window Shade Level
+			input "reverse", "bool", title: "Reverse", description: "Reverse blind direction", defaultValue: false, required: false, displayDuringSetup: false
+
+			// Right now the device does not auto-report SwitchMultilevelReport so we will use this to determine, based on the user's setup, how long to delay before SwitchMultilevelGet
 		}
 
 		main(["windowShade"])
@@ -132,22 +136,9 @@ def zwaveEvent(physicalgraph.zwave.commands.switchmultilevelv3.SwitchMultilevelR
 	handleLevelReport(cmd)
 }
 
-private handleLevelReport(physicalgraph.zwave.Command cmd) {
-	def descriptionText = null
-	def shadeValue = null
-	def level = cmd.value as Integer
-
-	if (!state.lastbatt || now() - state.lastbatt > 24 * 60 * 60 * 1000) {
-		log.debug "requesting battery"
-		state.lastbatt = (now() - 23 * 60 * 60 * 1000) // don't queue up multiple battery reqs in a row
-		result << response(["delay 15000", zwave.batteryV1.batteryGet().format()])
-	}
-
-	result
-}
-
 def zwaveEvent(physicalgraph.zwave.commands.switchmultilevelv3.SwitchMultilevelStopLevelChange cmd) {
-	[ createEvent(name: "windowShade", value: "partially open", displayed: false, descriptionText: "$device.displayName shade stopped"),  response(zwave.switchMultilevelV1.switchMultilevelGet().format()) ]
+	[ createEvent(name: "windowShade", value: "partially open", displayed: false, descriptionText: "$device.displayName shade stopped"),
+	  response(zwave.switchMultilevelV1.switchMultilevelGet().format()) ]
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.batteryv1.BatteryReport cmd) {
@@ -172,10 +163,91 @@ def zwaveEvent(physicalgraph.zwave.Command cmd) {
 	return []
 }
 
+/**
+ * Helper functions for converting between parabolic slat tilt values and
+ * SmartThings capability percentages, taking in account user preferences.
+ *
+ * Device behavior:
+ *      0 is 0% slats down (closed)
+ *   1-49 is 1-99% slats down (partially open)
+ *     50 is 100% open slats horizontal (open)
+ *  51-98 is 99-1% slats up (51 is 99% open) (partially open)
+ *     99 is 0% slats up (closed)
+ *
+ * Slat orientation:
+ *     Up: user view ->		\
+ *   Down: user view ->		/
+ *   Open: user view ->		--
+ */
+
+private getTILTLEVEL_FULLY_OPEN() { 50 }
+private getTILTLEVEL_FULLY_CLOSED_UP() { 99 }
+private getTILTLEVEL_FULLY_CLOSED_DOWN() { 0 }
+
+private getSlatDirectionMap() {[
+ 	DOWN: 0,
+ 	UP: 1
+]}
+
+private deviceEvtToCapability(deviceLevel) {
+	def capabilityLevel = deviceLevel
+	def direction = slatDirectionMap.DOWN
+
+	return [level: capabilityLevel, direction: direction]
+}
+
+private capabilityEvtToDevice(level, direction) {
+
+}
+
+/**
+ * Device handler helper function
+ */
+
+private buildWindowShadeEvents(level) {
+	def result = []
+	def descriptionText = null
+	def shadeValue = null
+
+	if (level >= 99) {
+		level = 100
+		shadeValue = "open"
+	} else if (level <= 0) {
+		level = 0
+		shadeValue = "closed"
+	} else {
+		shadeValue = "partially open"
+		descriptionText = "${device.displayName} tilt is ${shadeLevel.level}% open"
+	}
+
+	result << createEvent(name: "level", value: level, unit: "%")
+	result << createEvent(name: "shadeLevel", value: level, unit: "%")
+	result << createEvent(name: "windowShade", value: shadeValue, descriptionText: descriptionText)
+
+	return result
+}
+
+private handleLevelReport(physicalgraph.zwave.Command cmd) {
+	def shadeLevel = deviceEvtToCapability(cmd.value as Integer)
+	def result = buildWindowShadeEvents(shadeLevel.level)
+
+	if (!state.lastbatt || now() - state.lastbatt > 24 * 60 * 60 * 1000) {
+		log.debug "requesting battery"
+		state.lastbatt = (now() - 23 * 60 * 60 * 1000) // don't queue up multiple battery reqs in a row
+		result << response(["delay 15000", zwave.batteryV1.batteryGet().format()])
+	}
+
+	return result
+}
+
+/**
+ * Capability commands
+ */
+
 def open() {
 	log.debug "open()"
 
-	setShadeLevel(50)
+	setShadeLevel(100)
 }
 
 def close() {
@@ -198,14 +270,10 @@ def setLevel(value, duration = null) {
 
 def setShadeLevel(value) {
 	def descriptionText = null
-	def shadeValue = null
 
 	log.debug "setShadeLevel($value)"
-	Integer level = value as Integer
-
-	if (level < 0) level = 0
-	if (level > 99) level = 99
-	Integer tiltLevel = level as Integer //we will use this value to decide what level is sent to device (reverse or not reversed)
+	Integer level = Math.max(Math.min((value as Integer), 99), 0)
+	Integer tiltLevel = level / 2 // we will use this value to decide what level is sent to device (reverse or not reversed)
 
 	if (reverse) {  // Check to see if user wants blinds to operate in reverse direction
 		tiltLevel = 99 - level
@@ -223,11 +291,12 @@ def setShadeLevel(value) {
 		descriptionText = "${device.displayName} tilt level is ${level}% open"
 		sendEvent(name: "windowShade", value: "partially open" , descriptionText: descriptionText)
 	}
+
 	//log.debug "Level - ${level}%  & Tilt Level - ${tiltLevel}%"
 	sendEvent(name: "level", value: level, unit: "%", descriptionText: descriptionText)
 	sendEvent(name: "shadeLevel", value: level, unit: "%", descriptionText: descriptionText)
-	zwave.switchMultilevelV3.switchMultilevelSet(value: tiltLevel).format()
 
+	zwave.switchMultilevelV3.switchMultilevelSet(value: tiltLevel).format()
 }
 
 def presetPosition() {
