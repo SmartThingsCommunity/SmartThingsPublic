25ab48af636969620ce54f8930739ab675932695
RogerUI10 Corp qaST <roger_ui10corp_qast@samihub.com>
2020-05-26 15:51:18 -0400

    DevWs-acceptance for UI Automation QA containing containing DevWS Test: RogerUISubmission_100

---
+++ devicetypes/test-switch/devws-test-rogeruisubmission-100.src/devws-test-rogeruisubmission-100.groovy
@@ -0,0 +1,65 @@
+/**
+ *  DevWS Test: RogerUISubmission_100
+ *
+ *  Copyright 2020 RogerUI10 Corp qaST
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ */
+metadata {
+	definition (name: "DevWS Test: RogerUISubmission_100", namespace: "Test Switch", author: "RogerUI10 Corp qaST", cstHandler: true) {
+		capability "Color Control"
+		capability "Lock"
+	}
+
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'hue' attribute
+	// TODO: handle 'saturation' attribute
+	// TODO: handle 'color' attribute
+	// TODO: handle 'lock' attribute
+
+}
+
+// handle commands
+def setHue() {
+	log.debug "Executing 'setHue'"
+	// TODO: handle 'setHue' command
+}
+
+def setSaturation() {
+	log.debug "Executing 'setSaturation'"
+	// TODO: handle 'setSaturation' command
+}
+
+def setColor() {
+	log.debug "Executing 'setColor'"
+	// TODO: handle 'setColor' command
+}
+
+def lock() {
+	log.debug "Executing 'lock'"
+	// TODO: handle 'lock' command
+}
+
+def unlock() {
+	log.debug "Executing 'unlock'"
+	// TODO: handle 'unlock' command
+}
