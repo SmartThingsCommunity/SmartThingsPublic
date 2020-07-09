a50c3d830765845574d738064ed7af6c70b2cd5b
ericyew <eric.ycs@gmail.com>
2020-07-09 13:48:26 +1000

    Update and rename devicetypes/ericyew/smartsense-motion-sensor.groovy to devicetypes/ericyew/smartsense-motion-sensor.src/smartsense-motion-sensor.groovy

--- devicetypes/ericyew/smartsense-motion-sensor.src/smartsense-motion-sensor.groovy
+++ devicetypes/ericyew/smartsense-motion-sensor.src/smartsense-motion-sensor.groovy
@@ -17,7 +17,7 @@ import physicalgraph.zigbee.clusters.iaszone.ZoneStatus
 
 
 metadata {
-	definition (name: "SmartSense Motion Sensor", namespace: "smartthings", author: "SmartThings") {
+	definition (name: "SmartSense Motion Sensor", namespace: "ericyew", author: "SmartThings") {
 		capability "Motion Sensor"
 		capability "Configuration"
 		capability "Battery"
