1cd23c7fa0c3a8efc5b8999e0fb0b2b816b558dc
Konrad Klimczuk <k.klimczuk2@partner.samsung.com>
2020-07-17 15:30:56 +0200

    ICP-10928 - changes order of configuration commands, initially set color temperature to 7000.

--- devicetypes/smartthings/zigbee-rgbw-bulb.src/zigbee-rgbw-bulb.groovy
+++ devicetypes/smartthings/zigbee-rgbw-bulb.src/zigbee-rgbw-bulb.groovy
@@ -201,11 +201,11 @@ def configure() {
 	// enrolls with default periodic reporting until newer 5 min interval is confirmed
 	sendEvent(name: "checkInterval", value: 2 * 10 * 60 + 1 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
 
+	zigbee.setColorTemperature(7000) +
+	refresh() +
 	// OnOff, level minReportTime 0 seconds, maxReportTime 5 min. Reporting interval if no activity
 	zigbee.onOffConfig(0, 300) +
-	zigbee.levelConfig(0, 300) +
-	zigbee.setColorTemperature(6500) +
-	refresh()
+	zigbee.levelConfig(0, 300)
 }
 
 def setColorTemperature(value) {
