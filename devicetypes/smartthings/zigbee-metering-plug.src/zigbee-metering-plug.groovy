01c9c1f056a22f3417da77b3a1668b478ef0b7dc
남수 허 <thecloain@powermanager.co.kr>
2020-09-17 09:21:00 +0900

    1. Changed deviceJoinName(ST Dawon Outlet -> Dawon Outlet). 2. Changed the structure of getEnergyDiv(). 3. Added isDawonOutlet() .

--- devicetypes/smartthings/zigbee-metering-plug.src/zigbee-metering-plug.groovy
+++ devicetypes/smartthings/zigbee-metering-plug.src/zigbee-metering-plug.groovy
@@ -37,7 +37,7 @@ metadata {
         fingerprint manufacturer: "DAWON_DNS", model: "PM-C140-ZB", deviceJoinName: "Dawon Outlet" // DAWON DNS In-Wall Outlet PM-C140-ZB, raw description: 01 0104 0051 01 0A 0000 0002 0003 0004 0006 0019 0702 0B04 0008 0009 0A 0000 0002 0003 0004 0006 0019 0702 0B04 0008 0009
         
         fingerprint profileId: "0104", inClusters: "0000, 0002, 0003, 0006, 0702, 0B04", outClusters: "0003, 0019", manufacturer: "DAWON_DNS", model: "PM-B540-ZB",  deviceJoinName: "Dawon Outlet" // DAWON DNS Smart Plug
-        fingerprint profileId: "0104", inClusters: "0000, 0002, 0003, 0006, 0702, 0B04", outClusters: "0003, 0019", manufacturer: "DAWON_DNS", model: "ST-B550-ZB",  deviceJoinName: "ST Dawon Outlet" // DAWON DNS Smart Plug
+        fingerprint profileId: "0104", inClusters: "0000, 0002, 0003, 0006, 0702, 0B04", outClusters: "0003, 0019", manufacturer: "DAWON_DNS", model: "ST-B550-ZB",  deviceJoinName: "Dawon Outlet" // DAWON DNS Smart Plug
         fingerprint profileId: "0104", inClusters: "0000, 0003, 0006, 0702, 0B04", outClusters: "0003, 0019", manufacturer: "DAWON_DNS", model: "PM-C150-ZB",  deviceJoinName: "Dawon Outlet" // DAWON DNS In-Wall Outlet
         fingerprint profileId: "0104", inClusters: "0000, 0003, 0006, 0702, 0B04", outClusters: "0003, 0019", manufacturer: "DAWON_DNS", model: "PM-C250-ZB",  deviceJoinName: "Dawon Outlet" // DAWON DNS In-Wall Outlet
 	}
@@ -176,14 +176,19 @@ private int getPowerDiv() {
 }
 
 private int getEnergyDiv() {
-    if (device.getDataValue("manufacturer") == "DAWON_DNS") {
+    if (isDawonOutlet()) {
         1000
-    }
-    else {
-        isSengledOutlet() ? 10000 : 100
+    } else if (isSengledOutlet()) {
+        10000
+    } else {
+        100
     }
 }
 
 private boolean isSengledOutlet() {
     device.getDataValue("model") == "E1C-NB7"
+}
+
+private boolean isDawonOutlet() {
+    device.getDataValue("manufacturer") == "DAWON_DNS"
 }
