069c79e3af9290bad24034cb8af8479c60eaa13f
PKacprowiczS <41617389+PKacprowiczS@users.noreply.github.com>
2020-10-02 19:29:44 +0200

    [ICP-13618] Qubino Flush 2 Relay - energy usage is not reported (#43446)
    * Added daily energy usage report
    
    * Changes in getEnergyUsage method
    
    * Moved to querying energy usage after power report

--- devicetypes/qubino/qubino-flush-2-relay.src/qubino-flush-2-relay.groovy
+++ devicetypes/qubino/qubino-flush-2-relay.src/qubino-flush-2-relay.groovy
@@ -81,6 +81,7 @@ def installed() {
  	if (!childDevices && state.numberOfSwitches > 1) {
 		addChildSwitches(state.numberOfSwitches)
 	}
+
 	sendEvent(name: "checkInterval", value: 2 * 15 * 60 + 2 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID])
 	// Preferences template begin
 	state.currentPreferencesState = [:]
@@ -255,11 +256,15 @@ private changeSwitch(endpoint, cmd) {
 def zwaveEvent(physicalgraph.zwave.commands.meterv3.MeterReport cmd, ep = null) {
 	log.debug "Meter ${cmd}" + (ep ? " from endpoint $ep" : "")
 	if (ep == 1) {
-		createEvent(createMeterEventMap(cmd))
+		[
+				createEvent(createMeterEventMap(cmd)),
+				response(encap(zwave.meterV3.meterGet(scale: 0x00), 1))
+		]
 	} else if (ep) {
 		String childDni = "${device.deviceNetworkId}:$ep"
 		def child = childDevices.find { it.deviceNetworkId == childDni }
 		child?.sendEvent(createMeterEventMap(cmd))
+		response(encap(zwave.meterV3.meterGet(scale: 0x00), ep))
 	}
 }
 
