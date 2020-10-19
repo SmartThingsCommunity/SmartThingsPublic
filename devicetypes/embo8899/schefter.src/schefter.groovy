4f663f3ac85aaec4436be09e9b2218d45e151033
Enko Schefter <embo8899@gmail.com>
2020-10-19 06:25:35 -0700

    Motion 1
    Samotech Motion

---
+++ devicetypes/embo8899/schefter.src/schefter.groovy
@@ -0,0 +1,35 @@
+/**
+ *  SCHEFTER
+ *
+ *  Copyright 2020 Enko Schefter
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ */
+metadata {
+	definition (name: "SCHEFTER", namespace: "embo8899", author: "Enko Schefter", cstHandler: true) {
+		capability "Motion Sensor"
+	}
+
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'motion' attribute
+
+}
