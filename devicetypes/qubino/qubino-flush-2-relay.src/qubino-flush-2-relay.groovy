a494fe0df3ef628b393d077588f7ba54e45650e8
MGoralczykS <m.goralczyk@samsung.com>
2020-10-22 15:39:38 +0200

    Fixed powerMeter, handling on/off from device

--- devicetypes/qubino/qubino-flush-2-relay.src/qubino-flush-2-relay.groovy
+++ devicetypes/qubino/qubino-flush-2-relay.src/qubino-flush-2-relay.groovy
@@ -81,7 +81,6 @@ def installed() {
  	if (!childDevices && state.numberOfSwitches > 1) {
 		addChildSwitches(state.numberOfSwitches)
 	}
-
 	sendEvent(name: "checkInterval", value: 2 * 15 * 60 + 2 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID])
 	// Preferences template begin
 	state.currentPreferencesState = [:]
@@ -94,8 +93,9 @@ def installed() {
 	}
 	// Preferences template end
 	response([
-			refresh((1..state.numberOfSwitches).toList())
-	])
+			refresh((1..state.numberOfSwitches).toList()),
+			addToAssociationGroup()
+	].flatten())
 }
 
 def updated() {
@@ -127,6 +127,14 @@ def excludeParameterFromSync(preference){
 		log.warn "Preference no ${preference.parameterNumber} - ${preference.key} is not supported by this device"
 	}
 	return exclude
+}
+
+def addToAssociationGroup() {
+	def cmds = []
+	if(zwaveInfo?.model?.equals("0052")){
+		cmds += encap(zwave.associationV2.associationSet(groupingIdentifier: 2, nodeId: [zwaveHubNodeId]))
+	}
+	cmds
 }
 
 private syncConfiguration() {
@@ -237,6 +245,11 @@ def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicReport cmd, ep = null) 
 	changeSwitch(ep, cmd)
 }
 
+def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicSet cmd, ep = null) { 
+	log.debug "Basic ${cmd}" + (ep ? " from endpoint $ep" : "")
+	[changeSwitch(ep, cmd), response(["delay 2000", encap(zwave.meterV3.meterGet(scale: 2), endpoint)])]
+}
+
 def zwaveEvent(physicalgraph.zwave.commands.switchbinaryv1.SwitchBinaryReport cmd, ep = null) {
 	log.debug "Binary ${cmd}" + (ep ? " from endpoint $ep" : "")
 	changeSwitch(ep, cmd)
@@ -244,7 +257,7 @@ def zwaveEvent(physicalgraph.zwave.commands.switchbinaryv1.SwitchBinaryReport cm
 
 private changeSwitch(endpoint, cmd) {
 	def value = cmd.value ? "on" : "off"
-	if (endpoint == 1) {
+	if (endpoint == 1 || endpoint == null) {
 		createEvent(name: "switch", value: value, isStateChange: true, descriptionText: "Switch ${endpoint} is ${value}")
 	} else if (endpoint) {
 		String childDni = "${device.deviceNetworkId}:$endpoint"
@@ -255,10 +268,10 @@ private changeSwitch(endpoint, cmd) {
 
 def zwaveEvent(physicalgraph.zwave.commands.meterv3.MeterReport cmd, ep = null) {
 	log.debug "Meter ${cmd}" + (ep ? " from endpoint $ep" : "")
-	if (ep == 1) {
+	if (ep == 1 || ep == null) {
 		[
 				createEvent(createMeterEventMap(cmd)),
-				response(encap(zwave.meterV3.meterGet(scale: 0x00), 1))
+				cmd.scale == 2 ? response(encap(zwave.meterV3.meterGet(scale: 0x00), ep)) : null
 		]
 	} else if (ep) {
 		String childDni = "${device.deviceNetworkId}:$ep"
@@ -328,10 +341,10 @@ def childOnOff(deviceNetworkId, value) {
 	if (switchId != null) sendHubCommand onOffCmd(value, switchId)
 }
 
-private onOffCmd(value, endpoint = 1) {
+private onOffCmd(value, endpoint = null) {
 	delayBetween([
 			encap(zwave.basicV1.basicSet(value: value), endpoint),
-			encap(zwave.basicV1.basicGet(), endpoint),
+			encap(zwave.basicV1.basicGet(), endpoint)
 	])
 }
 
