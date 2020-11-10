2ded6000c3641549a469dfd5e7021a817b3b1f1c
madi tang <madi@focalcrest.com>
2020-11-10 10:54:13 +0800

    rename Mixtile Relay Switch

--- devicetypes/smartthings/zigbee-switch.src/zigbee-switch.groovy
+++ devicetypes/smartthings/zigbee-switch.src/zigbee-switch.groovy
@@ -99,7 +99,7 @@ metadata {
 		fingerprint profileId: "0104", inClusters: "0000, 0002, 0003, 0006", manufacturer: "DAWON_DNS", model: "PM-S150-ZB", deviceJoinName: "Dawon Switch" //DAWOS DNS In-Wall Switch PM-S150-ZB
 		fingerprint profileId: "0104", inClusters: "0000, 0002, 0003, 0006", manufacturer: "DAWON_DNS", model: "ST-S150-ZB", deviceJoinName: "Dawon Switch" //DAWOS DNS In-Wall Switch ST-S150-ZB
         // Mixtile
-        fingerprint profileId: "0104", inClusters: "0000, 0004, 0005, 0006", outClusters: "0006, 0019", manufacturer: "Mixtile", model: "mixtile.wallswtich.separating.v2", deviceJoinName: "Mixitle In-Wall Swtich" // Mixtile In-Wall Swtich
+        fingerprint profileId: "0104", inClusters: "0000, 0004, 0005, 0006", outClusters: "0006, 0019", manufacturer: "Mixtile", model: "W10-WW80SR01", deviceJoinName: "Mixitle Relay Swtich" // Mixtile Relay Swtich
 	}
 
 	// simulator metadata
