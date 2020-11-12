bb29079429744194d31194a2426910bc65d0a0c3
greens <steven.green@smartthings.com>
2020-11-12 12:36:20 -0800

    remove fidure custom attribute and re-map operating states

--- devicetypes/smartthings/fidure-thermostat.src/fidure-thermostat.groovy
+++ devicetypes/smartthings/fidure-thermostat.src/fidure-thermostat.groovy
@@ -46,8 +46,6 @@ metadata {
 
 		attribute "lastTimeSync", "string"
 
-		attribute "thermostatOperatingState", "string"
-
 		fingerprint profileId: "0104", inClusters: "0000,0003,0004,0005,0201,0204,0B05", outClusters: "000A, 0019", deviceJoinName: "Fidure Thermostat"
 		fingerprint manufacturer: "Fidure", model: "A1732R3" , deviceJoinName: "Fidure Thermostat"// same clusters as above
 
@@ -99,9 +97,7 @@ metadata {
 		}
 
 		standardTile("hvacStatus", "thermostatOperatingState", inactiveLabel: false, decoration: "flat") {
-			state "Resting",  label: 'Resting'
-			state "Heating",  icon:"st.thermostat.heating"
-			state "Cooling",  icon:"st.thermostat.cooling"
+			state "thermostatOperatingState", label:'${currentValue}'
 		}
 
 
@@ -501,7 +497,7 @@ def Program() {
 
 
 def getThermostatOperatingState(value) {
-	String[] m = [ "heating", "cooling", "fan", "Heat2", "Cool2", "Fan2", "Fan3"]
+	String[] m = [ "heating", "cooling", "fan only", "heating", "cooling", "fan only", "fan only"]
 	String desc = 'idle'
 	value = Integer.parseInt(''+value, 16)
 
