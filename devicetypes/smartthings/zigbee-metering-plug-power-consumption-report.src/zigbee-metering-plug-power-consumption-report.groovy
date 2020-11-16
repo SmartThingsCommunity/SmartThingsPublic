e06ec76e40757b433f91173e4c806cc59c7bd80c
dwd-kwon <59678391+dwd-kwon@users.noreply.github.com>
2020-11-16 10:24:53 +0900

    Update
    Tiles removal and Change 4 spaces to 1 tab.

--- devicetypes/smartthings/zigbee-metering-plug-power-consumption-report.src/zigbee-metering-plug-power-consumption-report.groovy
+++ devicetypes/smartthings/zigbee-metering-plug-power-consumption-report.src/zigbee-metering-plug-power-consumption-report.groovy
@@ -14,172 +14,138 @@
 import physicalgraph.zigbee.zcl.DataType
 
 metadata {
-    definition (name: "Zigbee Metering Plug Power Consumption Report", namespace: "smartthings", author: "SmartThings", ocfDeviceType: "oic.d.smartplug", mnmn: "SmartThings",  vid: "generic-switch-power-energy") {
-        capability "Energy Meter"
-        capability "Power Meter"
-        capability "Actuator"
-        capability "Switch"
-        capability "Refresh"
-        capability "Health Check"
-        capability "Sensor"
-        capability "Configuration"
-        capability "Power Consumption Report"
+	definition (name: "Zigbee Metering Plug Power Consumption Report", namespace: "smartthings", author: "SmartThings", ocfDeviceType: "oic.d.smartplug", mnmn: "SmartThings",  vid: "generic-switch-power-energy") {
+		capability "Energy Meter"
+		capability "Power Meter"
+		capability "Actuator"
+		capability "Switch"
+		capability "Refresh"
+		capability "Health Check"
+		capability "Sensor"
+		capability "Configuration"
+		capability "Power Consumption Report"
 
-        fingerprint manufacturer: "DAWON_DNS", model: "PM-B430-ZB", deviceJoinName: "Dawon Outlet" // DAWON DNS Smart Plug PM-B430-ZB (10A), raw description: 01 0104 0051 01 07 0000, 0004, 0003, 0006, 0019, 0702, 0B04 07 0000, 0004, 0003, 0006, 0019, 0702, 0B04
-        fingerprint manufacturer: "DAWON_DNS", model: "PM-B530-ZB", deviceJoinName: "Dawon Outlet" // DAWON DNS Smart Plug PM-B530-ZB (16A), raw description: 01 0104 0051 01 07 0000, 0004, 0003, 0006, 0019, 0702, 0B04 07 0000, 0004, 0003, 0006, 0019, 0702, 0B04
-        fingerprint manufacturer: "DAWON_DNS", model: "PM-C140-ZB", deviceJoinName: "Dawon Outlet" // DAWON DNS In-Wall Outlet PM-C140-ZB, raw description: 01 0104 0051 01 0A 0000 0002 0003 0004 0006 0019 0702 0B04 0008 0009 0A 0000 0002 0003 0004 0006 0019 0702 0B04 0008 0009
-        fingerprint profileId: "0104", inClusters: "0000, 0002, 0003, 0006, 0702, 0B04", outClusters: "0003, 0019", manufacturer: "DAWON_DNS", model: "PM-B540-ZB",  deviceJoinName: "Dawon Outlet" // DAWON DNS Smart Plug
-        fingerprint profileId: "0104", inClusters: "0000, 0002, 0003, 0006, 0702, 0B04", outClusters: "0003, 0019", manufacturer: "DAWON_DNS", model: "ST-B550-ZB",  deviceJoinName: "Dawon Outlet" // DAWON DNS Smart Plug
-        fingerprint profileId: "0104", inClusters: "0000, 0003, 0006, 0702, 0B04", outClusters: "0003, 0019", manufacturer: "DAWON_DNS", model: "PM-C150-ZB",  deviceJoinName: "Dawon Outlet" // DAWON DNS In-Wall Outlet
-        fingerprint profileId: "0104", inClusters: "0000, 0003, 0006, 0702, 0B04", outClusters: "0003, 0019", manufacturer: "DAWON_DNS", model: "PM-C250-ZB",  deviceJoinName: "Dawon Outlet" // DAWON DNS In-Wall Outlet
+		fingerprint manufacturer: "DAWON_DNS", model: "PM-B430-ZB", deviceJoinName: "Dawon Outlet" // DAWON DNS Smart Plug PM-B430-ZB (10A), raw description: 01 0104 0051 01 07 0000, 0004, 0003, 0006, 0019, 0702, 0B04 07 0000, 0004, 0003, 0006, 0019, 0702, 0B04
+		fingerprint manufacturer: "DAWON_DNS", model: "PM-B530-ZB", deviceJoinName: "Dawon Outlet" // DAWON DNS Smart Plug PM-B530-ZB (16A), raw description: 01 0104 0051 01 07 0000, 0004, 0003, 0006, 0019, 0702, 0B04 07 0000, 0004, 0003, 0006, 0019, 0702, 0B04
+		fingerprint manufacturer: "DAWON_DNS", model: "PM-C140-ZB", deviceJoinName: "Dawon Outlet" // DAWON DNS In-Wall Outlet PM-C140-ZB, raw description: 01 0104 0051 01 0A 0000 0002 0003 0004 0006 0019 0702 0B04 0008 0009 0A 0000 0002 0003 0004 0006 0019 0702 0B04 0008 0009
+		fingerprint profileId: "0104", inClusters: "0000, 0002, 0003, 0006, 0702, 0B04", outClusters: "0003, 0019", manufacturer: "DAWON_DNS", model: "PM-B540-ZB",  deviceJoinName: "Dawon Outlet" // DAWON DNS Smart Plug
+		fingerprint profileId: "0104", inClusters: "0000, 0002, 0003, 0006, 0702, 0B04", outClusters: "0003, 0019", manufacturer: "DAWON_DNS", model: "ST-B550-ZB",  deviceJoinName: "Dawon Outlet" // DAWON DNS Smart Plug
+		fingerprint profileId: "0104", inClusters: "0000, 0003, 0006, 0702, 0B04", outClusters: "0003, 0019", manufacturer: "DAWON_DNS", model: "PM-C150-ZB",  deviceJoinName: "Dawon Outlet" // DAWON DNS In-Wall Outlet
+		fingerprint profileId: "0104", inClusters: "0000, 0003, 0006, 0702, 0B04", outClusters: "0003, 0019", manufacturer: "DAWON_DNS", model: "PM-C250-ZB",  deviceJoinName: "Dawon Outlet" // DAWON DNS In-Wall Outlet
 	}
-
-    tiles(scale: 2){
-        multiAttributeTile(name:"switch", type: "generic", width: 6, height: 4, canChangeIcon: true){
-                tileAttribute("device.switch", key: "PRIMARY_CONTROL") {
-                        attributeState("on", label: '${name}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#00a0dc")
-                        attributeState("off", label: '${name}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff")
-                }
-        }
-        valueTile("power", "device.power", decoration: "flat", width: 2, height: 2) {
-                state "default", label:'${currentValue} W'
-        }
-        valueTile("energy", "device.energy", decoration: "flat", width: 2, height: 2) {
-                state "default", label:'${currentValue} kWh'
-        }
-        standardTile("refresh", "device.power", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
-                state "default", label:'', action:"refresh.refresh", icon:"st.secondary.refresh"
-        }
-        standardTile("reset", "device.energy", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
-                state "default", label:'reset kWh', action:"reset"
-        }
-
-        main(["switch"])
-        details(["switch","power","energy","refresh","reset"])
-    }
 }
 
 def getATTRIBUTE_READING_INFO_SET() { 0x0000 }
 def getATTRIBUTE_HISTORICAL_CONSUMPTION() { 0x0400 }
 
 def parse(String description) {
-    log.debug "description is $description"
-    def event = zigbee.getEvent(description)
-    def descMap = zigbee.parseDescriptionAsMap(description)
+	log.debug "description is $description"
+	def event = zigbee.getEvent(description)
+	def descMap = zigbee.parseDescriptionAsMap(description)
 
-    if (event) {
-        log.info "event enter:$event"
-        if (event.name == "switch" && !descMap.isClusterSpecific && descMap.commandInt == 0x0B) {
-            log.info "Ignoring default response with desc map: $descMap"
-            return [:]
-        } else if (event.name== "power") {
-            event.value = event.value/getPowerDiv()
-            event.unit = "W"
-        } else if (event.name== "energy") {
-            event.value = event.value/getEnergyDiv()
-            event.unit = "kWh"
-        }
-        log.info "event outer:$event"
-        sendEvent(event)
-    } else {
-        List result = []
-        log.debug "Desc Map: $descMap"
+	if (event) {
+		log.info "event enter:$event"
+		if (event.name == "switch" && !descMap.isClusterSpecific && descMap.commandInt == 0x0B) {
+			log.info "Ignoring default response with desc map: $descMap"
+			return [:]
+		} else if (event.name== "power") {
+			event.value = event.value/getPowerDiv()
+			event.unit = "W"
+		} else if (event.name== "energy") {
+			event.value = event.value/getEnergyDiv()
+			event.unit = "kWh"
+		}
+		log.info "event outer:$event"
+		sendEvent(event)
+	} else {
+		List result = []
+		log.debug "Desc Map: $descMap"
 
-        List attrData = [[clusterInt: descMap.clusterInt ,attrInt: descMap.attrInt, value: descMap.value]]
-        descMap.additionalAttrs.each {
-            attrData << [clusterInt: descMap.clusterInt, attrInt: it.attrInt, value: it.value]
-        }
+		List attrData = [[clusterInt: descMap.clusterInt ,attrInt: descMap.attrInt, value: descMap.value]]
+		descMap.additionalAttrs.each {
+			attrData << [clusterInt: descMap.clusterInt, attrInt: it.attrInt, value: it.value]
+		}
 
-        attrData.each {
-                def map = [:]
-                if (it.value && it.clusterInt == zigbee.SIMPLE_METERING_CLUSTER && it.attrInt == ATTRIBUTE_HISTORICAL_CONSUMPTION) {
-                        log.debug "power"
-                        map.name = "power"
-                        map.value = zigbee.convertHexToInt(it.value)/getPowerDiv()
-                        map.unit = "W"
-                }
-                else if (it.value && it.clusterInt == zigbee.SIMPLE_METERING_CLUSTER && it.attrInt == ATTRIBUTE_READING_INFO_SET) {
-                        log.debug "energy"
-                        map.name = "energy"
-                        map.value = zigbee.convertHexToInt(it.value)/getEnergyDiv()
-                        map.unit = "kWh"
-                        
-                        if (isDawonOutlet()) { 
-                            def currentEnergy = map.value
-                            def currentPowerConsumption = device.currentState("powerConsumption")?.value
-                            Map previousMap = currentPowerConsumption ? new groovy.json.JsonSlurper().parseText(currentPowerConsumption) : [:]
-                            def deltaEnergy = calculateDelta (currentEnergy, previousMap)
-                            Map reportMap = [:]
-                            reportMap["energy"] = currentEnergy
-                            reportMap["deltaEnergy"] = deltaEnergy 
-                            sendEvent("name": "powerConsumption", "value": reportMap.encodeAsJSON(), displayed: false)
-                        }
-                }
+		attrData.each {
+			def map = [:]
+			if (it.value && it.clusterInt == zigbee.SIMPLE_METERING_CLUSTER && it.attrInt == ATTRIBUTE_HISTORICAL_CONSUMPTION) {
+				log.debug "power"
+				map.name = "power"
+				map.value = zigbee.convertHexToInt(it.value)/getPowerDiv()
+				map.unit = "W"
+			}
+			else if (it.value && it.clusterInt == zigbee.SIMPLE_METERING_CLUSTER && it.attrInt == ATTRIBUTE_READING_INFO_SET) {
+				log.debug "energy"
+				map.name = "energy"
+				map.value = zigbee.convertHexToInt(it.value)/getEnergyDiv()
+				map.unit = "kWh"
 
-                if (map) {
-                        result << createEvent(map)
-                }
-                log.debug "Parse returned $map"
-        }
-        return result
-    }
+				def currentEnergy = map.value
+				def currentPowerConsumption = device.currentState("powerConsumption")?.value
+				Map previousMap = currentPowerConsumption ? new groovy.json.JsonSlurper().parseText(currentPowerConsumption) : [:]
+				def deltaEnergy = calculateDelta (currentEnergy, previousMap)
+				Map reportMap = [:]
+				reportMap["energy"] = currentEnergy
+				reportMap["deltaEnergy"] = deltaEnergy 
+				sendEvent("name": "powerConsumption", "value": reportMap.encodeAsJSON(), displayed: false)
+			}
+
+			if (map) {
+				result << createEvent(map)
+			}
+			log.debug "Parse returned $map"
+		}
+		return result
+	}
 }
 
 def off() {
-    def cmds = zigbee.off()
-    return cmds
+	def cmds = zigbee.off()
+	return cmds
 }
 
 def on() {
-    def cmds = zigbee.on()
-    return cmds
+	def cmds = zigbee.on()
+	return cmds
 }
 
 /**
  * PING is used by Device-Watch in attempt to reach the Device
  * */
 def ping() {
-    return refresh()
+	return refresh()
 }
 
 def refresh() {
-    log.debug "refresh"
-    zigbee.onOffRefresh() +
-    zigbee.electricMeasurementPowerRefresh() +
-    zigbee.readAttribute(zigbee.SIMPLE_METERING_CLUSTER, ATTRIBUTE_READING_INFO_SET)
+	log.debug "refresh"
+	zigbee.onOffRefresh() +
+	zigbee.electricMeasurementPowerRefresh() +
+	zigbee.readAttribute(zigbee.SIMPLE_METERING_CLUSTER, ATTRIBUTE_READING_INFO_SET)
 }
 
 def configure() {
-    // this device will send instantaneous demand and current summation delivered every 1 minute
-    sendEvent(name: "checkInterval", value: 2 * 60 + 10 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
-    log.debug "Configuring Reporting"
-    return refresh() +
-    	   zigbee.onOffConfig() +
-           zigbee.configureReporting(zigbee.SIMPLE_METERING_CLUSTER, ATTRIBUTE_READING_INFO_SET, DataType.UINT48, 1, 600, 1) +
-           zigbee.electricMeasurementPowerConfig(1, 600, 1) +
-           zigbee.simpleMeteringPowerConfig()
+	// this device will send instantaneous demand and current summation delivered every 1 minute
+	sendEvent(name: "checkInterval", value: 2 * 60 + 10 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
+	log.debug "Configuring Reporting"
+	return refresh() +
+		zigbee.onOffConfig() +
+		zigbee.configureReporting(zigbee.SIMPLE_METERING_CLUSTER, ATTRIBUTE_READING_INFO_SET, DataType.UINT48, 1, 600, 1) +
+		zigbee.electricMeasurementPowerConfig(1, 600, 1) +
+		zigbee.simpleMeteringPowerConfig()
 }
 
 private int getPowerDiv() {
-    1
+	1
 }
 
 private int getEnergyDiv() {
-    if (isDawonOutlet()) {
-        1000
-    } else {
-        100
-    }
+	1000
 }
 
 BigDecimal calculateDelta (BigDecimal currentEnergy, Map previousMap) {
-    if (previousMap?.'energy' == null) {
-        return 0;
-    }
-    BigDecimal lastAcumulated = BigDecimal.valueOf(previousMap ['energy']);
-    return currentEnergy.subtract(lastAcumulated).max(BigDecimal.ZERO);
-}
-
-private boolean isDawonOutlet() {
-    device.getDataValue("manufacturer") == "DAWON_DNS"
+	if (previousMap?.'energy' == null) {
+		return 0;
+	}
+	BigDecimal lastAcumulated = BigDecimal.valueOf(previousMap ['energy']);
+	return currentEnergy.subtract(lastAcumulated).max(BigDecimal.ZERO);
 }
