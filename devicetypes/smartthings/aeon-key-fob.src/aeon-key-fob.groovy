751944950161fdc0e873de90f77040756a6bc32b
Carter Swedal <carterswedal15@gmail.com>
2020-12-02 16:00:18 -0600

    BUG-1315: Remove checkInterval event from aeon key fob (#51554)
    https://smartthings.atlassian.net/browse/BUG-1315

--- devicetypes/smartthings/aeon-key-fob.src/aeon-key-fob.groovy
+++ devicetypes/smartthings/aeon-key-fob.src/aeon-key-fob.groovy
@@ -197,15 +197,14 @@ def initialize() {
 	def buttons = 1
 
 	if (zwaveInfo && zwaveInfo.mfr == "0086" && zwaveInfo.prod == "0001" && zwaveInfo.model == "0026") {
-		sendEvent(name: "checkInterval", value: 8 * 60 * 60 + 2 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID])
 		buttons = 1 // Only one button for Aeon Panic Button
 		results << response(zwave.batteryV1.batteryGet().format())
 	} else {
-		// Device only goes OFFLINE when Hub is off
-		sendEvent(name: "DeviceWatch-Enroll", value: JsonOutput.toJson([protocol: "zwave", scheme:"untracked"]), displayed: false)
 		buttons = 4 // Default for Key Fob
 	}
 
+	// These devices only go OFFLINE when Hub is off
+	sendEvent(name: "DeviceWatch-Enroll", value: JsonOutput.toJson([protocol: "zwave", scheme:"untracked"]), displayed: false)
 	sendEvent(name: "numberOfButtons", value: buttons, displayed: false)
 	sendEvent(name: "supportedButtonValues", value: JsonOutput.toJson(["pushed", "held"]), displayed: false)
 
