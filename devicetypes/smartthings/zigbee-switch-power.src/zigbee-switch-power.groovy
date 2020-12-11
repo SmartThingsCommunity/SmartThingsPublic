7f06cbcd73455cb15c6d17b90eee046760f7c0f6
Konrad Klimczuk <k.klimczuk2@partner.samsung.com>
2020-12-11 13:31:59 +0100

    WWST-7223 - fingerprint for Enbrighten Plug-in Smart Switch With Energy Monitoring 43095

--- devicetypes/smartthings/zigbee-switch-power.src/zigbee-switch-power.groovy
+++ devicetypes/smartthings/zigbee-switch-power.src/zigbee-switch-power.groovy
@@ -54,6 +54,9 @@ metadata {
 		fingerprint profileId: "0104", deviceId: "0051", inClusters: "0000, 0003, 0004, 0005, 0006, 0B04, 1000, 0702", outClusters: "0019", manufacturer: "AduroSmart Eria", model: "AD-SmartPlug3001", deviceJoinName: "Eria Switch" //Eria Zigbee Smart Plug
 		fingerprint profileId: "0104", deviceId: "010A", inClusters: "0000, 0003, 0004, 0005, 0006, 1000", outClusters: "0019", manufacturer: "AduroSmart Eria", model: "BPU3", deviceJoinName: "Eria Switch" //Eria Zigbee On/Off Plug
 		fingerprint profileId: "0104", deviceId: "0101", inClusters: "0000, 0003, 0004, 0005, 0006, 0008, 1000", outClusters: "0019", manufacturer: "AduroSmart Eria", model: "BDP3001", deviceJoinName: "Eria Switch" //Eria Zigbee Dimmable Plug
+
+		// Enbrighten
+		fingerprint manufacturer: "Jasco Products", model: "43095", deviceJoinName: "Enbrighten Switch" //Enbrighten Plug-in Smart Switch With Energy Monitoring 43095, Raw Description: 01 0104 0100 00 07 0000 0003 0004 0005 0006 0702 0B05 02 000A 0019
 	}
 
 	tiles(scale: 2) {
