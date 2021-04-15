45a911b3d1dd19c1d728c8b557f4cb47241f1f09
Aeotec-ccheng <63321041+Aeotec-ccheng@users.noreply.github.com>
2021-04-14 22:13:56 -0700

    MultiSensor 7 parameter settings update
    Separated MS6 and MS7 configuration SET settings (byte size difference).
    
    Edited the available settings for 30 - 12 hours rather than 20 seconds to 24 hours.

--- devicetypes/smartthings/aeon-multisensor-6.src/aeon-multisensor-6.groovy
+++ devicetypes/smartthings/aeon-multisensor-6.src/aeon-multisensor-6.groovy
@@ -74,12 +74,12 @@ metadata {
 
 	preferences {
 		input "motionDelayTime", "enum", title: "Motion Sensor Delay Time",
-			options: ["20 seconds", "40 seconds", "1 minute", "2 minutes", "3 minutes", "4 minutes"]
+			options: ["30 seconds", "40 seconds", "1 minute", "2 minutes", "3 minutes", "4 minutes"]
 
 		input "motionSensitivity", "enum", title: "Motion Sensor Sensitivity", options: ["maximum", "normal", "minimum", "disabled"]
 
 		input "reportInterval", "enum", title: "Report Interval", description: "How often the device should report in minutes",
-			options: ["8 minutes", "15 minutes", "30 minutes", "1 hour", "6 hours", "12 hours", "18 hours", "24 hours"]
+			options: ["1 minute", "2 minutes", "3 minutes", "4 minutes", "8 minutes", "15 minutes", "30 minutes", "1 hour", "6 hours", "12 hours"]
 	}
 
 	tiles(scale: 2) {
@@ -398,46 +398,96 @@ def ping() {
 def configure() {
 	// This sensor joins as a secure device if you double-click the button to include it
 	log.debug "${device.displayName} is configuring its settings"
+    
 	def request = []
+	
+	//added as MSR wasn't getting detected upon pair. 
+	request << zwave.manufacturerSpecificV2.manufacturerSpecificGet() 
+    
+	switch (state.MSR) {
+		case "0086-0002-0064":  // MultiSensor 6 EU
+		case "0086-0102-0064":  // MultiSensor 6 US
+		case "0086-0202-0064":  // MultiSensor 6 AU
+			//1. set association groups for hub - 2 groups are used to set battery refresh interval different than sensor report interval
+			request << zwave.associationV1.associationSet(groupingIdentifier: 1, nodeId: zwaveHubNodeId)
+            		request << zwave.associationV1.associationSet(groupingIdentifier: 2, nodeId: zwaveHubNodeId)
 
-	//1. set association groups for hub - 2 groups are used to set battery refresh interval different than sensor report interval
-	request << zwave.associationV1.associationSet(groupingIdentifier: 1, nodeId: zwaveHubNodeId)
-	request << zwave.associationV1.associationSet(groupingIdentifier: 2, nodeId: zwaveHubNodeId)
+            		//2. automatic report flags
+            		// param 101 -103 [4 bytes] 128: light sensor, 64 humidity, 32 temperature sensor, 15 ultraviolet sensor, 1 battery sensor
+            		// set value  241 (default for 101) to get all reports. Set value 0 for no reports (default for 102-103)
+			//association group 1
+            		request << zwave.configurationV1.configurationSet(parameterNumber: 101, size: 4, scaledConfigurationValue: 240)
 
-	//2. automatic report flags
-	// param 101 -103 [4 bytes] 128: light sensor, 64 humidity, 32 temperature sensor, 15 ultraviolet sensor, 1 battery sensor
-	// set value  241 (default for 101) to get all reports. Set value 0 for no reports (default for 102-103)
-	//association group 1
-	request << zwave.configurationV1.configurationSet(parameterNumber: 101, size: 4, scaledConfigurationValue: 240)
+            		//association group 2
+            		request << zwave.configurationV1.configurationSet(parameterNumber: 102, size: 4, scaledConfigurationValue: 1)
 
-	//association group 2
-	request << zwave.configurationV1.configurationSet(parameterNumber: 102, size: 4, scaledConfigurationValue: 1)
+            		//3. no-motion report x seconds after motion stops (default 20 secs)
+            		request << zwave.configurationV1.configurationSet(parameterNumber: 3, size: 2, scaledConfigurationValue: timeOptionValueMap[motionDelayTime] ?: 20)
 
-	//3. no-motion report x seconds after motion stops (default 20 secs)
-	request << zwave.configurationV1.configurationSet(parameterNumber: 3, size: 2, scaledConfigurationValue: timeOptionValueMap[motionDelayTime] ?: 20)
+            		//4. motionSensitivity 3 levels: 3-normal, 5-maximum (default), 1-minimum, 0 - disabled
+            		request << zwave.configurationV1.configurationSet(parameterNumber: 4, size: 1,
+                		configurationValue:
+                    			motionSensitivity == "normal" ? [3] :
+                        			motionSensitivity == "minimum" ? [1] :
+                            				motionSensitivity == "disabled" ? [0] : [5])
 
-	//4. motionSensitivity 3 levels: 3-normal, 5-maximum (default), 1-minimum, 0 - disabled
-	request << zwave.configurationV1.configurationSet(parameterNumber: 4, size: 1,
-		configurationValue:
-			motionSensitivity == "normal" ? [3] :
-				motionSensitivity == "minimum" ? [1] :
-					motionSensitivity == "disabled" ? [0] : [5])
+            		//5. Parameters 111-113: report interval for association group 1-3
+            		//association group 1 - set in preferences, default 8 mins
+            		request << zwave.configurationV1.configurationSet(parameterNumber: 111, size: 4, scaledConfigurationValue: timeOptionValueMap[reportInterval] ?: (8 * 60))
 
-	//5. Parameters 111-113: report interval for association group 1-3
-	//association group 1 - set in preferences, default 8 mins
-	request << zwave.configurationV1.configurationSet(parameterNumber: 111, size: 4, scaledConfigurationValue: timeOptionValueMap[reportInterval] ?: (8 * 60))
+            		//association group 2 - report battery every 6 hours
+            		request << zwave.configurationV1.configurationSet(parameterNumber: 112, size: 4, scaledConfigurationValue: 6 * 60 * 60)
 
-	//association group 2 - report battery every 6 hours
-	request << zwave.configurationV1.configurationSet(parameterNumber: 112, size: 4, scaledConfigurationValue: 6 * 60 * 60)
+            		//6. report automatically ONLY on threshold change
+            		//From manual:
+            		//Enable/disable the selective reporting only when measurements reach a certain threshold or percentage set in 41-44.
+            		//This is used to reduce network traffic.  (0 = disable, 1 = enable)
+            		//Note: If USB power, the Sensor will check the threshold every 10 seconds. If battery power, the Sensor will check the threshold
+            		//when it is waken up.
+            		request << zwave.configurationV1.configurationSet(parameterNumber: 40, size: 1, scaledConfigurationValue: 1)
+			break
+		case "0371-0002-0018":  // MultiSensor 7 EU
+		case "0371-0102-0018":  // MultiSensor 7 US
+		case "0371-0202-0018":  // MultiSensor 7 AU
+            		//1. set association groups for hub - 2 groups are used to set battery refresh interval different than sensor report interval
+            		request << zwave.associationV1.associationSet(groupingIdentifier: 1, nodeId: zwaveHubNodeId)
 
-	//6. report automatically ONLY on threshold change
-	//From manual:
-	//Enable/disable the selective reporting only when measurements reach a certain threshold or percentage set in 41-44.
-	//This is used to reduce network traffic.  (0 = disable, 1 = enable)
-	//Note: If USB power, the Sensor will check the threshold every 10 seconds. If battery power, the Sensor will check the threshold
-	//when it is waken up.
-	request << zwave.configurationV1.configurationSet(parameterNumber: 40, size: 1, scaledConfigurationValue: 1)
+            		//2. automatic report flags
+            		// param 101 -103 [4 bytes] 128: light sensor, 64 humidity, 32 temperature sensor, 15 ultraviolet sensor, 1 battery sensor
+            		// set value  241 (default for 101) to get all reports. Set value 0 for no reports (default for 102-103)
+            		//association group 1
+            		request << zwave.configurationV1.configurationSet(parameterNumber: 101, size: 1, scaledConfigurationValue: 240)
 
+            		//association group 2
+            		request << zwave.configurationV1.configurationSet(parameterNumber: 102, size: 1, scaledConfigurationValue: 1)
+
+            		//3. no-motion report x seconds after motion stops (default 30 secs)
+            		request << zwave.configurationV1.configurationSet(parameterNumber: 3, size: 2, scaledConfigurationValue: timeOptionValueMap[motionDelayTime] ?: 30)
+
+            		//4. motionSensitivity 3 levels: 6-normal, 11-maximum (default), 1-minimum, 0 - disabled
+            		request << zwave.configurationV1.configurationSet(parameterNumber: 4, size: 1,
+                		configurationValue:
+                    			motionSensitivity == "normal" ? [6] :
+                        			motionSensitivity == "minimum" ? [1] :
+                            				motionSensitivity == "disabled" ? [0] : [11])
+
+            		//5. Parameters 111-113: report interval for association group 1-3
+            		//association group 1 - set in preferences, default 8 mins
+            		request << zwave.configurationV1.configurationSet(parameterNumber: 111, size: 2, scaledConfigurationValue: timeOptionValueMap[reportInterval] ?: (8 * 60))
+
+            		//association group 2 - report battery every 6 hours
+            		request << zwave.configurationV1.configurationSet(parameterNumber: 112, size: 2, scaledConfigurationValue: 6 * 60 * 60)
+
+            		//6. report automatically ONLY on threshold change
+            		//From manual:
+            		//Enable/disable the selective reporting only when measurements reach a certain threshold or percentage set in 41-44.
+            		//This is used to reduce network traffic.  (0 = disable, 1 = enable)
+            		//Note: If USB power, the Sensor will check the threshold every 10 seconds. If battery power, the Sensor will check the threshold
+            		//when it is waken up.
+            		request << zwave.configurationV1.configurationSet(parameterNumber: 40, size: 1, scaledConfigurationValue: 1)
+			break
+	}
+	
 	//7. query sensor data
 	request << zwave.batteryV1.batteryGet()
 	request << zwave.sensorBinaryV2.sensorBinaryGet(sensorType: 0x0C) //motion
@@ -468,7 +518,7 @@ def configure() {
 
 private def getTimeOptionValueMap() {
 	[
-		"20 seconds": 20,
+        	"30 seconds": 30,
 		"40 seconds": 40,
 		"1 minute"  : 60,
 		"2 minutes" : 2 * 60,
