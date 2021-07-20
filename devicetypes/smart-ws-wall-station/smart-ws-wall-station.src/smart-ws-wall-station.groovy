7943f92da7fb8f5a81532b7a02210e6bc22085ed
ALL LED <graphics@allledgroup.com>
2021-07-20 15:22:22 +0100

    DevWs for ALL LED LTD containing containing SMART-WS- Wall Station

---
+++ devicetypes/smart-ws-wall-station/smart-ws-wall-station.src/smart-ws-wall-station.groovy
@@ -0,0 +1,35 @@
+/**
+ *  SMART-WS- Wall Station
+ *
+ *  Copyright 2021 ALL LED
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ */
+metadata {
+	definition (name: "SMART-WS- Wall Station", namespace: "SMART-WS- Wall Station", author: "ALL LED", cstHandler: true) {
+		capability "Remote Control Status"
+	}
+
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'remoteControlEnabled' attribute
+
+}
