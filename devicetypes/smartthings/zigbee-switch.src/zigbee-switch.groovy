de667969147050f507d557f33a4db04ef32ddaac
mingwei0827 <38943109+mingwei0827@users.noreply.github.com>
2021-08-09 15:13:08 +0800

    Update zigbee-switch.groovy

--- devicetypes/smartthings/zigbee-switch.src/zigbee-switch.groovy
+++ devicetypes/smartthings/zigbee-switch.src/zigbee-switch.groovy
@@ -31,8 +31,6 @@ metadata {
 		// eWeLink
 		fingerprint profileId: "0104", inClusters: "0000, 0003, 0004, 0005, 0006", outClusters: "0000", manufacturer: "eWeLink", model: "SA-003-Zigbee", deviceJoinName: "eWeLink Outlet", ocfDeviceType: "oic.d.smartplug" //eWeLink SmartPlug (SA-003)
 		fingerprint profileId: "0104", inClusters: "0000,0003,0004,00005,0006", outClusters: "0000", manufacturer: "eWeLink", model: "ZB-SW01", deviceJoinName: "eWeLink Switch"
-		fingerprint manufacturer: "eWeLink", model: "SWITCH-ZED01-1", deviceJoinName: "eWeLink ZED Switch" //eWeLink ZED01
-		fingerprint manufacturer: "eWeLink", model: "SWITCH-ZR02-1", deviceJoinName: "eWeLink ZR Switch" //eWeLink ZR02
 		
 		// LELLKI
 		fingerprint profileId: "0104", inClusters: "0000,0003,0004,00005,0006", outClusters: "0000", manufacturer: "LELLKI", model: "JZ-ZB-001", deviceJoinName: "LELLKI Switch"
