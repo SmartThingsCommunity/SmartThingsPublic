c8f0dac82a31f2429b1ab679a3f98ea83e789c48
Artur Sargsyan <a.sargsyan@heltun.com>
2021-10-19 14:37:42 +0400

    Changed "polling" capability to "Health Check" Minor changes

--- devicetypes/heltun/heltun-hls01-switch.src/heltun-hls01-switch.groovy
+++ devicetypes/heltun/heltun-hls01-switch.src/heltun-hls01-switch.groovy
@@ -17,92 +17,87 @@ metadata {
 		capability "Power Meter"
 		capability "Switch"
 		capability "Temperature Measurement"
-        capability "Voltage Measurement"
-        capability "Configuration"      
-        capability "Polling" 
-        capability "Refresh"
+		capability "Voltage Measurement"
+		capability "Configuration"      
+		capability "Health Check" 
+		capability "Refresh"
 
 		fingerprint mfr: "0344", prod: "0004", model: "000A", inClusters:"0x5E,0x85,0x59,0x8E,0x55,0x86,0x72,0x5A,0x73,0x81,0x87,0x98,0x9F,0x6C,0x70,0x25,0x31,0x32,0x71,0x22,0x7A", deviceJoinName: "HELTUN HLS01 Switch"
 	}																		
-	simulator {}
-	tiles {}
-    preferences {
-    	section { 
-            input (
-                type: "paragraph",
-                element: "paragraph",
-                title: "Parameter Configuration:",
-                description: "Set the Parameter ID and the corresponding Value to configure the parameters"
-            )
+	preferences {
+		section { 
+			input (
+				type: "paragraph",
+				element: "paragraph",
+				title: "Parameter Configuration:",
+				description: "Set the Parameter ID and the corresponding Value to configure the parameters"
+			)
 
             input (
-                name: "ParamId",
-                title: "Parameter ID:",
-                type: "number",
-                range: "0..65536",
-                required: false
-            )
-
-            input (
-                name: "ParamValue",
-                title: "Parameter Value:",
-                type: "number",
-                range: "0..2147483647",
-                required: false
-            )
-     }
-}
-}
-def updated() {
-	state.confSet = [
-            parameterNumber: null,
-            configurationValue: null
-        ]
-    if ((settings.ParamId != null) & (settings.ParamValue != null) & (settings.ParamValue != state.confSet.configurationValue) ) {     
-        
-        state.confSet = [
-            parameterNumber: settings.ParamId.toInteger(),
-            configurationValue: settings.ParamValue
-        ]
-        sendHubCommand(zwave.configurationV2.configurationGet(parameterNumber: state.confSet.parameterNumber))
-    }   
-}
-def zwaveEvent(physicalgraph.zwave.commands.configurationv2.ConfigurationReport cmd) {
-	state.confReport = [
-                parameterNumber: cmd.parameterNumber,
-                size: cmd.size,
-                configurationValue: cmd.configurationValue
-            ]
-    configureParameter()
-}
-def configureParameter() {
-	if (state.confSet.parameterNumber == state.confReport.parameterNumber && state.confSet.configurationValue != state.confReport.configurationValue[0]) {
-    sendHubCommand(zwave.configurationV2.configurationSet(parameterNumber: state.confSet.parameterNumber, size: state.confReport.size, scaledConfigurationValue: state.confSet.configurationValue))
-    
+				name: "ParamId",
+				title: "Parameter ID:",
+				type: "number",
+				range: "0..65536",
+				required: false
+			)
+            
+			input (
+				name: "ParamValue",
+				title: "Parameter Value:",
+				type: "number",
+				range: "0..2147483647",
+				required: false
+			)
+		}
 	}
 }
-def parse(String description) {
-    def result = null
-    def cmd = zwave.parse(description)
-    if (cmd) {result = zwaveEvent(cmd)}
-    return result
 
+def updated() {
+	state.confSet = [
+        parameterNumber: null,
+        configurationValue: null
+	]
+	if ((settings.ParamId != null) & (settings.ParamValue != null) & (settings.ParamValue != state.confSet.configurationValue) ) {     
+		state.confSet = [
+		parameterNumber: settings.ParamId.toInteger(),
+		configurationValue: settings.ParamValue
+		]
+		sendHubCommand(zwave.configurationV2.configurationGet(parameterNumber: state.confSet.parameterNumber))
+	}   
 }
+
+def zwaveEvent(physicalgraph.zwave.commands.configurationv2.ConfigurationReport cmd) {
+	state.confReport = [
+		parameterNumber: cmd.parameterNumber,
+		size: cmd.size,
+		configurationValue: cmd.configurationValue
+	]
+	if (state.confSet.parameterNumber == state.confReport.parameterNumber && state.confSet.configurationValue != state.confReport.configurationValue[0]) {
+	sendHubCommand(zwave.configurationV2.configurationSet(parameterNumber: state.confSet.parameterNumber, size: state.confReport.size, scaledConfigurationValue: state.confSet.configurationValue))
+    }
+}
+
+def parse(String description) {
+	def result = null
+	def cmd = zwave.parse(description)
+	if (cmd) {result = zwaveEvent(cmd)}
+	return result
+}
+
 def zwaveEvent(physicalgraph.zwave.commands.sensormultilevelv5.SensorMultilevelReport cmd) {
 	def locaScale = getTemperatureScale() //HubScale
 	def map = [:]
 	if (1 == cmd.sensorType) {
-    	def deviceScale = (cmd.scale == 1) ? "F" : "C" //DeviceScale
-    	def deviceTemp = cmd.scaledSensorValue
+		def deviceScale = (cmd.scale == 1) ? "F" : "C" //DeviceScale
+		def deviceTemp = cmd.scaledSensorValue
 		def scaledTemp = (deviceScale == locaScale) ? deviceTemp : (deviceScale == "F" ? roundC(fahrenheitToCelsius(deviceTemp)) : celsiusToFahrenheit(deviceTemp).toDouble().round(0).toInteger())
-    	map.name = "temperature"
-        map.value = scaledTemp
-        map.unit = locaScale
-        sendEvent(map)
-    } 
-    sendEvent(map)
-
+		map.name = "temperature"
+		map.value = scaledTemp
+		map.unit = locaScale
+		sendEvent(map)
+	}
 }
+
 def zwaveEvent(physicalgraph.zwave.commands.meterv3.MeterReport cmd) {
 	def map = [:]
 	if (cmd.meterType == 1) {
@@ -110,17 +105,17 @@ def zwaveEvent(physicalgraph.zwave.commands.meterv3.MeterReport cmd) {
 			map.name = "energy"
 			map.value = cmd.scaledMeterValue
 			map.unit = "kWh"
-            sendEvent(map)
+			sendEvent(map)
 		} else if (cmd.scale == 2) {
 			map.name = "power"
 			map.value = Math.round(cmd.scaledMeterValue)
 			map.unit = "W"
-            sendEvent(map)
+			sendEvent(map)
 		}else if (cmd.scale == 4) {
 			map.name = "voltage"
 			map.value = Math.round(cmd.scaledMeterValue)
 			map.unit = "V"
-            sendEvent(map)
+			sendEvent(map)
 		}else if (cmd.scale == 5) {
 			map.name = "current"
 			map.value = Math.round(cmd.scaledMeterValue)
@@ -128,53 +123,61 @@ def zwaveEvent(physicalgraph.zwave.commands.meterv3.MeterReport cmd) {
 		}      
 	}
 }
+
 def roundC (tempInC) {
 	return (Math.round(tempInC.toDouble() * 2))/2
 }
+
 def refresh() {
-	pollDevice()
-}
-def poll() {
-	pollDevice()
-}
-def pollDevice() {
 	def cmds = []
-    cmds << new physicalgraph.device.HubAction(zwave.sensorMultilevelV5.sensorMultilevelGet(sensorType:1).format())		// get Temperature
-    cmds << new physicalgraph.device.HubAction(zwave.meterV3.meterGet(scale: 0).format())								// get kWh
-    cmds << new physicalgraph.device.HubAction(zwave.meterV3.meterGet(scale: 2).format())								// get Watts
-    cmds << new physicalgraph.device.HubAction(zwave.meterV3.meterGet(scale: 4).format())								// get Voltage    
-	sendHubCommand(cmds, 1200)
+	cmds << new physicalgraph.device.HubAction(zwave.sensorMultilevelV5.sensorMultilevelGet(sensorType:1).format())		// get Temperature
+	cmds << new physicalgraph.device.HubAction(zwave.meterV3.meterGet(scale: 0).format())								// get kWh
+	cmds << new physicalgraph.device.HubAction(zwave.meterV3.meterGet(scale: 2).format())								// get Watts
+	cmds << new physicalgraph.device.HubAction(zwave.meterV3.meterGet(scale: 4).format())								// get Voltage    
+    sendHubCommand(cmds, 1200)
 }
+
+def ping() {
+	log.info "Ping Called"
+	refresh()
+}
+
+
 def zwaveEvent(physicalgraph.zwave.commands.clockv1.ClockReport cmd) {
 	def currDate = Calendar.getInstance(location.timeZone)
 	def time = [hour: currDate.get(Calendar.HOUR_OF_DAY), minute: currDate.get(Calendar.MINUTE), weekday: currDate.get(Calendar.DAY_OF_WEEK)]
-    if ((time.hour != cmd.hour) || (time.minute != cmd.minute) || (time.weekday != cmd.weekday)){
-    	sendHubCommand(new physicalgraph.device.HubAction(zwave.clockV1.clockSet(time).format()))
-    }
+	if ((time.hour != cmd.hour) || (time.minute != cmd.minute) || (time.weekday != cmd.weekday)){
+		sendHubCommand(new physicalgraph.device.HubAction(zwave.clockV1.clockSet(time).format()))
+	}
 }
+
 def zwaveEvent(physicalgraph.zwave.commands.switchbinaryv1.SwitchBinaryReport cmd) {
-    def state = cmd.value ? "on" : "off"
-	sendEvent(name: "switch", value: state)
+	def state = cmd.value ? "on" : "off"
+    sendEvent(name: "switch", value: state)
 }
+
 def resetEnergyMeter() {
 	sendHubCommand(new physicalgraph.device.HubAction(zwave.meterV3.meterReset().format()))
 }
+
 def on() {
 	delayBetween([
 		zwave.basicV1.basicSet(value: 0xFF).format(),
 		zwave.switchBinaryV1.switchBinaryGet().format()
 	])
 }
+
 def off() {
 	delayBetween([
 		zwave.basicV1.basicSet(value: 0x00).format(),
 		zwave.switchBinaryV1.switchBinaryGet().format()
 	])
 }
+
 def configure() {
-    delayBetween([
-                zwave.multiChannelAssociationV2.multiChannelAssociationRemove(groupingIdentifier: 1).format(),
-                zwave.multiChannelAssociationV2.multiChannelAssociationSet(groupingIdentifier: 1, nodeId: 1).format(),
-        ], 3000)
-    pollDevice()
+	delayBetween([
+		zwave.multiChannelAssociationV2.multiChannelAssociationRemove(groupingIdentifier: 1).format(),
+		zwave.multiChannelAssociationV2.multiChannelAssociationSet(groupingIdentifier: 1, nodeId: 1).format(),
+        ping()
+	], 3000)
 }
