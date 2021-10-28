9c117bf7c91ffe5397edda4c1d7030387296ee48
Shina System Co Ltd <sihas.iot@gmail.com>
2021-10-28 12:44:36 +0900

    DevWs for SHINA SYSTEM containing containing Zigbee Power Meter

--- devicetypes/smartthings/zigbee-power-meter.src/zigbee-power-meter.groovy
+++ devicetypes/smartthings/zigbee-power-meter.src/zigbee-power-meter.groovy
@@ -23,6 +23,7 @@ metadata {
         fingerprint profileId: "0104", deviceId:"0053", inClusters: "0000, 0003, 0004, 0B04, 0702", outClusters: "0019", manufacturer: "", model: "E240-KR080Z0-HA", deviceJoinName: "Energy Monitor" //Smart Sub-meter(CT Type)
         fingerprint profileId: "0104", deviceId:"0007", inClusters: "0000,0003,0702", outClusters: "000A", manufacturer: "Develco", model: "ZHEMI101", deviceJoinName: "frient Energy Monitor" // frient External Meter Interface (develco) 02 0104 0007 00 03 0000 0003 0702 01 000A
         fingerprint profileId: "0104", manufacturer: "Develco Products A/S", model: "EMIZB-132", deviceJoinName: "frient Energy Monitor" // frient Norwegian HAN (develco) 02 0104 0053 00 06 0000 0003 0020 0702 0704 0B04 03 0003 000A 0019
+        fingerprint inClusters: "0000,0001,0003,0020,0B04,0702", outClusters: "0003,0004,0019", manufacturer: "ShinaSystem", model: "PMM-300Z1", deviceJoinName: "SiHAS Energy Monitor" // SIHAS Power Meter
     }
 
     // tile definitions
@@ -133,11 +134,15 @@ def configure() {
            zigbee.electricMeasurementPowerConfig()
 }
 
-private getActivePowerDivisor() { 10 }
-private getPowerDivisor() { isFrientSensor() ? 1 : 1000 }
-private getEnergyDivisor() { isFrientSensor() ? 1 : 1000 }
+private getActivePowerDivisor() { isPMM300Z1() ? 1 : 10 }
+private getPowerDivisor() {  (isFrientSensor() || isPMM300Z1()) ? 1 : 1000 }
+private getEnergyDivisor() { (isFrientSensor() || isPMM300Z1()) ? 1 : 1000 }
 
 private Boolean isFrientSensor() {
 	device.getDataValue("manufacturer") == "Develco Products A/S" ||
 		device.getDataValue("manufacturer") == "Develco"
+}
+
+private Boolean isPMM300Z1() {
+    device.getDataValue("model") == "PMM-300Z1"
 }
