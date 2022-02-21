c693792d1283334672fd6cece75e8ee27c838e5d
aguilbault-Sinope <alderic.bourdeau-guilbault@sinopetech.com>
2022-02-21 15:16:47 -0500

    Implement dm2550zb into existing dth

--- devicetypes/smartthings/zigbee-dimmer.src/zigbee-dimmer.groovy
+++ devicetypes/smartthings/zigbee-dimmer.src/zigbee-dimmer.groovy
@@ -186,10 +186,86 @@ def parse(String description) {
 			}
 			sendHubCommand(zigbee.command(zigbee.LEVEL_CONTROL_CLUSTER, 0x00, "FE0000").collect { new physicalgraph.device.HubAction(it) }, 0)
 			sendEvent(zigbee.getEventFromAttrData(descMap.clusterInt, descMap.attrInt, descMap.encoding, descMap.value))
+		} else if (isSinope() && description?.startsWith("read attr -")) {
+            def result = []
+            result += createCustomMap(descMap)
+
+            // In the possibility of multiple attributes being reported in the same message, all the attributes will be in the same description. 
+			// the first attribute will be in the fields regularly used, but the otter attributes will be in the "additionalAttrs". 
+			// They should all be treated in the following part.
+            if(descMap.additionalAttrs)
+            {
+                def mapAdditionnalAttrs = descMap.additionalAttrs
+                mapAdditionnalAttrs.each{add ->
+                    traceEvent(settings.logFilter,"parse> mapAdditionnalAttributes : ( ${add} )",settings.trace)
+                    add.cluster = descMap.cluster
+                    result += createCustomMap(add)
+                }
+            }
 		} else {
 			log.warn "DID NOT PARSE MESSAGE for description : $description"
 			log.debug "${descMap}"
 		}
+	}
+}
+
+private def parseDescriptionAsMap(description) {
+    traceEvent(settings.logFilter, "parsing MAP ...", settings.trace, get_LOG_DEBUG())
+	(description - "read attr - ").split(",").inject([:]) 
+    {
+    	map, param ->
+		def nameAndValue = param.split(":")
+		map += [(nameAndValue[0].trim()):nameAndValue[1].trim()]
+	}
+}
+
+private def createCustomMap(descMap) {
+    def result = null
+	def map = [:]
+
+        if(descMap.cluster == "0000" && descMap.attrId == "0001")
+        {
+            traceEvent(settings.logFilter, "Parsing SwBuild Attribute", settings.trace, get_LOG_DEBUG())
+            map.name = "swBuild"
+            map.value = zigbee.convertHexToInt(descMap.value)
+            sendEvent(name: map.name, value: map.value)
+        }
+    return result
+}
+
+def updated() {
+    
+	if (!state.updatedLastRanAt || now() >= state.updatedLastRanAt + 2000)
+    {
+		state.updatedLastRanAt = now()   
+            
+        def cmds = []
+        if(checkSoftVersion() == true)
+        {
+            def MinLight = (MinimalIntensityParam)?MinimalIntensityParam.toInteger():0
+            def Time = getTiming(MinLight)
+            traceEvent(settings.logFilter, "Set timing to: $Time", settings.trace, get_LOG_DEBUG())
+            cmds += zigbee.writeAttribute(0xff01, 0x0055, 0x21, Time)
+                
+        }
+        else
+        {
+            traceEvent(settings.logFilter, "Minimal intensity is not supported by the device", settings.trace, get_LOG_DEBUG())
+        }
+
+        if(LedIntensityParam){
+            cmds += zigbee.writeAttribute(0xff01, 0x0052, 0x20, LedIntensityParam)//MaxIntensity On
+            cmds += zigbee.writeAttribute(0xff01, 0x0053, 0x20, LedIntensityParam)//MaxIntensity Off
+        }
+        else{ // set to default
+            cmds += zigbee.writeAttribute(0xff01, 0x0052, 0x20, 50)//MaxIntensity On
+            cmds += zigbee.writeAttribute(0xff01, 0x0053, 0x20, 50)//MaxIntensity Off
+        }
+        sendZigbeeCommands(cmds)
+
+	}
+	else {
+        traceEvent(settings.logFilter, "updated(): Ran within last 2 seconds so aborting", settings.trace, get_LOG_TRACE())
 	}
 }
 
@@ -220,7 +296,20 @@ def ping() {
 }
 
 def refresh() {
-	zigbee.onOffRefresh() + zigbee.levelRefresh()
+	if (isSinope()) {
+		def cmds = []
+		cmds += zigbee.readAttribute(0x0006, 0x0000) //read on/off
+		cmds += zigbee.readAttribute(0x0008, 0x0000) //read level
+		cmds += zigbee.readAttribute(0x0000, 0x0001) //read software version
+		cmds += zigbee.configureReporting(0x0006, 0x0000, 0x10, 0, 599, null) //configure reporting on/off
+		cmds += zigbee.configureReporting(0x0008, 0x0000, 0x20, 3, 602, 0x01) //configure reporting level
+		if(checkSoftVersion() == true) {//if the minimal intensity is supported
+			cmds += zigbee.writeAttribute(0xff01, 0x0055, 0x21, getTiming((MinimalIntensityParam)?MinimalIntensityParam.toInteger():0))
+		}
+    	return sendZigbeeCommands(cmds)
+	} else {
+		zigbee.onOffRefresh() + zigbee.levelRefresh()
+	}
 }
 
 def installed() {
@@ -228,6 +317,17 @@ def installed() {
 		if ((device.currentState("level")?.value == null) || (device.currentState("level")?.value == 0)) {
 			sendEvent(name: "level", value: 100)
 		}
+	} else if (isSinope()) {
+		def cmds = []
+    	cmds += zigbee.readAttribute(0x0006, 0x0000) //read on/off
+    	cmds += zigbee.readAttribute(0x0008, 0x0000) //read level
+    	cmds += zigbee.readAttribute(0x0000, 0x0001) //read software version
+    	cmds += zigbee.configureReporting(0x0006, 0x0000, 0x10, 0, 599, null) //configure reporting on/off
+    	cmds += zigbee.configureReporting(0x0008, 0x0000, 0x20, 3, 602, 0x01) //configure reporting level
+    	if(checkSoftVersion() == true){//if the minimal intensity is supported
+    		cmds += zigbee.writeAttribute(0xff01, 0x0055, 0x21, getTiming((MinimalIntensityParam)?MinimalIntensityParam.toInteger():0))
+    	}
+    	return sendZigbeeCommands(cmds)
 	}
 }
 
@@ -251,12 +351,143 @@ def isSengled() {
 	device.getDataValue("manufacturer") == "sengled"
 }
 
-def configure() {
-	log.debug "Configuring Reporting and Bindings."
-	// Device-Watch allows 2 check-in misses from device + ping (plus 1 min lag time)
-	// enrolls with default periodic reporting until newer 5 min interval is confirmed
-	sendEvent(name: "checkInterval", value: 2 * 10 * 60 + 1 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
+def isSinope() {
+	device.getDataValue("manufacturer") == "Sinope Technologies"
+}
 
-	// OnOff minReportTime 0 seconds, maxReportTime 5 min. Reporting interval if no activity
-	refresh() + zigbee.onOffConfig(0, 300) + zigbee.levelConfig()
+def configure() {
+	if (isSinope) {
+		traceEvent(settings.logFilter, "Configuring Reporting and Bindings", settings.trace, get_LOG_DEBUG())
+
+		//allow 30 minutes without reveiving any on/off report
+		sendEvent(name: "checkInterval", value: 2 * 10 * 60 + 1 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
+
+		return  zigbee.configureReporting(0x0006, 0x0000, 0x10, 0, 599, null) + //configure reporting on/off
+				zigbee.configureReporting(0x0008, 0x0000, 0x20, 3, 602, 0x01) + //configure reporting level
+				zigbee.readAttribute(0x0006, 0x0000) + //read on/off
+				zigbee.readAttribute(0x0008, 0x0000) + //read level
+				zigbee.readAttribute(0x0000, 0x0001) //read software version
+	} else {
+		log.debug "Configuring Reporting and Bindings."
+		// Device-Watch allows 2 check-in misses from device + ping (plus 1 min lag time)
+		// enrolls with default periodic reporting until newer 5 min interval is confirmed
+		sendEvent(name: "checkInterval", value: 2 * 10 * 60 + 1 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
+
+		// OnOff minReportTime 0 seconds, maxReportTime 5 min. Reporting interval if no activity
+		refresh() + zigbee.onOffConfig(0, 300) + zigbee.levelConfig()
+	}
+}
+
+private int getTiming(def setting) {
+	//getTiming is used to get the minimal time associated with the parameter "minimalIntensityParam"
+	def Timing
+    	switch(setting)
+    	{
+    	case(1):
+       		Timing = 100
+       		break;
+    	case(2):
+       		Timing = 250
+    		break;    
+    	case(3):
+       		Timing = 500
+    		break;
+    	case(4):
+       		Timing = 750
+    		break;
+    	case(5):
+       		Timing = 1000
+    		break;
+    	case(6):
+       		Timing = 1250
+    		break;
+    	case(7):
+       		Timing = 1500
+    		break;
+    	case(8):
+       		Timing = 1750
+    		break;
+    	case(9):
+       		Timing = 2000
+    		break;
+    	case(10):
+       		Timing = 2250
+    		break;
+    	default:
+       		Timing = 600
+       		break;
+    	}
+        return Timing
+}
+
+private boolean checkSoftVersion()
+{
+	def version
+    def versionMin = "106" //the first version to support the minimal intensity is the version 106
+    def Build = device.currentState("swBuild")?.value
+    traceEvent(settings.logFilter, "soft version: $Build", settings.trace, get_LOG_DEBUG())
+    
+    if(Build > versionMin)//if the version is under 107, the minimal light intensity is not supported.
+    {
+        traceEvent(settings.logFilter, "intensity supported", settings.trace, get_LOG_DEBUG())
+    	version = true
+    }
+    else
+    {
+        traceEvent(settings.logFilter, "intensity not supported", settings.trace, get_LOG_DEBUG())
+        version = false
+    }
+    return version
+}
+
+private void sendZigbeeCommands(cmds, delay = 1000) {
+	cmds.removeAll { it.startsWith("delay") }
+	// convert each command into a HubAction
+	cmds = cmds.collect { new physicalgraph.device.HubAction(it) }
+	sendHubCommand(cmds, delay)
+}
+
+private int get_LOG_ERROR() {
+	return 1
+}
+private int get_LOG_WARN() {
+	return 2
+}
+private int get_LOG_INFO() {
+	return 3
+}
+private int get_LOG_DEBUG() {
+	return 4
+}
+private int get_LOG_TRACE() {
+	return 5
+}
+
+def traceEvent(logFilter, message, displayEvent = false, traceLevel = 4, sendMessage = true) {
+	int LOG_ERROR = get_LOG_ERROR()
+	int LOG_WARN = get_LOG_WARN()
+	int LOG_INFO = get_LOG_INFO()
+	int LOG_DEBUG = get_LOG_DEBUG()
+	int LOG_TRACE = get_LOG_TRACE()
+
+	if (displayEvent || traceLevel < 4) {
+		switch (traceLevel) {
+			case LOG_ERROR:
+				log.error "${message}"
+				break
+			case LOG_WARN:
+				log.warn "${message}"
+				break
+			case LOG_INFO:
+				log.info "${message}"
+				break
+			case LOG_TRACE:
+				log.trace "${message}"
+				break
+			case LOG_DEBUG:
+			default:
+				log.debug "${message}"
+				break
+		}
+	}
 }
