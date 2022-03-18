0342d7cbd2fb53dbcb86036025b9717457afbedd
Kevin LaFramboise <kevin@thesmartesthouse.com>
2022-03-17 21:31:12 -0400

    Zooz ZEN32 Scene Controller

--- devicetypes/zooz/zooz-zen32-scene-controller-button.src/zooz-zen32-scene-controller-button.groovy
+++ devicetypes/zooz/zooz-zen32-scene-controller-button.src/zooz-zen32-scene-controller-button.groovy
@@ -1,29 +1,31 @@
 /*
- *  Zooz ZEN32 Scene Controller Button
- *
- *  Changelog:
- *
- *    2022-02-27
- *      - Publication Release
- *
- *  Copyright 2022 Zooz
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
+*  Zooz ZEN32 Scene Controller Button
+*
+*  Changelog:
+*
+*    2022-03-17
+*      - Requested changes
+*    2022-02-27
+*      - Publication Release
+*
+*  Copyright 2022 Zooz
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
 */
 
 metadata {
-    definition (
+	definition (
 		name: "Zooz ZEN32 Scene Controller Button",
 		namespace: "Zooz",
 		author: "Kevin LaFramboise (krlaframboise)",
@@ -34,10 +36,10 @@ metadata {
 		capability "Sensor"
 		capability "Button"
 		capability "Refresh"
-        capability "platemusic11009.zoozLedColor"
+		capability "platemusic11009.zoozLedColor"
 		capability "platemusic11009.zoozLedBrightness"
 		capability "platemusic11009.zoozLedMode"
-    }
+	}
 
 	preferences() {}
 }
@@ -48,13 +50,13 @@ def parse(String description) {
 }
 
 def installed() {
-    log.debug "installed()..."
+	log.debug "installed()..."
 	initialize()
 }
 
 def updated() {
 	log.debug "updated().."
-    initialize()
+	initialize()
 }
 
 void initialize() {
--- devicetypes/zooz/zooz-zen32-scene-controller.src/zooz-zen32-scene-controller.groovy
+++ devicetypes/zooz/zooz-zen32-scene-controller.src/zooz-zen32-scene-controller.groovy
@@ -1,25 +1,27 @@
 /*
- *  Zooz ZEN32 Scene Controller
- *
- *  Changelog:
- *
- *    2022-02-27
- *      - Publication Release
- *
- *  Copyright 2022 Zooz
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
+*  Zooz ZEN32 Scene Controller
+*
+*  Changelog:
+*
+*    2022-03-17
+*      - Requested changes
+*    2022-02-27
+*      - Publication Release
+*
+*  Copyright 2022 Zooz
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
 */
 
 import groovy.transform.Field
@@ -99,20 +101,20 @@ metadata {
 		configParams.each { name, param ->
 			if (param.options) {
 				input name, "enum",
-					title: param.title,
-					description: "Default: ${param.options[param.defaultVal]}",
-					required: false,
-					displayDuringSetup: false,
-					defaultValue: param.defaultVal,
-					options: param.options
+				title: param.title,
+				description: "Default: ${param.options[param.defaultVal]}",
+				required: false,
+				displayDuringSetup: false,
+				defaultValue: param.defaultVal,
+				options: param.options
 			} else if (param.range) {
 				input name, "number",
-					title: param.title,
-					description: "${param.desc} - Default: ${param.defaultVal}",
-					required: false,
-					displayDuringSetup: false,
-					defaultValue: param.defaultVal,
-					range: param.range
+				title: param.title,
+				description: "${param.desc} - Default: ${param.defaultVal}",
+				required: false,
+				displayDuringSetup: false,
+				defaultValue: param.defaultVal,
+				range: param.range
 			}
 		}
 	}
@@ -224,9 +226,6 @@ def off() {
 
 def refresh() {
 	log.debug "refresh()..."
-
-	initialize()
-
 	List<String> cmds = [
 		switchBinaryGetCmd(),
 		versionGetCmd()
@@ -237,7 +236,6 @@ def refresh() {
 			cmds << configGetCmd(param)
 		}
 	}
-
 	sendHubCommand(cmds)
 }
 
